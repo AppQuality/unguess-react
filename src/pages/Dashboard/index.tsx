577fb4bef926c3c36763c71cfd771dd8b752b768
Luca Cannarozzo <cannarozzoluca@live.it>
2022-04-26 14:55:36 +0200

    chore(design): update design system to 2.8.33

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,12 @@
 // import { LoginForm } from "@appquality/unguess-design-system";
 import { useTranslation } from "react-i18next";
 import { Page } from "src/features/templates/Page";
-import { Grid, Row, Col, XXXL, theme } from "@appquality/unguess-design-system";
-import { Counters } from "./Counters";
-import { Separator } from "./Separator";
+import { Grid } from "@appquality/unguess-design-system";
 import { SuggestedCampaigns } from "./SuggestedCampaigns";
 import { CampaignsList } from "./campaigns-list";
 import { useAppDispatch, useAppSelector } from "src/app/hooks";
 import { projectFilterChanged } from "src/features/campaignsFilter/campaignsFilterSlice";
 import { DashboardHeaderContent } from "./headerContent";
-import styled from "styled-components";
 
 export default function Dashboard() {
   const { t } = useTranslation();
@@ -20,8 +17,7 @@ export default function Dashboard() {
   if(status === "logged") dispatch(projectFilterChanged(0)); //Reset filters
 
   return (
-    <Page title={t("__PAGE_TITLE_PRIMARY_DASHBOARD")} pageHeader={<DashboardHeaderContent />} route={""}>
-      
+    <Page title={t("__PAGE_TITLE_PRIMARY_DASHBOARD")} pageHeader={<DashboardHeaderContent />} route={""}> 
       <Grid>
         <SuggestedCampaigns />
         <CampaignsList />
