58823ff6708c5e1cfa845793aad98b0e5142155d
Luca Cannarozzo <cannarozzoluca@live.it>
2022-04-30 16:30:55 +0200

    refactor(project): replace slice selectors with rkt queries

--- src/pages/Dashboard/Project.tsx
+++ src/pages/Dashboard/Project.tsx
@@ -2,56 +2,54 @@
 import { useTranslation } from "react-i18next";
 import { Page } from "src/features/templates/Page";
 import { Grid, PageLoader } from "@appquality/unguess-design-system";
-import { useAppDispatch, useAppSelector } from "src/app/hooks";
+import { useAppDispatch } from "src/app/hooks";
 import { CampaignsList } from "./campaigns-list";
 import { useNavigate, useParams } from "react-router-dom";
 import { useLocalizeRoute } from "src/hooks/useLocalizedRoute";
-import { useEffect } from "react";
-import { getSingleProject } from "src/features/projects/actions";
-import { projectFilterChanged, resetFilters } from "src/features/campaignsFilter/campaignsFilterSlice";
+import {
+  projectFilterChanged,
+  resetFilters,
+} from "src/features/campaignsFilter/campaignsFilterSlice";
 import { ActionCards } from "./ActionCards";
 import { DashboardHeaderContent } from "./headerContent";
-
+import { useGetProjectsByPidQuery } from "src/features/api/endpoints/projects";
 
 export default function Project() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-
   const notFoundRoute = useLocalizeRoute("oops");
-
-  const { status } = useAppSelector((state) => state.projects);
 
   var { projectId } = useParams();
   
-  useEffect(() => {
-    if(projectId && !isNaN(Number(projectId))) {
-      dispatch(getSingleProject(parseInt(projectId)));
-    }
-  }, [dispatch, projectId]);
+  if (!projectId || isNaN(Number(projectId))) {
+    navigate(notFoundRoute, { replace: true });
+  }
+  
+  const project = useGetProjectsByPidQuery({ pid: projectId ? parseInt(projectId) : 0 });
 
-  const { currentProject: project } = useAppSelector((state) => state.projects);
+  if (project.isError)
+    navigate(notFoundRoute, { replace: true });
 
-  if(!project && status !== 'loading') navigate(notFoundRoute, { replace: true }); 
-
-  if(project) {
+  if (project.isSuccess && project.data) {
     dispatch(resetFilters());
-    dispatch(projectFilterChanged(project.id))
+    dispatch(projectFilterChanged(project.data.id));
   }
 
-  return status === 'loading' ? <PageLoader /> : (
+  return project.isFetching || project.isLoading ? (
+    <PageLoader />
+  ) : (
     <Page
       title={t("__PAGE_TITLE_PRIMARY_DASHBOARD_SINGLE_PROJECT")}
       route={"projects"}
-      pageHeader={<DashboardHeaderContent title={project?.name || "undefined"}/>}
+      pageHeader={
+        <DashboardHeaderContent title={project?.data?.name || "undefined"} />
+      }
     >
-  
-
       <Grid>
         <ActionCards />
         <CampaignsList />
       </Grid>
-      
-   </Page>
+    </Page>
   );
 }
