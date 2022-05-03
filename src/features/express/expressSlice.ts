6cdb4d27a77a18ed9b95de39ab1b7e85a74bf4e5
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-03 10:48:51 +0200

    feat(express): setup basic redux state for express wizard

---
+++ src/features/express/expressSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface ExpressWizardState {
+    steps: { [key: string]: Step };
+    isOpen?: boolean;
+    isDirty?: boolean;
+    currentStep?: number;
+}
+
+export interface Step {
+    data: { [key: string]: string };
+    isValid?: boolean;
+}
+
+const initialState: ExpressWizardState = {
+  steps: {}
+};
+
+const expressSlice = createSlice({
+  name: "express",
+  initialState,
+  reducers: {
+    openWizard: (state) => {
+        state.isOpen = true;
+    },
+    closeWizard: (state) => {
+        state.isOpen = false;
+    },
+    setCurrentStep: (state, action) => {
+        state.currentStep = action.payload;
+    },
+    addStep: (state, action) => {
+        state.steps[action.payload.id] = action.payload;
+    },
+    removeStep: (state, action) => {
+        delete state.steps[action.payload];
+    },
+    resetWizard: (state) => {
+        state.steps = {};
+    }
+  },
+});
+
+export const {
+    openWizard,
+    closeWizard,
+    setCurrentStep,
+    addStep,
+    removeStep,
+    resetWizard
+} = expressSlice.actions;
+
+export default expressSlice.reducer;
