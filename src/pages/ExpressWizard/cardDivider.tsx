3d3b878c3c478a8b5594e4e8af38b6446b211b52
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-05 11:48:10 +0200

    fix(card-divider): restore grey color

--- src/pages/ExpressWizard/cardDivider.tsx
+++ src/pages/ExpressWizard/cardDivider.tsx
@@ -2,6 +2,6 @@ import { Divider } from "@appquality/unguess-design-system";
 import styled from "styled-components";
 
 export const CardDivider = styled(Divider)`
-  background-color: ${({theme}) => theme.palette.grey[100]};
+  background-color: ${({theme}) => theme.palette.grey[200]};
   margin: ${({theme}) => theme.space.base * 4}px 0;
 `;
