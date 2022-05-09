e4035a40c5fe80c6266f6ab2601b9934375be798
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-09 15:18:53 +0200

    fix(when-step): fix working day calc

--- src/pages/ExpressWizard/steps/when.tsx
+++ src/pages/ExpressWizard/steps/when.tsx
@@ -1,5 +1,15 @@
-import { Alert, Col, Grid, Label, Message, Paragraph, Row, Span, XXL } from "@appquality/unguess-design-system";
-import { Datepicker } from '@zendeskgarden/react-datepickers';
+import {
+  Alert,
+  Col,
+  Grid,
+  Label,
+  Message,
+  Paragraph,
+  Row,
+  Span,
+  XXL,
+} from "@appquality/unguess-design-system";
+import { Datepicker } from "@zendeskgarden/react-datepickers";
 import { Field, Input } from "@zendeskgarden/react-forms";
 import { FormikProps } from "formik";
 import styled from "styled-components";
@@ -9,6 +19,7 @@ import { CardDivider } from "../cardDivider";
 import { t } from "i18next";
 import { useState } from "react";
 import initialValues from "../wizardInitialValues";
+import addBusinessDays from "date-fns/addBusinessDays";
 
 const StepTitle = styled(XXL)`
   margin-bottom: ${({ theme }) => theme.space.base * 2}px;
@@ -23,94 +34,124 @@ const StyledFormField = styled.div`
 `;
 
 const StyledAlert = styled(Alert)`
-    margin-top: ${({ theme }) => theme.space.lg};
+  margin-top: ${({ theme }) => theme.space.lg};
 `;
 
 const StyledMessage = styled(Message)`
-    margin-top: ${({ theme }) => theme.space.sm};
+  margin-top: ${({ theme }) => theme.space.sm};
 `;
 
-export const WhenStep = ({ errors, touched, validateField, validateForm, handleChange, values, ...props }: FormikProps<WizardModel>) => {
-    const [state, setState] = useState(initialValues.campaign_date);
-    const maxDays = 30;
+export const WhenStep = ({
+  errors,
+  touched,
+  validateField,
+  validateForm,
+  handleChange,
+  values,
+  ...props
+}: FormikProps<WizardModel>) => {
+  const [startDate, setDate] = useState(initialValues.campaign_date);
+  const START_DATE_MAX_VALUE = 30;
+  const BUSINESS_DAYS_TO_ADD = 2;
 
-    const handleDateChange = (date: Date) => {
-        let endDate = new Date(new Date().setDate(new Date(date).getDate() + 2));
-
-        // If the end date is not a saturday or sunday, pick the next working day (monday)
-        if (endDate.getDay() === 6) {
-            endDate = new Date(new Date().setDate(new Date(date).getDate() + 3));
-        } else if (endDate.getDay() === 0) {
-            endDate = new Date(new Date().setDate(new Date(date).getDate() + 2));
-        }
-    
-        setState(date);
-        props.setFieldValue('campaign_date', date);
-        props.setFieldValue('campaign_date_end', endDate.toLocaleDateString());
+  const handleDateChange = (date: Date) => {
+    //We have to add 2 business days to determine the end date
+    let endDate = addBusinessDays(date, BUSINESS_DAYS_TO_ADD);
+    setDate(date);
+    if (values.campaign_language === "en") {
+      endDate = addBusinessDays(date, BUSINESS_DAYS_TO_ADD + 1);
     }
 
-    return (
-        <>
-            <StyledFormField>
-                <StepTitle>
-                    <Span isBold>{t("__EXPRESS_WIZARD_STEP_WHEN_LABEL")}</Span> {t("__EXPRESS_WIZARD_STEP_WHEN_LABEL_EXTRA")}
-                </StepTitle>
-                <Paragraph>{t("__EXPRESS_WIZARD_STEP_WHEN_DESCRIPTION")}</Paragraph>
-            </StyledFormField>
-            <CardDivider />
-            <StyledFormField>
-                <StyledAlert type="warning">
-                    <StyledAlert.Title>
-                        {t("__EXPRESS_WIZARD_STEP_WHEN_ALERT_TITLE")}
-                    </StyledAlert.Title>
-                    {t("__EXPRESS_WIZARD_STEP_WHEN_ALERT_DESCRIPTION")}
-                </StyledAlert>
-            </StyledFormField>
-            <StyledFormField>
-                <Grid>
-                    <Row>
-                        <Col xs={6}>
-                            <Field>
-                                <Label>{t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_LABEL")}</Label>
-                                <Datepicker
-                                    value={state} 
-                                    formatDate={(date: Date) => date.toLocaleDateString()}
-                                    onChange={handleDateChange}
-                                    minValue={new Date()}
-                                    maxValue={new Date(new Date().setDate(new Date().getDate() + maxDays))}
-                                >
-                                    <Input
-                                        type={"text"}
-                                        placeholder={t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_PLACEHOLDER")}
-                                        {...props.getFieldProps('campaign_date')}
-                                        {...errors.campaign_date && { validation: "error" }}
-                                    />
-                                </Datepicker>
-                                {errors.campaign_date && <StyledMessage validation="error">{errors.campaign_date}</StyledMessage>}
-                            </Field>
-                        </Col>
-                        <Col xs={6}>
-                            <Label>{t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_RESULTS_LABEL")}</Label>
-                            <Input
-                                type={"text"}
-                                placeholder={t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_RESULTS_PLACEHOLDER")}
-                                readOnly
-                                disabled
-                                {...props.getFieldProps('campaign_date_end')}
-                            />
-                        </Col>
-                    </Row>
-                </Grid>
-            </StyledFormField>
-            <StyledFormField>
-                <Paragraph>
-                    {t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_DESCRIPTION")}
-                </Paragraph>
-            </StyledFormField>
-        </>
-    );
+    props.setFieldValue("campaign_date", date);
+    props.setFieldValue("campaign_date_end", endDate);
+  };
+
+  return (
+    <>
+      <StyledFormField>
+        <StepTitle>
+          <Span isBold>{t("__EXPRESS_WIZARD_STEP_WHEN_LABEL")}</Span>{" "}
+          {t("__EXPRESS_WIZARD_STEP_WHEN_LABEL_EXTRA")}
+        </StepTitle>
+        <Paragraph>{t("__EXPRESS_WIZARD_STEP_WHEN_DESCRIPTION")}</Paragraph>
+      </StyledFormField>
+      <CardDivider />
+      <StyledFormField>
+        <StyledAlert type="warning">
+          <StyledAlert.Title>
+            {t("__EXPRESS_WIZARD_STEP_WHEN_ALERT_TITLE")}
+          </StyledAlert.Title>
+          {t("__EXPRESS_WIZARD_STEP_WHEN_ALERT_DESCRIPTION")}
+        </StyledAlert>
+      </StyledFormField>
+      <StyledFormField>
+        <Grid>
+          <Row>
+            <Col xs={6}>
+              <Field>
+                <Label>
+                  {t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_LABEL")}
+                </Label>
+                <Datepicker
+                  value={startDate}
+                  formatDate={(date: Date) => date.toLocaleDateString()}
+                  onChange={handleDateChange}
+                  minValue={new Date()}
+                  maxValue={
+                    new Date(
+                      new Date().setDate(
+                        new Date().getDate() + START_DATE_MAX_VALUE
+                      )
+                    )
+                  }
+                >
+                  <Input
+                    type={"text"}
+                    placeholder={t(
+                      "__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_PLACEHOLDER"
+                    )}
+                    {...props.getFieldProps("campaign_date")}
+                    {...(errors.campaign_date && { validation: "error" })}
+                  />
+                </Datepicker>
+                {errors.campaign_date && (
+                  <StyledMessage validation="error">
+                    {errors.campaign_date}
+                  </StyledMessage>
+                )}
+              </Field>
+            </Col>
+            <Col xs={6}>
+              <Label>
+                {t(
+                  "__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_RESULTS_LABEL"
+                )}
+              </Label>
+              <Input
+                type={"text"}
+                placeholder={t(
+                  "__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_RESULTS_PLACEHOLDER"
+                )}
+                readOnly
+                disabled
+                {...props.getFieldProps("campaign_date_end")}
+                value={values?.campaign_date_end?.toLocaleDateString()}
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </StyledFormField>
+      <StyledFormField>
+        <Paragraph>
+          {t("__EXPRESS_WIZARD_STEP_WHEN_FIELD_CAMPAIGN_DATE_DESCRIPTION")}
+        </Paragraph>
+      </StyledFormField>
+    </>
+  );
 };
 
 export const WhenStepValidationSchema = Yup.object().shape({
-    campaign_date: Yup.string().required(t("__EXPRESS_WIZARD_STEP_WHO_FIELD_CAMPAIGN_DATE_REQUIRED")),
+  campaign_date: Yup.string().required(
+    t("__EXPRESS_WIZARD_STEP_WHO_FIELD_CAMPAIGN_DATE_REQUIRED")
+  ),
 });
--- src/pages/ExpressWizard/steps/who.tsx
+++ src/pages/ExpressWizard/steps/who.tsx
@@ -1,4 +1,15 @@
-import { Hint, Col, Label, Message, Paragraph, Radio, Row, Span, XL, XXL } from "@appquality/unguess-design-system";
+import {
+  Hint,
+  Col,
+  Label,
+  Message,
+  Paragraph,
+  Radio,
+  Row,
+  Span,
+  XL,
+  XXL,
+} from "@appquality/unguess-design-system";
 import { Field } from "@zendeskgarden/react-forms";
 import { FormikProps } from "formik";
 import styled from "styled-components";
@@ -7,7 +18,6 @@ import { WizardModel } from "../wizardModel";
 import { CardDivider } from "../cardDivider";
 import { t } from "i18next";
 import { useState } from "react";
-import initialValues from "../wizardInitialValues";
 
 const StepTitle = styled(XXL)`
   margin-bottom: ${({ theme }) => theme.space.base * 2}px;
@@ -24,87 +34,98 @@ const StyledFormField = styled.div`
 const StyledLanguageTitle = styled(XL)`
   padding-top: ${({ theme }) => theme.space.md};
   margin-bottom: ${({ theme }) => theme.space.sm};
-  color: ${({ theme }) => theme.palette.grey[800]}; 
+  color: ${({ theme }) => theme.palette.grey[800]};
 `;
 
 const StyledRadioField = styled(Field)`
-    margin-top: ${({ theme }) => theme.space.base * 4}px;
+  margin-top: ${({ theme }) => theme.space.base * 4}px;
 `;
 
 const StyledHint = styled(Hint)`
-    color: ${({ theme }) => theme.palette.yellow[800]};
+  color: ${({ theme }) => theme.palette.yellow[800]};
 `;
 
-export const WhoStep = ({ errors, touched, validateField, validateForm, handleChange, values, ...props }: FormikProps<WizardModel>) => {
-    const [radioValue, setRadioValue] = useState(initialValues.campaign_language);
+export const WhoStep = ({
+  errors,
+  touched,
+  validateField,
+  validateForm,
+  handleChange,
+  values,
+  ...props
+}: FormikProps<WizardModel>) => {
+  const [radioValue, setRadioValue] = useState(values.campaign_language);
 
-    const handleRadioClick = (value: string) => {
-        setRadioValue(value);
-        props.setFieldValue('campaign_language', value);
-    }
+  const handleRadioClick = (value: string) => {
+    setRadioValue(value);
+    props.setFieldValue("campaign_language", value);
+  };
 
-    return (
-        <>
-            <StepTitle>
-                <Span isBold>{t("__EXPRESS_WIZARD_STEP_WHO_LABEL")}</Span> {t("__EXPRESS_WIZARD_STEP_WHO_LABEL_EXTRA")}
-            </StepTitle>
-            <Paragraph>{t("__EXPRESS_WIZARD_STEP_WHO_DESCRIPTION")}</Paragraph>
-            <CardDivider />
-            <StyledFormField>
-                <StyledLanguageTitle>
-                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_TITLE")}
-                </StyledLanguageTitle>
-                <Paragraph>
-                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_DESCRIPTION")}
-                </Paragraph>
-            </StyledFormField>
-            <StyledFormField>
-                <Label>
-                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_LABEL")}
+  return (
+    <>
+      <StepTitle>
+        <Span isBold>{t("__EXPRESS_WIZARD_STEP_WHO_LABEL")}</Span>{" "}
+        {t("__EXPRESS_WIZARD_STEP_WHO_LABEL_EXTRA")}
+      </StepTitle>
+      <Paragraph>{t("__EXPRESS_WIZARD_STEP_WHO_DESCRIPTION")}</Paragraph>
+      <CardDivider />
+      <StyledFormField>
+        <StyledLanguageTitle>
+          {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_TITLE")}
+        </StyledLanguageTitle>
+        <Paragraph>
+          {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_DESCRIPTION")}
+        </Paragraph>
+      </StyledFormField>
+      <StyledFormField>
+        <Label>{t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_LABEL")}</Label>
+        <Row>
+          <Col>
+            <StyledRadioField>
+              <Radio
+                {...props.getFieldProps("campaign_language")}
+                {...(errors.campaign_language && { validation: "error" })}
+                value="it"
+                checked={radioValue === "it"}
+                onChange={(e) => handleRadioClick(e.target.value)}
+              >
+                <Label isRegular={true}>
+                  {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_1")}
                 </Label>
-                <Row>
-                    <Col>
-                        <StyledRadioField>
-                            <Radio
-                                {...props.getFieldProps('campaign_language')}
-                                {...errors.campaign_language && { validation: "error" }}
-                                value="it"
-                                checked={radioValue === 'it'}
-                                onChange={e => handleRadioClick(e.target.value)}
-                            >
-                                <Label isRegular={true}>
-                                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_1")}
-                                </Label>
-                            </Radio>
-                        </StyledRadioField>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <StyledRadioField>
-                            <Radio
-                                {...props.getFieldProps('campaign_language')}
-                                {...errors.campaign_language && { validation: "error" }}
-                                value="en"
-                                checked={radioValue === 'en'}
-                                onChange={e => handleRadioClick(e.target.value)}
-                            >
-                                <Label isRegular={true}>
-                                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_2")}
-                                </Label>
-                                <StyledHint>
-                                    {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_2_HINT")}
-                                </StyledHint>
-                            </Radio>
-                        </StyledRadioField>
-                    </Col>
-                </Row>
-                {errors.campaign_language && <Message validation="error">{errors.campaign_language}</Message>}
-            </StyledFormField>
-        </>
-    );
+              </Radio>
+            </StyledRadioField>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <StyledRadioField>
+              <Radio
+                {...props.getFieldProps("campaign_language")}
+                {...(errors.campaign_language && { validation: "error" })}
+                value="en"
+                checked={radioValue === "en"}
+                onChange={(e) => handleRadioClick(e.target.value)}
+              >
+                <Label isRegular={true}>
+                  {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_2")}
+                </Label>
+                <StyledHint>
+                  {t("__EXPRESS_WIZARD_STEP_WHO_FIELD_LANGUAGE_OPTION_2_HINT")}
+                </StyledHint>
+              </Radio>
+            </StyledRadioField>
+          </Col>
+        </Row>
+        {errors.campaign_language && (
+          <Message validation="error">{errors.campaign_language}</Message>
+        )}
+      </StyledFormField>
+    </>
+  );
 };
 
 export const WhoStepValidationSchema = Yup.object().shape({
-    campaign_language: Yup.string().required(t("__EXPRESS_WIZARD_STEP_WHO_FIELD_CAMPAIGN_LANGUAGE_REQUIRED")),
+  campaign_language: Yup.string().required(
+    t("__EXPRESS_WIZARD_STEP_WHO_FIELD_CAMPAIGN_LANGUAGE_REQUIRED")
+  ),
 });
