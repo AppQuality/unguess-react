8e6d089afd7579ebdf6a2e23d418a9cb7748a4f3
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-23 11:24:00 +0200

    feat(strapi): add GetFullServiceById query in order to receive all data from strapi

--- src/features/backoffice/strapi.ts
+++ src/features/backoffice/strapi.ts
@@ -1,17 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetServicesByIdApiResponse } from ".";
+
+interface GetFullServicesByIdArgs {
+    id: string;
+    populate?: string[];
+}
 
 export const strapiSlice = createApi({
-    reducerPath: "strapi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_STRAPI_API_URL,
-        prepareHeaders: (headers) => {
-            if (process.env.REACT_APP_STRAPI_API_TOKEN) {
-                const token = process.env.REACT_APP_STRAPI_API_TOKEN;
-                headers.set("Authorization", `Bearer ${token}`);
-            }
+  reducerPath: "strapi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_STRAPI_API_URL,
+    prepareHeaders: (headers) => {
+      if (process.env.REACT_APP_STRAPI_API_TOKEN) {
+        const token = process.env.REACT_APP_STRAPI_API_TOKEN;
+        headers.set("Authorization", `Bearer ${token}`);
+      }
 
-            return headers;
-        },
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getFullServicesById: builder.query<
+      GetServicesByIdApiResponse,
+      GetFullServicesByIdArgs
+    >({
+      query: (queryArg) => ({ url: `/services/${queryArg.id}` }),
     }),
-    endpoints: (builder) => ({}),
+  }),
 });
+
+
+export const { useGetFullServicesByIdQuery } = strapiSlice;
