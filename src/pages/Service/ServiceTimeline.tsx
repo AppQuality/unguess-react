72075247f8848773ca28077da998e087781956d9
Marco Bonomo <marco.bonomo@unguess.io>
2022-05-24 17:00:44 +0200

    feat(catalog-detail): card borders and radius + pageheader border + translations

--- src/pages/Service/ServiceTimeline.tsx
+++ src/pages/Service/ServiceTimeline.tsx
@@ -1,5 +1,5 @@
-import { Card, Col, Divider, Grid, MD, OrderedList, Paragraph, Row, Timeline, XXL } from "@appquality/unguess-design-system";
-import { useTranslation } from "react-i18next";
+import { Card, Col, Divider, Grid, MD, OrderedList, Paragraph, Row, Span, Timeline, XXL } from "@appquality/unguess-design-system";
+import { Trans, useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { ReactComponent as CheckIcon } from "src/assets/icons/check-icon.svg";
 import { WaterButton } from "../ExpressWizard/waterButton";
@@ -17,6 +17,8 @@ const CardContainer = styled(Card)`
     padding: ${({ theme }) => theme.space.base * 6}px;
     background-color: ${({ theme }) => theme.palette.white};
     margin-bottom: ${({ theme }) => theme.space.md};
+    border-radius: ${({ theme }) => theme.borderRadii.xl};
+    border-color: ${({ theme }) => theme.palette.grey[300]};
 `;
 
 const StickyContainerTitle = styled(MD)`
@@ -39,10 +41,18 @@ const StyledOrderListItem = styled(OrderedList.Item)`
 
 const TimelineCard = styled(Card)`
     margin-bottom: ${({ theme }) => theme.space.md};
+    border-radius: ${({ theme }) => theme.borderRadii.xl};
+    border-color: ${({ theme }) => theme.palette.grey[300]};
 `;
 
 const StepTitle = styled(XXL)`
+    color: ${({ theme }) => theme.palette.grey[800]};
+    font-weight: ${({ theme }) => theme.fontWeights.medium};
     margin-bottom: ${({ theme }) => theme.space.md};
+
+    span {
+        color: ${({ theme }) => theme.colors.primaryHue};
+    }
 `;
 
 const StepParagraph = styled(Paragraph)`
@@ -75,6 +85,7 @@ const ServiceTimeline = (
     const { t } = useTranslation();
     const { activeWorkspace } = useAppSelector((state) => state.navigation);
     const STRAPI_URL = process.env.REACT_APP_STRAPI_URL || "";
+
     const why = data ? data.data?.attributes?.why : {};
     const reasons = why?.reasons || [];
     const requirements = data ? data.data?.attributes?.requirements : {};
@@ -96,7 +107,8 @@ const ServiceTimeline = (
                                 <StyledOrderedList>
                                     <StyledOrderListItem>
                                         <Link
-                                            to={"why-card"}
+                                            to={"why"}
+                                            containerId={"why-card"}
                                             spy={true}
                                             smooth={true}
                                             offset={-100}
@@ -107,7 +119,8 @@ const ServiceTimeline = (
                                     </StyledOrderListItem>
                                     <StyledOrderListItem>
                                         <Link
-                                            to={"what-card"}
+                                            to={"what"}
+                                            containerId={"what-card"}
                                             spy={true}
                                             smooth={true}
                                             offset={-100}
@@ -118,7 +131,8 @@ const ServiceTimeline = (
                                     </StyledOrderListItem>
                                     <StyledOrderListItem>
                                         <Link
-                                            to={"how-card"}
+                                            to={"how"}
+                                            containerId={"how-card"}
                                             spy={true}
                                             smooth={true}
                                             offset={-100}
@@ -133,7 +147,11 @@ const ServiceTimeline = (
                     </Col>
                     <Col xs={12} lg={6}>
                         <TimelineCard id={"why-card"} className={"why-card"}>
-                            <StepTitle>{t("__CATALOG_DETAIL_STICKY_CONTAINER_ABOUT_WHY_ITEM")}</StepTitle>
+                            <StepTitle>
+                                <Trans i18nKey="__CATALOG_DETAIL_TIMELINE_WHY_TITLE">
+                                    <Span isBold>Why</Span> to choose this campaign
+                                </Trans>
+                            </StepTitle>
                             <StepParagraph>{t("__CATALOG_DETAIL_TIMELINE_WHY_DESCRIPTION")}</StepParagraph>
                             <StyledDivider />
                             <Timeline>
@@ -196,7 +214,11 @@ const ServiceTimeline = (
                             </AdvantagesContainer>
                         </TimelineCard>
                         <TimelineCard id={"what-card"} className={"what-card"}>
-                            <StepTitle>{t("__CATALOG_DETAIL_STICKY_CONTAINER_ABOUT_WHAT_ITEM")}</StepTitle>
+                            <StepTitle>
+                                <Trans i18nKey="__CATALOG_DETAIL_TIMELINE_WHAT_TITLE">
+                                    <Span isBold>What</Span> you get
+                                </Trans>
+                            </StepTitle>
                             <StepParagraph>{what?.description}</StepParagraph>
                             <>
                                 <SectionTitle>
@@ -207,7 +229,11 @@ const ServiceTimeline = (
                             </>
                         </TimelineCard>
                         <TimelineCard id={"how-card"} className={"how-card"}>
-                            <StepTitle>{t("__CATALOG_DETAIL_STICKY_CONTAINER_ABOUT_HOW_ITEM")}</StepTitle>
+                            <StepTitle>
+                                <Trans i18nKey="__CATALOG_DETAIL_TIMELINE_HOW_TITLE">
+                                    <Span isBold>How</Span> does it work
+                                </Trans>
+                            </StepTitle>
                             <StepParagraph>{t("__CATALOG_DETAIL_TIMELINE_HOW_DESCRIPTION")}</StepParagraph>
                             <Timeline>
                                 {timeline.map((item, index) => {
