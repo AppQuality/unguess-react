68d9a9e247e1a2338aabdee247e9c6345dea3f83
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-25 19:27:37 +0200

    ci(push): add validation step to push action

--- list-staged.config.js
+++ list-staged.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  '*.{ts,tsx}': (filenames) => ['yarn format:fix', 'yarn validate'],
+  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': (filenames) => [
+    'yarn format:fix',
+    'yarn lint',
+    'yarn type:check',
+    'yarn format:check',
+  ],
 };
