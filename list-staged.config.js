8afce44a914a742764400288723e389756f8d152
Luca Cannarozzo <cannarozzoluca@live.it>
2022-05-25 19:30:53 +0200

    ci(push): restore checkout in each step

--- list-staged.config.js
+++ list-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': (filenames) => [
+  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': () => [
     'yarn format:fix',
     'yarn lint',
     'yarn type:check',
