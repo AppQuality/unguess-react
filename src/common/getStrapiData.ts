88a3d395ba58b610130f4818c04b43e2d0f40771
Marco Bonomo <marco.bonomo@unguess.io>
2022-06-10 18:35:31 +0200

    feat(strapi): define getStrapiData function

--- src/common/getStrapiData.ts
+++ src/common/getStrapiData.ts
@@ -9,34 +9,60 @@ interface StrapiObject {
   [key: string]: string | number | boolean | object | undefined;
 }
 
-interface StrapiList {
-  items: StrapiObject[];
+interface StrapiListItem {
+  id: number;
+  attributes: object;
 }
 
 export default (
-  strapiData: CategoryListResponse | ServiceListResponse | undefined
-): StrapiList | StrapiObject => {
+  strapiData:
+    | CategoryListResponse
+    | ServiceListResponse
+    | CategoryResponse
+    | ServiceResponse
+    | undefined
+): any => {
   if (strapiData !== undefined) {
     if (strapiData.data !== undefined) {
       if (Array.isArray(strapiData.data)) {
         const items: Array<StrapiObject> = [];
-        strapiData.data.forEach((item, index) => {
+        strapiData.data.forEach((item) => {
           items.push({
             ...(item.id && { id: item.id }),
             ...item.attributes,
           });
         });
 
-        items.forEach((item, index) => {
-          Object.keys(item).forEach((key) => {
-            console.log('key', key);
-            if (typeof key === 'object') {
+        items.forEach((item) => {
+          Object.entries(item).forEach(([key, value]) => {
+            const obj: any = value;
+            if (typeof obj === 'object') {
+              if (obj.data !== undefined) {
+                if (Array.isArray(obj.data)) {
+                  const childrens: Array<StrapiObject> = [];
+                  obj.data.forEach((children: StrapiListItem) => {
+                    // override item key with formatted children
+                    if (item[`${key}`] !== undefined) {
+                      childrens.push({
+                        ...(children.id && { id: children.id }),
+                        ...children.attributes,
+                      });
+                    }
+                  });
+
+                  item[`${key}`] = childrens;
+                }
+              }
             }
           });
         });
+
+        return items; // Return list
       }
+
+      return strapiData.data; // Return single object
     }
   }
 
-  return {};
+  return {}; // Return empty object
 };
