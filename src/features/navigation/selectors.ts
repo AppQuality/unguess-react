fee124182247e1eab374c847d2bc3efe3ce98837
Luca Cannarozzo <cannarozzoluca@live.it>
2022-06-13 14:23:33 +0200

    fix(navigation): invalidate active workspace if not exists in available user workspaces

--- src/features/navigation/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from 'src/app/store';
-
-export const selectActiveWorkspace = (state: RootState) =>
-  state.navigation.activeWorkspace;
---
+++ src/features/navigation/utils.ts
@@ -0,0 +1,14 @@
+import { RootState } from 'src/app/store';
+import { Workspace } from '../api';
+
+export const selectActiveWorkspace = (state: RootState) =>
+  state.navigation.activeWorkspace;
+
+export const isValidWorkspace = (
+  workspace: Workspace,
+  workspaces: Workspace[]
+) => {
+  const workspaceExists = workspaces.find((ws) => ws.id === workspace.id);
+
+  return workspaceExists !== undefined;
+};
