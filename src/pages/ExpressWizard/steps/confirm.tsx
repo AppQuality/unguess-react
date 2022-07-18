17ec2badfdd5b9cd1191afe6631f2c366ff18440
Luca Cannarozzo <cannarozzoluca@live.it>
2022-07-18 12:37:29 +0200

    feat(confirm-step): add conditional cards for what and when steps

--- src/pages/ExpressWizard/steps/confirm.tsx
+++ src/pages/ExpressWizard/steps/confirm.tsx
@@ -1,7 +1,6 @@
 import * as Yup from 'yup';
 import { FormikProps } from 'formik';
 import { t } from 'i18next';
-import { Trans } from 'react-i18next';
 import {
   Label,
   Paragraph,
@@ -13,25 +12,16 @@ import {
   theme as globalTheme,
 } from '@appquality/unguess-design-system';
 import styled from 'styled-components';
-import { ReactComponent as WhatIcon } from 'src/assets/icons/step-what-icon.svg';
-import { ReactComponent as WhereIcon } from 'src/assets/icons/step-where-icon.svg';
-import { ReactComponent as WhoIcon } from 'src/assets/icons/step-who-icon.svg';
-import { ReactComponent as WhenIcon } from 'src/assets/icons/step-when-icon.svg';
 import { ReactComponent as MoreIcon } from 'src/assets/icons/step-more-icon.svg';
 import { ReactComponent as InfoIcon } from 'src/assets/icons/info-icon.svg';
 import { Textarea } from '@zendeskgarden/react-forms';
-import { useAppSelector } from 'src/app/hooks';
-import { format } from 'date-fns';
-import { Devices } from './confirm/devices';
-import { Browsers } from './confirm/browsers';
-import { OperativeSystems } from './confirm/operativeSystems';
-import { ConfirmOutOfScope } from './confirm/confirmOutOfScope';
-import { getLanguage } from '../getLanguage';
 import { CardDivider } from '../cardDivider';
 import { WizardModel } from '../wizardModel';
 import { WizardCol } from '../wizardCol';
 import { WhereConfirm } from './confirm/whereConfirm';
 import { WhoConfirm } from './confirm/whoConfirm';
+import { WhenConfirm } from './confirm/whenConfirm';
+import { WhatConfirm } from './confirm/whatConfirm';
 
 const StepTitle = styled(XXL)`
   margin-bottom: ${({ theme }) => theme.space.base * 2}px;
@@ -78,26 +68,11 @@ const TextareaNote = styled(Paragraph)`
 
 export const ConfirmationStep = (props: FormikProps<WizardModel>) => {
   const { values, getFieldProps } = props;
-  const { project } = useAppSelector((state) => state.express);
-  const lang = getLanguage(values.campaign_language || 'en');
 
-  const dateStartText = format(
-    values.campaign_date || new Date(),
-    'EEEE d MMMM Y',
-    { locale: lang.locale }
-  );
-  const dateEndText = format(
-    values.campaign_date_end || new Date(),
-    'EEEE d MMMM Y',
-    { locale: lang.locale }
-  );
-  const productType =
-    values.product_type === 'webapp'
-      ? t('__EXPRESS_WIZARD_STEP_WHAT_FIELD_PRODUCT_TYPE_WEBAPP_LABEL')
-      : t('__EXPRESS_WIZARD_STEP_WHAT_FIELD_PRODUCT_TYPE_MOBILEAPP_LABEL');
-
+  const hasWhatStep = values.campaign_name;
   const hasWhereStep = values.isAndroid || values.isIOS || values.hasOutOfScope;
   const hasWhoStep = values.campaign_language;
+  const hasWhenStep = values.campaign_date;
 
   return (
     <>
@@ -106,36 +81,16 @@ export const ConfirmationStep = (props: FormikProps<WizardModel>) => {
       </StepTitle>
       <Paragraph>{t('__EXPRESS_WIZARD_STEP_RECAP_DESCRIPTION')}</Paragraph>
       <CardDivider />
-      <StyledFormField
-        style={{ marginTop: `${globalTheme.space.base * 10}px` }}
-      >
-        <StyledCard>
-          <Grid>
-            <Row>
-              <WizardCol xs={12} sm={1}>
-                <WhatIcon />
-              </WizardCol>
-              <WizardCol xs={12} sm={11}>
-                <StyledLabel>
-                  {t('__EXPRESS_WIZARD_STEP_WHAT_LABEL')}
-                </StyledLabel>
-                <StyledParagraph>
-                  <Trans i18nKey="__EXPRESS_WIZARD_STEP_RECAP_WHAT_CONTENT_TEXT">
-                    Stai lanciando la campagna&nbsp;
-                    <Span isBold>
-                      {{ campaign_name: values.campaign_name }}
-                    </Span>
-                    &nbsp; all&apos;interno del progetto&nbsp;
-                    <Span isBold>{{ project_name: project?.name }}</Span>&nbsp;
-                    per&nbsp;
-                    <Span isBold>{{ product_type: productType }}</Span>.
-                  </Trans>
-                </StyledParagraph>
-              </WizardCol>
-            </Row>
-          </Grid>
-        </StyledCard>
-      </StyledFormField>
+
+      {hasWhatStep ? (
+        <StyledFormField
+          style={{ marginTop: `${globalTheme.space.base * 10}px` }}
+        >
+          <StyledCard>
+            <WhatConfirm {...props} />
+          </StyledCard>
+        </StyledFormField>
+      ) : null}
 
       {hasWhereStep ? (
         <StyledFormField
@@ -157,30 +112,16 @@ export const ConfirmationStep = (props: FormikProps<WizardModel>) => {
         </StyledFormField>
       ) : null}
 
-      <StyledFormField style={{ marginTop: `${globalTheme.space.base * 7}px` }}>
-        <StyledCard>
-          <Grid>
-            <Row>
-              <WizardCol xs={12} sm={1}>
-                <WhenIcon />
-              </WizardCol>
-              <WizardCol xs={12} sm={11}>
-                <StyledLabel>
-                  {t('__EXPRESS_WIZARD_STEP_WHEN_LABEL')}
-                </StyledLabel>
-                <StyledParagraph>
-                  <Trans i18nKey="__EXPRESS_WIZARD_STEP_RECAP_WHEN_CONTENT_TEXT">
-                    Campaign starts on&nbsp;
-                    <Span isBold>{{ campaign_date: dateStartText }}</Span> and
-                    ends on&nbsp;
-                    <Span isBold>{{ campaign_date_end: dateEndText }}</Span>.
-                  </Trans>
-                </StyledParagraph>
-              </WizardCol>
-            </Row>
-          </Grid>
-        </StyledCard>
-      </StyledFormField>
+      {hasWhenStep ? (
+        <StyledFormField
+          style={{ marginTop: `${globalTheme.space.base * 7}px` }}
+        >
+          <StyledCard>
+            <WhenConfirm {...props} />
+          </StyledCard>
+        </StyledFormField>
+      ) : null}
+
       <StyledFormField style={{ marginTop: `${globalTheme.space.base * 7}px` }}>
         <StyledCard>
           <Grid>
