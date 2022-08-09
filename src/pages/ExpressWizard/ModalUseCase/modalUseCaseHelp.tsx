06398ac845d46e422274efcbc6ae37fe0560af40
Marco Bonomo <marco.bonomo@unguess.io>
2022-08-09 16:30:06 +0200

    feat(use-cases-modal): get help content from strapi

--- src/pages/ExpressWizard/ModalUseCase/modalUseCaseHelp.tsx
+++ src/pages/ExpressWizard/ModalUseCase/modalUseCaseHelp.tsx
@@ -4,12 +4,16 @@ import {
   XL,
   XXL,
   theme as globalTheme,
+  Skeleton,
 } from '@appquality/unguess-design-system';
 import { ReactComponent as HelpImg } from 'src/assets/modal-use-case-help.svg';
 import { ReactComponent as CheckIcon } from 'src/assets/icons/check-icon.svg';
 import { ReactComponent as CancelIcon } from 'src/assets/icons/cancel-icon.svg';
 import styled from 'styled-components';
-import { useTranslation } from 'react-i18next';
+import { useAppSelector } from 'src/app/hooks';
+import { useGeti18nExpressTypesByIdQuery } from 'src/features/backoffice/strapi';
+import i18n from 'src/i18n';
+import { extractStrapiData } from 'src/common/getStrapiData';
 
 export const ScrollingContainer = styled.div`
   overflow-x: hidden;
@@ -35,127 +39,114 @@ const HelpContainer = styled.div`
   overflow-x: hidden;
 `;
 
+const GroupTitle = styled.div`
+  margin-bottom: ${({ theme }) => theme.space.sm};
+  color: ${({ theme }) => theme.palette.grey[600]};
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
+  margin-top: ${({ theme }) => theme.space.lg};
+  text-transform: uppercase;
+`;
+
 export const ModalUseCaseHelp = () => {
-  const { t } = useTranslation();
+  const { expressTypeId } = useAppSelector((state) => state.express);
+
+  const { data, isLoading, isFetching, isError } =
+    useGeti18nExpressTypesByIdQuery({
+      id: expressTypeId.toString(),
+      locale: i18n.language,
+      populate: {
+        use_cases_help: {
+          populate: {
+            suggestions: {
+              populate: '*',
+            },
+          },
+        },
+      },
+    });
+
+  const expressType = extractStrapiData(data);
+  const { use_cases_help: useCaseHelp } = expressType;
+
   return (
     <ScrollingContainer>
       <HelpContainer>
         <HelpImg />
-        <XXL style={{ marginTop: globalTheme.space.lg }}>
-          {t('__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_TITLE')}
-        </XXL>
-        <Paragraph style={{ marginBottom: globalTheme.space.xxl }}>
-          {t('__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_DESCRIPTION')}
-        </Paragraph>
-        <Paragraph style={{ marginBottom: globalTheme.space.sm }}>
-          {t('__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_1_TITLE')}
-        </Paragraph>
-        <Timeline>
-          <Timeline.Item icon={<CheckIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_1_ITEM_1_TITLE'
-                )}
+        {data && !isError && !isFetching && !isLoading ? (
+          <>
+            {useCaseHelp.title ? (
+              <XXL style={{ marginTop: globalTheme.space.lg }}>
+                {useCaseHelp.title}
+              </XXL>
+            ) : null}
+            {useCaseHelp.description ? (
+              <Paragraph style={{ marginBottom: globalTheme.space.sm }}>
+                {useCaseHelp.description}
               </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_1_ITEM_1_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item icon={<CheckIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_1_ITEM_2_TITLE'
-                )}
-              </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_1_ITEM_2_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-        </Timeline>
-        <Paragraph
-          style={{
-            marginTop: globalTheme.space.lg,
-            marginBottom: globalTheme.space.sm,
-          }}
-        >
-          {t('__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_2_TITLE')}
-        </Paragraph>
-        <Timeline>
-          <Timeline.Item icon={<CancelIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_2_ITEM_1_TITLE'
-                )}
-              </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_2_ITEM_1_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item icon={<CancelIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_2_ITEM_2_TITLE'
-                )}
-              </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_2_ITEM_2_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-        </Timeline>
-        <XL
-          style={{
-            marginTop: globalTheme.space.lg,
-            marginBottom: globalTheme.space.sm,
-          }}
-        >
-          {t('__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_3_TITLE')}
-        </XL>
-        <Timeline>
-          <Timeline.Item icon={<CancelIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_3_ITEM_1_TITLE'
-                )}
-              </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_3_ITEM_1_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item icon={<CancelIcon />} hiddenLine>
-            <Timeline.Content>
-              <Paragraph style={{ fontWeight: 500 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_3_ITEM_2_TITLE'
-                )}
-              </Paragraph>
-              <Paragraph style={{ marginTop: 0 }}>
-                {t(
-                  '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_HELP_SECTION_3_ITEM_2_SUBTITLE'
-                )}
-              </Paragraph>
-            </Timeline.Content>
-          </Timeline.Item>
-        </Timeline>
+            ) : null}
+            {useCaseHelp.suggestions && useCaseHelp.suggestions.length > 0
+              ? useCaseHelp.suggestions.map(
+                  (suggestion: any, index: number) => (
+                    <>
+                      {suggestion.group_title && {
+                        ...(index === useCaseHelp.suggestions.length - 1 ? (
+                          <XL
+                            style={{
+                              marginTop: globalTheme.space.xl,
+                              marginBottom: globalTheme.space.sm,
+                              color: globalTheme.palette.grey[800],
+                              fontWeight: globalTheme.fontWeights.medium,
+                            }}
+                          >
+                            {suggestion.group_title}
+                          </XL>
+                        ) : (
+                          <GroupTitle>{suggestion.group_title}</GroupTitle>
+                        )),
+                      }}
+
+                      {suggestion.items && suggestion.items.length > 0 && (
+                        <Timeline>
+                          {suggestion.items.map((item: any) => (
+                            <Timeline.Item
+                              icon={
+                                item.is_pros ? <CheckIcon /> : <CancelIcon />
+                              }
+                              hiddenLine
+                            >
+                              <Timeline.Content>
+                                <Paragraph style={{ fontWeight: 500 }}>
+                                  {item.title}
+                                </Paragraph>
+                                <Paragraph style={{ marginTop: 0 }}>
+                                  {item.content}
+                                </Paragraph>
+                              </Timeline.Content>
+                            </Timeline.Item>
+                          ))}
+                        </Timeline>
+                      )}
+                    </>
+                  )
+                )
+              : null}
+          </>
+        ) : (
+          <>
+            <Skeleton
+              height="30px"
+              style={{ marginTop: globalTheme.space.md }}
+            />
+            <Skeleton
+              height="30px"
+              style={{ marginTop: globalTheme.space.md }}
+            />
+            <Skeleton
+              height="300px"
+              style={{ marginTop: globalTheme.space.md }}
+            />
+          </>
+        )}
       </HelpContainer>
     </ScrollingContainer>
   );
