0680a8ae20c9225722c549d50713c74f5b8ba2e0
Luca Cannarozzo <cannarozzoluca@live.it>
2022-08-10 16:07:31 +0200

    refactor(expresses): prepared other expresses

--- src/pages/ExpressWizard/steps/useSteps.tsx
+++ src/pages/ExpressWizard/steps/useSteps.tsx
@@ -5,9 +5,24 @@ import {
   Who as WhoExpress1,
   Where as WhereExpress1,
   When as WhenExpress1,
-  How as HowExpress1,
   Confirmation as ConfirmationExpress1,
 } from './express-1';
+
+import {
+  What as WhatExpress2,
+  Who as WhoExpress2,
+  Where as WhereExpress2,
+  How as HowExpress2,
+  Confirmation as ConfirmationExpress2,
+} from './express-2';
+
+import {
+  What as WhatExpress3,
+  Who as WhoExpress3,
+  Where as WhereExpress3,
+  How as HowExpress3,
+  Confirmation as ConfirmationExpress3,
+} from './express-3';
 
 import { WizardButtonsProps } from './types';
 import { WizardModel } from '../wizardModel';
@@ -24,9 +39,21 @@ export interface StepItem {
 export const useExpressStep = (type: string): Array<StepItem> => {
   switch (type) {
     case 'unmoderated-usability-testing':
-      return [];
+      return [
+        WhatExpress3,
+        WhereExpress3,
+        WhoExpress3,
+        HowExpress3,
+        ConfirmationExpress3,
+      ];
     case 'bug-hunting':
-      return [];
+      return [
+        WhatExpress2,
+        WhereExpress2,
+        WhoExpress2,
+        HowExpress2,
+        ConfirmationExpress2,
+      ];
     default: // exploratory-test
       return [
         WhatExpress1,
