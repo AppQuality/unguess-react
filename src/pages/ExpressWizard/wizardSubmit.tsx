b0da36e0c2afeea456a57cd9112a0159efcd1737
Luca Cannarozzo <cannarozzoluca@live.it>
2022-09-07 18:56:50 +0200

    feat(when): add friendly date name related to current date

--- src/pages/ExpressWizard/wizardSubmit.tsx
+++ src/pages/ExpressWizard/wizardSubmit.tsx
@@ -1,17 +1,26 @@
 import { FormikProps } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as ChevronDownIcon } from 'src/assets/icons/chevron-down-stroke.svg';
+import { ReactComponent as CheckIcon } from 'src/assets/icons/check-lg-stroke.svg';
+import { ReactComponent as EmptyIcon } from 'src/assets/icons/empty.svg';
 import {
+  MD,
+  Paragraph,
   SM,
   Spinner,
   SplitButton,
   theme as globalTheme,
+  Timeline,
   TooltipModal,
 } from '@appquality/unguess-design-system';
-import { useRef, useState } from 'react';
+import i18n from 'src/i18n';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { addBusinessDays, format, formatRelative } from 'date-fns';
+import { EXPRESS_BUSINESS_DAYS_TO_ADD } from 'src/constants';
 import { WaterButton } from '../../common/components/waterButton';
 import { WizardModel } from './wizardModel';
+import { getLanguage } from './getLanguage';
 
 const StyledDiv = styled.div`
   /** Horizontal Align */
@@ -23,18 +32,58 @@ const StyledDiv = styled.div`
 
 const HelpText = styled(SM)`
   color: ${({ theme }) => theme.palette.grey[600]};
-  margin-left: ${({ theme }) => theme.space.sm};
   max-width: 250px;
   position: absolute;
-  right: 0;
+  right: ${({ theme }) => theme.space.xs};
+`;
+
+const InteractiveTimelineItem = styled(Timeline.Item)`
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.grey[100]};
+    svg {
+      background-color: ${({ theme }) => theme.palette.grey[100]};
+    }
+  }
 `;
 
 export const WizardSubmit = (props: FormikProps<WizardModel>) => {
   const { t } = useTranslation();
-  const { errors, isSubmitting, handleSubmit } = props;
+  const { errors, isSubmitting, handleSubmit, values, setFieldValue } = props;
 
   const triggerRef = useRef<HTMLButtonElement>(null);
   const [refElement, setRefElement] = useState<HTMLButtonElement | null>();
+  const [selectedDateSpot, setSelectedDateSpot] = useState<number>(0);
+
+  const lang = getLanguage(i18n.language || 'en');
+  const start_date = values.campaign_date ?? new Date();
+  const requiredDuration =
+    values.campaign_language === 'en'
+      ? EXPRESS_BUSINESS_DAYS_TO_ADD + 1
+      : EXPRESS_BUSINESS_DAYS_TO_ADD;
+
+  // format(endDate, 'EEEE d MMMM Y', { locale: lang.locale })
+  const dateSpots = [
+    addBusinessDays(start_date, 1),
+    addBusinessDays(start_date, 5),
+  ];
+
+  useEffect(() => {
+    const endDate = addBusinessDays(
+      dateSpots[selectedDateSpot],
+      requiredDuration
+    );
+
+    setFieldValue('campaign_date', dateSpots[selectedDateSpot]);
+    setFieldValue('campaign_date_end', endDate);
+    setFieldValue(
+      'campaign_date_end_text',
+      format(endDate, 'EEEE d MMMM Y', { locale: lang.locale })
+    );
+  }, [selectedDateSpot]);
+
+  console.log('values', values);
 
   return (
     <StyledDiv>
@@ -75,11 +124,47 @@ export const WizardSubmit = (props: FormikProps<WizardModel>) => {
         placement="auto"
         hasArrow={false}
       >
-        <TooltipModal.Title>Tooltip modal header</TooltipModal.Title>
+        <TooltipModal.Title>
+          <MD isBold style={{ color: globalTheme.palette.grey[800] }}>
+            {t('__EXPRESS_WIZARD_SUBMIT_PLANNING_TOOLTIP_TITLE')}
+          </MD>
+        </TooltipModal.Title>
         <TooltipModal.Body>
-          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot
-          courgette tatsoi pea sprouts fava bean collard greens dandelion okra
-          wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+          <Timeline>
+            {dateSpots.map((date, index) => (
+              <InteractiveTimelineItem
+                onClick={() => setSelectedDateSpot(index)}
+                icon={
+                  index === selectedDateSpot ? (
+                    <CheckIcon width={24} />
+                  ) : (
+                    <EmptyIcon />
+                  )
+                }
+                hiddenLine
+              >
+                <Timeline.Content>
+                  <Paragraph style={{ fontWeight: 500 }}>
+                    {formatRelative(date, start_date, {
+                      locale: {
+                        ...lang.locale,
+                        formatRelative: (token) =>
+                          lang.relativeDateFormat[token as string],
+                      },
+                    })}
+                  </Paragraph>
+                  <MD style={{ color: globalTheme.palette.grey[600] }}>
+                    {t(
+                      '__EXPRESS_WIZARD_SUBMIT_PLANNING_TOOLTIP_FIRST_RESULTS'
+                    )}{' '}
+                    {format(addBusinessDays(date, requiredDuration), 'EEEE d', {
+                      locale: lang.locale,
+                    })}
+                  </MD>
+                </Timeline.Content>
+              </InteractiveTimelineItem>
+            ))}
+          </Timeline>
         </TooltipModal.Body>
         <TooltipModal.Close aria-label="Close" />
       </TooltipModal>
