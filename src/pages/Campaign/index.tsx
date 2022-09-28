7b991a46e6d3bd9905f038b2ea69561dc4801f19
Luca Cannarozzo <cannarozzoluca@live.it>
2022-09-28 18:53:49 +0200

    feat(campaign-dashboard): retrieve reports from api

--- src/pages/Campaign/index.tsx
+++ src/pages/Campaign/index.tsx
@@ -1,29 +1,57 @@
 // import { LoginForm } from "@appquality/unguess-design-system";
 import { useTranslation } from 'react-i18next';
 import { Page } from 'src/features/templates/Page';
-import { Grid } from '@appquality/unguess-design-system';
+import { Col, Grid, Row } from '@appquality/unguess-design-system';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
+import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
+import { useGetCampaignsByCidReportsQuery } from 'src/features/api';
 import { CampaignPageHeader } from './pageHeader';
 
 const Campaign = () => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const notFoundRoute = useLocalizeRoute('oops');
 
-  const { status } = useAppSelector((state) => state.user);
+  const { campaignId } = useParams();
+
+  if (!campaignId || Number.isNaN(Number(campaignId))) {
+    navigate(notFoundRoute);
+  }
+
+  const {
+    isLoading,
+    isFetching,
+    isError,
+    error,
+    data: reports,
+  } = useGetCampaignsByCidReportsQuery({
+    cid: Number(campaignId),
+  });
+
+  if (isError) {
+    console.log(error);
+    navigate(notFoundRoute);
+  }
 
   return (
     <Page
-      title={t('__PAGE_TITLE_PRIMARY_DASHBOARD')}
+      title={t('__PAGE_TITLE_CAMPAIGN_DASHBOARD')}
       pageHeader={
-        <CampaignPageHeader pageTitle={t('__PAGE_TITLE_PRIMARY_DASHBOARD')} />
+        <CampaignPageHeader pageTitle={t('__PAGE_TITLE_CAMPAIGN_DASHBOARD')} />
       }
-      route=""
+      route="campaigns"
     >
       <Grid>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi
-        laudantium ex quaerat aperiam, esse repudiandae. In, molestiae
-        repudiandae, consequatur repellat similique porro ex neque doloremque
-        fugiat cupiditate, ducimus accusantium quo!
+        <Row>
+          {reports &&
+            reports.map((report) => (
+              <Col xs={12} md={4} lg={3}>
+                {report.title}
+              </Col>
+            ))}
+        </Row>
       </Grid>
     </Page>
   );
