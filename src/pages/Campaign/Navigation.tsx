bbff3c803802cd46eed259292233276ff60f6309
Iacopo Leardini <iacopo.lea@gmail.com>
2022-11-10 16:01:58 +0100

    wip(mother-dashboard): init navigation

--- src/pages/Campaign/Navigation.tsx
+++ src/pages/Campaign/Navigation.tsx
@@ -1,9 +1,76 @@
+import { Anchor } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import {
+  StickyNavItem,
+  StickyNavItemLabel,
+  StyledDivider,
+} from 'src/common/components/navigation';
 import { StickyContainer } from 'src/common/components/StickyContainer';
 
 export const Navigation = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  return <StickyContainer>pippo</StickyContainer>;
+  return (
+    <StickyContainer>
+      <StickyNavItem
+        to="campaign-overview"
+        containerId="main"
+        spy
+        smooth
+        duration={500}
+        offset={-350}
+      >
+        {t('__MOTHER_DASHBOARD_NAVIGATION_OVERVIEW', 'Campaign Overview')}
+      </StickyNavItem>
+      <StickyNavItemLabel>
+        {t('__MOTHER_DASHBOARD_NAVIGATION_DETAILS', 'Details')}
+      </StickyNavItemLabel>
+      <StickyNavItem
+        to="unique-bug-distribution"
+        containerId="main"
+        spy
+        smooth
+        duration={500}
+        offset={-350}
+      >
+        {t(
+          '__MOTHER_DASHBOARD_NAVIGATION_DISTRIBUTION',
+          'Unique bugs distribution'
+        )}
+      </StickyNavItem>
+      <StickyNavItem
+        to="devices-and-types"
+        containerId="main"
+        spy
+        smooth
+        duration={500}
+        offset={-350}
+      >
+        {t('__MOTHER_DASHBOARD_NAVIGATION_DEVICES', 'Devices and types')}
+      </StickyNavItem>
+      <StickyNavItemLabel>
+        {t('__MOTHER_DASHBOARD_NAVIGATION_OTHER', 'Other')}
+      </StickyNavItemLabel>
+      <StickyNavItem
+        to="reports"
+        containerId="main"
+        spy
+        smooth
+        duration={500}
+        offset={-350}
+      >
+        {t('__MOTHER_DASHBOARD_NAVIGATION_REPORTS', 'Reports & Attachments')}
+      </StickyNavItem>
+      <StyledDivider />
+      <Anchor
+        isExternal
+        onClick={() => {
+          window.open('https://unguess.io/services/', '_blank');
+        }}
+      >
+        {t('__MOTHER_DASHBOARD_NAVIGATION_BUG_DETAILS', 'Bug details')}
+      </Anchor>
+    </StickyContainer>
+  );
 };
--- src/pages/Services/CategoriesNav.tsx
+++ src/pages/Services/CategoriesNav.tsx
@@ -1,4 +1,4 @@
-import { Anchor, MD, Skeleton } from '@appquality/unguess-design-system';
+import { Anchor, Skeleton } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from 'src/app/hooks';
@@ -9,50 +9,14 @@ import {
   useGeti18nCategoriesQuery,
   useGeti18nServicesFeaturedQuery,
 } from 'src/features/backoffice/strapi';
-import styled from 'styled-components';
-import { Divider } from 'src/common/components/divider';
-import { Link } from 'react-scroll';
 import { hasEnoughCoins } from 'src/common/utils';
 import { extractStrapiData } from 'src/common/getStrapiData';
 import { StickyContainer } from 'src/common/components/StickyContainer';
-
-const StyledDivider = styled(Divider)`
-  margin-top: ${({ theme }) => theme.space.base * 3}px;
-  margin-bottom: ${({ theme }) => theme.space.base * 6}px;
-`;
-
-const StickyNavItem = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  border-top-left-radius: ${({ theme }) => theme.space.lg};
-  border-bottom-left-radius: ${({ theme }) => theme.space.lg};
-  padding: ${({ theme }) => theme.space.sm} ${({ theme }) => theme.space.md};
-  cursor: pointer;
-  color: ${({ theme }) => theme.palette.blue[600]};
-  margin: ${({ theme }) => theme.space.sm} 0;
-  background-color: transparent;
-  transition: background-color 0.1s ease-in-out;
-  text-decoration: none;
-
-  &:hover {
-    color: ${({ theme }) => theme.palette.blue[700]};
-    background-color: ${({ theme }) => theme.palette.kale[100]};
-    transition: all 0.1s ease-in-out;
-    text-decoration: none;
-  }
-
-  &.active {
-    color: ${({ theme }) => theme.palette.blue[700]};
-    background-color: ${({ theme }) => theme.palette.kale[100]};
-    font-weight: ${({ theme }) => theme.fontWeights.medium};
-  }
-`;
-
-const StickyNavItemLabel = styled(MD)`
-  color: ${({ theme }) => theme.palette.blue[700]};
-  font-weight: ${({ theme }) => theme.fontWeights.medium};
-`;
+import {
+  StyledDivider,
+  StickyNavItem,
+  StickyNavItemLabel,
+} from 'src/common/components/navigation';
 
 const CategoriesNav = () => {
   const { t } = useTranslation();
