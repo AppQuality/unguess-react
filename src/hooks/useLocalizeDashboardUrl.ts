702327956fdef7b6fc3d4ea3c1209dc422260967
Davide Bizzi <davide.bizzi@app-quality.com>
2022-11-21 18:24:53 +0100

    fix: Use ux dashboard url only if media are available

--- src/hooks/useLocalizeDashboardUrl.ts
+++ src/hooks/useLocalizeDashboardUrl.ts
@@ -1,18 +1,5 @@
 import { CampaignActionProps } from 'src/pages/Dashboard/types';
 import i18n from 'src/i18n';
-import { Output } from 'src/features/api';
-
-const isReactCampaign = (outputs: Output[]): boolean => {
-  let isReact = true;
-
-  if (outputs.length) {
-    outputs.forEach((output) => {
-      if (output === 'media') isReact = false;
-    });
-  }
-
-  return isReact;
-};
 
 export const getLocalizedFunctionalDashboardUrl = (
   aCampaignId: number,
@@ -39,25 +26,15 @@ export const getLocalizedUXDashboardUrl = (
       }/it/dashboard-campagne-esperienziali/?cid=${aCampaignId}`;
 
 export function getLocalizeDashboardRoute(props: CampaignActionProps): string {
-  const { campaignId, cpFamily, outputs } = props;
+  const { campaignId, outputs } = props;
 
   const currentLang = i18n.language || 'en';
-  let localizedRoute = '';
 
-  if (isReactCampaign(outputs)) {
-    localizedRoute = `${
-      currentLang === 'en' ? '' : `/${currentLang}`
-    }/campaigns/${campaignId}`;
-  } else if (cpFamily.toLocaleLowerCase() === 'functional') {
-    localizedRoute = getLocalizedFunctionalDashboardUrl(
-      campaignId,
-      currentLang
-    );
-  } else {
-    localizedRoute = getLocalizedUXDashboardUrl(campaignId, currentLang);
+  if (outputs.some((o) => o === 'media')) {
+    return getLocalizedUXDashboardUrl(campaignId, currentLang);
   }
 
-  // in case of base route ("") we already have a forward slash
-  const re = /\/$/;
-  return re.test(localizedRoute) ? localizedRoute : `${localizedRoute}/`;
+  return `${
+    currentLang === 'en' ? '' : `/${currentLang}`
+  }/campaigns/${campaignId}/`;
 }
