f2f19634f5cb913153fdf27a938c54200b80c058
Davide Bizzi <davide.bizzi@app-quality.com>
2022-11-23 18:59:00 +0100

    feat: Add basic structure for bugcard component

---
+++ src/pages/Campaign/widgets/BugCard.tsx
@@ -0,0 +1,114 @@
+import {
+  SpecialCard,
+  Tag,
+  MD,
+  SM,
+  Anchor,
+} from '@appquality/unguess-design-system';
+import React from 'react';
+import styled from 'styled-components';
+import { SEVERITY_COLORS } from 'src/constants';
+
+const BugCardContainer = styled(SpecialCard)<
+  React.ComponentProps<typeof Tag> & {
+    borderColor: string;
+  }
+>`
+  border-left: 2px solid ${(props) => props.borderColor};
+  border-radius: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: auto;
+`;
+
+const BugCard = ({
+  children,
+  severity,
+}: {
+  children: React.ReactNode;
+  severity: Severities;
+}) => (
+  <BugCardContainer borderColor={SEVERITY_COLORS[severity as Severities]}>
+    {children}
+  </BugCardContainer>
+);
+
+const StyledSM = styled(SM)`
+  margin-bottom: ${({ theme }) => theme.space.xxs};
+`;
+
+const BugCardTopTitle = ({ children }: { children: React.ReactNode }) => (
+  <StyledSM isBold>{children}</StyledSM>
+);
+BugCard.TopTitle = BugCardTopTitle;
+
+const BugCardTitle = ({
+  children,
+  url,
+}: {
+  children: React.ReactNode;
+  url: string;
+}) => (
+  <Anchor href={url} target="_blank">
+    <MD isBold>{children}</MD>
+  </Anchor>
+);
+BugCard.Title = BugCardTitle;
+
+const StyledPill = styled(Tag)<
+  React.ComponentProps<typeof Tag> & {
+    background?: string;
+    color?: string;
+  }
+>`
+  ${({ background }) => background && `background-color: ${background};`}
+  ${({ color }) => color && `color: ${color};`}
+`;
+
+const BugCardPill = ({
+  children,
+  severity,
+}: {
+  children: React.ReactNode;
+  severity?: Severities;
+}) => {
+  let background;
+  let color;
+  if (severity) {
+    background = SEVERITY_COLORS[severity as Severities];
+    color = 'white';
+  }
+
+  return (
+    <StyledPill isPill background={background} color={color}>
+      {children}
+    </StyledPill>
+  );
+};
+BugCard.Pill = BugCardPill;
+
+BugCard.Footer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: ${({ theme }) => theme.space.sm};
+  margin-bottom: ${({ theme }) => theme.space.xxs};
+  & > * {
+    &:not(:first-child):not(:last-child) {
+      margin: 0 ${({ theme }) => theme.space.xxs};
+    }
+    &:first-child {
+      margin-right: ${({ theme }) => theme.space.xxs};
+    }
+    &:last-child {
+      margin-left: ${({ theme }) => theme.space.xxs};
+    }
+  }
+`;
+BugCard.Separator = styled.div`
+  height: 16px;
+  width: 1px;
+  background-color: #e6e6e6;
+`;
+
+export { BugCard };
