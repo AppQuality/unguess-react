c06fa5a38d4767eb2b28a119aca750144982eb85
Marco Bonomo <marco.bonomo@unguess.io>
2022-11-24 15:49:32 +0100

    feat(cp-nav): add media detail link + init media navigation

--- src/pages/Campaign/navigation/media/ExternalLink.tsx
+++ src/pages/Campaign/navigation/media/ExternalLink.tsx
@@ -1,6 +1,6 @@
 import { Anchor } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
-import { getLocalizedFunctionalDashboardUrl } from 'src/hooks/useLocalizeDashboardUrl';
+import { getLocalizedUXDashboardUrl } from 'src/hooks/useLocalizeDashboardUrl';
 import i18n from 'src/i18n';
 import styled from 'styled-components';
 
@@ -9,7 +9,7 @@ const StyledAnchor = styled(Anchor)`
   margin-bottom: ${({ theme }) => theme.space.sm};
 `;
 
-export const BugsNavigationLink = ({ campaignId }: { campaignId: number }) => {
+export const MediaNavigationLink = ({ campaignId }: { campaignId: number }) => {
   const { t } = useTranslation();
 
   return (
@@ -18,12 +18,12 @@ export const BugsNavigationLink = ({ campaignId }: { campaignId: number }) => {
       onClick={() => {
         // eslint-disable-next-line security/detect-non-literal-fs-filename
         window.open(
-          getLocalizedFunctionalDashboardUrl(campaignId, i18n.language),
+          getLocalizedUXDashboardUrl(campaignId, i18n.language),
           '_blank'
         );
       }}
     >
-      {t('__CAMPAIGN_PAGE_NAVIGATION_BUG_EXTERNAL_LINK_LABEL')}
+      {t('__CAMPAIGN_PAGE_NAVIGATION_MEDIA_EXTERNAL_LINK_LABEL')}
     </StyledAnchor>
   );
 };
