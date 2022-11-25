c018b61ad58b6b8c5453f0d34ba9ef917ac2969f
Iacopo Leardini <iacopo.lea@gmail.com>
2022-11-25 11:18:25 +0100

    refactor(list item): general component

--- src/pages/Campaign/widgets/ListItemTable/ListItem.tsx
+++ src/pages/Campaign/widgets/ListItemTable/ListItem.tsx
@@ -1,11 +1,16 @@
 import {
-  Span,
   theme as globalTheme,
   Title,
   SM,
   Progress,
 } from '@appquality/unguess-design-system';
 import styled from 'styled-components';
+
+export interface ListItemProps {
+  title: string;
+  numerator: number;
+  denominator: number;
+}
 
 const ListItemTitle = styled.div`
   display: flex;
@@ -17,28 +22,24 @@ const ListItemWrapper = styled.div`
   margin-top: ${(p) => p.theme.space.xxs};
 `;
 
-export const ListItem = ({
-  item,
-}: {
-  item: { usecase: string; unique: number; total: number };
-}) => (
+export const ListItem = ({ item }: { item: ListItemProps }) => (
   <ListItemWrapper>
     <ListItemTitle>
       <Title style={{ color: globalTheme.palette.blue[600] }}>
-        {item.usecase}
+        {item.title}
       </Title>
       <div>
         <SM tag="span" isBold color={globalTheme.palette.grey[700]}>
-          {item.unique}
+          {item.numerator}
         </SM>
         /
         <SM tag="span" color={globalTheme.palette.grey[600]}>
-          {item.total}
+          {item.denominator}
         </SM>
       </div>
     </ListItemTitle>
     <Progress
-      value={Math.round((item.unique / item.total) * 100)}
+      value={Math.round((item.numerator / item.denominator) * 100)}
       size="small"
       color="#02807A"
     />
