4abb11de69cd0493625170c1b8365cd745f6bb6b
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-11-30 16:17:24 +0100

    fix(list): set correct header

--- src/pages/Campaign/widgets/UniqueBugs4UseCase/ListUniqueBugs4UseCase.tsx
+++ src/pages/Campaign/widgets/UniqueBugs4UseCase/ListUniqueBugs4UseCase.tsx
@@ -1,3 +1,4 @@
+import { XL } from '@appquality/unguess-design-system';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
