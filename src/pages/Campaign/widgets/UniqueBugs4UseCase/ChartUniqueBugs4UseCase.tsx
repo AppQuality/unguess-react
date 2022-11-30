06e69bd5e60ea4876993ed7a0d85def89307d8f2
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-11-30 18:02:59 +0100

    feat(wip pie unique bugs): get data  by CP

--- src/pages/Campaign/widgets/UniqueBugs4UseCase/ChartUniqueBugs4UseCase.tsx
+++ src/pages/Campaign/widgets/UniqueBugs4UseCase/ChartUniqueBugs4UseCase.tsx
@@ -1,47 +1,45 @@
 import { PieChart } from '@appquality/unguess-design-system';
+import { PieDatum } from '@appquality/unguess-design-system/build/stories/charts/pie/_types';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetCampaignsByCidWidgetsQuery } from 'src/features/api';
 
-const pieChartProps = {
-  width: '100%',
-  height: '270px',
-  data: [
-    {
-      id: 'sass',
-      label: 'sass',
-      value: 309,
-      color: 'hsl(162, 70%, 50%)',
+export const ChartUniqueBugs4UseCase = () => {
+  const { campaignId } = useParams();
+  const { data } = useGetCampaignsByCidWidgetsQuery({
+    cid: Number(campaignId),
+    s: 'bugs-by-usecase',
+  });
+  const [total, setTotal] = useState(0);
+  const [items, setItems] = useState<PieDatum[]>([]);
+
+  useEffect(() => {
+    if (data && 'kind' in data && data.kind === 'bugsByUseCase') {
+      const newTotal = data.data.reduce(
+        (acc, current) => acc + current.bugs,
+        0
+      );
+      const currentItems = data.data.map((item) => ({
+        id: item.title,
+        label: 'label test',
+        value: item.bugs,
+      }));
+      setItems(currentItems);
+      setTotal(newTotal);
+    }
+  }, [data]);
+  const pieChartProps = {
+    width: '100%',
+    height: '270px',
+    data: items,
+    centerItem: {
+      label: 'Tot. bugs',
+      value: String(total),
     },
-    {
-      id: 'make',
-      label: 'make',
-      value: 420,
-      color: 'hsl(175, 70%, 50%)',
-    },
-    {
-      id: 'erlang',
-      label: 'erlang',
-      value: 300,
-      color: 'hsl(159, 70%, 50%)',
-    },
-    {
-      id: 'lisp',
-      label: 'lisp',
-      value: 491,
-      color: 'hsl(243, 70%, 50%)',
-    },
-    {
-      id: 'go',
-      label: 'go',
-      value: 108,
-      color: 'hsl(163, 70%, 50%)',
-    },
-  ],
-  centerItem: {
-    label: 'Tot. bugs',
-    value: '27',
-  },
+  };
+  return (
+    <div>
+      <PieChart {...pieChartProps} />
+    </div>
+  );
 };
-export const ChartUniqueBugs4UseCase = () => (
-  <div>
-    <PieChart {...pieChartProps} />
-  </div>
-);
