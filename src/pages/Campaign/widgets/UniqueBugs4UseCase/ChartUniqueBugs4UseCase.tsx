a4dfc27dee1d8ce8aca4fbb65be47a596501388f
Iacopo Leardini <iacopo.lea@gmail.com>
2022-12-01 17:45:20 +0100

    feat(bug by usecase): use max items

--- src/pages/Campaign/widgets/UniqueBugs4UseCase/ChartUniqueBugs4UseCase.tsx
+++ src/pages/Campaign/widgets/UniqueBugs4UseCase/ChartUniqueBugs4UseCase.tsx
@@ -1,21 +1,29 @@
 import { PieChart, Skeleton } from '@appquality/unguess-design-system';
 import { BugsByUseCaseVisualizationProps } from './types';
 import { useBugsByUsecase } from './useBugsByUsecase';
+import { useMaxItems } from './useMaxItems';
 
 export const ChartUniqueBugs4UseCase = ({
   campaignId,
 }: BugsByUseCaseVisualizationProps) => {
   const { items, total, isLoading, isError } = useBugsByUsecase(campaignId);
+  const newItems = useMaxItems(items, 6);
+
   if (isLoading || isError) {
     return <Skeleton />;
   }
   return (
-    <PieChart
-      legend
-      width="100%"
-      height="270px"
-      centerItem={{ label: 'Tot. Bugs', value: total.toString() }}
-      data={items}
-    />
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <PieChart
+        legend
+        width="100%"
+        height="270px"
+        centerItem={{ label: 'Tot. Bugs', value: total.toString() }}
+        data={newItems}
+        theme={{ labels: { text: { fontSize: 10 } } }}
+      />
+    </div>
   );
 };
