6b9ec9cfda886bfe68a0ebf259b19f6ccc20c9b4
Davide Bizzi <davide.bizzi@app-quality.com>
2022-12-02 19:52:59 +0100

    refactor: Move components to folders

--- src/pages/Campaign/widgets/TotalBugsByOsAndDevices/ListTotalBugsByDevice/index.tsx
+++ src/pages/Campaign/widgets/TotalBugsByOsAndDevices/ListTotalBugsByDevice/index.tsx
@@ -2,8 +2,8 @@ import { Skeleton, XL } from '@appquality/unguess-design-system';
 import { useEffect, useMemo, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { theme } from 'src/app/theme';
-import { List } from '../List';
-import { ListItem } from '../List/ListItem';
+import { List } from '../../List';
+import { ListItem } from '../../List/ListItem';
 import { useListBugsByDevice } from './useListBugsByDevice';
 
 export const ListTotalBugsByDevice = ({
