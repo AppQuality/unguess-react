28500d94ae57549fa033d33d8076b9ca2a284447
Marco Bonomo <marco.bonomo@unguess.io>
2022-12-05 13:03:03 +0100

    fix(waffle-tooltip): fix tooltip color + adjust tooltip width

--- src/pages/Campaign/widgets/UniqueBugs/WaffleTooltip.tsx
+++ src/pages/Campaign/widgets/UniqueBugs/WaffleTooltip.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 const WaffleTooltipContainer = styled.div`
   padding: ${({ theme }) => theme.space.xxs} ${({ theme }) => theme.space.xs};
+  color: ${({ theme }) => theme.colors.primaryHue};
+  max-width: 261px;
+  min-width: 200px;
+  text-align: center;
 `;
 
 const WaffleTooltip = ({
@@ -18,7 +22,10 @@ const WaffleTooltip = ({
     case 'unique':
       return (
         <WaffleTooltipContainer>
-          <Trans i18nKey="__CAMPAIGN_PAGE_WIDGET_UNIQUE_BUGS_WAFFLE_TOOLTIP_UNIQUE">
+          <Trans
+            tag="paragraph"
+            i18nKey="__CAMPAIGN_PAGE_WIDGET_UNIQUE_BUGS_WAFFLE_TOOLTIP_UNIQUE"
+          >
             {{ value }} unique bugs - {{ percent: percentage }}%
           </Trans>
         </WaffleTooltipContainer>
