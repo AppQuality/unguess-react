9e2e45c5a25be6bdca081229d08768e6edaac531
Davide Bizzi <davide.bizzi@app-quality.com>
2023-01-11 13:14:59 +0100

    rework: Use opacity for severity pills

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -9,20 +9,11 @@ export const SEVERITY_COLORS: Record<Severities, string> = {
   medium: CHARTS_COLOR_PALETTE.blueRoyal,
   low: CHARTS_COLOR_PALETTE.darkPine,
 };
-
-export const SEVERITY_BACKGROUNDS: Record<Severities, string> = {
-  critical: '#FFEEEE',
-  high: '#FFF7EE',
-  medium: '#EEF7FF',
-  low: '#EEFFFE',
-};
-
 const theme = {
   ...baseTheme,
   colors: {
     ...baseTheme.colors,
     bySeverity: SEVERITY_COLORS,
-    severitiesBackground: SEVERITY_BACKGROUNDS,
     darkPine: CHARTS_COLOR_PALETTE.darkPine,
   },
 };
