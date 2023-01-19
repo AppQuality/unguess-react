a06db3b837369812d9dd3c3d84d1a049b6555dde
Davide Bizzi <davide.bizzi@app-quality.com>
2023-01-19 11:06:35 +0100

    fix(bugs): Style info pills

--- src/common/components/pills/IconPill.tsx
+++ src/common/components/pills/IconPill.tsx
@@ -1,21 +1,21 @@
 import { Span, Tag } from '@appquality/unguess-design-system';
-import { theme as globalTheme } from 'src/app/theme';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
+
+const StyledAvatar = styled(Tag.Avatar)``;
 
 const StyledTag = styled(Tag)`
   pointer-events: none;
   padding: 0;
   margin-right: ${({ theme }) => theme.space.xs};
+  ${StyledAvatar} {
+    margin-left: 0;
+    margin-right: ${({ theme }) => theme.space.xxs};
+  }
 `;
 
 const StyledChild = styled(Span)`
   color: ${({ theme }) => theme.palette.grey[700]};
-`;
-
-const StyledAvatar = styled(StyledTag.Avatar)`
-  margin-left: 0 !important;
-  margin-right: ${({ theme }) => theme.space.xxs} !important;
 `;
 
 const PillContainer = styled.div`
@@ -24,12 +24,17 @@ const PillContainer = styled.div`
   display: flex;
   align-items: center;
 `;
-interface PillProps extends React.HTMLAttributes<HTMLDivElement> {
-  title: string;
+
+interface PillProps {
+  title: ReactNode;
+  id?: string;
+  className?: string;
+  style?: React.CSSProperties;
   background?: string;
   color?: string;
   icon?: ReactNode;
   children?: ReactNode;
+  size?: Parameters<typeof Tag>[0]['size'];
 }
 
 export const IconPill = ({
@@ -38,14 +43,13 @@ export const IconPill = ({
   icon,
   title,
   children,
+  size,
   ...props
 }: PillProps) => (
   <PillContainer {...props}>
-    <StyledTag isPill hue={background ?? 'white'} size="large">
+    <StyledTag isPill hue={background ?? 'white'} size={size || 'large'}>
       {icon && <StyledAvatar>{icon}</StyledAvatar>}
-      <Span isBold style={{ color: color ?? globalTheme.palette.grey[700] }}>
-        {title}
-      </Span>
+      {title}
     </StyledTag>
     {children && <StyledChild>{children}</StyledChild>}
   </PillContainer>
