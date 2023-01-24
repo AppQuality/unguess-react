93d4dcbaacb104af348e3ea06872aa8b572b19b8
Iacopo Leardini <iacopo.lea@gmail.com>
2023-01-24 16:39:13 +0100

    feat(groupBy): style and icon

--- src/pages/Bugs/Filters/GroupBy.tsx
+++ src/pages/Bugs/Filters/GroupBy.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Dropdown,
   Item,
   Menu,
@@ -9,13 +8,41 @@ import { Field } from '@zendeskgarden/react-dropdowns';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
 import { setPageView } from 'src/features/bugsPage/bugsPageSlice';
+import styled from 'styled-components';
+
+import { ReactComponent as Icon } from './assets/layers_icon.svg';
 
 export const GroupBy = () => {
   const bugsPageSlice = useAppSelector((state) => state.bugsPage);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const { pageView } = bugsPageSlice;
+  const broupByOptions: Array<typeof pageView> = [
+    'byUsecase',
+    'bySeverity',
+    'ungrouped',
+  ];
 
+  const label = (view: typeof pageView) => {
+    switch (view) {
+      case 'byUsecase':
+        return t('__BUGS_GROUP_BY_USE_CASE', 'By use case');
+      case 'bySeverity':
+        return t('__BUGS_GROUP_BY_SEVERITY', 'By severity');
+      case 'ungrouped':
+        return t('__BUGS_GROUP_BY_UNGROUPED', 'Ungrouped');
+      default:
+        return null;
+    }
+  };
+
+  const SelectLabel = styled.span`
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 8px;
+    }
+  `;
   return (
     <div>
       <Dropdown
@@ -26,13 +53,17 @@ export const GroupBy = () => {
       >
         <Field>
           <Select isCompact isPrimary>
-            {pageView}
+            <SelectLabel>
+              <Icon /> {label(pageView)}
+            </SelectLabel>
           </Select>
         </Field>
         <Menu>
-          <Item value="byUsecase">{t('By use case')}</Item>
-          <Item value="bySeverity">{t('By severity')}</Item>
-          <Item value="ungrouped">{t('Ungrouped')}</Item>
+          {broupByOptions.map((item) => (
+            <Item key={item} value={item}>
+              {label(item)}
+            </Item>
+          ))}
         </Menu>
       </Dropdown>
     </div>
