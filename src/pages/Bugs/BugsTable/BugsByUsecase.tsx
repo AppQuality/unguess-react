73c67d0ce6a1adb582b1414e137118bb9acd358e
Iacopo Leardini <iacopo.lea@gmail.com>
2023-01-26 11:13:41 +0100

    refactor(bugs-table): tooltip in a separate component

--- src/pages/Bugs/BugsTable/BugsByUsecase.tsx
+++ src/pages/Bugs/BugsTable/BugsByUsecase.tsx
@@ -1,18 +1,9 @@
-import {
-  Accordion,
-  IconButton,
-  MD,
-  SM,
-  Tooltip,
-} from '@appquality/unguess-design-system';
-import { theme } from 'src/app/theme';
-import { Trans, useTranslation } from 'react-i18next';
+import { Accordion, MD } from '@appquality/unguess-design-system';
 import { ColumnDefinitionType } from 'src/common/components/Table';
-import { ReactComponent as InfoStrokeIcon } from '@zendeskgarden/svg-icons/src/16/info-stroke.svg';
-import styled from 'styled-components';
 import { EmptyState } from './components/EmptyState';
 import SingleGroupTable from './components/SingleGroupTable';
 import { BugByUsecaseType, TableDatum } from './types';
+import { CompletionTooltip } from './components/CompletionTooltip';
 
 export const BugsByUsecase = ({
   bugsByUseCases,
@@ -21,7 +12,6 @@ export const BugsByUsecase = ({
   bugsByUseCases: BugByUsecaseType[];
   columns: ColumnDefinitionType<TableDatum, keyof TableDatum>[];
 }) => {
-  const { t } = useTranslation();
   // seems that sections index are only odd numbers ¯\_(ツ)_/¯
   // i.e. [1, 3, 5, 7]
   const defaultExpandedSections = Array.from(
@@ -33,116 +23,6 @@ export const BugsByUsecase = ({
     return <EmptyState />;
   }
 
-  const StyledSM = styled(SM)<{ color: string }>`
-    color: ${(p) => p.theme.palette.grey[600]}};
-    span {
-      color: ${(p) => p.color};
-    }
-  `;
-
-  const completionBreakpoints = [25, 50, 75];
-
-  const CompletionText = styled.div`
-    display: flex;
-    align-items: center;
-  `;
-
-  const getCompletionText = (percentage: number) => {
-    if (percentage < completionBreakpoints[0]) {
-      return (
-        <CompletionText>
-          <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_1">
-            <StyledSM color={theme.palette.red[800]} isBold>
-              Use case <span>starting tests</span>
-            </StyledSM>
-          </Trans>
-          <Tooltip
-            content={t('__BUGS_PAGE_USECASE_COMPLETION_1_TOOLTIP')}
-            size="large"
-            type="light"
-            placement="bottom-end"
-            hasArrow={false}
-          >
-            <IconButton size="small">
-              <InfoStrokeIcon />
-            </IconButton>
-          </Tooltip>
-        </CompletionText>
-      );
-    }
-    if (
-      completionBreakpoints[0] <= percentage &&
-      percentage < completionBreakpoints[1]
-    ) {
-      return (
-        <CompletionText>
-          <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_2">
-            <StyledSM color={theme.colors.gubbioLight} isBold>
-              Use case <span>in progress</span>
-            </StyledSM>
-          </Trans>
-          <Tooltip
-            content={t('__BUGS_PAGE_USECASE_COMPLETION_2_TOOLTIP')}
-            size="large"
-            type="light"
-            placement="bottom-end"
-            hasArrow={false}
-          >
-            <IconButton size="small">
-              <InfoStrokeIcon />
-            </IconButton>
-          </Tooltip>
-        </CompletionText>
-      );
-    }
-    if (
-      completionBreakpoints[1] <= percentage &&
-      percentage < completionBreakpoints[2]
-    ) {
-      return (
-        <CompletionText>
-          <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_3">
-            <StyledSM color={theme.colors.blueRoyal} isBold>
-              Use case <span>almost ready</span>
-            </StyledSM>
-          </Trans>
-          <Tooltip
-            content={t('__BUGS_PAGE_USECASE_COMPLETION_3_TOOLTIP')}
-            size="large"
-            type="light"
-            placement="bottom-end"
-            hasArrow={false}
-          >
-            <IconButton size="small">
-              <InfoStrokeIcon />
-            </IconButton>
-          </Tooltip>
-        </CompletionText>
-      );
-    }
-    // last case
-    return (
-      <CompletionText>
-        <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_4">
-          <StyledSM color={theme.colors.darkPine} isBold>
-            Use case <span>completed</span>
-          </StyledSM>
-        </Trans>
-        <Tooltip
-          content={t('__BUGS_PAGE_USECASE_COMPLETION_4_TOOLTIP')}
-          size="large"
-          type="light"
-          placement="bottom-end"
-          hasArrow={false}
-        >
-          <IconButton size="small">
-            <InfoStrokeIcon />
-          </IconButton>
-        </Tooltip>
-      </CompletionText>
-    );
-  };
-
   return (
     <Accordion
       level={3}
@@ -150,20 +30,25 @@ export const BugsByUsecase = ({
       isExpandable
       isBare
     >
-      {bugsByUseCases.map((item) => (
-        <SingleGroupTable
-          key={item.useCase.id}
-          title={
-            <>
-              {item.useCase.title.full}
-              <MD tag="span">{` (${item.bugs.length})`}</MD>
-            </>
-          }
-          item={item}
-          columns={columns}
-          footer={getCompletionText(item.useCase.completion)}
-        />
-      ))}
+      {bugsByUseCases.map(
+        (item) =>
+          item.bugs.length > 0 && (
+            <SingleGroupTable
+              key={item.useCase.id}
+              title={
+                <>
+                  {item.useCase.title.full}
+                  <MD tag="span">{` (${item.bugs.length})`}</MD>
+                </>
+              }
+              item={item}
+              columns={columns}
+              footer={
+                <CompletionTooltip percentage={item.useCase.completion} />
+              }
+            />
+          )
+      )}
     </Accordion>
   );
 };
---
+++ src/pages/Bugs/BugsTable/components/CompletionTooltip.tsx
@@ -0,0 +1,116 @@
+import { IconButton, SM, Tooltip } from '@appquality/unguess-design-system';
+import { Trans, useTranslation } from 'react-i18next';
+import { theme } from 'src/app/theme';
+import styled from 'styled-components';
+import { ReactComponent as InfoStrokeIcon } from '@zendeskgarden/svg-icons/src/16/info-stroke.svg';
+
+export const CompletionTooltip = ({ percentage }: { percentage: number }) => {
+  const { t } = useTranslation();
+  const StyledSM = styled(SM)<{ color: string }>`
+    color: ${(p) => p.theme.palette.grey[600]}};
+    span {
+      color: ${(p) => p.color};
+    }
+  `;
+
+  const completionBreakpoints = [25, 50, 75];
+
+  const CompletionText = styled.div`
+    display: flex;
+    align-items: center;
+  `;
+
+  if (percentage < completionBreakpoints[0]) {
+    return (
+      <CompletionText>
+        <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_1">
+          <StyledSM color={theme.palette.red[800]} isBold>
+            Use case <span>starting tests</span>
+          </StyledSM>
+        </Trans>
+        <Tooltip
+          content={t('__BUGS_PAGE_USECASE_COMPLETION_1_TOOLTIP')}
+          size="large"
+          type="light"
+          placement="bottom-end"
+          hasArrow={false}
+        >
+          <IconButton size="small">
+            <InfoStrokeIcon />
+          </IconButton>
+        </Tooltip>
+      </CompletionText>
+    );
+  }
+  if (
+    completionBreakpoints[0] <= percentage &&
+    percentage < completionBreakpoints[1]
+  ) {
+    return (
+      <CompletionText>
+        <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_2">
+          <StyledSM color={theme.colors.gubbioLight} isBold>
+            Use case <span>in progress</span>
+          </StyledSM>
+        </Trans>
+        <Tooltip
+          content={t('__BUGS_PAGE_USECASE_COMPLETION_2_TOOLTIP')}
+          size="large"
+          type="light"
+          placement="bottom-end"
+          hasArrow={false}
+        >
+          <IconButton size="small">
+            <InfoStrokeIcon />
+          </IconButton>
+        </Tooltip>
+      </CompletionText>
+    );
+  }
+  if (
+    completionBreakpoints[1] <= percentage &&
+    percentage < completionBreakpoints[2]
+  ) {
+    return (
+      <CompletionText>
+        <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_3">
+          <StyledSM color={theme.colors.blueRoyal} isBold>
+            Use case <span>almost ready</span>
+          </StyledSM>
+        </Trans>
+        <Tooltip
+          content={t('__BUGS_PAGE_USECASE_COMPLETION_3_TOOLTIP')}
+          size="large"
+          type="light"
+          placement="bottom-end"
+          hasArrow={false}
+        >
+          <IconButton size="small">
+            <InfoStrokeIcon />
+          </IconButton>
+        </Tooltip>
+      </CompletionText>
+    );
+  }
+  // last case
+  return (
+    <CompletionText>
+      <Trans i18nKey="__BUGS_PAGE_USECASE_COMPLETION_4">
+        <StyledSM color={theme.colors.darkPine} isBold>
+          Use case <span>completed</span>
+        </StyledSM>
+      </Trans>
+      <Tooltip
+        content={t('__BUGS_PAGE_USECASE_COMPLETION_4_TOOLTIP')}
+        size="large"
+        type="light"
+        placement="bottom-end"
+        hasArrow={false}
+      >
+        <IconButton size="small">
+          <InfoStrokeIcon />
+        </IconButton>
+      </Tooltip>
+    </CompletionText>
+  );
+};
