18b13dc704102108c218df59cb2d94138617545e
Iacopo Leardini <iacopo.lea@gmail.com>
2023-01-26 11:37:53 +0100

    feat(bugs-table): empty use cases

--- src/pages/Bugs/BugsTable/BugsByUsecase.tsx
+++ src/pages/Bugs/BugsTable/BugsByUsecase.tsx
@@ -1,5 +1,9 @@
 import { Accordion, MD } from '@appquality/unguess-design-system';
+import { useMemo } from 'react';
 import { ColumnDefinitionType } from 'src/common/components/Table';
+import { theme } from 'src/app/theme';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import { EmptyState } from './components/EmptyState';
 import SingleGroupTable from './components/SingleGroupTable';
 import { BugByUsecaseType, TableDatum } from './types';
@@ -12,6 +16,15 @@ export const BugsByUsecase = ({
   bugsByUseCases: BugByUsecaseType[];
   columns: ColumnDefinitionType<TableDatum, keyof TableDatum>[];
 }) => {
+  const { t } = useTranslation();
+  const emptyUseCases = useMemo(
+    () => bugsByUseCases.filter((item) => item.bugs.length === 0),
+    [bugsByUseCases]
+  );
+  const useCases = useMemo(
+    () => bugsByUseCases.filter((item) => item.bugs.length > 0),
+    [bugsByUseCases]
+  );
   // seems that sections index are only odd numbers ¯\_(ツ)_/¯
   // i.e. [1, 3, 5, 7]
   const defaultExpandedSections = Array.from(
@@ -19,7 +32,11 @@ export const BugsByUsecase = ({
     (_, i) => i + (i + 1)
   );
 
-  if (!bugsByUseCases.length) {
+  const EmptyUsecase = styled(MD)`
+    color: ${theme.palette.grey[500]};
+  `;
+
+  if (!useCases.length) {
     return <EmptyState />;
   }
 
@@ -30,24 +47,29 @@ export const BugsByUsecase = ({
       isExpandable
       isBare
     >
-      {bugsByUseCases.map(
-        (item) =>
-          item.bugs.length > 0 && (
-            <SingleGroupTable
-              key={item.useCase.id}
-              title={
-                <>
-                  {item.useCase.title.full}
-                  <MD tag="span">{` (${item.bugs.length})`}</MD>
-                </>
-              }
-              item={item}
-              columns={columns}
-              footer={
-                <CompletionTooltip percentage={item.useCase.completion} />
-              }
-            />
-          )
+      {useCases.map((item) => (
+        <SingleGroupTable
+          key={item.useCase.id}
+          title={
+            <>
+              {item.useCase.title.full}
+              <MD tag="span">{` (${item.bugs.length})`}</MD>
+            </>
+          }
+          item={item}
+          columns={columns}
+          footer={<CompletionTooltip percentage={item.useCase.completion} />}
+        />
+      ))}
+      {emptyUseCases.length > 1 && (
+        <EmptyUsecase isBold>
+          {t('other use cases')} <MD tag="span">(0)</MD>
+        </EmptyUsecase>
+      )}
+      {emptyUseCases.length === 1 && (
+        <EmptyUsecase isBold>
+          {emptyUseCases[0].useCase.title.full} <MD tag="span">(0)</MD>
+        </EmptyUsecase>
       )}
     </Accordion>
   );
