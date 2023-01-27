2b7d2bd1d9ca6753c0b80d9fa3ee41f662421a11
Iacopo Leardini <iacopo.lea@gmail.com>
2023-01-27 12:00:35 +0100

    fix(bugs-table): remove unused vars

--- src/pages/Bugs/BugsTable/BugsByUsecase.tsx
+++ src/pages/Bugs/BugsTable/BugsByUsecase.tsx
@@ -1,5 +1,5 @@
 import { Accordion, MD } from '@appquality/unguess-design-system';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { EmptyState } from './components/EmptyState';
 import SingleGroupTable from './components/SingleGroupTable';
