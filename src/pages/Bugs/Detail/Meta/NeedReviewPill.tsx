4a2acf5db1f0b4310dbfdabb30be350225fcb48d
Davide Bizzi <davide.bizzi@app-quality.com>
2023-01-27 13:22:47 +0100

    feat(bugs): Add tooltip to needreview pill

--- src/pages/Bugs/Detail/Meta/NeedReviewPill.tsx
+++ src/pages/Bugs/Detail/Meta/NeedReviewPill.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@appquality/unguess-design-system';
 import { IconPill } from 'src/common/components/pills/IconPill';
 import { ReactComponent as NeedReviewIcon } from 'src/assets/icons/pill-icon-need-review.svg';
 import { useTranslation } from 'react-i18next';
@@ -6,11 +7,17 @@ import { InfoTitle } from './InfoTitle';
 export const NeedReviewPill = () => {
   const { t } = useTranslation();
   return (
-    <IconPill
-      size="medium"
-      iconPosition="right"
-      title={<InfoTitle>{t('__BUGS_PAGE_BUG_DETAIL_NEED_REVIEW')}</InfoTitle>}
-      icon={<NeedReviewIcon />}
-    />
+    <Tooltip content={t('__BUGS_PAGE_BUG_DETAIL_NEED_REVIEW_TOOLTIP')}>
+      <span>
+        <IconPill
+          size="medium"
+          iconPosition="right"
+          title={
+            <InfoTitle>{t('__BUGS_PAGE_BUG_DETAIL_NEED_REVIEW')}</InfoTitle>
+          }
+          icon={<NeedReviewIcon />}
+        />
+      </span>
+    </Tooltip>
   );
 };
