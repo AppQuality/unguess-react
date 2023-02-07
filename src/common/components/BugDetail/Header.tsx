4ee0b8d4e0bdb76572d172b26dbecef90be25fbd
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-02-07 11:50:58 +0100

    feat(bug-header): shown father icon if bug has not father and refactor bug header

--- src/common/components/BugDetail/Header.tsx
+++ src/common/components/BugDetail/Header.tsx
@@ -1,18 +1,26 @@
-import { IconButton, SM, Span } from '@appquality/unguess-design-system';
+import {
+  IconButton,
+  Skeleton,
+  SM,
+  Span,
+  theme as globalTheme,
+} from '@appquality/unguess-design-system';
 import { Trans } from 'react-i18next';
 import { useAppDispatch } from 'src/app/hooks';
 import { ReactComponent as CloseIcon } from 'src/assets/icons/close-icon.svg';
 import { ReactComponent as LinkIcon } from 'src/assets/icons/external-link-icon.svg';
+import { ReactComponent as FatherIcon } from 'src/assets/icons/father-icon.svg';
 import { Bug } from 'src/features/api';
 import { selectBug } from 'src/features/bugsPage/bugsPageSlice';
 import { getLocalizedBugUrl } from 'src/hooks/useLocalizeDashboardUrl';
 import i18n from 'src/i18n';
 import styled from 'styled-components';
+import { useSiblings } from './BugDuplicates/useSiblings';
 import { ShareButton } from './ShareBug';
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   margin-bottom: ${({ theme }) => theme.space.sm};
@@ -21,11 +29,15 @@ const Container = styled.div`
 const Tester = styled(SM)`
   color: ${({ theme }) => theme.palette.grey[600]};
 `;
-
+const ActionDetailPreview = styled.div`
+  margin-left: auto;
+`;
 export default ({
+  campaignId,
   bug,
   isPreview,
 }: {
+  campaignId: number;
   bug: Bug & {
     reporter: {
       tester_id: number;
@@ -35,6 +47,12 @@ export default ({
   isPreview?: boolean;
 }) => {
   const dispatch = useAppDispatch();
+  const {
+    data: siblings,
+    isLoading,
+    isFetching,
+    isError,
+  } = useSiblings({ cid: campaignId });
 
   const goToBug = () => {
     window.location.href = getLocalizedBugUrl(
@@ -43,9 +61,19 @@ export default ({
       i18n.language
     );
   };
+  // console.log(siblings, isLoading, isFetching, isError)
 
+  if (isLoading || isFetching) return <Skeleton height="20px" />;
   return (
     <Container>
+      {!siblings?.father && (
+        <FatherIcon
+          style={{
+            color: globalTheme.palette.grey[500],
+            marginRight: globalTheme.space.xxs,
+          }}
+        />
+      )}
       <Tester>
         <Trans i18nKey="__BUGS_PAGE_DETAIL_HEADER">
           ID <Span isBold>{{ bug_id: bug.id }}</Span> by{' '}
@@ -54,7 +82,7 @@ export default ({
         </Trans>
       </Tester>
       {isPreview && (
-        <div>
+        <ActionDetailPreview>
           <IconButton onClick={goToBug}>
             <LinkIcon />
           </IconButton>
@@ -70,7 +98,7 @@ export default ({
           >
             <CloseIcon />
           </IconButton>
-        </div>
+        </ActionDetailPreview>
       )}
     </Container>
   );
