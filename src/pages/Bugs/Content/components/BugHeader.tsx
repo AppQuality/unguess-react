51f51d71f824d42a69bd49161bf8d2c7438ae1a4
Iacopo Leardini <iacopo.lea@gmail.com>
2023-02-28 16:46:45 +0100

    fix(Tag): remove some rounded corners

--- src/pages/Bugs/Content/components/BugHeader.tsx
+++ src/pages/Bugs/Content/components/BugHeader.tsx
@@ -50,7 +50,7 @@ export default ({
 
   return (
     <Container>
-      <Tag isRegular hue="rgba(0,0,0,0)">
+      <Tag isPill={false} isRegular hue="rgba(0,0,0,0)">
         {!bug.duplicated_of_id && (
           <Tag.Avatar>
             <FatherIcon
