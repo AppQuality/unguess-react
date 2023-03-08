8eb2e2f5a686f9c4f587dab2d7faf3fb99a449f4
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-03-08 16:25:06 +0100

    feat(bug priority in table): bug Column Priority added to table

--- src/common/components/PriorityIcon.tsx
+++ src/common/components/PriorityIcon.tsx
@@ -4,7 +4,11 @@ import { ReactComponent as LowIcon } from 'src/assets/icons/bug-priority-low.svg
 import { ReactComponent as HighIcon } from 'src/assets/icons/bug-priority-high.svg';
 import { ReactComponent as HighestIcon } from 'src/assets/icons/bug-priority-highest.svg';
 
-export const PriorityIcon = ({ priority }: { priority: string }) => {
+export const PriorityIcon = ({
+  priority,
+}: {
+  priority: string | undefined;
+}) => {
   switch (priority) {
     case 'medium':
       return <MediumIcon />;
