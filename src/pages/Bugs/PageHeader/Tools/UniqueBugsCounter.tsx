5e62436ff1b56e783092e03da0fdda54ba760a39
Iacopo Leardini <iacopo.lea@gmail.com>
2023-03-08 11:10:33 +0100

    feat(header): see counter also on mobile

--- src/pages/Bugs/PageHeader/Tools/UniqueBugsCounter.tsx
+++ src/pages/Bugs/PageHeader/Tools/UniqueBugsCounter.tsx
@@ -5,11 +5,9 @@ import { theme as globalTheme } from 'src/app/theme';
 import { useUniqueBugs } from 'src/pages/Campaign/widgets/UniqueBugs/useUniqueBugs';
 
 const CounterContainer = styled.div`
-  display: none;
+  display: inline-block;
   margin-right: ${({ theme }) => theme.space.xs};
-  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
-    display: inline-block;
-  }
+}
 `;
 
 export const UniqueBugsCounter = ({
