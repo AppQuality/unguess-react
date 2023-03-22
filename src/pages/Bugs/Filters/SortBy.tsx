25422ec9d4c3321a9f818ab5f6cd95e70329781e
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-03-22 10:12:55 +0100

    feat(dropdown title): dropdown title syncronization with state change

--- src/pages/Bugs/Filters/SortBy.tsx
+++ src/pages/Bugs/Filters/SortBy.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Dropdown,
   Item,
@@ -16,55 +17,59 @@ import ItemGroup from './ItemGroup';
 
 
 
-export const SortBy = () => (
-  <Dropdown
-    selectedItem="severity"
-    onSelect={(sorting) => {
-      // eslint-disable-next-line no-alert
-      alert(`selected sorting: ${sorting}`);
-    }}
-  >
-    <Field>
-      <Select isCompact isPrimary>
-        <DropdownLabel>
-          <SortIcon />
-          <Span>By Highest Severity</Span>
-        </DropdownLabel>
-      </Select>
-    </Field>
-    <Menu>
-      <Dropdown.HeaderItem>Sort By:</Dropdown.HeaderItem>
-      <Dropdown.Separator />
-
-      <ItemGroup title='Severity'>
-        <Item
-          key={`severity.${ORDER__HIGH__TO__LOW.key}`}
-          value={ORDER__HIGH__TO__LOW.key}
-        >
-          {ORDER__HIGH__TO__LOW.displayName}
-        </Item>
-        <Item
-          key={`severity.${ORDER__LOW__TO__HIGH.key}`}
-          value={ORDER__LOW__TO__HIGH.key}
-        >
-          {ORDER__LOW__TO__HIGH.displayName}
-        </Item>
-      </ItemGroup>
-      <ItemGroup title='Priority'>
-        <Item
-          key={`priority.${ORDER__HIGH__TO__LOW.key}`}
-          value={ORDER__HIGH__TO__LOW.key}
-        >
-          {ORDER__HIGH__TO__LOW.displayName}
-        </Item>
-
-        <Item
-          key={`priority.${ORDER__LOW__TO__HIGH.key}`}
-          value={ORDER__LOW__TO__HIGH.key}
-        >
-          {ORDER__LOW__TO__HIGH.displayName}
-        </Item>
-      </ItemGroup>
-    </Menu>
-  </Dropdown>
-);
+export const SortBy = () => {
+  const [title, setTitle] = useState('')
+  return (
+    <Dropdown
+      selectedItem="severity"
+      onSelect={(sorting) => {
+        // eslint-disable-next-line no-alert
+        alert(`selected sorting: ${sorting}`);
+        setTitle(sorting);
+      }}
+    >
+      <Field>
+        <Select isCompact isPrimary>
+          <DropdownLabel>
+            <SortIcon />
+            <Span>{title}</Span>
+          </DropdownLabel>
+        </Select>
+      </Field>
+      <Menu>
+        <Dropdown.HeaderItem>Sort By:</Dropdown.HeaderItem>
+        <Dropdown.Separator />
+  
+        <ItemGroup title='Severity'>
+          <Item
+            key={`severity.${ORDER__HIGH__TO__LOW.key}`}
+            value={ORDER__HIGH__TO__LOW.key}
+          >
+            {ORDER__HIGH__TO__LOW.displayName}
+          </Item>
+          <Item
+            key={`severity.${ORDER__LOW__TO__HIGH.key}`}
+            value={ORDER__LOW__TO__HIGH.key}
+          >
+            {ORDER__LOW__TO__HIGH.displayName}
+          </Item>
+        </ItemGroup>
+        <ItemGroup title='Priority'>
+          <Item
+            key={`priority.${ORDER__HIGH__TO__LOW.key}`}
+            value={ORDER__HIGH__TO__LOW.key}
+          >
+            {ORDER__HIGH__TO__LOW.displayName}
+          </Item>
+  
+          <Item
+            key={`priority.${ORDER__LOW__TO__HIGH.key}`}
+            value={ORDER__LOW__TO__HIGH.key}
+          >
+            {ORDER__LOW__TO__HIGH.displayName}
+          </Item>
+        </ItemGroup>
+      </Menu>
+    </Dropdown>
+  )
+};
