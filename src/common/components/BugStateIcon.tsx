14dbeaee877ac76936162e07b729369d6dfa6b30
Iacopo Leardini <iacopo.lea@gmail.com>
2023-04-05 17:22:33 +0200

    chore: update api and schema

---
+++ src/common/components/BugStateIcon.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as CircleFill } from 'src/assets/icons/circle-full-fill.svg';
+import styled from 'styled-components';
+
+export const BugStateIcon = styled(CircleFill)`
+  width: auto;
+  height: 100%;
+  max-height: 10px;
+  margin: 0 2px;
+`;
