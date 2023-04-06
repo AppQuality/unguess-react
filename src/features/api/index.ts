7456c6a3c0b6f235918aeb9e3a9967d29dcd0274
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-04-06 12:30:48 +0200

    feat(schema and api): schema and api updated with custom statuses

--- src/features/api/index.ts
+++ src/features/api/index.ts
@@ -549,8 +549,7 @@ export type GetCampaignsByCidUsecasesApiArg = {
   /** Campaign id */
   cid: string;
 };
-export type GetCampaignsByCidWidgetsApiResponse =
-  /** status 200 OK */
+export type GetCampaignsByCidWidgetsApiResponse = /** status 200 OK */
   | WidgetBugsByUseCase
   | WidgetBugsByDevice
   | WidgetCampaignProgress
