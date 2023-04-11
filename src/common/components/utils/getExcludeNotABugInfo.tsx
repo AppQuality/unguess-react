585f09b5b3dd41ab4d826d15e59c966d432b4b96
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-04-11 16:01:49 +0200

    feat(get exclude not a bug info): custom function to get data related to exclude not a bug action

---
+++ src/common/components/utils/getExcludeNotABugInfo.tsx
@@ -0,0 +1,8 @@
+import { TFunction } from "i18next";
+
+export const getExcludeNotABugInfo = (t?: TFunction) => ({
+    customStatusId: 7,
+    customStatusName: 'not a bug',
+    actionIdentifier: 'excludeNotABug',
+    recapTitle: t ? t('__BUGS_EXCLUDED_NOT_A_BUG') : '',
+});
