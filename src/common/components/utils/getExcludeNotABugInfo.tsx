6a8f7eae8e6e89326276e6981a9afdaeeddf4931
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-04-11 18:46:54 +0200

    feat(getexcludenotabuginfo): added drawerTitle in getExcludeNotABugInfo

--- src/common/components/utils/getExcludeNotABugInfo.tsx
+++ src/common/components/utils/getExcludeNotABugInfo.tsx
@@ -4,5 +4,6 @@ export const getExcludeNotABugInfo = (t?: TFunction) => ({
   customStatusId: 7,
   customStatusName: 'not a bug',
   actionIdentifier: 'excludeNotABug',
-  recapTitle: t ? t('__BUGS_EXCLUDED_NOT_A_BUG') : '',
+  recapTitle: t ? t('__BUGS_EXCLUDED_NOT_A_BUG') : 'Excluded not a Bug',
+  drawerTitle: t ? t('__BUGS_EXCLUDE_NOT_A_BUG') : 'Exclude “Not a bug”',
 });
