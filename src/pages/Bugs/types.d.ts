cf586016301f2d124fdae88f7d23e3fcad031ae3
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-04-11 14:51:34 +0200

    feat(bugs types): added bugItem type

--- src/pages/Bugs/types.d.ts
+++ src/pages/Bugs/types.d.ts
@@ -6,3 +6,4 @@ export type TableBugType = ItemOfArray<ApiBug>;
 export interface CounterItems {
   [key: string | number]: number;
 }
+export type BugItem = (Bug & { tags?: { tag_id: number; tag_name: string; }[] | undefined; siblings: number; });
