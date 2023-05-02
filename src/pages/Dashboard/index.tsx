7b5555788893b066a96a6188880f61720652c6a7
Marco Bonomo <marco.bonomo@unguess.io>
2023-05-02 18:29:51 +0200

    🐛 fix(App.tsx): fix ToastProvider children error ✨ feat(Dashboard/index.tsx): add welcome back notification when user logs in

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,15 @@
 // import { LoginForm } from "@appquality/unguess-design-system";
 import { useTranslation } from 'react-i18next';
 import { Page } from 'src/features/templates/Page';
-import { Grid } from '@appquality/unguess-design-system';
+import {
+  Grid,
+  Notification,
+  useToast,
+} from '@appquality/unguess-design-system';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
 import { projectFilterChanged } from 'src/features/campaignsFilter/campaignsFilterSlice';
 import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
+import { useEffect } from 'react';
 import { SuggestedCampaigns } from './SuggestedCampaigns';
 import { CampaignsList } from './campaigns-list';
 import { DashboardHeaderContent } from './headerContent';
@@ -16,6 +21,31 @@ const Dashboard = () => {
   const { status } = useAppSelector((state) => state.user);
 
   if (status === 'logged') dispatch(projectFilterChanged(0)); // Reset filters
+
+  const { addToast } = useToast();
+
+  // TODO: remove this, it's just a test
+  useEffect(() => {
+    if (status === 'logged') {
+      addToast(
+        ({ close }) => (
+          <Notification
+            onClose={() => {
+              console.log('closed');
+              close();
+            }}
+            message="Welcome back!"
+            type="success"
+            closeText="Dismiss"
+            isPrimary
+          />
+        ),
+        {
+          placement: 'top',
+        }
+      );
+    }
+  }, [status]);
 
   return (
     <Page
