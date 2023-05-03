63f0bbfe7b0ed35ad8f1d53ab9b8b0b25c84b6b7
Marco Bonomo <marco.bonomo@unguess.io>
2023-05-03 13:20:25 +0200

    🔥 refactor(Dashboard/index.tsx): remove unused imports and useEffect call

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,10 @@
 // import { LoginForm } from "@appquality/unguess-design-system";
 import { useTranslation } from 'react-i18next';
 import { Page } from 'src/features/templates/Page';
-import {
-  Grid,
-  Notification,
-  useToast,
-} from '@appquality/unguess-design-system';
+import { Grid } from '@appquality/unguess-design-system';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
 import { projectFilterChanged } from 'src/features/campaignsFilter/campaignsFilterSlice';
 import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
-import { useEffect } from 'react';
 import { SuggestedCampaigns } from './SuggestedCampaigns';
 import { CampaignsList } from './campaigns-list';
 import { DashboardHeaderContent } from './headerContent';
