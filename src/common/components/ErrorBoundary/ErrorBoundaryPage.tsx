5b2adc20246f06c3af5679f6bc4ccfd4f942ccc1
alexSp84 <alessandro.spinelli84@outlook.it>
2023-05-04 10:06:07 +0200

    feat: added navigation to error boundary page

--- src/common/components/ErrorBoundary/ErrorBoundaryPage.tsx
+++ src/common/components/ErrorBoundary/ErrorBoundaryPage.tsx
@@ -1,10 +1,23 @@
-const ErrorBoundaryPage = () => (
-  <div>
-    <h1>Sorry.. there was an error</h1>
-    <button type="button" onClick={() => window.location.reload()}>
-      Refresh
-    </button>
-  </div>
-);
+import { useTranslation } from 'react-i18next';
+import { Logged } from '../../../features/templates/Logged';
+import { Container } from '../../../pages/ExpressWizard/wizardHeader';
+import { GoogleTagManager } from '../../GoogleTagManager';
+
+const ErrorBoundaryPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <GoogleTagManager title="Error page">
+      <Logged route="">
+        <Container id="error-container">
+          <h1>Sorry.. there was an error</h1>
+          <button type="button" onClick={() => window.location.reload()}>
+            Refresh
+          </button>
+        </Container>
+      </Logged>
+    </GoogleTagManager>
+  );
+};
 
 export default ErrorBoundaryPage;
