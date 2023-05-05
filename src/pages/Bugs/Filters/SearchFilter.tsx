80686586d77e9197d82bb1509e7c103a08e7a8c9
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-05-05 10:18:59 +0200

    fix: move class to parent div

--- src/pages/Bugs/Filters/SearchFilter.tsx
+++ src/pages/Bugs/Filters/SearchFilter.tsx
@@ -41,19 +41,20 @@ export const SearchFilter = () => {
   if (!data) return null;
 
   return (
-    <MediaInput
-      className="input-search-bugs"
-      isCompact
-      end={
-        data.search ? (
-          <ClickableXIcon onClick={() => setSearchInput('')} />
-        ) : undefined
-      }
-      key="search-input"
-      onChange={(e) => setSearchInput(e.target.value)}
-      start={<SearchIcon />}
-      value={searchInput}
-      placeholder={t('__BUGS_SEARCH_INPUT_PLACEHOLDER')}
-    />
+    <div className="input-search-bugs">
+      <MediaInput
+        isCompact
+        end={
+          data.search ? (
+            <ClickableXIcon onClick={() => setSearchInput('')} />
+          ) : undefined
+        }
+        key="search-input"
+        onChange={(e) => setSearchInput(e.target.value)}
+        start={<SearchIcon />}
+        value={searchInput}
+        placeholder={t('__BUGS_SEARCH_INPUT_PLACEHOLDER')}
+      />
+    </div>
   );
 };
