4f7dd323ba37b77fba66bc2af05d22629290efb9
alexSp84 <alessandro.spinelli84@outlook.it>
2023-05-05 12:13:02 +0200

    feat: added error element props to Route components

--- src/common/components/ErrorBoundary/index.tsx
+++ src/common/components/ErrorBoundary/index.tsx
@@ -7,6 +7,8 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  error?: Error;
+  errorInfo?: ErrorInfo;
 }
 
 class ErrorBoundary extends Component<Props, State> {
@@ -15,12 +17,13 @@ class ErrorBoundary extends Component<Props, State> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(): State {
+  static getDerivedStateFromError(error: Error): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.state = { hasError: true, error, errorInfo };
     // eslint-disable-next-line no-console
     console.error('Uncaught error:', error, errorInfo);
   }
