45afb145ba28b70a17616960f312ddd761be428c
Iacopo Leardini <iacopo.lea@gmail.com>
2023-05-16 19:01:18 +0200

    chore: theme type

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,6 +2,6 @@ import 'styled-components';
 import { theme } from 'src/app/theme';
 
 declare module 'styled-components' {
-  const Theme = typeof theme;
+  type Theme = typeof theme;
   export interface DefaultTheme extends Theme {}
 }
