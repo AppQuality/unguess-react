e058b292de7b918c8f1afddb6f61220bf8a4be9b
alexSp84 <alessandro.spinelli84@outlook.it>
2023-05-31 11:13:42 +0200

    feat(permission settings): added toastr to the click on the copy Link button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
         </Helmet>
         <ToastProvider
           limit={5}
-          zIndex={theme.levels.modal}
+          zIndex={500}
           placementProps={{
             top: {
               style: {
