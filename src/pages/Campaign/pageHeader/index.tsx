ad7a886a93339b12f1d03ef3eedb6f60819973af
Marco Bonomo <marco.bonomo@unguess.io>
2023-06-14 16:45:23 +0200

    🎨 style(Meta/index.tsx): add gap to ButtonWrapper and adjust FooterContainer styles ✨ feat(Meta/index.tsx): add CampaignSettings component to ButtonWrapper 🎨 style(Meta/index.tsx): adjust FooterContainer styles for smaller screens 🎨 style(pageHeader/index.tsx): remove StyledMetasWrapper and add PageHeader.Meta directly to PageHeader

--- src/pages/Campaign/pageHeader/index.tsx
+++ src/pages/Campaign/pageHeader/index.tsx
@@ -1,15 +1,10 @@
 import { Anchor, PageHeader } from '@appquality/unguess-design-system';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
 import { Metas } from './Meta';
 import { HeaderSkeleton } from './HeaderSkeleton';
 import { EditableTitle } from './EditableTitle';
 import { useCampaign } from './useCampaign';
-
-const StyledMetasWrapper = styled(PageHeader.Meta)`
-  width: 100%;
-`;
 
 const CampaignPageHeader = ({ campaignId }: { campaignId: number }) => {
   const navigate = useNavigate();
@@ -35,9 +30,9 @@ const CampaignPageHeader = ({ campaignId }: { campaignId: number }) => {
           <PageHeader.Title>
             <EditableTitle campaignId={campaignId} />
           </PageHeader.Title>
-          <StyledMetasWrapper>
+          <PageHeader.Meta>
             <Metas campaign={campaign} />
-          </StyledMetasWrapper>
+          </PageHeader.Meta>
         </PageHeader.Main>
       </PageHeader>
     </LayoutWrapper>
