baddd6531f383232be9087aeccc6ae9e91754e62
Marco Bonomo <marco.bonomo@unguess.io>
2023-06-19 16:58:02 +0200

    🎨 style(userItem.tsx): add color to user name and email 🔥 refactor(userItem.tsx): remove unused variable 'displayName' ✨ feat(userItem.tsx): add email to user item if name is present

--- src/common/components/inviteUsers/userItem.tsx
+++ src/common/components/inviteUsers/userItem.tsx
@@ -7,6 +7,9 @@ import {
   Dropdown,
   Item,
   Ellipsis,
+  SM,
+  MD,
+  getColor,
 } from '@appquality/unguess-design-system';
 import { ReactComponent as ChevronIcon } from 'src/assets/icons/chevron-down-stroke.svg';
 import { GetWorkspacesByWidUsersApiResponse } from 'src/features/api';
@@ -51,17 +54,36 @@ export const UserItem = ({
   };
 
   const isMe = userData?.email === user.email;
-  const displayName = user.name.length ? user.name : user.email;
 
   return (
     <>
       <UserListItem key={`profile_${user.profile_id}`}>
-        <Avatar avatarType="text">{getInitials(displayName)}</Avatar>
+        <Avatar avatarType="text">
+          {getInitials(user.name.length ? user.name : user.email)}
+        </Avatar>
         <div>
           <StyledEllipsis>
-            {displayName}{' '}
-            {isMe && t('__WORKSPACE_SETTINGS_CURRENT_MEMBER_YOU_LABEL')}
+            <MD
+              isBold
+              style={{
+                color: getColor(appTheme.colors.primaryHue, 600),
+              }}
+            >
+              {user.name.length ? user.name : user.email}{' '}
+              {isMe && t('__WORKSPACE_SETTINGS_CURRENT_MEMBER_YOU_LABEL')}
+            </MD>
           </StyledEllipsis>
+          {user.name.length > 0 && (
+            <StyledEllipsis>
+              <SM
+                style={{
+                  color: appTheme.palette.grey[700],
+                }}
+              >
+                {user.email}
+              </SM>
+            </StyledEllipsis>
+          )}
         </div>
         {onResendInvite && onRemoveUser ? (
           <div style={{ marginLeft: 'auto' }}>
