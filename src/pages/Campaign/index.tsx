c3ed7b684a9db0edfcb61bde416b362df39740a5
Davide Bizzi <davide.bizzi@unguess.io>
2023-07-05 14:59:01 +0200

    rework: Move setworkspace on campaign page handler

--- src/pages/Campaign/index.tsx
+++ src/pages/Campaign/index.tsx
@@ -1,35 +1,21 @@
 import { Col } from '@appquality/unguess-design-system';
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useAppDispatch } from 'src/app/hooks';
 import { StickyContainer } from 'src/common/components/StickyContainer';
 import {
   StickyNavItem,
   StickyNavItemLabel,
   StyledDivider,
 } from 'src/common/components/navigation';
-import { useGetCampaignWithWorkspaceQuery } from 'src/features/api/customEndpoints/getCampaignWithWorkspace';
-import { setWorkspace } from '../../features/navigation/navigationSlice';
 import CampaignPage from './CampaignPage';
 import { EmptyState } from './EmptyState';
 import { useWidgets } from './useWidgets';
 
 const Campaign = () => {
   const { campaignId } = useParams();
-  const dispatch = useAppDispatch();
-  const { data: { workspace } = {} } = useGetCampaignWithWorkspaceQuery({
-    cid: campaignId?.toString() ?? '0',
-  });
   const { widgets } = useWidgets({
     campaignId: campaignId ? Number(campaignId) : 0,
   });
   const { all, footers, items, itemsWithTitles } = widgets;
-
-  useEffect(() => {
-    if (workspace) {
-      dispatch(setWorkspace(workspace));
-    }
-  }, [workspace, dispatch]);
 
   return (
     <CampaignPage>
