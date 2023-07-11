72559b2ab30d239bbe4fa918a1f1a4d2cb1c7967
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-11 16:31:21 +0200

    feat(manual-page): fix layout

--- src/pages/Manual/components/ManualDetails.tsx
+++ src/pages/Manual/components/ManualDetails.tsx
@@ -1,12 +1,8 @@
-import { Button, Title, Editor } from '@appquality/unguess-design-system';
-// import { ReactComponent as BugIcon } from 'src/assets/icons/bugs-icon.svg';
-import { ReactComponent as BugIcon } from 'src/assets/icons/edit-icon.svg';
+import { Title, Editor } from '@appquality/unguess-design-system';
 import styled from 'styled-components';
 import { AnimatedContainer } from 'src/common/components/animatedContainer';
 import { appTheme } from 'src/app/theme';
 import { ManualResponse } from 'src/features/backoffice';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 
 const StyledDiv = styled.div`
   > div:focus {
@@ -22,41 +18,21 @@ export const ManualDetails = ({
   manual,
 }: {
   manual: NonNullable<ManualResponse['data']>['attributes'] & { id: number };
-}) => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(`/campaigns/${manual.campaignId}/bugform`);
-  };
-  const { t } = useTranslation();
+}) => (
+  <AnimatedContainer>
+    {/* Title */}
+    <Title
+      style={{
+        marginBottom: appTheme.space.md,
+        fontSize: appTheme.fontSizes.xxl,
+      }}
+    >
+      {manual.title}
+    </Title>
 
-  return (
-    <AnimatedContainer>
-      {/* Title */}
-      <Title
-        style={{
-          marginBottom: appTheme.space.md,
-          fontSize: appTheme.fontSizes.xxl,
-        }}
-      >
-        {manual.title}
-      </Title>
-
-      {/* Editor */}
-      <StyledDiv>
-        <Editor editable={false}>{manual.content}</Editor>
-      </StyledDiv>
-
-      <Button
-        isAccent
-        style={{ marginTop: appTheme.space.md }}
-        onClick={handleClick}
-        isPrimary
-      >
-        <Button.StartIcon>
-          <BugIcon fill={appTheme.palette.white} />
-        </Button.StartIcon>
-        {t('__PUBLIC_MANUAL_CTA_GOTO_BUG_FORM')}
-      </Button>
-    </AnimatedContainer>
-  );
-};
+    {/* Editor */}
+    <StyledDiv>
+      <Editor editable={false}>{manual.content}</Editor>
+    </StyledDiv>
+  </AnimatedContainer>
+);
--- src/pages/Manual/index.tsx
+++ src/pages/Manual/index.tsx
@@ -8,13 +8,14 @@ import {
   ContainerCard,
   Button,
 } from '@appquality/unguess-design-system';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { extractStrapiData } from 'src/common/getStrapiData';
 import { ManualResponse } from 'src/features/backoffice';
 import { useGeti18nManualsQuery } from 'src/features/backoffice/strapi';
 import i18n from 'src/i18n';
 import { ReactComponent as EmptyImg } from 'src/assets/modal-use-case-empty.svg';
 import { appTheme } from 'src/app/theme';
+import { ReactComponent as BugIcon } from 'src/assets/icons/bugs-icon.svg';
 import styled from 'styled-components';
 import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
 import { BrandLogo } from 'src/common/components/navigation/header/brandLogo';
@@ -93,11 +94,11 @@ const Manual = () => {
   });
 
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   if (isLoading) return <PageLoader />;
 
   if (isError) {
-    console.log('🚀 ~ file: index.tsx:19 ~ Manual ~ isError:', isError);
     return <div>Error...</div>;
   }
 
@@ -108,17 +109,25 @@ const Manual = () => {
     // eslint-disable-next-line prefer-destructuring
     manual = extractStrapiData(data)[0];
   }
+  const handleClick = () => {
+    if (!manual) return;
+    navigate(`/campaigns/${manual.campaignId}/bugform`);
+  };
 
   return (
-    /** grey100 */
-    <div style={{ background: '#f8f9f9', maxWidth: '100%' }}>
+    <div style={{ background: appTheme.palette.grey[100], maxWidth: '100%' }}>
       <ManualHeader>
         <LayoutWrapper>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <BrandLogo size="full" />
-            <Button isAccent isPrimary>
-              test_description
-            </Button>
+            {manual && (
+              <Button onClick={handleClick} isPrimary isAccent>
+                <Button.StartIcon>
+                  <BugIcon fill={appTheme.palette.white} />
+                </Button.StartIcon>
+                {t('__PUBLIC_MANUAL_CTA_GOTO_BUG_FORM')}
+              </Button>
+            )}
           </div>
         </LayoutWrapper>
       </ManualHeader>
