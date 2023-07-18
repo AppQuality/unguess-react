861f10c0623488930071c26791a5f1f2ca4b7966
Davide Bizzi <davide.bizzi@unguess.io>
2023-07-18 11:03:48 +0200

    fix: Use hardcoded url

--- src/pages/Bugform/index.tsx
+++ src/pages/Bugform/index.tsx
@@ -1,17 +1,19 @@
 import IframeResizer from 'iframe-resizer-react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { Header } from 'src/common/components/navigation/header/header';
+import { isDev } from 'src/common/isDevEnvironment';
 
 const BugForm = () => {
   const { campaignId } = useParams<{ campaignId: string }>();
   const [query] = useSearchParams();
   const token = query.get('token') || '';
+  const tryberUrl = isDev() ? 'https://dev.tryber.me' : 'https:// tryber.me';
   return (
     <div>
       <Header logo="full" loggedIn={false} />
       <IframeResizer
         heightCalculationMethod="lowestElement"
-        src={`${process.env.REACT_APP_TRYBER_URL}/vdp/${campaignId}/${token}`}
+        src={`${tryberUrl}/vdp/${campaignId}/${token}`}
         style={{ width: '1px', minWidth: '100%', height: 0 }}
       />
     </div>
