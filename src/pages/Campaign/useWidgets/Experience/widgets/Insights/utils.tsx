be3ef59c1a33797a6fd5bc71f97ee4a0351394cd
Marco Bonomo <marco.bonomo@unguess.io>
2023-07-25 17:45:46 +0200

    ✨ feat(utils.tsx): add utility functions for handling insights severity, icons, tags, and clusters

---
+++ src/pages/Campaign/useWidgets/Experience/widgets/Insights/utils.tsx
@@ -0,0 +1,79 @@
+import { Ellipsis, Tag } from '@appquality/unguess-design-system';
+import { TFunction } from 'react-i18next';
+import { ReactComponent as MajorIssueIcon } from 'src/assets/icons/insight-major-issue.svg';
+import { ReactComponent as MinorIssueIcon } from 'src/assets/icons/insight-minor-issue.svg';
+import { SeverityTag } from 'src/common/components/tag/SeverityTag';
+
+interface InsightSeverity {
+  id: number;
+  name: string;
+}
+
+function getSeverityIcon(severity: InsightSeverity) {
+  switch (severity.id) {
+    case 1:
+      return <MajorIssueIcon />;
+    case 2:
+      return <MinorIssueIcon />;
+    default:
+      return null;
+  }
+}
+
+function getSeverityTag(severity: InsightSeverity, text?: string) {
+  switch (severity.id) {
+    case 1:
+      return (
+        <SeverityTag hasBackground severity="critical">
+          <Ellipsis>{text ?? severity.name}</Ellipsis>
+        </SeverityTag>
+      );
+    case 2:
+      return (
+        <SeverityTag hasBackground severity="high">
+          <Ellipsis>{text ?? severity.name}</Ellipsis>
+        </SeverityTag>
+      );
+    default:
+      return null;
+  }
+}
+
+function getClusterTag(
+  cluster: string | Array<{ id: number; name: string }>,
+  t: TFunction
+) {
+  if (cluster === 'all') {
+    return (
+      <Tag>
+        <Ellipsis>{t('__CAMPAIGN_PAGE_INSIGHTS_ALL_CLUSTERS')}</Ellipsis>
+      </Tag>
+    );
+  }
+
+  if (Array.isArray(cluster))
+    return cluster.map((c) => (
+      <Tag key={c.id}>
+        <Ellipsis>{c.name}</Ellipsis>
+      </Tag>
+    ));
+
+  return null;
+}
+
+function getSeverity(severity: InsightSeverity) {
+  switch (severity.id) {
+    case 1:
+      return 'critical';
+    case 2:
+      return 'high';
+    case 3:
+      return 'medium';
+    case 4:
+      return 'low';
+    default:
+      return 'medium';
+  }
+}
+
+export { getSeverityIcon, getSeverityTag, getClusterTag, getSeverity };
