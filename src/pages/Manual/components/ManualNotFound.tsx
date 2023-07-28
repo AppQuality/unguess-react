269f691e2550a4b7922dcf7cd7001036451cd7c6
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-28 13:06:18 +0200

    style(public-manual): update style for empty state manual

--- src/pages/Manual/components/ManualNotFound.tsx
+++ src/pages/Manual/components/ManualNotFound.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, XL } from '@appquality/unguess-design-system';
+import { Paragraph, XXL } from '@appquality/unguess-design-system';
 import { appTheme } from 'src/app/theme';
 import { ReactComponent as EmptyManualImg } from 'src/assets/icons/public-manual-empty.svg';
 
@@ -21,8 +21,12 @@ const ManualNotFound = () => {
   return (
     <CenteredContainer>
       <EmptyManualImg style={{ margin: appTheme.space.lg }} />
-      <XL isBold>{t('__PUBLIC_MANUAL_NOT_FOUND_TITLE')}</XL>
-      <Paragraph>{t('__PUBLIC_MANUAL_NOT_FOUND_TEXT')}</Paragraph>
+      <XXL style={{ color: appTheme.palette.blue[600] }} isBold>
+        {t('__PUBLIC_MANUAL_NOT_FOUND_TITLE')}
+      </XXL>
+      <Paragraph style={{ color: appTheme.palette.grey[600] }}>
+        {t('__PUBLIC_MANUAL_NOT_FOUND_TEXT')}
+      </Paragraph>
       <ButtonGoToSupportCenter />
     </CenteredContainer>
   );
