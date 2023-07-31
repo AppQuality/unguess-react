10eb67e5055f8fec9de5991595c1d30e2e5d576e
Marco Bonomo <marco.bonomo@unguess.io>
2023-07-31 19:16:24 +0200

    🐛 fix(utils.tsx): update function signatures to match API response structure

--- src/pages/Campaign/useWidgets/Experience/widgets/Insights/utils.tsx
+++ src/pages/Campaign/useWidgets/Experience/widgets/Insights/utils.tsx
@@ -3,13 +3,13 @@ import { TFunction } from 'react-i18next';
 import { ReactComponent as MajorIssueIcon } from 'src/assets/icons/insight-major-issue.svg';
 import { ReactComponent as MinorIssueIcon } from 'src/assets/icons/insight-minor-issue.svg';
 import { SeverityTag } from 'src/common/components/tag/SeverityTag';
+import { GetCampaignsByCidUxApiResponse } from 'src/features/api';
 
-interface InsightSeverity {
-  id: number;
-  name: string;
-}
-
-function getSeverityIcon(severity: InsightSeverity) {
+function getSeverityIcon(
+  severity: NonNullable<
+    GetCampaignsByCidUxApiResponse['findings']
+  >[number]['severity']
+) {
   switch (severity.id) {
     case 1:
       return <MajorIssueIcon />;
@@ -20,7 +20,12 @@ function getSeverityIcon(severity: InsightSeverity) {
   }
 }
 
-function getSeverityTag(severity: InsightSeverity, text?: string) {
+function getSeverityTag(
+  severity: NonNullable<
+    GetCampaignsByCidUxApiResponse['findings']
+  >[number]['severity'],
+  text?: string
+) {
   switch (severity.id) {
     case 1:
       return (
@@ -40,7 +45,9 @@ function getSeverityTag(severity: InsightSeverity, text?: string) {
 }
 
 function getClusterTag(
-  cluster: string | Array<{ id: number; name: string }>,
+  cluster: NonNullable<
+    GetCampaignsByCidUxApiResponse['findings']
+  >[number]['cluster'],
   t: TFunction
 ) {
   if (cluster === 'all') {
@@ -61,7 +68,11 @@ function getClusterTag(
   return null;
 }
 
-function getSeverity(severity: InsightSeverity) {
+function getSeverity(
+  severity: NonNullable<
+    GetCampaignsByCidUxApiResponse['findings']
+  >[number]['severity']
+) {
   switch (severity.id) {
     case 1:
       return 'critical';
