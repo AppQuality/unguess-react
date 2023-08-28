18ba0fffdd25c04903c9ee7324a7ef9dea6097fb
Davide Bizzi <davide.bizzi@unguess.io>
2023-08-28 12:56:44 +0200

    fix: Do not crash if react env is not defined

--- src/common/isDevEnvironment.ts
+++ src/common/isDevEnvironment.ts
@@ -1,2 +1,6 @@
-export const isDev = () =>
-  ['staging', 'local'].includes(react_env.REACT_APP_ENV);
+export const isDev = () => {
+  if (typeof react_env === 'undefined') {
+    return true;
+  }
+  return ['staging', 'local'].includes(react_env.REACT_APP_ENV);
+};
