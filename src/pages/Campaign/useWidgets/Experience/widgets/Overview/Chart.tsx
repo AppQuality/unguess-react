5a87224a6ae43fabb532cd8ae718a4e13fc99fde
Davide Bizzi <davide.bizzi@unguess.io>
2023-09-05 16:23:46 +0200

    fix: Allow newline in sentiment tooltip

--- src/pages/Campaign/useWidgets/Experience/widgets/Overview/Chart.tsx
+++ src/pages/Campaign/useWidgets/Experience/widgets/Overview/Chart.tsx
@@ -108,7 +108,12 @@ export const Chart = ({
                     </Span>
                   </div>
                   {data?.customData && (
-                    <MD style={{ marginTop: appTheme.space.xs }}>
+                    <MD
+                      style={{
+                        marginTop: appTheme.space.xs,
+                        whiteSpace: 'pre-wrap',
+                      }}
+                    >
                       {data?.customData}
                     </MD>
                   )}
