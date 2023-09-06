769e8e399e9c8591ba2267fa2d3452d68fe49360
Marco Bonomo <marco.bonomo@unguess.io>
2023-09-06 17:42:05 +0200

    🐛 fix(CampaignWidgets.tsx): import Skeleton component from '@appquality/unguess-design-system' ✨ feat(CampaignWidgets.tsx): add loading state for widgets using isLoading variable

--- src/pages/Campaign/CampaignWidgets.tsx
+++ src/pages/Campaign/CampaignWidgets.tsx
@@ -1,9 +1,9 @@
-import { Col, Grid, Row } from '@appquality/unguess-design-system';
-import { StickyContainer } from 'src/common/components/StickyContainer';
+import { Col, Grid, Row, Skeleton } from '@appquality/unguess-design-system';
 import {
   StickyNavItem,
   StickyNavItemLabel,
   StyledDivider,
+  StickyNavContainer,
 } from 'src/common/components/navigation';
 import { useParams } from 'react-router-dom';
 import { EmptyState } from './EmptyState';
@@ -11,7 +11,7 @@ import { useWidgets } from './useWidgets';
 
 export const CampaignWidgets = () => {
   const { campaignId } = useParams();
-  const { widgets } = useWidgets({
+  const { widgets, isLoading } = useWidgets({
     campaignId: campaignId ? Number(campaignId) : 0,
   });
   const { all, footers, items, itemsWithTitles } = widgets;
@@ -21,37 +21,43 @@ export const CampaignWidgets = () => {
   ) : (
     <Grid gutters="xl">
       <Row>
-        <Col xs={12} lg={3}>
-          <StickyContainer>
-            {itemsWithTitles.map((widget) => {
-              switch (widget.type) {
-                case 'title':
-                  return (
-                    <StickyNavItemLabel>{widget.title}</StickyNavItemLabel>
-                  );
-                case 'item':
-                  return (
-                    <StickyNavItem
-                      id={`anchor-${widget.id}`}
-                      to={widget.id}
-                      containerId="main"
-                      spy
-                      smooth
-                      duration={500}
-                      offset={-30}
-                    >
-                      {widget.title}
-                    </StickyNavItem>
-                  );
-                default:
-                  return null;
-              }
-            })}
-            {footers.length > 0 && <StyledDivider />}
-            {footers.map((widget) => widget.content)}
-          </StickyContainer>
+        <Col xs={12} lg={2}>
+          <StickyNavContainer>
+            {isLoading ? (
+              <Skeleton height="300px" />
+            ) : (
+              <>
+                {itemsWithTitles.map((widget) => {
+                  switch (widget.type) {
+                    case 'title':
+                      return (
+                        <StickyNavItemLabel>{widget.title}</StickyNavItemLabel>
+                      );
+                    case 'item':
+                      return (
+                        <StickyNavItem
+                          id={`anchor-${widget.id}`}
+                          to={widget.id}
+                          containerId="main"
+                          spy
+                          smooth
+                          duration={500}
+                          offset={-30}
+                        >
+                          {widget.title}
+                        </StickyNavItem>
+                      );
+                    default:
+                      return null;
+                  }
+                })}
+                {footers.length > 0 && <StyledDivider />}
+                {footers.map((widget) => widget.content)}
+              </>
+            )}
+          </StickyNavContainer>
         </Col>
-        <Col xs={12} lg={9}>
+        <Col xs={12} lg={10}>
           {items.map((widget) => widget.content)}
         </Col>
       </Row>
