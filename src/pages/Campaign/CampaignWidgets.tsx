c879990e967622b3c3f803cf4144d8d1bde59d04
Marco Bonomo <marco.bonomo@unguess.io>
2023-09-07 16:29:49 +0200

    🔧 fix(CampaignWidgets.tsx): remove unused imports and fix indentation ✨ feat(CampaignWidgets.tsx): replace StickyNavContainer with AsideNav component

--- src/pages/Campaign/CampaignWidgets.tsx
+++ src/pages/Campaign/CampaignWidgets.tsx
@@ -1,11 +1,11 @@
-import { Col, Grid, Row, Skeleton } from '@appquality/unguess-design-system';
+import { Col, Grid, Row } from '@appquality/unguess-design-system';
 import {
   StickyNavItem,
   StickyNavItemLabel,
   StyledDivider,
-  StickyNavContainer,
 } from 'src/common/components/navigation';
 import { useParams } from 'react-router-dom';
+import { AsideNav } from 'src/common/components/navigation/asideNav';
 import { EmptyState } from './EmptyState';
 import { useWidgets } from './useWidgets';
 
@@ -22,40 +22,36 @@ export const CampaignWidgets = () => {
     <Grid gutters="xl">
       <Row>
         <Col xs={12} lg={2}>
-          <StickyNavContainer>
-            {isLoading ? (
-              <Skeleton height="300px" />
-            ) : (
-              <>
-                {itemsWithTitles.map((widget) => {
-                  switch (widget.type) {
-                    case 'title':
-                      return (
-                        <StickyNavItemLabel>{widget.title}</StickyNavItemLabel>
-                      );
-                    case 'item':
-                      return (
-                        <StickyNavItem
-                          id={`anchor-${widget.id}`}
-                          to={widget.id}
-                          containerId="main"
-                          spy
-                          smooth
-                          duration={500}
-                          offset={-30}
-                        >
-                          {widget.title}
-                        </StickyNavItem>
-                      );
-                    default:
-                      return null;
-                  }
-                })}
-                {footers.length > 0 && <StyledDivider />}
-                {footers.map((widget) => widget.content)}
-              </>
-            )}
-          </StickyNavContainer>
+          <AsideNav isLoading={isLoading} containerId="main">
+            <>
+              {itemsWithTitles.map((widget) => {
+                switch (widget.type) {
+                  case 'title':
+                    return (
+                      <StickyNavItemLabel>{widget.title}</StickyNavItemLabel>
+                    );
+                  case 'item':
+                    return (
+                      <StickyNavItem
+                        id={`anchor-${widget.id}`}
+                        to={widget.id}
+                        containerId="main"
+                        spy
+                        smooth
+                        duration={500}
+                        offset={-30}
+                      >
+                        {widget.title}
+                      </StickyNavItem>
+                    );
+                  default:
+                    return null;
+                }
+              })}
+              {footers.length > 0 && <StyledDivider />}
+              {footers.map((widget) => widget.content)}
+            </>
+          </AsideNav>
         </Col>
         <Col xs={12} lg={10}>
           {items.map((widget) => widget.content)}
