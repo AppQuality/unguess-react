6c2a981cff95f513a6ee3a91f01023281d9e7bc8
Marco Bonomo <marco.bonomo@unguess.io>
2023-09-07 16:45:37 +0200

    🔧 chore(asideNav/index.tsx): restructure menu in StickyNavContainer component 🔥 refactor(asideNav/index.tsx): remove unused components and imports

--- src/common/components/navigation/asideNav/index.tsx
+++ src/common/components/navigation/asideNav/index.tsx
@@ -1,9 +1,76 @@
-/* TODO: Move all the components (StickyNavItem, StickyNavItemExternal, StickyNavItemLabel, StyledDivider) 
-    inside this component instead of using "items" as a ReactNode. 
-    At the end remove the same components from navigation file. */
+// TODO: Structure the menu in this components instead of using the children
 
-import { Skeleton } from '@appquality/unguess-design-system';
-import { StickyNavContainer } from '../../navigation';
+import styled from 'styled-components';
+import { Divider } from 'src/common/components/divider';
+import { Link } from 'react-scroll';
+import {
+  Anchor,
+  ContainerCard,
+  MD,
+  Skeleton,
+} from '@appquality/unguess-design-system';
+import { retrieveComponentStyles } from '@zendeskgarden/react-theming';
+
+export const StickyNavContainer = styled(ContainerCard)`
+  position: sticky;
+  top: ${({ theme }) => theme.space.md};
+  z-index: 1;
+  padding: ${({ theme }) => theme.space.sm};
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    display: block;
+    background-color: transparent;
+    border-color: transparent;
+    padding: ${({ theme }) => theme.space.xs};
+  }
+`;
+
+export const StyledDivider = styled(Divider)`
+  margin-top: ${({ theme }) => theme.space.base * 6}px;
+  margin-bottom: ${({ theme }) => theme.space.base * 6}px;
+`;
+
+export const StickyNavItem = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: ${({ theme }) => theme.space.sm} ${({ theme }) => theme.space.xs}
+    ${({ theme }) => theme.space.sm} ${({ theme }) => theme.space.md};
+  cursor: pointer;
+  margin-bottom: ${({ theme }) => theme.space.xs};
+  transition: all 0.1s ease-in-out;
+  text-decoration: none;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  ${(props) => retrieveComponentStyles('navigation.hoverableItem', props)};
+
+  &.isCurrent {
+    font-weight: ${({ theme }) => theme.fontWeights.semibold};
+  }
+`;
+
+StickyNavItem.defaultProps = {
+  activeClass: 'isCurrent',
+};
+
+export const StickyNavItemLabel = styled(MD)`
+  padding-top: ${({ theme }) => theme.space.sm};
+  margin-bottom: ${({ theme }) => theme.space.xs};
+`;
+
+export const StickyNavItemExternal = styled(Anchor)`
+  display: block;
+  padding: ${({ theme }) => theme.space.sm} ${({ theme }) => theme.space.xs}
+    ${({ theme }) => theme.space.sm} ${({ theme }) => theme.space.md};
+`;
 
 interface IAsideNav {
   containerId: string;
