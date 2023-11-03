ea052df7f402bd750739efa4eba0035915cebc40
Marco Bonomo <marco.bonomo@unguess.io>
2023-11-03 16:10:42 +0100

    🎨 style(Drawer/index.tsx): import MD and appTheme from unguess-design-system and app/theme respectively ✨ feat(Drawer/index.tsx): add CustomStatusForm component to Drawer.Body 🔥 chore(Drawer/index.tsx): remove unused import Button from unguess-design-system 🔧 fix(Drawer/index.tsx): add isBasic prop to Button component in Drawer.FooterItem

--- src/pages/Bug/Drawer/index.tsx
+++ src/pages/Bug/Drawer/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Drawer } from '@appquality/unguess-design-system';
+import { Button, Drawer, MD } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
 import {
@@ -6,6 +6,8 @@ import {
   setCustomStatusDrawerOpen,
   updateCustomStatus,
 } from 'src/features/bugsPage/bugsPageSlice';
+import { appTheme } from 'src/app/theme';
+import { CustomStatusForm } from './CustomStatusForm';
 
 export const CustomStatusDrawer = () => {
   const { t } = useTranslation();
@@ -35,10 +37,19 @@ export const CustomStatusDrawer = () => {
       <Drawer.Header>
         {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_HEADER_TITLE')}
       </Drawer.Header>
-      <Drawer.Body>custom status</Drawer.Body>
+      <Drawer.Body>
+        <MD style={{ marginBottom: appTheme.space.lg }}>
+          {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_BODY_DESCRIPTION')}
+        </MD>
+        <CustomStatusForm />
+      </Drawer.Body>
       <Drawer.Footer>
         <Drawer.FooterItem>
-          <Button id="custom-status-drawer-reset" onClick={onResetClick}>
+          <Button
+            id="custom-status-drawer-reset"
+            onClick={onResetClick}
+            isBasic
+          >
             {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_RESET_BUTTON')}
           </Button>
         </Drawer.FooterItem>
