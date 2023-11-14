b9d4b8c8b12ab4038551379883590376b5452cc4
Marco Bonomo <marco.bonomo@unguess.io>
2023-11-14 10:21:27 +0100

    🔧 chore(Circle.tsx): adjust size of Circle component

--- src/common/components/CustomStatusDrawer/Circle.tsx
+++ src/common/components/CustomStatusDrawer/Circle.tsx
@@ -4,9 +4,10 @@ import { ReactComponent as CircleFill } from 'src/assets/icons/circle-full-fill.
 export const Circle = styled(CircleFill)<{
   color: string;
 }>`
-  width: 10px;
-  height: 10px;
-  min-width: 10px;
+  width: 13px;
+  height: 13px;
+  min-width: 13px;
+  min-height: 13px;
   margin: auto 0;
   border-radius: 50%;
   color: ${({ color }) => color};
