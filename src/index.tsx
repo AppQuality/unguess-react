f196102b64e1c9f66fe06037ad2fb20db933332b
Luca Cannarozzo <cannarozzoluca@live.it>
2024-01-26 19:35:45 +0100

    chore(app): Add workbox service worker and dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/App';
 import { store } from './app/store';
 import { fetchUser } from './features/user/actions/fetchUser';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const main = () => {
   store.dispatch(fetchUser());
@@ -19,6 +20,12 @@ const main = () => {
 };
 
 main();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
