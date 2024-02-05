380fda1894e2263c5169678eae664440530264d6
Marco Bonomo <marco.bonomo@unguess.io>
2024-02-05 17:48:46 +0100

    🐛 fix(AllBugs.tsx): rename UseCaseTable component to AllBugsTable ✨ feat(AllBugs.tsx): add support for fetching bugs data using useBugs hook 🐛 fix(AllBugs.tsx): handle loading and error states in AllBugs component ✨ feat(BugsByState.tsx): add support for fetching bugs by state data using useBugsByState hook 🐛 fix(BugsByState.tsx): handle loading and error states in BugsByState component ✨ feat(BugsByUsecase.tsx): add support for fetching bugs by use case data using useBugsByUseCase hook 🐛 fix(BugsByUsecase.tsx): handle loading and error states in BugsByUsecase component
    🔀 refactor(BugsTable/index.tsx): remove unused imports and components, simplify rendering logic
    ✨ feat(BugsTable/index.tsx): add support for default view based on selected filters

--- src/pages/Bugs/Content/BugsTable/AllBugs.tsx
+++ src/pages/Bugs/Content/BugsTable/AllBugs.tsx
@@ -1,30 +1,49 @@
 import { appTheme } from 'src/app/theme';
 import useWindowSize from 'src/hooks/useWindowSize';
-import { TableBugType } from '../../types';
+import { styled } from 'styled-components';
 import { InfoRow } from './components/InfoRow';
-import UseCaseTable from './components/SingleGroupTable';
+import AllBugsTable from './components/SingleGroupTable';
 import BugCards from './components/BugCards';
+import { useBugs } from './hooks/useBugs';
+import { LoadingState } from './components/LoadingState';
+import { EmptyState } from './components/EmptyState';
 
-export const AllBugs = ({
-  campaignId,
-  bugs,
-}: {
-  campaignId: number;
-  bugs: TableBugType[];
-}) => {
+const Wrapper = styled.div<{
+  isFetching?: boolean;
+}>`
+  padding-top: ${(p) => p.theme.space.lg};
+
+  ${(p) =>
+    p.isFetching &&
+    `
+    opacity: 0.5;
+    pointer-events: none;
+  `}
+`;
+
+export const AllBugs = ({ campaignId }: { campaignId: number }) => {
   const { width } = useWindowSize();
-
   const breakpointMd = parseInt(appTheme.breakpoints.md, 10);
   const isMdBreakpoint = width < breakpointMd;
+  const { data, isLoading, isFetching, isError } = useBugs(campaignId);
+  const { allBugs: bugs } = data;
+
+  if (isLoading || isError) {
+    return <LoadingState />;
+  }
+
+  if (!bugs.length) {
+    return <EmptyState />;
+  }
 
   return (
-    <>
+    <Wrapper isFetching={isFetching}>
       <InfoRow bugs={bugs} />
       {isMdBreakpoint ? (
         <BugCards bugs={bugs} />
       ) : (
-        <UseCaseTable campaignId={campaignId} item={{ bugs }} />
+        <AllBugsTable campaignId={campaignId} item={{ bugs }} />
       )}
-    </>
+    </Wrapper>
   );
 };
--- src/pages/Bugs/Content/BugsTable/BugsByState.tsx
+++ src/pages/Bugs/Content/BugsTable/BugsByState.tsx
@@ -1,31 +1,38 @@
 import { Accordion, MD } from '@appquality/unguess-design-system';
 import { useMemo } from 'react';
-import { getSelectedFilters } from 'src/features/bugsPage/bugsPageSlice';
 import { useTranslation } from 'react-i18next';
 import { getCustomStatusInfo } from 'src/common/components/utils/getCustomStatusInfo';
+import { styled } from 'styled-components';
 import { EmptyState } from './components/EmptyState';
-import BugStateAccordion from './components/BugStateAccordion';
-import { BugByStateType } from './types';
 import { EmptyGroup } from './components/EmptyGroup';
+import { useBugsByState } from './hooks/useBugsByState';
+import { LoadingState } from './components/LoadingState';
+import BugStateAccordion from './components/SingleGroupAccordion';
+
+const Wrapper = styled.div<{
+  isFetching?: boolean;
+}>`
+  padding-top: ${(p) => p.theme.space.lg};
+
+  ${(p) =>
+    p.isFetching &&
+    `
+    opacity: 0.5;
+    pointer-events: none;
+  `}
+`;
 
 export const BugsByState = ({
   campaignId,
-  bugsByStates,
+  isDefaultView,
 }: {
   campaignId: number;
-  bugsByStates: BugByStateType[];
+  isDefaultView: boolean;
 }) => {
   const { t } = useTranslation();
-  const selectedFilters = getSelectedFilters();
+  const { data, isError, isFetching, isLoading } = useBugsByState(campaignId);
+  const { bugsByStates } = data;
 
-  const isDefaultView = useMemo(
-    () =>
-      !selectedFilters.search &&
-      selectedFilters.severities?.length === 0 &&
-      selectedFilters.types?.length === 0 &&
-      !selectedFilters.read,
-    [selectedFilters]
-  );
   const emptyBugStates = useMemo(
     () => bugsByStates.filter((item) => item.bugs.length === 0),
     [bugsByStates]
@@ -35,49 +42,55 @@ export const BugsByState = ({
     [bugsByStates]
   );
 
+  if (isLoading || isError) {
+    return <LoadingState />;
+  }
+
   if (!bugStates.length) {
     return <EmptyState />;
   }
 
   return (
-    <Accordion
-      level={3}
-      defaultExpandedSections={Array.from(bugsByStates, (_, i) => i)}
-      isExpandable
-      isBare
-    >
-      {bugStates.map((item) => (
-        <BugStateAccordion
-          campaignId={campaignId}
-          key={item.state.id}
-          title={
-            <>
-              {t('__BUG_STATUS')}:{' '}
-              {getCustomStatusInfo(item.state.name as BugState, t).text}
-              <MD tag="span">{` (${item.bugs.length})`}</MD>
-            </>
-          }
-          item={item}
-        />
-      ))}
-      {isDefaultView ? (
-        <EmptyGroup isBold>
-          {t('__BUGS_PAGE_WARNING_POSSIBLE_EMPTY_STATUS')}
-        </EmptyGroup>
-      ) : (
-        <>
-          {emptyBugStates.length > 1 && (
-            <EmptyGroup isBold>
-              {t('__BUGS_PAGE_OTHER_STATUSES')} <MD tag="span">(0)</MD>
-            </EmptyGroup>
-          )}
-          {emptyBugStates.length === 1 && (
-            <EmptyGroup isBold>
-              {emptyBugStates[0].state.name} <MD tag="span">(0)</MD>
-            </EmptyGroup>
-          )}
-        </>
-      )}
-    </Accordion>
+    <Wrapper isFetching={isFetching}>
+      <Accordion
+        level={3}
+        defaultExpandedSections={Array.from(bugsByStates, (_, i) => i)}
+        isExpandable
+        isBare
+      >
+        {bugStates.map((item) => (
+          <BugStateAccordion
+            campaignId={campaignId}
+            key={item.state.id}
+            title={
+              <>
+                {t('__BUG_STATUS')}:{' '}
+                {getCustomStatusInfo(item.state.name as BugState, t).text}
+                <MD tag="span">{` (${item.bugs.length})`}</MD>
+              </>
+            }
+            item={item}
+          />
+        ))}
+        {isDefaultView ? (
+          <EmptyGroup isBold>
+            {t('__BUGS_PAGE_WARNING_POSSIBLE_EMPTY_STATUS')}
+          </EmptyGroup>
+        ) : (
+          <>
+            {emptyBugStates.length > 1 && (
+              <EmptyGroup isBold>
+                {t('__BUGS_PAGE_OTHER_STATUSES')} <MD tag="span">(0)</MD>
+              </EmptyGroup>
+            )}
+            {emptyBugStates.length === 1 && (
+              <EmptyGroup isBold>
+                {emptyBugStates[0].state.name} <MD tag="span">(0)</MD>
+              </EmptyGroup>
+            )}
+          </>
+        )}
+      </Accordion>
+    </Wrapper>
   );
 };
