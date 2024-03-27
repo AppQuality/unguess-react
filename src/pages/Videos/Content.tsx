11c55eabb49fa04fe71be824df198c61393de099
Marco Bonomo <marco.bonomo@unguess.io>
2024-03-27 12:47:50 +0100

    🔧 fix(i18n): add empty translations for videos page in English and Italian 🚀 feat(Videos): update VideosPageContent and VideosPageHeader components

--- src/pages/Videos/Content.tsx
+++ src/pages/Videos/Content.tsx
@@ -1,7 +1,59 @@
-const VideosPageContent = () => (
-  <div>
-    <h1>Videos Page Content</h1>
-  </div>
-);
+import { Button, Col, Grid, Row } from '@appquality/unguess-design-system';
+import { useTranslation } from 'react-i18next';
+import { Link, useParams } from 'react-router-dom';
+import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
+import { GetCampaignsByCidVideoApiResponse } from 'src/features/api';
+import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
+
+const VideosPageContent = ({
+  items: { items },
+}: {
+  items: GetCampaignsByCidVideoApiResponse;
+}) => {
+  const { campaignId } = useParams();
+  const { t } = useTranslation();
+
+  return (
+    <LayoutWrapper>
+      <Grid>
+        {items.map((item, index) => (
+          <Row>
+            <Col>
+              <h2>
+                UseCase #{index + 1} ({item.videos.length} video)
+              </h2>
+              {item.usecase.title}
+              {item.usecase.description}
+              {item.videos.map((video) => {
+                const videoUrl = useLocalizeRoute(
+                  `campaigns/${campaignId}/videos/${video.id}`
+                );
+
+                return (
+                  <>
+                    <hr />
+                    <p>
+                      <p>{video.id}</p>
+                      <p>{video.url}</p>
+                      <p>{video.streamUrl}</p>
+                      <p>
+                        {video.tester.name} {video.tester.id}
+                      </p>
+                      <Link to={videoUrl}>
+                        <Button id="button-bugs-list-header" isPrimary isAccent>
+                          {t('__VIDEOS_PAGE_BUTTON_DETAIL_VIDEO')}
+                        </Button>
+                      </Link>
+                    </p>
+                  </>
+                );
+              })}
+            </Col>
+          </Row>
+        ))}
+      </Grid>
+    </LayoutWrapper>
+  );
+};
 
 export default VideosPageContent;
