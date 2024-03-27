9f618d863e5383300b476e1a8a58919326f020b4
Marco Bonomo <marco.bonomo@unguess.io>
2024-03-27 12:47:50 +0100

    🔧 fix(i18n): add empty translations for videos page in English and Italian 🚀 feat(Videos): update VideosPageContent and VideosPageHeader components

--- src/pages/Videos/PageHeader.tsx
+++ src/pages/Videos/PageHeader.tsx
@@ -1,11 +1,28 @@
 import { PageHeader } from '@appquality/unguess-design-system';
+import { useTranslation } from 'react-i18next';
+import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
+import { PageTitle } from 'src/common/components/PageTitle';
 
-const VideosPageHeader = () => (
-  <PageHeader title="Videos">
-    <PageHeader.Main mainTitle="Videos">
-      <p>Videos Page Header</p>
-    </PageHeader.Main>
-  </PageHeader>
-);
+const VideosPageHeader = () => {
+  const { t } = useTranslation();
+
+  return (
+    <LayoutWrapper>
+      <PageHeader>
+        <PageHeader.Main
+          mainTitle={t('__VIDEOS_PAGE_TITLE')}
+          mainDescription={t('__VIDEOS_PAGE_DESCRIPTION')}
+        >
+          <PageHeader.Title>
+            <PageTitle>{t('__VIDEOS_PAGE_TITLE')}</PageTitle>
+          </PageHeader.Title>
+          <PageHeader.Description>
+            {t('__VIDEOS_PAGE_DESCRIPTION')}
+          </PageHeader.Description>
+        </PageHeader.Main>
+      </PageHeader>
+    </LayoutWrapper>
+  );
+};
 
 export default VideosPageHeader;
--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,20 +1,38 @@
 import { Page } from 'src/features/templates/Page';
+import { useGetCampaignsByCidVideoQuery } from 'src/features/api';
+import { Skeleton } from '@appquality/unguess-design-system';
+import { useParams } from 'react-router-dom';
 import VideosPageHeader from './PageHeader';
 import VideosPageContent from './Content';
 
 const VideosPage = () => {
-  console.log('VideosPage');
+  const { campaignId } = useParams();
+
+  const {
+    data: videos,
+    isFetching,
+    isLoading,
+    isError,
+  } = useGetCampaignsByCidVideoQuery({
+    cid: campaignId || '',
+  });
+
+  if (isError) return null;
 
   return (
     <Page
       title="Videos Page"
       className="videos-page"
       pageHeader={<VideosPageHeader />}
-      route="videos-page"
-      excludeMarginTop
-      excludeMarginBottom
+      route="videos"
     >
-      <VideosPageContent />
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+          {videos && <VideosPageContent items={videos} />}
+        </div>
+      )}
     </Page>
   );
 };
