2d5d3c6861df3d7fc234eb65cf385a219132034c
Marco Bonomo <marco.bonomo@unguess.io>
2024-04-24 17:54:14 +0200

    🔧 refactor(Videos/index.tsx): refactor VideosPage component to use i18n translation for title

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,16 +1,21 @@
 import { Page } from 'src/features/templates/Page';
+import { useTranslation } from 'react-i18next';
 import VideosPageHeader from './PageHeader';
 import VideosPageContent from './Content';
 
-const VideosPage = () => (
-  <Page
-    title="Videos Page"
-    className="videos-page"
-    pageHeader={<VideosPageHeader />}
-    route="videos"
-  >
-    <VideosPageContent />
-  </Page>
-);
+const VideosPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Page
+      title={t('__VIDEOS_PAGE_TITLE')}
+      className="videos-page"
+      pageHeader={<VideosPageHeader />}
+      route="videos"
+    >
+      <VideosPageContent />
+    </Page>
+  );
+};
 
 export default VideosPage;
