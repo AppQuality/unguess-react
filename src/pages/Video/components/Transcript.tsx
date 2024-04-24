e20c78680b5f1331baf1181e5c04fb93df7681fe
Marco Bonomo <marco.bonomo@unguess.io>
2024-04-24 18:23:43 +0200

    🚀 feat(Transcript.tsx): add support for fetching video transcript by vid 🐛 fix(Transcript.tsx): update component to display video transcript on page

--- src/pages/Video/components/Transcript.tsx
+++ src/pages/Video/components/Transcript.tsx
@@ -1,5 +1,12 @@
-import { ContainerCard, LG } from '@appquality/unguess-design-system';
+import {
+  ContainerCard,
+  LG,
+  Paragraph,
+  Skeleton,
+} from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { useGetVideoByVidQuery } from 'src/features/api';
 import { styled } from 'styled-components';
 
 const StyledContainerCard = styled(ContainerCard)`
@@ -8,10 +15,25 @@ const StyledContainerCard = styled(ContainerCard)`
 
 const Transcript = () => {
   const { t } = useTranslation();
+  const { campaignId, videoId } = useParams();
+
+  const {
+    data: video,
+    isFetching: isFetchingVideo,
+    isLoading: isLoadingVideo,
+    isError: isErrorVideo,
+  } = useGetVideoByVidQuery({
+    vid: videoId || '',
+  });
+
+  if (!video || isErrorVideo || !video.transcript) return null;
+
+  if (isFetchingVideo || isLoadingVideo) return <Skeleton />;
 
   return (
     <StyledContainerCard>
       <LG>{t('__VIDEO_PAGE_TRANSCRIPT_TITLE')}</LG>
+      <Paragraph>{video.transcript}</Paragraph>
     </StyledContainerCard>
   );
 };
