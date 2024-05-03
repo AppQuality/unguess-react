81ca19fb488b76c81abc4f60282c2bd18f4b91ae
Marco Bonomo <marco.bonomo@unguess.io>
2024-05-03 10:54:25 +0200

    ✨ feat(ObservationForm.tsx): add ObservationForm component for video page

---
+++ src/pages/Video/components/ObservationForm.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import * as Yup from 'yup';
+import { Form, Formik, FormikHelpers, FormikProps } from 'formik';
+
+const ObservationForm = ({
+  onSubmit,
+}: {
+  onSubmit: (
+    values: { email: string; message?: string },
+    actions: FormikHelpers<{ email: string }>
+  ) => void;
+}) => {
+  const { t } = useTranslation();
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email(t('__WORKSPACE_SETTINGS_ADD_MEMBER_INVALID_EMAIL_ERROR'))
+      .required(t('__WORKSPACE_SETTINGS_ADD_MEMBER_REQUIRED_EMAIL_ERROR')),
+  });
+
+  return <>Form</>;
+};
