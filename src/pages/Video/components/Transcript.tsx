4df04881daace87a570f0501e885f6bddb7c56dd
ZecD <dominik.zecoli@gmail.com>
2024-05-03 18:32:45 +0200

    WIP add transcript highlights

--- src/pages/Video/components/Transcript.tsx
+++ src/pages/Video/components/Transcript.tsx
@@ -1,16 +1,18 @@
-import {
-  ContainerCard,
-  LG,
-  Paragraph,
-  Skeleton,
-} from '@appquality/unguess-design-system';
+import { ContainerCard, LG, Skeleton } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { useGetVideoByVidQuery } from 'src/features/api';
 import { styled } from 'styled-components';
+import { TranscriptParagraph } from './TranscriptParagraph';
 
 const StyledContainerCard = styled(ContainerCard)`
-  margin: ${({ theme }) => theme.space.xxl} 0;
+  margin: ${({ theme }) => theme.space.xl} 0;
+  padding: ${({ theme }) => theme.space.xl};
+  gap: ${({ theme }) => theme.space.sm};
+`;
+const StyledTitle = styled(LG)`
+  margin-bottom: ${({ theme }) => theme.space.sm};
+  color: ${({ theme }) => theme.palette.grey[800]};
 `;
 
 const Transcript = () => {
@@ -32,8 +34,12 @@ const Transcript = () => {
 
   return (
     <StyledContainerCard>
-      <LG>{t('__VIDEO_PAGE_TRANSCRIPT_TITLE')}</LG>
-      <Paragraph>Transcript</Paragraph>
+      <StyledTitle isBold>{t('__VIDEO_PAGE_TRANSCRIPT_TITLE')}</StyledTitle>
+      <TranscriptParagraph
+        words={`T71338, Apple iPhone 13, use case three. Okay, so I am on the product page and I'm just gonna scroll through the photos. Looks like a very nice cotton -only shirt. I love the photos, it shows the product very good.`}
+        startTime="00:00"
+        endTime="00:00"
+      />
     </StyledContainerCard>
   );
 };
