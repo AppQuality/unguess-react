61014ad8a6297ea6d0f4fd7c0c64846b826069d9
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2024-05-16 16:10:50 +0200

    refactor(VideosPageContent): optimize loading skeleton display

--- src/pages/Videos/Content.tsx
+++ src/pages/Videos/Content.tsx
@@ -44,6 +44,8 @@ const VideosPageContent = () => {
 
   if (isError) return null;
 
+  if (isLoading) return <Skeleton height="300px" style={{ borderRadius: 0 }} />;
+
   if (!videos || videos.length === 0) {
     return <Empty />;
   }
@@ -52,71 +54,64 @@ const VideosPageContent = () => {
 
   return (
     <LayoutWrapper isNotBoxed>
-      {isLoading ? (
-        <Skeleton height="300px" style={{ borderRadius: 0 }} />
-      ) : (
-        <div style={{ opacity: isFetching ? 0.5 : 1 }}>
-          <Grid>
-            {!!usecases?.length && (
-              <Row>
-                <Col>
-                  <Wrapper isFetching={false}>
-                    <Accordion
-                      level={3}
-                      defaultExpandedSections={Array.from(
-                        usecases,
-                        (_, i) => i
-                      )}
-                      isExpandable
-                      isBare
-                    >
-                      {usecases.map((uc) => (
-                        <Accordion.Section
-                          style={{ marginBottom: appTheme.space.lg }}
-                        >
-                          <StyledAccordionHeader>
-                            <StyledAccordionLabel>
-                              <InfoRow
-                                usecase={uc.usecase.title}
-                                videos={uc.videos.total}
-                              />
-                            </StyledAccordionLabel>
-                          </StyledAccordionHeader>
-                          <Accordion.Panel style={{ padding: 0 }}>
-                            {!!uc.videos.desktop.length && (
-                              <VideoContainer
-                                title={t('__VIDEOS_LIST_DESKTOP_TITLE')}
-                                video={uc.videos.desktop}
-                              />
-                            )}
-                            {!!uc.videos.tablet.length && (
-                              <VideoContainer
-                                title={t('__VIDEOS_LIST_TABLET_TITLE')}
-                                video={uc.videos.tablet}
-                              />
-                            )}
-                            {!!uc.videos.smartphone.length && (
-                              <VideoContainer
-                                title={t('__VIDEOS_LIST_SMARTPHONE_TITLE')}
-                                video={uc.videos.smartphone}
-                              />
-                            )}
-                            <AccordionFooter>
-                              <CompletionTooltip
-                                percentage={uc.usecase.completion}
-                              />
-                            </AccordionFooter>
-                          </Accordion.Panel>
-                        </Accordion.Section>
-                      ))}
-                    </Accordion>
-                  </Wrapper>
-                </Col>
-              </Row>
-            )}
-          </Grid>
-        </div>
-      )}
+      <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+        <Grid>
+          {!!usecases?.length && (
+            <Row>
+              <Col>
+                <Wrapper isFetching={false}>
+                  <Accordion
+                    level={3}
+                    defaultExpandedSections={Array.from(usecases, (_, i) => i)}
+                    isExpandable
+                    isBare
+                  >
+                    {usecases.map((uc) => (
+                      <Accordion.Section
+                        style={{ marginBottom: appTheme.space.lg }}
+                      >
+                        <StyledAccordionHeader>
+                          <StyledAccordionLabel>
+                            <InfoRow
+                              usecase={uc.usecase.title}
+                              videos={uc.videos.total}
+                            />
+                          </StyledAccordionLabel>
+                        </StyledAccordionHeader>
+                        <Accordion.Panel style={{ padding: 0 }}>
+                          {!!uc.videos.desktop.length && (
+                            <VideoContainer
+                              title={t('__VIDEOS_LIST_DESKTOP_TITLE')}
+                              video={uc.videos.desktop}
+                            />
+                          )}
+                          {!!uc.videos.tablet.length && (
+                            <VideoContainer
+                              title={t('__VIDEOS_LIST_TABLET_TITLE')}
+                              video={uc.videos.tablet}
+                            />
+                          )}
+                          {!!uc.videos.smartphone.length && (
+                            <VideoContainer
+                              title={t('__VIDEOS_LIST_SMARTPHONE_TITLE')}
+                              video={uc.videos.smartphone}
+                            />
+                          )}
+                          <AccordionFooter>
+                            <CompletionTooltip
+                              percentage={uc.usecase.completion}
+                            />
+                          </AccordionFooter>
+                        </Accordion.Panel>
+                      </Accordion.Section>
+                    ))}
+                  </Accordion>
+                </Wrapper>
+              </Col>
+            </Row>
+          )}
+        </Grid>
+      </div>
     </LayoutWrapper>
   );
 };
