017dc1bf67290c8e9c0689badd77a62d59c0f9e0
Marco Bonomo <marco.bonomo@unguess.io>
2024-05-28 17:56:09 +0200

    🔥 refactor(Actions.tsx): remove unused imports and handleUseCaseExport function

--- src/pages/Video/Actions.tsx
+++ src/pages/Video/Actions.tsx
@@ -1,13 +1,4 @@
-import {
-  Button,
-  LG,
-  Skeleton,
-  Tooltip,
-  Tag,
-  useToast,
-  Notification,
-} from '@appquality/unguess-design-system';
-import { useTranslation } from 'react-i18next';
+import { LG, Skeleton, Tag } from '@appquality/unguess-design-system';
 import { useParams } from 'react-router-dom';
 import { appTheme } from 'src/app/theme';
 import { Meta } from 'src/common/components/Meta';
@@ -21,8 +12,6 @@ import {
 } from 'src/features/api';
 import styled from 'styled-components';
 import { useRef } from 'react';
-import queryString from 'query-string';
-import { ReactComponent as DownloadIcon } from 'src/assets/icons/download-stroke.svg';
 import { formatDuration } from '../Videos/utils/formatDuration';
 import { NoObservations } from './components/NoObservations';
 import { Observation } from './components/Observation';
@@ -54,10 +43,8 @@ const MetaContainer = styled.div`
 `;
 
 const Actions = () => {
-  const { campaignId, videoId } = useParams();
-  const { t } = useTranslation();
+  const { videoId } = useParams();
   const refScroll = useRef<HTMLDivElement>(null);
-  const { addToast } = useToast();
 
   const {
     data: video,
@@ -76,56 +63,6 @@ const Actions = () => {
   } = useGetVideosByVidObservationsQuery({
     vid: videoId || '',
   });
-
-  const handleUseCaseExport = () => {
-    fetch(`${process.env.REACT_APP_CROWD_WP_URL}/wp-admin/admin-ajax.php`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: queryString.stringify({
-        id: campaignId,
-        action: 'ug_generate_research_report',
-      }),
-    })
-      .then((data) => data.json())
-      .then((res) => {
-        if (res.success) {
-          window.location.href = `${process.env.REACT_APP_CROWD_WP_URL}/wp-content/themes/unguess/report/temp/${res.data.file}`;
-          addToast(
-            ({ close }) => (
-              <Notification
-                onClose={close}
-                type="success"
-                message={t('__VIDEO_PAGE_ACTIONS_EXPORT_TOAST_SUCCESS_MESSAGE')}
-                closeText={t('__TOAST_CLOSE_TEXT')}
-                isPrimary
-              />
-            ),
-            { placement: 'top' }
-          );
-        } else {
-          addToast(
-            ({ close }) => (
-              <Notification
-                onClose={close}
-                type="error"
-                message={t('__VIDEO_PAGE_ACTIONS_EXPORT_TOAST_ERROR_MESSAGE')}
-                closeText={t('__TOAST_CLOSE_TEXT')}
-                isPrimary
-              />
-            ),
-            { placement: 'top' }
-          );
-          // eslint-disable-next-line no-console
-          console.error(res);
-        }
-      })
-      .catch((e) => {
-        // eslint-disable-next-line no-console
-        console.error(e.message);
-      });
-  };
 
   if (!video || isErrorVideo) return null;
   if (!observations || isErrorObservations) return null;
