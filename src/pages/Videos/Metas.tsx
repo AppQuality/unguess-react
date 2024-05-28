690976069eb306000533f503820a94b8bee22860
Marco Bonomo <marco.bonomo@unguess.io>
2024-05-28 17:56:17 +0200

    🚀 feat(Metas.tsx): add export functionality for campaign research report 🔧 fix(Metas.tsx): add missing imports for Button, useToast, Notification 🔧 fix(Metas.tsx): add missing imports for DownloadIcon, queryString, useParams

--- src/pages/Videos/Metas.tsx
+++ src/pages/Videos/Metas.tsx
@@ -1,4 +1,11 @@
-import { MD, Paragraph, Skeleton } from '@appquality/unguess-design-system';
+import {
+  Button,
+  MD,
+  Paragraph,
+  Skeleton,
+  useToast,
+  Notification,
+} from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { capitalizeFirstLetter } from 'src/common/capitalizeFirstLetter';
 import { Meta } from 'src/common/components/Meta';
@@ -12,6 +19,9 @@ import {
 } from 'src/features/api';
 import { CampaignStatus } from 'src/types';
 import styled from 'styled-components';
+import { ReactComponent as DownloadIcon } from 'src/assets/icons/download-stroke.svg';
+import queryString from 'query-string';
+import { useParams } from 'react-router-dom';
 import { getAllSeverityTags } from './utils/getSeverityTagsWithCount';
 
 const ButtonWrapper = styled.div`
@@ -75,7 +85,10 @@ const FooterContainer = styled.div`
 
 export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
   const { status } = campaign;
+  const { campaignId } = useParams();
   const { t } = useTranslation();
+  const { addToast } = useToast();
+
   const {
     data: videos,
     isFetching,
@@ -86,10 +99,74 @@ export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
     (total, item) => total + item.videos.length,
     0
   );
+
   const severities = videos ? getAllSeverityTags(videos.items) : [];
+
+  const handleUseCaseExport = () => {
+    fetch(`${process.env.REACT_APP_CROWD_WP_URL}/wp-admin/admin-ajax.php`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: queryString.stringify({
+        id: campaignId,
+        action: 'ug_generate_research_report',
+      }),
+    })
+      .then((data) => data.json())
+      .then((res) => {
+        if (res.success) {
+          window.location.href = `${process.env.REACT_APP_CROWD_WP_URL}/wp-content/themes/unguess/report/temp/${res.data.file}`;
+          addToast(
+            ({ close }) => (
+              <Notification
+                onClose={close}
+                type="success"
+                message={t('__VIDEO_PAGE_ACTIONS_EXPORT_TOAST_SUCCESS_MESSAGE')}
+                closeText={t('__TOAST_CLOSE_TEXT')}
+                isPrimary
+              />
+            ),
+            { placement: 'top' }
+          );
+        } else {
+          addToast(
+            ({ close }) => (
+              <Notification
+                onClose={close}
+                type="error"
+                message={t('__VIDEO_PAGE_ACTIONS_EXPORT_TOAST_ERROR_MESSAGE')}
+                closeText={t('__TOAST_CLOSE_TEXT')}
+                isPrimary
+              />
+            ),
+            { placement: 'top' }
+          );
+          // eslint-disable-next-line no-console
+          console.error(res);
+        }
+      })
+      .catch((e) => {
+        addToast(
+          ({ close }) => (
+            <Notification
+              onClose={close}
+              type="error"
+              message={t('__VIDEO_PAGE_ACTIONS_EXPORT_TOAST_ERROR_MESSAGE')}
+              closeText={t('__TOAST_CLOSE_TEXT')}
+              isPrimary
+            />
+          ),
+          { placement: 'top' }
+        );
+        // eslint-disable-next-line no-console
+        console.error(e.message);
+      });
+  };
 
   if (isFetching || isLoading) return <Skeleton width="200px" height="20px" />;
   if (isError) return null;
+
   return (
     <FooterContainer>
       <PageMeta>
@@ -117,6 +194,12 @@ export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
       </PageMeta>
       <ButtonWrapper>
         <CampaignSettings />
+        <Button isAccent isPrimary size="small" onClick={handleUseCaseExport}>
+          <Button.StartIcon>
+            <DownloadIcon />
+          </Button.StartIcon>
+          {t('__VIDEO_PAGE_ACTIONS_EXPORT_BUTTON_LABEL')}
+        </Button>
       </ButtonWrapper>
     </FooterContainer>
   );
