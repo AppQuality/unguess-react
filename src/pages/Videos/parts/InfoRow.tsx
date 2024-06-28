e4a6e27116bb3158d6df52e203052a72499e04e3
Marco Bonomo <marco.bonomo@unguess.io>
2024-06-28 17:08:45 +0200

    🔨 refactor(Content.tsx): simplify InfoRow component props and usage 🔧 fix(useVideos.ts): update type definition for CampaignVideos array

--- src/pages/Videos/parts/InfoRow.tsx
+++ src/pages/Videos/parts/InfoRow.tsx
@@ -1,42 +1,55 @@
-import { MD, SM, Span } from '@appquality/unguess-design-system';
+import { Editor, MD, SM, Span } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
 import styled from 'styled-components';
+import { CampaignVideos } from '../useVideos';
 
 export const StyledSM = styled(SM)`
   color: ${(p) => p.theme.palette.grey[600]};
 `;
 
 const StyledDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   padding: ${(p) => p.theme.space.xs} 0;
-  column-gap: ${(p) => p.theme.space.xs};
-  ${StyledSM} {
-    flex-shrink: 0;
+`;
+
+const EditorContainer = styled.div`
+  margin-top: ${({ theme }) => theme.space.xxs};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+
+  * {
+    user-select: none;
+    padding: 0;
+    background-color: transparent;
+    font-weight: ${({ theme }) => theme.fontWeights.regular};
+  }
+
+  *:focus {
+    box-shadow: none;
+    outline: none !important;
   }
 `;
 
-export const InfoRow = ({
-  videos,
-  usecase,
-}: {
-  videos: number;
-  usecase: string;
-}) => {
+export const InfoRow = ({ uc }: { uc: CampaignVideos[number] }) => {
   const { t } = useTranslation();
+  const { usecase, videos } = uc;
+
   return (
-    <StyledDiv>
-      <MD>
-        <Span isBold>{usecase} </Span>
-        <Span style={{ color: appTheme.palette.grey[600] }}>{`(${videos} ${t(
-          '__VIDEOS_LIST_USECASE_INFO',
-          {
-            count: videos,
-          }
-        )})`}</Span>
-      </MD>
-    </StyledDiv>
+    <>
+      <StyledDiv>
+        <MD>
+          <Span isBold>{usecase.title} </Span>
+          <Span style={{ color: appTheme.palette.grey[600] }}>
+            {`(${videos.total} ${t('__VIDEOS_LIST_USECASE_INFO', {
+              count: videos.total,
+            })})`}
+          </Span>
+        </MD>
+      </StyledDiv>
+      <EditorContainer>
+        <Editor key={Math.random()} editable={false}>
+          {usecase.description}
+        </Editor>
+      </EditorContainer>
+    </>
   );
 };
