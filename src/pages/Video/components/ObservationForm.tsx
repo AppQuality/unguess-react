a7bcfc3785a2ea818279748321ff81b8c0748a5a
ZecD <dominik.zecoli@gmail.com>
2024-06-28 17:08:39 +0200

    changed to fixed 8 to match db

--- src/pages/Video/components/ObservationForm.tsx
+++ src/pages/Video/components/ObservationForm.tsx
@@ -154,9 +154,9 @@ const ObservationForm = ({
       .flatMap((paragraph) =>
         paragraph.words.filter(
           (word) =>
-            Number(word.start.toFixed(2)) >=
-              Number(observation.start.toFixed(2)) &&
-            Number(word.end.toFixed(2)) <= Number(observation.end.toFixed(2))
+            Number(word.start.toFixed(8)) >=
+              Number(observation.start.toFixed(8)) &&
+            Number(word.end.toFixed(8)) <= Number(observation.end.toFixed(8))
         )
       )
       .map((word) => word.word)
