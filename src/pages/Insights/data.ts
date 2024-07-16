62e4b1ff687d9cee559e4fc6374c4f79e37535c1
Iacopo Leardini <iacopo.lea@gmail.com>
2024-07-16 15:59:58 +0200

    wip: fake data

---
+++ src/pages/Insights/data.ts
@@ -0,0 +1,146 @@
+export const insights = [
+  {
+    id: 1,
+    title: 'insight #1',
+    description: 'insight #1 description',
+    severity: {
+      id: 1,
+      name: 'high',
+    },
+    observations: [
+      {
+        id: 1,
+        title: 'observation #1',
+        description: 'observation #1 description',
+        /** Format: float */
+        start: 1,
+        /** Format: float */
+        end: 3,
+        tags: [
+          {
+            group: {
+              id: 1,
+              name: 'group #1',
+            },
+            tag: {
+              id: 1,
+              name: 'tag #1',
+              style: 'color: #000000',
+              usageNumber: 1,
+            },
+          },
+        ],
+        quotes: 'lorem ipsum',
+        video: {
+          id: 1,
+          poster: 'https://via.placeholder.com/150',
+          url: 'https://via.placeholder.com/150',
+          streamUrl: 'https://via.placeholder.com/150',
+        },
+      },
+      {
+        id: 2,
+        title: 'observation #2',
+        description: 'observation #2 description',
+        /** Format: float */
+        start: 1,
+        /** Format: float */
+        end: 3,
+        tags: [
+          {
+            group: {
+              id: 1,
+              name: 'group #1',
+            },
+            tag: {
+              id: 1,
+              name: 'tag #1',
+              style: 'color: #000000',
+              usageNumber: 1,
+            },
+          },
+        ],
+        quotes: 'lorem ipsum',
+        video: {
+          id: 1,
+          poster: 'https://via.placeholder.com/150',
+          url: 'https://via.placeholder.com/150',
+          streamUrl: 'https://via.placeholder.com/150',
+        },
+      },
+    ],
+    comment: 'comment #1',
+  },
+  {
+    id: 2,
+    title: 'insight #2',
+    description: 'insight #2 description',
+    severity: {
+      id: 1,
+      name: 'high',
+    },
+    observations: [
+      {
+        id: 3,
+        title: 'observation #3',
+        description: 'observation #3 description',
+        /** Format: float */
+        start: 1,
+        /** Format: float */
+        end: 3,
+        tags: [
+          {
+            group: {
+              id: 1,
+              name: 'group #1',
+            },
+            tag: {
+              id: 1,
+              name: 'tag #1',
+              style: 'color: #000000',
+              usageNumber: 1,
+            },
+          },
+        ],
+        quotes: 'lorem ipsum',
+        video: {
+          id: 1,
+          poster: 'https://via.placeholder.com/150',
+          url: 'https://via.placeholder.com/150',
+          streamUrl: 'https://via.placeholder.com/150',
+        },
+      },
+      {
+        id: 4,
+        title: 'observation #4',
+        description: 'observation #4 description',
+        /** Format: float */
+        start: 1,
+        /** Format: float */
+        end: 3,
+        tags: [
+          {
+            group: {
+              id: 1,
+              name: 'group #1',
+            },
+            tag: {
+              id: 1,
+              name: 'tag #1',
+              style: 'color: #000000',
+              usageNumber: 1,
+            },
+          },
+        ],
+        quotes: 'lorem ipsum',
+        video: {
+          id: 1,
+          poster: 'https://via.placeholder.com/150',
+          url: 'https://via.placeholder.com/150',
+          streamUrl: 'https://via.placeholder.com/150',
+        },
+      },
+    ],
+    comment: 'comment #2',
+  },
+];
