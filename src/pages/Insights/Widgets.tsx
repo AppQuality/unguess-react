2320a0b6813389b4d37ef677c64c81b167f220e7
Kariamos <minucci9292@gmail.com>
2024-07-18 17:53:28 +0200

    wip widgets ux

--- src/pages/Insights/Widgets.tsx
+++ src/pages/Insights/Widgets.tsx
@@ -2,9 +2,17 @@ import { LG } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { styled } from 'styled-components';
 import { UserAnalysisWidget } from './Widgets/UserAnalysisWidget';
+import { ObservedThemesWidget } from './Widgets/UserAnalysisWidget/ObservedThemesWidget';
+import { ProgressMonitoringWidget } from './Widgets/ProgressMonitoringWidget';
 
 const Container = styled.div`
   margin-top: ${({ theme }) => theme.space.lg};
+  display: flex;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.space.lg};
+  @media ${LG} {
+    flex-direction: row;
+  }
 `;
 
 const Widgets = () => {
@@ -13,6 +21,8 @@ const Widgets = () => {
   return (
     <Container>
       <UserAnalysisWidget />
+      <ObservedThemesWidget />
+      <ProgressMonitoringWidget />
     </Container>
   );
 };
