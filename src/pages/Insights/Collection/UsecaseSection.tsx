2978921b29ed7c73c44fba406a8e763d6bed3518
Iacopo Leardini <iacopo.lea@gmail.com>
2024-07-18 19:05:43 +0200

    refactor: optimize rendering of ClusterCard and ObservationCard components

--- src/pages/Insights/Collection/UsecaseSection.tsx
+++ src/pages/Insights/Collection/UsecaseSection.tsx
@@ -1,47 +1,80 @@
-import { MD } from '@appquality/unguess-design-system';
+import { Accordion, LG, Tag } from '@appquality/unguess-design-system';
 import { v4 as uuidv4 } from 'uuid';
 import { useMemo } from 'react';
+import { styled } from 'styled-components';
+import { Grape as GrapeType, Observation } from 'src/features/api';
 import { Grape } from './Grape';
+import { ObservationCard } from './ObservationCard';
+import { CardGrid } from './CardGrid';
 
 interface UsecaseSectionProps {
-  usecase: {
-    usecaseId: number;
-    usecaseTitle: string;
-    grapes: {
-      title: string;
-      severity: string;
-      usersNumber: number;
-      observations: {
-        id: number;
-        title: string;
-        description: string;
-        start: number;
-        end: number;
-        quotes: string;
-        uxNote: string;
-      }[];
-    }[];
-  };
+  usecaseId: number;
+  usecaseTitle: string;
+  grapes: GrapeType[];
+  ungrouped: Observation[];
 }
 
-export const UsecaseSection = ({ usecase }: UsecaseSectionProps) => {
-  const grapes = useMemo(
+const StyledSection = styled.section`
+  margin-top: ${({ theme }) => theme.space.md};
+  margin-bottom: ${({ theme }) => theme.space.md};
+`;
+
+export const UsecaseSection = ({
+  usecaseId,
+  usecaseTitle,
+  grapes,
+  ungrouped,
+}: UsecaseSectionProps) => {
+  const memoizedGrapes = useMemo(
     () =>
-      usecase.grapes.map((grape) => ({
+      grapes.map((grape) => ({
         ...grape,
         internalId: uuidv4(),
       })),
-    [usecase.grapes]
+    [grapes]
+  );
+  const observationsCount = useMemo(
+    () =>
+      grapes.reduce(
+        (acc, grape) => acc + grape.observations.length,
+        ungrouped.length
+      ),
+    [ungrouped, memoizedGrapes]
   );
 
+  const UsecaseTitle = styled.div`
+    display: flex;
+    width: 100%;
+    margin-bottom: ${({ theme }) => theme.space.md};
+  `;
+
   return (
-    <section>
-      <MD>
-        {usecase.usecaseTitle} ({usecase.grapes.length})
-      </MD>
-      {grapes.map((grape) => (
-        <Grape key={grape.internalId} grape={grape} />
-      ))}
-    </section>
+    <StyledSection>
+      <UsecaseTitle>
+        <LG style={{ flex: '1 0 auto' }}>
+          <h3>{usecaseTitle}</h3>
+        </LG>
+        <div style={{ flex: '0 0 auto' }}>
+          <Tag hue="" size="small" /> <small>{grapes.length} tit</small>{' '}
+          <Tag hue="" size="small" /> <small>{observationsCount} obs</small>
+        </div>
+      </UsecaseTitle>
+      <Accordion
+        id={usecaseId.toString()}
+        level={3}
+        isExpandable
+        defaultExpandedSections={[]}
+        isBare
+      >
+        {memoizedGrapes.map((grape) => (
+          <Grape key={grape.internalId} grape={grape} />
+        ))}
+        <CardGrid>
+          {ungrouped.map((observation) => (
+            <ObservationCard key={observation.id} quote={observation.quotes} />
+          ))}
+        </CardGrid>
+      </Accordion>
+    </StyledSection>
   );
 };
