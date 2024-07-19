b4de239a4a4e5fa4bf8a42c0fd60de20ca248d51
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2024-07-19 10:46:31 +0200

    fix(CampaignCard): reduce hooks drilling and fix onClick issues

--- src/pages/Dashboard/CampaignItem.tsx
+++ src/pages/Dashboard/CampaignItem.tsx
@@ -1,23 +1,27 @@
 import { CampaignCard } from '@appquality/unguess-design-system';
+import { format } from 'date-fns';
 import { HTMLAttributes } from 'react';
 import { useTranslation } from 'react-i18next';
 import { CampaignWithOutput } from 'src/features/api';
-import { format } from 'date-fns';
-import { CampaignActionProps, CAMPAING_STATUSES } from './types';
+import { getLocalizeDashboardRoute } from 'src/hooks/useLocalizeDashboardUrl';
+import { CAMPAING_STATUSES } from './types';
 
 export const CampaignItem = ({
   campaign,
-  onCampaignClicked,
   ...props
 }: {
   campaign: CampaignWithOutput;
-  onCampaignClicked: (args: CampaignActionProps) => void;
   size?: number;
 } & HTMLAttributes<HTMLDivElement>) => {
   const { t } = useTranslation();
   const { type, project, family } = campaign;
   const isFunctional = family.name.toLowerCase() === 'functional';
   const title = campaign.customer_title ?? campaign.title;
+  const targetUrl = getLocalizeDashboardRoute({
+    campaignId: campaign.id,
+    cpFamily: campaign.family.name,
+    outputs: campaign.outputs || [],
+  });
 
   return (
     <CampaignCard
@@ -30,13 +34,9 @@ export const CampaignItem = ({
       type={isFunctional ? 'FUNCTIONAL' : 'EXPERIENTIAL'}
       status={campaign.status.name.toUpperCase() as CAMPAING_STATUSES}
       pillText={type.name}
-      onClick={() =>
-        onCampaignClicked({
-          campaignId: campaign.id,
-          cpFamily: family.name,
-          outputs: campaign.outputs || [],
-        })
-      }
+      onClick={() => {
+        window.location.href = targetUrl;
+      }}
       {...props}
     />
   );
--- src/pages/Dashboard/project-items/list.tsx
+++ src/pages/Dashboard/project-items/list.tsx
@@ -1,30 +1,21 @@
-import { Row, Col } from '@appquality/unguess-design-system';
+import { Col, Row } from '@appquality/unguess-design-system';
 import { appTheme } from 'src/app/theme';
 import { Campaign } from 'src/features/api';
-import { getLocalizeDashboardRoute } from 'src/hooks/useLocalizeDashboardUrl';
 import { CampaignItem } from '../CampaignItem';
-import { CampaignActionProps } from '../types';
 
-const CardGroup = ({ items }: { items: Array<Campaign> }) => {
-  const clickToggle = (props: CampaignActionProps) => {
-    window.location.href = getLocalizeDashboardRoute(props);
-  };
-
-  return (
-    <>
-      {items.map((campaign) => (
-        <Col size={3} xs={12} md={6} lg={3}>
-          <CampaignItem
-            key={`campaign_${campaign.id}`}
-            campaign={campaign}
-            onCampaignClicked={clickToggle}
-            style={{ marginBottom: `${appTheme.space.base * 4}px` }}
-          />
-        </Col>
-      ))}
-    </>
-  );
-};
+const CardGroup = ({ items }: { items: Array<Campaign> }) => (
+  <>
+    {items.map((campaign) => (
+      <Col size={3} xs={12} md={6} lg={3}>
+        <CampaignItem
+          key={`campaign_${campaign.id}`}
+          campaign={campaign}
+          style={{ marginBottom: `${appTheme.space.base * 4}px` }}
+        />
+      </Col>
+    ))}
+  </>
+);
 
 export const CardList = ({ campaigns }: { campaigns: Array<Campaign> }) => (
   <Row>
