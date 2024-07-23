a03930e83729ee703754f1ca26e4c81c85669306
Marco Bonomo <marco.bonomo@unguess.io>
2024-07-23 15:56:15 +0200

    🔨 refactor(schema.ts): update all paths to use double quotes for consistency 🔧 chore(schema.ts): fix typo in 'existent' to 'existing' in comments
    🔧 fix(schema.ts): change all single quotes to double quotes for consistency
    🔧 fix(schema.ts): update object keys to use double quotes for consistency
    
    🔧 fix(schema.ts): update enum values to use double quotes instead of single quotes
    🔧 fix(schema.ts): update enum values to use double quotes consistently throughout the file
    
    🔧 fix(schema.ts): change single quotes to double quotes for consistency
    🔧 fix(schema.ts): change single quotes to double quotes for consistency
    
    🔨 refactor(schema.ts): change single quotes to double quotes for consistency
    🔨 refactor(schema.ts): change object key quotes to double quotes for consistency
    
    🔨 refactor(schema.ts): change single quotes to double quotes for consistency
    🔨 refactor(schema.ts): change single quotes to double quotes for consistency
    
    🔨 refactor(schema.ts): change all single quotes to double quotes for consistency
    🔨 refactor(schema.ts): change all object keys to use double quotes for consistency
    
    🔨 refactor(schema.ts): change single quotes to double quotes for consistency
    🔥 chore(schema.ts): remove unnecessary comments and whitespace changes
    
    🔨 refactor(api): simplify status comments in API response types
    🌐 chore(translation): add and update translation keys for insight form
    🚀 feat(FormProvider): add support for creating and updating insights
    
    🔨 refactor(InsightForm.tsx): restructure form layout and add support for
    severity tags
    🔥 remove(NewInsight.tsx): delete unused NewInsightForm component
    
    🚀 feat(ObservationForm.tsx): export RadioTag styled component for reusability

--- src/pages/Insights/InsightForm.tsx
+++ src/pages/Insights/InsightForm.tsx
@@ -1,107 +1,231 @@
 import {
   Button,
-  Checkbox,
+  Col,
+  Grid,
   Input,
   Label,
   Message,
+  Radio,
+  Row,
+  SM,
+  Skeleton,
+  Textarea,
 } from '@appquality/unguess-design-system';
 import { Field, FieldProps, useFormikContext } from 'formik';
-import { Field as ZendeskField } from '@zendeskgarden/react-forms';
+import { Field as FormField } from '@zendeskgarden/react-forms';
 import { useTranslation } from 'react-i18next';
+import {
+  GetCampaignsByCidVideoTagsApiResponse,
+  useGetCampaignsByCidVideoTagsQuery,
+} from 'src/features/api';
 import { appTheme } from 'src/app/theme';
+import { useParams } from 'react-router-dom';
+import { getColorWithAlpha } from 'src/common/utils';
+import { useState } from 'react';
+import { styled } from 'styled-components';
 import { InsightFormValues } from './FormProvider';
+import { RadioTag } from '../Video/components/ObservationForm';
+import { ObservationCard } from './Collection/ObservationCard';
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.space.md};
+`;
 
 const InsightForm = () => {
   const { t } = useTranslation();
+  const { campaignId } = useParams();
+  const [selectedSeverity, setSelectedSeverity] = useState<
+    GetCampaignsByCidVideoTagsApiResponse[number]['tags'][number] | undefined
+  >();
+  const { values, isSubmitting, setFieldValue, errors } =
+    useFormikContext<InsightFormValues>();
+
   const {
-    values: { observations },
-    isSubmitting,
-    resetForm,
-  } = useFormikContext<InsightFormValues>();
+    data: tags,
+    isLoading,
+    isFetching,
+  } = useGetCampaignsByCidVideoTagsQuery({
+    cid: campaignId || '',
+  });
+
+  const severities = tags?.find((tag) => tag.group.name === 'severity');
 
   return (
-    <>
-      <Label>{t('__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_TITLE_LABEL')}</Label>
-      <Field name="title">
-        {({ field, form, meta }: FieldProps) => (
-          <>
-            <Input
-              {...field}
-              placeholder={t(
-                '__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_TITLE_PLACEHOLDER'
+    <FormContainer>
+      <div>
+        <Label>{t('__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_TITLE_LABEL')}</Label>
+        <Field name="title">
+          {({ field, form, meta }: FieldProps) => (
+            <div style={{ marginTop: appTheme.space.sm }}>
+              <Input
+                {...field}
+                placeholder={t(
+                  '__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_TITLE_PLACEHOLDER'
+                )}
+                onChange={(e) => {
+                  form.setFieldValue('title', e.target.value);
+                }}
+              />
+              {meta.touched && meta.error && (
+                <Message
+                  validation="error"
+                  style={{ marginTop: appTheme.space.sm }}
+                >
+                  {meta.error}
+                </Message>
               )}
-              onChange={(e) => {
-                form.setFieldValue('title', e.target.value);
-              }}
-            />
-            {meta.touched && meta.error && (
-              <Message
-                validation="error"
-                style={{ marginTop: appTheme.space.sm }}
-              >
-                {meta.error}
-              </Message>
-            )}
-          </>
-        )}
-      </Field>
-      <div style={{ margin: `${appTheme.space.md} 0` }}>
+            </div>
+          )}
+        </Field>
+      </div>
+      <div>
+        <Label>
+          {t('__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_DESCRIPTION_LABEL')}
+        </Label>
+        <Field name="description">
+          {({ field, form, meta }: FieldProps) => (
+            <>
+              <Textarea
+                {...field}
+                placeholder={t(
+                  '__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_DESCRIPTION_PLACEHOLDER'
+                )}
+                onChange={(e) => {
+                  form.setFieldValue('description', e.target.value);
+                }}
+                isResizable
+                rows={2}
+              />
+              {meta.touched && meta.error && (
+                <Message
+                  validation="error"
+                  style={{ marginTop: appTheme.space.sm }}
+                >
+                  {meta.error}
+                </Message>
+              )}
+            </>
+          )}
+        </Field>
+      </div>
+      {severities && severities.tags.length > 0 && (
+        <div
+          style={{
+            opacity: isFetching ? 0.5 : 1,
+          }}
+        >
+          <Label>
+            {t('__VIDEO_PAGE_ACTIONS_OBSERVATION_FORM_FIELD_SEVERITY_LABEL')}
+          </Label>
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <>
+              <div style={{ marginTop: appTheme.space.sm }}>
+                {severities.tags.map((severity) => (
+                  <RadioTag
+                    color={severity.style}
+                    style={{
+                      backgroundColor: getColorWithAlpha(severity.style, 0.1),
+                      marginBottom: appTheme.space.sm,
+                    }}
+                  >
+                    <FormField>
+                      <Radio
+                        checked={selectedSeverity?.id === severity.id}
+                        onChange={() => {
+                          setSelectedSeverity(severity);
+                          setFieldValue('severity', severity.id);
+                        }}
+                      >
+                        <Label
+                          style={{
+                            color: severity.style,
+                            paddingRight: appTheme.space.xxs,
+                          }}
+                        >
+                          {severity.name}
+                        </Label>
+                      </Radio>
+                    </FormField>
+                  </RadioTag>
+                ))}
+              </div>
+              {errors.severity && (
+                <Message
+                  validation="error"
+                  style={{ marginTop: appTheme.space.sm }}
+                >
+                  {errors.severity}
+                </Message>
+              )}
+            </>
+          )}
+        </div>
+      )}
+      <div>
         <Label>
           {t('__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_OBSERVATIONS_LABEL')}
         </Label>
-        <div style={{ marginTop: appTheme.space.sm }}>
-          {observations.map(
-            (observation: InsightFormValues['observations'][number]) => (
-              <Field
-                key={observation.id}
-                name={`observations.${observation.id}`}
-              >
-                {({ form }: FieldProps) => (
-                  <ZendeskField>
-                    <Checkbox
-                      checked={
-                        !!observations.find((o) => o.id === observation.id)
-                      }
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          form.setFieldValue('observations', [
-                            ...observations,
-                            observation,
-                          ]);
-                        } else {
-                          form.setFieldValue(
-                            'observations',
-                            observations.filter((o) => o.id !== observation.id)
-                          );
-                        }
-                      }}
-                    >
-                      <Label isRegular>{observation.title}</Label>
-                    </Checkbox>
-                  </ZendeskField>
-                )}
-              </Field>
-            )
-          )}
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: appTheme.space.xs,
+            marginTop: appTheme.space.xs,
+          }}
+        >
+          <SM color={appTheme.palette.grey[600]}>
+            {t('__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_OBSERVATIONS_SELECTED')}
+          </SM>
+          <SM isBold color={appTheme.palette.grey[600]}>
+            {values.observations.length}{' '}
+            {t(
+              '__INSIGHTS_PAGE_INSIGHT_FORM_FIELD_OBSERVATIONS_SELECTED_COUNT',
+              {
+                count: values.observations.length,
+              }
+            )}
+          </SM>
         </div>
+        {values.observations.length > 0 && (
+          <Grid style={{ marginTop: appTheme.space.sm }}>
+            <Row>
+              {values.observations.map(
+                (observation: InsightFormValues['observations'][number]) => (
+                  <Col sm={6}>
+                    <ObservationCard
+                      key={observation.id}
+                      observation={observation}
+                    />
+                  </Col>
+                )
+              )}
+            </Row>
+          </Grid>
+        )}
       </div>
-      <Button
-        type="reset"
-        isBasic
-        disabled={isSubmitting}
-        style={{
-          marginRight: appTheme.space.sm,
-        }}
-        onClick={() => {
-          resetForm();
-        }}
-      >
-        {t('__INSIGHTS_PAGE_INSIGHT_FORM_BUTTON_UNDO')}
-      </Button>
-      <Button isPrimary type="submit">
-        {t('__INSIGHTS_PAGE_INSIGHT_FORM_BUTTON_SAVE')}
-      </Button>
-    </>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          type="reset"
+          isBasic
+          disabled={isSubmitting}
+          style={{
+            marginRight: appTheme.space.sm,
+          }}
+          onClick={() => {}}
+        >
+          {t('__INSIGHTS_PAGE_INSIGHT_FORM_BUTTON_UNDO')}
+        </Button>
+        <Button isPrimary type="submit" disabled={isSubmitting}>
+          {values.id === -1
+            ? t('__INSIGHTS_PAGE_INSIGHT_FORM_BUTTON_CREATE')
+            : t('__INSIGHTS_PAGE_INSIGHT_FORM_BUTTON_UPDATE')}
+        </Button>
+      </div>
+    </FormContainer>
   );
 };
 export { InsightForm };
