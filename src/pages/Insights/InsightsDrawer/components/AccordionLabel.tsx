8a82fd005a60502518b5b6689a084c0cf74c0e73
Iacopo Leardini <iacopo.lea@gmail.com>
2024-07-24 16:41:02 +0200

    feat: add tooltip and notification for publishing/unpublishing insights

--- src/pages/Insights/InsightsDrawer/components/AccordionLabel.tsx
+++ src/pages/Insights/InsightsDrawer/components/AccordionLabel.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
-import { Accordion, IconButton, LG } from '@appquality/unguess-design-system';
-import { ReactComponent as Icon } from '@zendeskgarden/svg-icons/src/16/arrow-up-stroke.svg';
+import {
+  Accordion,
+  IconButton,
+  LG,
+  Tooltip,
+  useToast,
+  Notification,
+} from '@appquality/unguess-design-system';
+import { ReactComponent as Published } from '@zendeskgarden/svg-icons/src/16/eye-stroke.svg';
+import { ReactComponent as NotPublished } from '@zendeskgarden/svg-icons/src/16/eye-hide-fill.svg';
 import { usePatchInsightsByIidMutation } from 'src/features/api';
+import { useTranslation } from 'react-i18next';
 
 const Style = styled(Accordion.Label)`
   display: flex;
@@ -17,16 +26,58 @@ interface Props {
 }
 
 export const AccordionLabel = ({ title, id, isPublished }: Props) => {
-  const [patchInsight] = usePatchInsightsByIidMutation();
+  const { t } = useTranslation();
+  const { addToast } = useToast();
+  const [patchInsight, result] = usePatchInsightsByIidMutation();
   const handlePublish = () => {
-    patchInsight({ iid: id, body: { visible: isPublished ? 0 : 1 } });
+    let notificationProps = {};
+    patchInsight({ iid: id, body: { visible: isPublished ? 0 : 1 } })
+      .unwrap()
+      .then(() => {
+        notificationProps = {
+          type: 'success',
+          message: isPublished
+            ? `Insight "${title}" succesfully unpublished`
+            : `Insight "${title}" succesfully published`,
+        };
+      })
+      .catch((e) => {
+        notificationProps = {
+          type: 'error',
+          message: e.message ? e.message : t('_TOAST_GENERIC_ERROR_MESSAGE'),
+        };
+      })
+      .finally(() => {
+        addToast(
+          ({ close }) => (
+            <Notification
+              onClose={close}
+              closeText="X"
+              isPrimary
+              {...notificationProps}
+            />
+          ),
+          { placement: 'top' }
+        );
+      });
   };
   return (
     <Style>
       <LG isBold>{title}</LG>
-      <IconButton onClick={handlePublish}>
-        <Icon />
-      </IconButton>
+      <Tooltip
+        content={
+          isPublished
+            ? 'Click to unpublish this insight'
+            : 'Click to publish insight'
+        }
+      >
+        <IconButton
+          onClick={handlePublish}
+          disabled={result.status === 'pending'}
+        >
+          {isPublished ? <Published /> : <NotPublished />}
+        </IconButton>
+      </Tooltip>
     </Style>
   );
 };
