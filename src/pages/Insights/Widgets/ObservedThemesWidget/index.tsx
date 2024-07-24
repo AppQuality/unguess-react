0ca38c7a156fed6172779e561d11356042cdf70e
ZecD <dominik.zecoli@gmail.com>
2024-07-24 17:31:31 +0200

    added graph + fixed labels for widgets

--- src/pages/Insights/Widgets/ObservedThemesWidget/index.tsx
+++ src/pages/Insights/Widgets/ObservedThemesWidget/index.tsx
@@ -4,7 +4,7 @@ import { appTheme } from 'src/app/theme';
 import { BasicWidget } from 'src/pages/Campaign/widgetCards/BasicWidget';
 
 import { CapitalizeFirstLetter } from 'src/pages/Campaign/widgetCards/common/CapitalizeFirstLetter';
-import { useUxTotalTitleVsObservations } from '../hooks/useUxTotalTitleVsObservations';
+import { useUxTotalTitleVsRecurrentTitles } from '../hooks/useUxTotalTitleVsRecurrentTitles';
 
 export const ObservedThemesWidget = ({
   campaignId,
@@ -12,8 +12,8 @@ export const ObservedThemesWidget = ({
   campaignId: string;
 }) => {
   const { t } = useTranslation();
-  const { countObservation, countTitleTag } =
-    useUxTotalTitleVsObservations(campaignId);
+  const { countRecurrentTitles, countTitleTag } =
+    useUxTotalTitleVsRecurrentTitles(campaignId);
   return (
     <BasicWidget className="observed-themes-widget">
       <BasicWidget.Header
@@ -23,50 +23,52 @@ export const ObservedThemesWidget = ({
           {t('_CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_HEADER')}
         </CapitalizeFirstLetter>
       </BasicWidget.Header>
-      <BasicWidget.Description
-        header=""
-        content={
-          <span
-            style={{
-              color: appTheme.palette.blue[600],
-            }}
-          >
-            {countTitleTag}
-            <XL
-              tag="span"
-              isBold
-              color={appTheme.palette.blue[600]}
-              style={{ marginLeft: appTheme.space.xs }}
+      <div style={{ marginTop: appTheme.space.lg }}>
+        <BasicWidget.Description
+          header={t('_CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_DESCRIPTION_HEADER')}
+          content={
+            <span
+              style={{
+                color: appTheme.palette.blue[600],
+              }}
             >
-              <Trans
-                i18nKey="__CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_COUNT_LABEL"
-                count={15}
+              {countTitleTag}
+              <XL
+                tag="span"
+                isBold
+                color={appTheme.palette.blue[600]}
+                style={{ marginLeft: appTheme.space.xs }}
               >
-                titles
-              </Trans>
-            </XL>
-          </span>
-        }
-        footer={
-          <Trans
-            // TODO: change the translation key
-            i18nKey="__CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_TOTAL_LABEL"
-            defaults="out of <bold>{{total}}</bold> total observations"
-            count={countObservation}
-            components={{
-              bold: (
-                <Span
-                  isBold
-                  style={{ color: appTheme.components.text.primaryColor }}
-                />
-              ),
-            }}
-            values={{
-              total: countObservation,
-            }}
-          />
-        }
-      />
+                <Trans
+                  i18nKey="__CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_COUNT_LABEL"
+                  count={15}
+                >
+                  topics
+                </Trans>
+              </XL>
+            </span>
+          }
+          footer={
+            <Trans
+              // TODO: change the translation key
+              i18nKey="__CAMPAIGN_WIDGET_UX_OBSERVED_THEMES_TOTAL_LABEL"
+              defaults="out of <bold>{{total}}</bold> total observations"
+              count={countTitleTag}
+              components={{
+                bold: (
+                  <Span
+                    isBold
+                    style={{ color: appTheme.components.text.primaryColor }}
+                  />
+                ),
+              }}
+              values={{
+                total: countTitleTag,
+              }}
+            />
+          }
+        />
+      </div>
     </BasicWidget>
   );
 };
