d5f2e088979f59c917d269845f2daee7f9b5edbd
Kariamos <minucci9292@gmail.com>
2024-08-02 12:00:46 +0200

    fixed other labels

--- src/pages/Insights/InsightsDrawer/hooks/usePublishInsight.tsx
+++ src/pages/Insights/InsightsDrawer/hooks/usePublishInsight.tsx
@@ -21,8 +21,8 @@ export const usePublishInsight = ({ title, id, isPublished }: Props) => {
         notificationProps = {
           type: 'success',
           message: isPublished
-            ? `${`Insight "${title}" ${t('_TOAST_UNPUBLISHED_MESSAGE')}`}`
-            : `${`Insight "${title}" ${t('_TOAST_PUBLISHED_MESSAGE')}`}`,
+            ? `${`Scoperta "${title}" ${t('_TOAST_UNPUBLISHED_MESSAGE')}`}`
+            : `${`Scoperta "${title}" ${t('_TOAST_PUBLISHED_MESSAGE')}`}`,
         };
       })
       .catch((e) => {
