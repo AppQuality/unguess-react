236a17fa1d4d8dad05e4701fd9d29428c8c16694
Iacopo Leardini <iacopo.lea@gmail.com>
2024-08-17 14:01:19 +0200

    feat: Add mock data for Insights page tests

--- tests/e2e/insights.spec.ts
+++ tests/e2e/insights.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '../fixtures/app';
 import { Insights } from '../fixtures/Insights';
 
-test.describe('Home page', () => {
+test.describe('Insights page', () => {
   let insightsPage: Insights;
 
   test.beforeEach(async ({ page }) => {
     insightsPage = new Insights(page);
     await insightsPage.loggedIn();
     await insightsPage.insideWorkspace();
+    await insightsPage.mockCampaign();
+    await insightsPage.mockInsights();
     await insightsPage.open();
   });
 
   test('has a drawer button that open a sidebar with saved insights', async () => {
-    await insightsPage.mockCampaign();
-    await insightsPage.mockInsights();
     await insightsPage.showSavedInsightsSection();
     await expect(insightsPage.elements().drawer()).toBeVisible();
   });
