66c3eafdac76d9a504d42217b15bf4e1e95bfe73
Marco Bonomo <marco.bonomo@unguess.io>
2024-08-22 16:19:39 +0200

    🔧 chore(CardGrid.tsx): adjust margin-bottom size to theme.space.md

--- src/pages/Insights/Collection/components/CardGrid.tsx
+++ src/pages/Insights/Collection/components/CardGrid.tsx
@@ -4,7 +4,7 @@ export const CardGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: ${({ theme }) => theme.space.md};
-  margin-bottom: ${({ theme }) => theme.space.xl};
+  margin-bottom: ${({ theme }) => theme.space.md};
   background-color: ${({ theme }) => theme.palette.grey[100]};
   border-radius: ${({ theme }) => theme.borderRadii.lg};
   padding: ${({ theme }) => theme.space.sm};
