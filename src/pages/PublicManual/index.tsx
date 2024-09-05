edd2fec784ddd4c24249c8fea00a72e9ceb83219
sinatragianpaolo <gianpaolosinatra@gmail.com>
2024-09-05 09:19:57 +0200

    add-public-manual-page

---
+++ src/pages/PublicManual/index.tsx
@@ -0,0 +1,10 @@
+const PublicManual = () => (
+  <>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempore
+    dignissimos sit veniam, quisquam maiores assumenda dolor quae fugit
+    reiciendis excepturi quia asperiores adipisci. Esse fugit sunt natus sequi
+    fuga!
+  </>
+);
+
+export default PublicManual;
