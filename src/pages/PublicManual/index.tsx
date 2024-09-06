fe94fe920cf5803c48ff8de58fd33563e6f541b0
Davide Bizzi <davide.bizzi@unguess.io>
2024-09-06 10:21:18 +0200

    rework: Refactor manual

--- src/pages/PublicManual/index.tsx
+++ src/pages/PublicManual/index.tsx
@@ -1,39 +1,51 @@
 import { useState } from 'react';
 import { useGetPublicManualQuery } from 'src/features/api';
 
-const PublicManual = () => {
-  const [password, setPassword] = useState('');
-  const [finalPassword, setFinalPassword] = useState('');
-
-  const { data, error } = useGetPublicManualQuery({
-    pass: finalPassword,
-  });
+const PasswordInput = ({
+  setPassword,
+}: {
+  setPassword: (password: string) => void;
+}) => {
+  const [value, setValue] = useState('');
 
   return (
     <>
-      {error && finalPassword !== '' && <div>{JSON.stringify(error)}</div>}
-      {data ? (
-        <>{JSON.stringify(data)}</>
-      ) : (
-        <>
-          <h6>password</h6>
-          <input
-            type="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <button
-            type="submit"
-            onClick={() => {
-              setFinalPassword(password);
-            }}
-          >
-            Submit
-          </button>
-        </>
-      )}
-      {JSON.stringify(data)}
+      <h6>password</h6>
+      <input
+        type="password"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <button
+        type="submit"
+        onClick={() => {
+          setPassword(value);
+        }}
+      >
+        Submit
+      </button>
+    </>
+  );
+};
+
+const Content = ({ data }: { data: { text?: string } }) => (
+  <div>{data.text}</div>
+);
+
+const PublicManual = () => {
+  const [password, setPassword] = useState('');
+
+  const { data, error } = useGetPublicManualQuery({
+    pass: password,
+  });
+
+  if (data) return <Content data={data} />;
+
+  return (
+    <>
+      {error && password !== '' && <div>{JSON.stringify(error)}</div>}
+      <PasswordInput setPassword={setPassword} />
     </>
   );
 };
