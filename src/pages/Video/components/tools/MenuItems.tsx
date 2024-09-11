3e2a890d46b0b04a1f3bd965f4563f67ad03dacb
Kariamos <minucci9292@gmail.com>
2024-09-11 18:06:29 +0200

    wip:  translations for transcript

---
+++ src/pages/Video/components/tools/MenuItems.tsx
@@ -0,0 +1,86 @@
+import {
+  Button,
+  Label,
+  NextItem,
+  Paragraph,
+  SM,
+} from '@appquality/unguess-design-system';
+import { ReactComponent as TranslateIcon } from '@zendeskgarden/svg-icons/src/16/translation-exists-stroke.svg';
+import { ReactComponent as BoltIcon } from '@zendeskgarden/svg-icons/src/16/lightning-bolt-stroke.svg';
+import { ReactComponent as SmileyIcon } from '@zendeskgarden/svg-icons/src/16/smiley-stroke.svg';
+import { styled } from 'styled-components';
+import { useToolsContext } from './context/toolsContext';
+
+export const StyledContainerMenuItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const MenuItems = () => {
+  const { setActiveItem } = useToolsContext();
+
+  return (
+    <>
+      <NextItem value="sentiment">
+        <Button
+          isPill={false}
+          isBasic
+          isStretched
+          size="large"
+          value="sentiment"
+          onClick={() => {
+            setActiveItem('sentiment');
+            alert("Hello, I'm the sentiment");
+          }}
+        >
+          <Button.StartIcon>
+            <SmileyIcon />
+          </Button.StartIcon>
+          <Paragraph>
+            <Label isRegular>Sentiment</Label>
+            <SM> Aggiorna piano PRO</SM>
+          </Paragraph>
+        </Button>
+      </NextItem>
+      <Button
+        isPill={false}
+        isBasic
+        isStretched
+        size="large"
+        value="autotag"
+        onClick={() => {
+          setActiveItem('autotag');
+          alert("Hello, I'm the autotag");
+        }}
+      >
+        <Button.StartIcon>
+          <BoltIcon />
+        </Button.StartIcon>
+        <Paragraph>
+          <Label isRegular>Autotag</Label>
+          <SM> Aggiorna piano PRO</SM>
+        </Paragraph>
+      </Button>
+      <Button
+        isPill={false}
+        isBasic
+        isStretched
+        size="large"
+        value="translations"
+        onClick={() => {
+          setActiveItem('translations');
+          alert("Hello, I'm the translations");
+        }}
+      >
+        <Button.StartIcon>
+          <TranslateIcon />
+        </Button.StartIcon>
+        <Paragraph>
+          <Label isRegular>Traduzione Transcript</Label>
+        </Paragraph>
+      </Button>
+    </>
+  );
+};
--- src/pages/Video/components/tools/index.tsx
+++ src/pages/Video/components/tools/index.tsx
@@ -1,24 +1,44 @@
-import { IconButton } from '@appquality/unguess-design-system';
+import {
+  Button,
+  Dropdown,
+  Item,
+  Menu,
+  Trigger,
+  Label,
+} from '@appquality/unguess-design-system';
 import { useRef } from 'react';
-import { ReactComponent as LinkIcon } from 'src/assets/icons/link-stroke.svg';
+
+import { ReactComponent as AIMenuIcon } from 'src/assets/icons/ai-icon.svg';
+
 import { useToolsContext } from './context/toolsContext';
-import { ToolsTooltipModal } from './TooltipModal';
+import { MenuItems } from './MenuItems';
 
 export const Tools = () => {
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const { setReferenceElement } = useToolsContext();
+  const { setReferenceElement, showMenuPanel, setShowMenuPanel } =
+    useToolsContext();
 
   return (
-    <>
-      <IconButton
-        ref={buttonRef}
-        onClick={() => {
-          setReferenceElement(buttonRef.current);
-        }}
-      >
-        <LinkIcon />
-      </IconButton>
-      <ToolsTooltipModal />
-    </>
+    <Dropdown>
+      <Trigger>
+        <Button
+          isBasic
+          size="large"
+          ref={buttonRef}
+          onClick={() => {
+            setReferenceElement(buttonRef.current);
+            setShowMenuPanel(!showMenuPanel);
+          }}
+        >
+          <AIMenuIcon />
+          <Label style={{ marginLeft: '0.5rem' }} isRegular>
+            Generate AI
+          </Label>
+        </Button>
+      </Trigger>
+      <Menu hasArrow>
+        <MenuItems />
+      </Menu>
+    </Dropdown>
   );
 };
