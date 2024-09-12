ff7bd1280dc3eb11720e13c1ed48c091cef95ce3
Marco Bonomo <marco.bonomo@unguess.io>
2024-09-12 17:49:42 +0200

    🔧 fix(translation.json): add empty strings for new tools content in translation 🚀 feat(ToolsMenu.tsx): refactor ToolsMenu to use separate components for flags 🆕 feat(ToolsAutotagFlag.tsx): create component for Autotag flag in ToolsMenu 🆕 feat(ToolsSentimentFlag.tsx): create component for Sentiment flag in ToolsMenu 🆕 feat(ToolsTranslate.tsx): create component for Translate feature in ToolsMenu
    🚚 chore(ToolsTranslateFlag.tsx): add ToolsTranslateFlag component
    🔥 chore(Tools): remove unused modals and modals index file

--- src/pages/Video/components/tools/index.tsx
+++ src/pages/Video/components/tools/index.tsx
@@ -4,33 +4,27 @@ import { appTheme } from 'src/app/theme';
 import { useTranslation } from 'react-i18next';
 import { ToolsMenu } from './ToolsMenu';
 import { useToolsContext } from './context/ToolsContext';
-import { ModalSentiment, ModalAutotag, ModalTranslate } from './modals';
 
 export const Tools = () => {
   const { t } = useTranslation();
-  const { activeItem, setActiveItem } = useToolsContext();
+  const { setActiveItem } = useToolsContext();
 
   return (
-    <>
-      <Dropdown>
-        <Trigger>
-          <Button
-            isBasic
-            size="large"
-            style={{ marginLeft: appTheme.space.md }}
-            onClick={() => setActiveItem('menu')}
-          >
-            <Button.StartIcon>
-              <AIMenuIcon />
-            </Button.StartIcon>
-            {t('__TOOLS_MENU_ITEM_BUTTON_LABEL')}
-          </Button>
-        </Trigger>
-        <ToolsMenu />
-      </Dropdown>
-      {activeItem === 'sentiment' && <ModalSentiment />}
-      {activeItem === 'autotag' && <ModalAutotag />}
-      {activeItem === 'translate' && <ModalTranslate />}
-    </>
+    <Dropdown>
+      <Trigger>
+        <Button
+          isBasic
+          size="large"
+          style={{ marginLeft: appTheme.space.md }}
+          onClick={() => setActiveItem('menu')}
+        >
+          <Button.StartIcon>
+            <AIMenuIcon />
+          </Button.StartIcon>
+          {t('__TOOLS_MENU_ITEM_BUTTON_LABEL')}
+        </Button>
+      </Trigger>
+      <ToolsMenu />
+    </Dropdown>
   );
 };
