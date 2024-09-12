eb29777b058815610e795c16caab4dfa57afbd13
Marco Bonomo <marco.bonomo@unguess.io>
2024-09-12 16:17:26 +0200

    🚀 feat(Transcript.tsx): add ToolsContextProvider for Tools component 🚀 feat(ToolsMenu.tsx): update state management to use ToolsContext 🚀 feat(ToolsContext.tsx): create context and provider for Tools component 🚀 feat(Tools/index.tsx): update Tools component to use ToolsContext 🚀 feat(modals): add ModalAutotag, ModalSentiment, ModalTranslate components

---
+++ src/pages/Video/components/tools/modals/index.ts
@@ -0,0 +1,5 @@
+import { ModalAutotag } from './ModalAutotag';
+import { ModalSentiment } from './ModalSentiment';
+import { ModalTranslate } from './ModalTranslate';
+
+export { ModalAutotag, ModalSentiment, ModalTranslate };
