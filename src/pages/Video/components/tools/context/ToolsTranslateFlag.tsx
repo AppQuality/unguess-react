bafd482fd12043b420b6aa65263c930b68be4107
Marco Bonomo <marco.bonomo@unguess.io>
2024-09-12 17:49:42 +0200

    🔧 fix(translation.json): add empty strings for new tools content in translation 🚀 feat(ToolsMenu.tsx): refactor ToolsMenu to use separate components for flags 🆕 feat(ToolsAutotagFlag.tsx): create component for Autotag flag in ToolsMenu 🆕 feat(ToolsSentimentFlag.tsx): create component for Sentiment flag in ToolsMenu 🆕 feat(ToolsTranslate.tsx): create component for Translate feature in ToolsMenu
    🚚 chore(ToolsTranslateFlag.tsx): add ToolsTranslateFlag component
    🔥 chore(Tools): remove unused modals and modals index file

---
+++ src/pages/Video/components/tools/context/ToolsTranslateFlag.tsx
@@ -0,0 +1,28 @@
+import { Button, MD, Separator, Span } from '@appquality/unguess-design-system';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as ArrowLeft } from 'src/assets/icons/chevron-left-icon.svg';
+import { appTheme } from 'src/app/theme';
+import { MenuButton } from '../MenuButton';
+import { useToolsContext } from './ToolsContext';
+
+const ToolsTranslateFlag = () => {
+  const { t } = useTranslation();
+  const { activeItem, setActiveItem } = useToolsContext();
+
+  if (activeItem !== 'translate') return null;
+
+  return (
+    <>
+      <MenuButton onClick={() => setActiveItem('menu')}>
+        <Button.StartIcon>
+          <ArrowLeft />
+        </Button.StartIcon>
+        <Span isBold>{t('__TOOLS_TRANSLATE_PREVIOUS_ITEM')}</Span>
+      </MenuButton>
+      <Separator style={{ marginBottom: appTheme.space.md }} />
+      <MD>{t('__TOOLS_TRANSLATE_CONTENT')}</MD>
+    </>
+  );
+};
+
+export { ToolsTranslateFlag };
