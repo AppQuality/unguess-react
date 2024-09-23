47bf9cb17bec5b2c8d7e36960cdc17225a8b2b2d
Iacopo Leardini <iacopo.lea@gmail.com>
2024-09-23 12:25:13 +0200

    chore: Update Playwright config to use "github" reporter on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: process.env.CI
-      ? 'export NODE_OPTIONS=--openssl-legacy-provider && export NODE_ENV=test && yarn start'
+      ? 'export NODE_ENV=test && yarn start:ssl'
       : 'export NODE_ENV=test && yarn start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
