1671d16d1357f00e8bf9cb7a4853210133fcacfe
Davide Bizzi <davide.bizzi@unguess.io>
2024-09-23 16:31:54 +0200

    feat: Add new transcript component

--- src/pages/Video/components/Transcript.tsx
+++ src/pages/Video/components/Transcript.tsx
@@ -1,5 +1,6 @@
 import {
   ContainerCard,
+  EditorWithHighlight,
   Highlight,
   LG,
   Notification,
@@ -22,8 +23,8 @@ import useDebounce from 'src/hooks/useDebounce';
 import { styled } from 'styled-components';
 import { useVideoContext } from '../context/VideoContext';
 import { ObservationTooltip } from './ObservationTooltip';
-import { SearchBar } from './SearchBar';
 import { ParagraphMeta } from './ParagraphMeta';
+import { SearchBar } from './SearchBar';
 
 export const StyledContainerCard = styled(ContainerCard)`
   margin: ${({ theme }) => theme.space.xl} 0;
@@ -66,6 +67,108 @@ const TagsWrapper = styled.div`
   flex-direction: column;
   gap: ${({ theme }) => theme.space.xs};
 `;
+
+const NewTranscript = ({
+  currentTime,
+  setCurrentTime,
+}: {
+  currentTime: number;
+  setCurrentTime: (time: number, forcePlay?: boolean) => void;
+}) => {
+  const { videoId } = useParams();
+  const {
+    data: video,
+    isFetching: isFetchingVideo,
+    isLoading: isLoadingVideo,
+    isError: isErrorVideo,
+  } = useGetVideosByVidQuery({
+    vid: videoId || '',
+  });
+  const { t } = useTranslation();
+  const { setOpenAccordion } = useVideoContext();
+  const { addToast } = useToast();
+
+  const {
+    data: observations,
+    isFetching: isFetchingObservations,
+    isLoading: isLoadingObservations,
+    isError: isErrorObservations,
+  } = useGetVideosByVidObservationsQuery({
+    vid: videoId || '',
+  });
+
+  const [postVideoByVidObservations] = usePostVideosByVidObservationsMutation();
+
+  const editor = EditorWithHighlight.useEditor(
+    {
+      currentTime,
+      onSetCurrentTime: (time) => {
+        setCurrentTime(time, true);
+      },
+      content:
+        video && video.transcript
+          ? video.transcript.paragraphs.map((p) => ({
+              ...p,
+              speaker: p.speaker ? p.speaker : 0,
+            }))
+          : undefined,
+      observations: observations?.map((o) => ({
+        id: o.id,
+        type: 'positive',
+        start: o.start,
+        end: o.end,
+        text: o.title,
+      })),
+    },
+    [observations]
+  );
+
+  if (!video || isErrorVideo || !video.transcript) return null;
+  if (!editor) return null;
+  if (isFetchingVideo || isLoadingVideo) return <Skeleton />;
+
+  return (
+    <div style={{ background: '#fff', padding: '20px', margin: '60px' }}>
+      <EditorWithHighlight.FloatingMenu
+        editor={editor}
+        onClick={(ed, { start, end }) => {
+          // @ts-ignore
+          ed.commands.addObservation('type', 'title');
+
+          postVideoByVidObservations({
+            vid: videoId || '',
+            body: {
+              start,
+              end,
+            },
+          })
+            .unwrap()
+            .then((res) => {
+              setOpenAccordion({ id: res.id });
+            })
+            .catch((err) => {
+              addToast(
+                ({ close }) => (
+                  <Notification
+                    onClose={close}
+                    type="error"
+                    message={t('__VIDEO_PAGE_PLAYER_ERROR_OBSERVATION')}
+                    closeText={t('__TOAST_CLOSE_TEXT')}
+                    isPrimary
+                  />
+                ),
+                { placement: 'top' }
+              );
+              // eslint-disable-next-line no-console
+              console.error(err);
+            });
+        }}
+      />
+      <EditorWithHighlight.Search editor={editor} />
+      <EditorWithHighlight editor={editor} />;
+    </div>
+  );
+};
 
 const Transcript = ({
   currentTime,
@@ -154,93 +257,99 @@ const Transcript = ({
   )
     return <Skeleton />;
   return (
-    <div style={{ padding: `0 ${appTheme.space.xxl}` }}>
-      <StyledContainerCard>
-        <TranscriptHeader>
-          <TitleWrapper>
-            <IconTitleContainer>
-              <InfoIcon />
-              <LG color={appTheme.palette.grey[800]} isBold>
-                {t('__VIDEO_PAGE_TRANSCRIPT_TITLE')}
-              </LG>
-            </IconTitleContainer>
-            <SM>{t('__VIDEO_PAGE_TRANSCRIPT_INFO')}</SM>
-          </TitleWrapper>
-          {isSearchable && (
-            <SearchBar
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-          )}
-        </TranscriptHeader>
-        <div ref={containerRef}>
-          <HighlightContainer ref={wrapperRef}>
-            <Highlight
-              search={sanitizeInput(debouncedValue)}
-              onSelectionButtonClick={handleAddObservation}
-              i18n={{
-                selectionButtonLabel: t('__VIDEO_PAGE_ADD_OBSERVATION'),
-              }}
-            >
-              <ChipsWrap id="chips-wrap">
-                {video.transcript?.paragraphs.map((p) => (
-                  <ParagraphMeta
-                    speakers={video.transcript?.speakers || 0}
-                    start={p.start}
-                    end={p.end}
-                    speakerIndex={p.speaker || 0}
-                    setCurrentTime={(time) => setCurrentTime(time, true)}
-                  >
-                    {p.words.map((item, index) => (
-                      <Highlight.Word
-                        size="md"
-                        key={`${item.word + index}`}
-                        start={item.start}
-                        end={item.end}
-                        observations={observations?.map((o) => ({
-                          id: o.id,
-                          start: o.start,
-                          end: o.end,
-                          color:
-                            o.tags.find(
-                              (tag) =>
-                                tag.group.name.toLowerCase() === 'severity'
-                            )?.tag.style || appTheme.palette.grey[600],
-                          hue: getColorWithAlpha(
-                            o.tags.find(
-                              (tag) =>
-                                tag.group.name.toLowerCase() === 'severity'
-                            )?.tag.style || appTheme.palette.grey[600],
-                            0.1
-                          ),
-                          label: o.title,
-                          tags: o.tags,
-                        }))}
-                        currentTime={currentTime}
-                        text={item.word}
-                        tooltipContent={(obs) => (
-                          <TagsWrapper>
-                            {obs.map((o) => (
-                              <ObservationTooltip
-                                observationId={o.id}
-                                start={o.start}
-                                color={o.color}
-                                label={o.label}
-                                seekPlayer={setCurrentTime}
-                              />
-                            ))}
-                          </TagsWrapper>
-                        )}
-                      />
-                    ))}
-                  </ParagraphMeta>
-                ))}
-              </ChipsWrap>
-            </Highlight>
-          </HighlightContainer>
-        </div>
-      </StyledContainerCard>
-    </div>
+    <>
+      <NewTranscript
+        currentTime={currentTime * 1000}
+        setCurrentTime={setCurrentTime}
+      />
+      <div style={{ padding: `0 ${appTheme.space.xxl}` }}>
+        <StyledContainerCard>
+          <TranscriptHeader>
+            <TitleWrapper>
+              <IconTitleContainer>
+                <InfoIcon />
+                <LG color={appTheme.palette.grey[800]} isBold>
+                  {t('__VIDEO_PAGE_TRANSCRIPT_TITLE')}
+                </LG>
+              </IconTitleContainer>
+              <SM>{t('__VIDEO_PAGE_TRANSCRIPT_INFO')}</SM>
+            </TitleWrapper>
+            {isSearchable && (
+              <SearchBar
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            )}
+          </TranscriptHeader>
+          <div ref={containerRef}>
+            <HighlightContainer ref={wrapperRef}>
+              <Highlight
+                search={sanitizeInput(debouncedValue)}
+                onSelectionButtonClick={handleAddObservation}
+                i18n={{
+                  selectionButtonLabel: t('__VIDEO_PAGE_ADD_OBSERVATION'),
+                }}
+              >
+                <ChipsWrap id="chips-wrap">
+                  {video.transcript?.paragraphs.map((p) => (
+                    <ParagraphMeta
+                      speakers={video.transcript?.speakers || 0}
+                      start={p.start}
+                      end={p.end}
+                      speakerIndex={p.speaker || 0}
+                      setCurrentTime={(time) => setCurrentTime(time, true)}
+                    >
+                      {p.words.map((item, index) => (
+                        <Highlight.Word
+                          size="md"
+                          key={`${item.word + index}`}
+                          start={item.start}
+                          end={item.end}
+                          observations={observations?.map((o) => ({
+                            id: o.id,
+                            start: o.start,
+                            end: o.end,
+                            color:
+                              o.tags.find(
+                                (tag) =>
+                                  tag.group.name.toLowerCase() === 'severity'
+                              )?.tag.style || appTheme.palette.grey[600],
+                            hue: getColorWithAlpha(
+                              o.tags.find(
+                                (tag) =>
+                                  tag.group.name.toLowerCase() === 'severity'
+                              )?.tag.style || appTheme.palette.grey[600],
+                              0.1
+                            ),
+                            label: o.title,
+                            tags: o.tags,
+                          }))}
+                          currentTime={currentTime}
+                          text={item.word}
+                          tooltipContent={(obs) => (
+                            <TagsWrapper>
+                              {obs.map((o) => (
+                                <ObservationTooltip
+                                  observationId={o.id}
+                                  start={o.start}
+                                  color={o.color}
+                                  label={o.label}
+                                  seekPlayer={setCurrentTime}
+                                />
+                              ))}
+                            </TagsWrapper>
+                          )}
+                        />
+                      ))}
+                    </ParagraphMeta>
+                  ))}
+                </ChipsWrap>
+              </Highlight>
+            </HighlightContainer>
+          </div>
+        </StyledContainerCard>
+      </div>
+    </>
   );
 };
 
