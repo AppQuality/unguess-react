4341c957b31b1acb041a6fd484e49b3bc6271dec
Davide Bizzi <davide.bizzi@unguess.io>
2024-09-24 16:13:38 +0200

    feat: Update with definitive version of des sys

--- src/pages/Bugs/Content/BugsTable/utils/mapBugsToTableData.tsx
+++ src/pages/Bugs/Content/BugsTable/utils/mapBugsToTableData.tsx
@@ -71,7 +71,7 @@ export const mapBugsToTableData = (bugs: TableBugType[]) => {
                   placement="bottom"
                   type="light"
                   size="medium"
-                  onClick={(e: any) => {
+                  onClick={(e) => {
                     e.stopPropagation();
                   }}
                 >
@@ -97,7 +97,7 @@ export const mapBugsToTableData = (bugs: TableBugType[]) => {
             placement="bottom"
             type="light"
             size="medium"
-            onClick={(e: any) => {
+            onClick={(e) => {
               e.stopPropagation();
             }}
           >
