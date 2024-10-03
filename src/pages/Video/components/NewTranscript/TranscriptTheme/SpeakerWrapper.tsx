de468d52b9225d81168ba8ebd3a407f04911b498
Davide Bizzi <davide.bizzi@unguess.io>
2024-10-03 11:12:01 +0200

    feat: Improve transcript ui

--- src/pages/Video/components/NewTranscript/TranscriptTheme/SpeakerWrapper.tsx
+++ src/pages/Video/components/NewTranscript/TranscriptTheme/SpeakerWrapper.tsx
@@ -1,45 +1,46 @@
-import { IconButton, SM } from '@appquality/unguess-design-system';
+import { Button, SM } from '@appquality/unguess-design-system';
 import { ReactComponent as PlayIcon } from 'src/assets/icons/play-fill.svg';
 import { formatDuration } from 'src/pages/Videos/utils/formatDuration';
 import { styled } from 'styled-components';
 
-const StyledSM = styled(SM)`
+const Wrapper = styled.div`
+  margin-bottom: ${({ theme }) => theme.space.xxs};
   display: flex;
   align-items: center;
   gap: ${({ theme }) => theme.space.xs};
-  user-select: none;
-  margin-bottom: ${({ theme }) => theme.space.sm};
-
-  & > b {
-    margin-right: ${({ theme }) => theme.space.xxs};
-  }
 `;
-
-const StyledDiv = styled.div`
-  margin-bottom: ${({ theme }) => theme.space.lg};
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.space.xs};
 `;
 
 const SpeakerWrapper = ({
   start,
   end,
   setCurrentTime,
+  speaker,
 }: {
   start: number;
   end: number;
   setCurrentTime?: (time: { start: number; end: number }) => void;
+  speaker: string;
 }) => (
-  <StyledDiv>
-    <StyledSM />
-    <IconButton
+  <Wrapper>
+    <SM isBold>{speaker}</SM>
+    <Button
+      isBasic
       size="small"
       onClick={
         setCurrentTime ? () => setCurrentTime({ start, end }) : undefined
       }
     >
-      <PlayIcon />
-    </IconButton>
-    {formatDuration(start)} - {formatDuration(end)}
-  </StyledDiv>
+      <ButtonWrapper>
+        <PlayIcon width="12" />
+        {formatDuration(start)}
+      </ButtonWrapper>
+    </Button>
+  </Wrapper>
 );
 
 export default SpeakerWrapper;
