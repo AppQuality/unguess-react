324d4667bea639ff97204dafa584d32d132f372b
Davide Bizzi <davide.bizzi@unguess.io>
2024-10-08 12:06:03 +0200

    chore: remove aliases from craco config

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,5 @@
-const path = require(`path`);
-
-const alias = () => ({
-  react: `./node_modules/react`,
-  'styled-components': `./node_modules/styled-components`,
-  '@zendeskgarden/react-theming': `./node_modules/@zendeskgarden/react-theming`,
-  '@zendeskgarden/react-forms': `./node_modules/@zendeskgarden/react-forms`,
-});
-
-const SRC = `./src`;
-const aliases = alias(SRC);
-
-const resolvedAliases = Object.fromEntries(
-  Object.entries(aliases).map(([key, value]) => [
-    key,
-    path.resolve(__dirname, value),
-  ])
-);
-
 module.exports = {
   webpack: {
-    // alias: resolvedAliases,
     configure: (webpackConfig) => {
       webpackConfig.module.rules.push({
         test: /\.mjs$/,
