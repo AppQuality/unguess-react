de26b2af562b1523adbad78a3f032332976d9541
Davide Bizzi <davide.bizzi@unguess.io>
2024-10-15 18:05:28 +0200

    rework: Use dotsmenu on status edit

--- src/common/components/CustomStatusDrawer/DotsMenu.tsx
+++ src/common/components/CustomStatusDrawer/DotsMenu.tsx
@@ -1,46 +1,15 @@
 import {
-  Button,
-  Dropdown,
-  DropdownField as Field,
-  Menu,
-  Select,
+  DotsMenu as DotsMenuComponent,
   Span,
 } from '@appquality/unguess-design-system';
 import { ArrayHelpers, FormikProps } from 'formik';
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
-import { ReactComponent as DotsIcon } from 'src/assets/icons/dots-icon.svg';
 import { ReactComponent as TrashIcon } from 'src/assets/icons/trash-stroke.svg';
-import { styled } from 'styled-components';
 import { Circle } from './Circle';
 import { ColorPicker } from './ColorPicker';
 import { CustomStatusFormProps } from './formModel';
-
-const DotsButton = styled(DotsIcon)`
-  cursor: pointer;
-`;
-
-const StyledButton = styled(Button)`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  border-radius: 0;
-`;
-
-const DropdownWrapper = styled.div`
-  width: ${({ theme }) => theme.space.base * 4}px;
-  height: ${({ theme }) => theme.space.base * 4}px;
-  margin: auto 0;
-`;
-
-const StyledMenu = styled(Menu)`
-  overflow: visible;
-`;
-
-const StyledDropdown = styled(Dropdown)`
-  position: relative;
-`;
 
 export const DotsMenu = ({
   arrayHelpers,
@@ -61,43 +30,46 @@ export const DotsMenu = ({
 
   return (
     <>
-      <DropdownWrapper>
-        <StyledDropdown>
-          <Field>
-            <Select isBare>
-              <DotsButton />
-            </Select>
-          </Field>
-          <StyledMenu zIndex={1}>
-            <StyledButton
-              isBasic
-              isStretched
-              onClick={() => {
-                colorPickerRef.current?.click();
-              }}
-            >
-              <Circle color={`#${status.color}`} />
-              <Span style={{ color: appTheme.palette.grey[700] }}>
-                {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_EDIT_COLOR_BUTTON')}
-              </Span>
-            </StyledButton>
-            <StyledButton
-              isBasic
-              onClick={() => {
-                arrayHelpers.remove(field_id);
-              }}
-              style={{ color: appTheme.palette.red[500] }}
-            >
-              <Button.StartIcon>
-                <TrashIcon color={appTheme.palette.red[500]} />
-              </Button.StartIcon>
-              {t(
-                '__BUGS_PAGE_CUSTOM_STATUS_DRAWER_DELETE_CUSTOM_STATUS_BUTTON'
-              )}
-            </StyledButton>
-          </StyledMenu>
-        </StyledDropdown>
-      </DropdownWrapper>
+      <DotsMenuComponent
+        style={{ alignSelf: 'center' }}
+        onSelect={(value) => {
+          if (value === 'remove') {
+            arrayHelpers.remove(field_id);
+            return;
+          }
+          if (value === 'openColorPicker') {
+            colorPickerRef.current?.click();
+          }
+        }}
+      >
+        <DotsMenuComponent.Item
+          value="openColorPicker"
+          icon={
+            <Circle
+              color={`#${status.color}`}
+              style={{ marginRight: appTheme.space.xs }}
+            />
+          }
+        >
+          <Span style={{ color: appTheme.palette.grey[700] }}>
+            {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_EDIT_COLOR_BUTTON')}
+          </Span>
+        </DotsMenuComponent.Item>
+        <DotsMenuComponent.Item
+          value="remove"
+          icon={
+            <TrashIcon
+              color={appTheme.palette.red[500]}
+              style={{ marginRight: appTheme.space.xs }}
+            />
+          }
+        >
+          <span style={{ color: appTheme.palette.red[500] }}>
+            {t('__BUGS_PAGE_CUSTOM_STATUS_DRAWER_DELETE_CUSTOM_STATUS_BUTTON')}
+          </span>
+        </DotsMenuComponent.Item>
+      </DotsMenuComponent>
+
       <ColorPicker
         ref={colorPickerRef}
         onSelect={(color) => {
