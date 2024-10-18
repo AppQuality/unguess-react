e825569b70467afb8855e1aeb26fbbd7c03b5f88
ZecD <dominik.zecoli@gmail.com>
2024-10-18 17:07:01 +0200

    feat: Add translation progress bar label
    - Added a new translation progress bar label in the Italian translation file.
    - Updated the TranslationLoader component to display the translation progress bar label using the translation hook.
    
    Refactor: Remove unnecessary code and fix flex-direction
    
    - Removed an unnecessary console.log statement in the Transcript Header component.
    - Fixed the flex-direction issue in the Transcript Header component.
    
    fix: Fix loading issue in TranslationLoader component
    
    - Fixed a loading issue in the TranslationLoader component.
    
    feat: Change behavior of translation buttons
    
    - Implemented changes to the behavior of translation buttons.
    
    Refactor: Update useTranslationTools hook
    
    - Updated the useTranslationTools hook to handle quick translation and preferred language.
    - Added logic to determine if quick translation is available and if the video is already translated in the preferred language.

--- src/pages/Video/components/tools/hooks/useTranslationTools.tsx
+++ src/pages/Video/components/tools/hooks/useTranslationTools.tsx
@@ -2,43 +2,82 @@ import { useParams } from 'react-router-dom';
 import { FEATURE_FLAG_AI_TRANSLATION } from 'src/constants';
 import { useFeatureFlag } from 'src/hooks/useFeatureFlag';
 import {
+  GetVideosByVidTranslationApiResponse,
   useGetVideosByVidQuery,
   useGetVideosByVidTranslationQuery,
 } from 'src/features/api';
+import { useEffect, useState } from 'react';
 import { usePreferredLanguage } from '../usePreferredLanguage';
 import { useToolsContext } from '../context/ToolsContext';
 
 export const useTranslationTools = () => {
   const { videoId } = useParams();
+  const [data, setData] = useState<{
+    hasQuickTranslate?: boolean;
+    preferredLanguage?: string;
+    translation?: GetVideosByVidTranslationApiResponse;
+    canQuickTranslate?: boolean;
+  }>({
+    hasQuickTranslate: false,
+    canQuickTranslate: true,
+  });
+
   const { hasFeatureFlag } = useFeatureFlag();
   const hasAIFeatureFlag = hasFeatureFlag(FEATURE_FLAG_AI_TRANSLATION);
   const preferredLanguage = usePreferredLanguage();
-  const { isOpen, setIsOpen, language, setLanguage } = useToolsContext();
+  const { language } = useToolsContext();
 
-  const { data: translation, isLoading: isLoadingTranslation } =
-    useGetVideosByVidTranslationQuery(
-      {
-        vid: videoId || '',
-        ...(language && { lang: language }),
-      },
-      {
-        skip: !hasAIFeatureFlag || !language || !preferredLanguage,
-      }
-    );
-  const { data: video } = useGetVideosByVidQuery({
-    vid: videoId || '',
-  });
+  const {
+    data: video,
+    isLoading: isLoadingVideo,
+    isError: isErrorVideo,
+  } = useGetVideosByVidQuery(
+    {
+      vid: videoId || '',
+    },
+    {
+      skip: !videoId,
+    }
+  );
 
-  const canTranslate =
-    !!preferredLanguage &&
-    video &&
-    video.language.localeCompare(preferredLanguage) !== 0 &&
-    (!translation ||
-      translation.language.localeCompare(preferredLanguage) !== 0);
-  const isTranslating =
-    translation &&
-    translation.language === preferredLanguage &&
-    translation.processing === 1;
+  const {
+    data: translation,
+    isLoading: isLoadingTranslation,
+    isError: isErrorTranslation,
+  } = useGetVideosByVidTranslationQuery(
+    {
+      vid: videoId || '',
+      ...(language && { lang: language }),
+    },
+    {
+      skip: !hasAIFeatureFlag || !videoId || !language,
+    }
+  );
 
-  const isProcessing = isLoadingTranslation || translation?.processing === 1;
+  useEffect(() => {
+    if (video) {
+      const hasQuickTranslate =
+        !!preferredLanguage && // Exists a preferred language
+        video.language.localeCompare(preferredLanguage) !== 0; // Video language is different from preferred language
+
+      const isPrefTranslated = !!(
+        preferredLanguage &&
+        translation?.language.localeCompare(preferredLanguage) === 0
+      );
+
+      setData({
+        hasQuickTranslate,
+        preferredLanguage,
+        canQuickTranslate: hasQuickTranslate && !isPrefTranslated,
+        translation,
+      });
+    }
+  }, [video, translation, preferredLanguage]);
+
+  return {
+    isError: !hasAIFeatureFlag || isErrorVideo || isErrorTranslation,
+    isProcessing:
+      isLoadingVideo || isLoadingTranslation || translation?.processing === 1,
+    data,
+  };
 };
