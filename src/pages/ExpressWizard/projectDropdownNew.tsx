76bd60fcc2ea132343ffeae0bf4057559db69f35
Davide Bizzi <davide.bizzi@unguess.io>
2024-10-18 13:02:57 +0200

    feat: Udpdate project dropdown

--- src/pages/ExpressWizard/projectDropdownNew.tsx
+++ src/pages/ExpressWizard/projectDropdownNew.tsx
@@ -6,13 +6,10 @@ import {
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from 'src/app/hooks';
 
-import { useEffect, useState } from 'react';
 import { ReactComponent as FolderIcon } from 'src/assets/icons/folder-icon.svg';
-import { ReactComponent as AddIcon } from 'src/assets/icons/grid-add.svg';
-import { Project, useGetWorkspacesByWidProjectsQuery } from 'src/features/api';
+import { useGetWorkspacesByWidProjectsQuery } from 'src/features/api';
 import { setExpressProject } from 'src/features/express/expressSlice';
 import { useActiveWorkspace } from 'src/hooks/useActiveWorkspace';
-import useDebounce from 'src/hooks/useDebounce';
 
 export const ProjectDropdown = () => {
   const { t } = useTranslation();
@@ -26,51 +23,40 @@ export const ProjectDropdown = () => {
     wid: activeWorkspace?.id.toString() || '',
   });
 
-  const projects = data?.items || [];
+  const projects = data?.items;
 
-  // Get projects
-  const placeholder: Project = {
-    id: 0,
-    name: t('__WIZARD_EXPRESS_DEFAULT_ITEM'),
-    campaigns_count: 0,
-    workspaceId: 0,
-  };
-
-  const [selectedItem, setSelectedItem] = useState<Project>();
-  const [inputValue, setInputValue] = useState('');
-  const [matchingOptions, setMatchingOptions] = useState(projects);
-
-  const debouncedInputValue = useDebounce<string>(inputValue, 300);
-
-  const filterMatchingOptions = (value: string) => {
-    const matchedOptions = projects.filter(
-      (prj) =>
-        prj.name.trim().toLowerCase().indexOf(value.trim().toLowerCase()) !== -1
-    );
-
-    setMatchingOptions(matchedOptions);
-  };
-
-  useEffect(() => {
-    filterMatchingOptions(debouncedInputValue);
-    if (project && project.id) {
-      const selectedProject = projects.find((p) => p.id === project.id);
-      if (selectedProject) {
-        setSelectedItem(selectedProject);
-      }
-    } else {
-      setSelectedItem(undefined);
-    }
-  }, [debouncedInputValue, project, projects]);
+  if (!projects) return null;
 
   return isLoading || isFetching ? (
     <Skeleton height="32px" width="100%" />
   ) : (
     <Field>
       <Autocomplete
+        isCreatable
         startIcon={<FolderIcon />}
-        {...(projectLocked && project && selectedItem && { disabled: true })}
-        options={[]}
+        {...(projectLocked && project && { isDisabled: true })}
+        onCreateNewOption={async (value) => {
+          const v = { name: value, id: -1, campaigns_count: 0 };
+          dispatch(setExpressProject(v));
+          return {
+            id: '-1',
+            label: value,
+            value: v,
+          };
+        }}
+        onOptionClick={({ selectionValue, inputValue }) => {
+          if (!inputValue || !selectionValue) return;
+          const value = projects.find(
+            (prj) => prj.id.toString() === selectionValue
+          );
+          dispatch(setExpressProject(value));
+        }}
+        options={(projects || []).map((prj) => ({
+          id: prj.id.toString(),
+          value: prj.id.toString(),
+          label: prj.name,
+        }))}
+        placeholder={t('__WIZARD_EXPRESS_DEFAULT_ITEM')}
       />
     </Field>
   );
