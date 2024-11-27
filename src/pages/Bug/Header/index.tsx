3229bb8fc3b8df6dc96c4a0cce823ddfd6d01f62
Iacopo Leardini <iacopo.lea@gmail.com>
2024-11-27 14:53:32 +0100

    Feat(Bug page header): split Header component into separate files and show params

--- src/pages/Bug/Header/index.tsx
+++ src/pages/Bug/Header/index.tsx
@@ -1,93 +1,45 @@
 import { useAppDispatch } from 'src/app/hooks';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import {
-  Anchor,
   Button,
+  XL,
   PageHeader,
   Skeleton,
+  Tooltip,
 } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import {
-  GetCampaignsByCidApiResponse,
   GetCampaignsByCidBugsAndBidApiResponse,
   useGetCampaignsByCidQuery,
-  useGetProjectsByPidQuery,
 } from 'src/features/api';
 import { ReactComponent as ShareIcon } from 'src/assets/icons/share-stroke.svg';
-import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
 import { ShareButton } from 'src/common/components/BugDetail/ShareBug';
 import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
 import { styled } from 'styled-components';
 import {
   setCampaignId,
   setPermissionSettingsTitle,
-} from '../../features/navigation/navigationSlice';
+} from 'src/features/navigation/navigationSlice';
+import { BreadCrumbs } from './Breadcrumb';
 
 interface Props {
   campaignId: string;
   bug: Exclude<GetCampaignsByCidBugsAndBidApiResponse, undefined>;
 }
 
-const ActionContainer = styled.div`
-  display: flex;
-  align-items: end;
-  width: 100%;
-  justify-content: space-between;
-`;
-
-const BreadCrumbs = ({
-  campaign,
-  children,
-}: {
-  campaign: GetCampaignsByCidApiResponse;
-  children?: React.ReactNode;
-}) => {
-  const { t } = useTranslation();
-
-  const projectRoute = useLocalizeRoute(`projects/${campaign.project.id}`);
-  const campaignRoute = useLocalizeRoute(`campaigns/${campaign.id}`);
-  const bugsRoute = useLocalizeRoute(`campaigns/${campaign.id}/bugs`);
-
-  const {
-    currentData: project,
-    isLoading,
-    isFetching,
-  } = useGetProjectsByPidQuery({
-    pid: campaign.project.id.toString(),
-  });
-
-  if (isLoading || isFetching) {
-    return <Skeleton width="200px" height="12px" />;
-  }
-
-  return (
-    <ActionContainer>
-      <PageHeader.Breadcrumbs>
-        {project ? (
-          <Link to={projectRoute}>
-            <Anchor id="breadcrumb-parent">{project.name}</Anchor>
-          </Link>
-        ) : (
-          campaign.project.name
-        )}
-        <Link to={campaignRoute}>
-          <Anchor>{campaign.customer_title}</Anchor>
-        </Link>
-        <Link to={bugsRoute}>
-          <Anchor>{t('__PAGE_TITLE_BUGS_COLLECTION')}</Anchor>
-        </Link>
-      </PageHeader.Breadcrumbs>
-      {children}
-    </ActionContainer>
-  );
-};
-
 const StyledContainer = styled(LayoutWrapper)`
   border-bottom: 1px solid ${({ theme }) => theme.palette.grey[200]};
 `;
 
-export const Header = ({ campaignId, bug }: Props) => {
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Header = ({ campaignId, bug }: Props) => {
   const dispatch = useAppDispatch();
   const {
     isLoading: isCampaignLoading,
@@ -98,6 +50,14 @@ export const Header = ({ campaignId, bug }: Props) => {
     cid: campaignId,
   });
   const { t } = useTranslation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const order = useMemo(() => searchParams.get('order'), [searchParams]);
+  const orderBy = useMemo(() => searchParams.get('orderBy'), [searchParams]);
+  const filterBy = useMemo(
+    () => searchParams.getAll('filterBy'),
+    [searchParams]
+  );
 
   useEffect(() => {
     if (campaign) {
@@ -111,6 +71,13 @@ export const Header = ({ campaignId, bug }: Props) => {
     };
   }, [campaign]);
 
+  const handlePagination = useCallback(() => {
+    console.log(
+      'searchParams',
+      searchParams.forEach((value, key) => {})
+    );
+  }, [searchParams]);
+
   if (isCampaignLoading || isCampaignFetching || isCampaignError || !campaign) {
     return (
       <LayoutWrapper>
@@ -120,11 +87,25 @@ export const Header = ({ campaignId, bug }: Props) => {
       </LayoutWrapper>
     );
   }
-
   return (
     <StyledContainer isNotBoxed>
       <PageHeader style={{ border: 'none' }}>
-        <BreadCrumbs campaign={campaign}>
+        <BreadCrumbs campaign={campaign} />
+        <Wrapper>
+          <XL>BUGID: {bug.id}</XL>
+          <span>
+            orderBy: {orderBy} {order}
+          </span>
+          <span>
+            applied filters: {filterBy.length}{' '}
+            <Tooltip
+              content={
+                <span>{filterBy.map((filter) => filter).join(', ')}</span>
+              }
+            >
+              <span>(i)</span>
+            </Tooltip>
+          </span>
           <ShareButton bug={bug}>
             {(setModalOpen) => (
               <Button onClick={() => setModalOpen(true)}>
@@ -135,8 +116,10 @@ export const Header = ({ campaignId, bug }: Props) => {
               </Button>
             )}
           </ShareButton>
-        </BreadCrumbs>
+        </Wrapper>
       </PageHeader>
     </StyledContainer>
   );
 };
+
+export default Header;
