de2150fe84bbac46d1d0f85f755df1bb8e9e8f7d
Kariamos <minucci9292@gmail.com>
2024-11-29 14:28:31 +0100

    feat(bug-chat-shortcut): refactor BugShortcutHelper to use Chat.Shortcuts for improved shortcut display

--- src/pages/Bug/BugShortcutHelper.tsx
+++ src/pages/Bug/BugShortcutHelper.tsx
@@ -1,16 +1,8 @@
-import {
-  MD,
-  Modal,
-  ModalClose,
-  ShortcutTag,
-} from '@appquality/unguess-design-system';
-import React from 'react';
+import { Chat, MD, Modal, ModalClose } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as ArrowLeft } from 'src/assets/icons/ai-icon.svg';
 import { ReactComponent as DoubleCheck } from 'src/assets/icons/check-double-stroke.svg';
-import { ReactComponent as MentionIcon } from 'src/assets/icons/at-stroke.svg';
 import { ReactComponent as UserGroupIcon } from 'src/assets/icons/user-group-stroke.svg';
-import { ReactComponent as EnterStroke } from 'src/assets/icons/enter-stroke.svg';
 import { ReactComponent as BoldIcon } from 'src/assets/icons/bold-stroke.svg';
 import { ReactComponent as ItalicIcon } from 'src/assets/icons/italic-stroke.svg';
 
@@ -41,34 +33,6 @@ const ShortcutItemWrapper = styled.div`
   }
 `;
 
-const ShortcutWrapper = styled.div`
-  display: flex;
-  gap: ${({ theme }) => theme.space.xs};
-`;
-
-const ShortcutItem = ({
-  icon,
-  shortcut,
-  children,
-}: {
-  icon: React.ReactNode;
-  shortcut: (React.ReactNode | 'ctrl')[];
-  children: React.ReactNode;
-}) => (
-  <ShortcutItemWrapper>
-    <div className="description">
-      {icon}
-      {children}:
-    </div>
-    <ShortcutWrapper>
-      {shortcut.map((key) => {
-        if (key === 'ctrl') return <ShortcutTag ctrl />;
-        return <ShortcutTag text={key} />;
-      })}
-    </ShortcutWrapper>
-  </ShortcutItemWrapper>
-);
-
 export const BugShortcutHelper = ({ onClose }: { onClose: () => void }) => {
   const { t } = useTranslation();
   return (
@@ -91,21 +55,26 @@ export const BugShortcutHelper = ({ onClose }: { onClose: () => void }) => {
             )}
           </MD>
           <div>
-            <ShortcutItem icon={<BoldIcon />} shortcut={['ctrl', 'B']}>
-              {t('__BUGS_PAGE_SHORTCUT_BOLD_TEXT', 'Bold Text')}
-            </ShortcutItem>
-            <ShortcutItem icon={<ItalicIcon />} shortcut={['ctrl', 'I']}>
-              {t('__BUGS_PAGE_SHORTCUT_OBLIQUE_TEXT', 'Oblique Text')}
-            </ShortcutItem>
-            <ShortcutItem icon={<UserGroupIcon />} shortcut={[<MentionIcon />]}>
-              {t('__BUGS_PAGE_SHORTCUT_MENTION', 'Mention')}
-            </ShortcutItem>
-            <ShortcutItem
-              icon={<DoubleCheck />}
-              shortcut={['ctrl', <EnterStroke />]}
-            >
-              {t('__BUGS_PAGE_SHORTCUT_SEND_TEXT', 'Send')}
-            </ShortcutItem>
+            <ShortcutItemWrapper>
+              <Chat.Shortcut icon={<BoldIcon />} type="bold">
+                {t('__BUGS_PAGE_SHORTCUT_BOLD_TEXT', 'Bold Text')}
+              </Chat.Shortcut>
+            </ShortcutItemWrapper>
+            <ShortcutItemWrapper>
+              <Chat.Shortcut icon={<ItalicIcon />} type="italic">
+                {t('__BUGS_PAGE_SHORTCUT_OBLIQUE_TEXT', 'Oblique Text')}
+              </Chat.Shortcut>
+            </ShortcutItemWrapper>
+            <ShortcutItemWrapper>
+              <Chat.Shortcut icon={<UserGroupIcon />} type="mention">
+                {t('__BUGS_PAGE_SHORTCUT_MENTION', 'Mention')}
+              </Chat.Shortcut>
+            </ShortcutItemWrapper>
+            <ShortcutItemWrapper>
+              <Chat.Shortcut icon={<DoubleCheck />} type="send">
+                {t('__BUGS_PAGE_SHORTCUT_SEND_TEXT', 'Send')}
+              </Chat.Shortcut>
+            </ShortcutItemWrapper>
           </div>
         </ContentWrapper>
       </Modal.Body>
