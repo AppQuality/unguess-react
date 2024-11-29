df03ad4a391303a90ae323013b68ce654c2ac3b9
Davide Bizzi <davide.bizzi@unguess.io>
2024-11-29 10:29:07 +0100

    fix: update unguess-design-system to version 4.0.13 and refactor ShortcutHelper to use PlayerProvider for shortcuts

--- src/pages/Video/components/PageHeader/ShortcutHelper.tsx
+++ src/pages/Video/components/PageHeader/ShortcutHelper.tsx
@@ -3,12 +3,10 @@ import {
   MD,
   Modal,
   ModalClose,
-  ShortcutTag,
+  PlayerProvider,
 } from '@appquality/unguess-design-system';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ReactComponent as ArrowLeft } from 'src/assets/icons/arrow-left.svg';
-import { ReactComponent as ArrowRight } from 'src/assets/icons/arrow-right.svg';
 import { ReactComponent as ForwardIcon } from 'src/assets/icons/forward.svg';
 import { ReactComponent as KeyboardIcon } from 'src/assets/icons/keyboard.svg';
 import { ReactComponent as MuteIcon } from 'src/assets/icons/mute.svg';
@@ -30,46 +28,10 @@ const ContentWrapper = styled.div`
 `;
 
 const ShortcutItemWrapper = styled.div`
-  display: flex;
   margin: ${({ theme }) => theme.space.sm} 0;
   padding: ${({ theme }) => theme.space.xxs} 0;
   border-bottom: 1px solid ${({ theme }) => theme.palette.grey[300]};
-  justify-content: space-between;
-
-  .description {
-    display: flex;
-    gap: ${({ theme }) => theme.space.xs};
-    align-items: center;
-  }
 `;
-
-const ShortcutWrapper = styled.div`
-  display: flex;
-  gap: ${({ theme }) => theme.space.xs};
-`;
-
-const ShortcutItem = ({
-  icon,
-  shortcut,
-  children,
-}: {
-  icon: React.ReactNode;
-  shortcut: (React.ReactNode | 'ctrl')[];
-  children: React.ReactNode;
-}) => (
-  <ShortcutItemWrapper>
-    <div className="description">
-      {icon}
-      {children}:
-    </div>
-    <ShortcutWrapper>
-      {shortcut.map((key) => {
-        if (key === 'ctrl') return <ShortcutTag ctrl />;
-        return <ShortcutTag text={key} />;
-      })}
-    </ShortcutWrapper>
-  </ShortcutItemWrapper>
-);
 
 const ShortcutHelper = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -88,24 +50,47 @@ const ShortcutHelper = () => {
             <ContentWrapper>
               <MD isBold>{t('UX_SHORTCUT_MODAL_CONTENT_TITLE')}</MD>
               <div>
-                <ShortcutItem icon={<PlayIcon />} shortcut={['Space']}>
-                  {t('UX_SHORTCUT_PLAY_PAUSE')}
-                </ShortcutItem>
-                <ShortcutItem icon={<MuteIcon />} shortcut={['M']}>
-                  {t('UX_SHORTCUT_MUTE')}
-                </ShortcutItem>
-                <ShortcutItem
-                  icon={<ForwardIcon />}
-                  shortcut={[<ArrowRight />]}
-                >
-                  {t('UX_SHORTCUT_FORWARD')}
-                </ShortcutItem>
-                <ShortcutItem icon={<RewindIcon />} shortcut={[<ArrowLeft />]}>
-                  {t('UX_SHORTCUT_REWIND')}
-                </ShortcutItem>
-                <ShortcutItem icon={<TagIcon />} shortcut={['S']}>
-                  {t('UX_SHORTCUT_ADD_OBSERVATION')}
-                </ShortcutItem>
+                <ShortcutItemWrapper>
+                  <PlayerProvider.Shortcut
+                    icon={<PlayIcon />}
+                    type="play/pause"
+                  >
+                    {t('UX_SHORTCUT_PLAY_PAUSE')}
+                  </PlayerProvider.Shortcut>
+                </ShortcutItemWrapper>
+
+                <ShortcutItemWrapper>
+                  <PlayerProvider.Shortcut icon={<MuteIcon />} type="mute">
+                    {t('UX_SHORTCUT_MUTE')}
+                  </PlayerProvider.Shortcut>
+                </ShortcutItemWrapper>
+
+                <ShortcutItemWrapper>
+                  <PlayerProvider.Shortcut
+                    icon={<ForwardIcon />}
+                    type="forward"
+                  >
+                    {t('UX_SHORTCUT_FORWARD')}
+                  </PlayerProvider.Shortcut>
+                </ShortcutItemWrapper>
+
+                <ShortcutItemWrapper>
+                  <PlayerProvider.Shortcut
+                    icon={<RewindIcon />}
+                    type="backward"
+                  >
+                    {t('UX_SHORTCUT_REWIND')}
+                  </PlayerProvider.Shortcut>
+                </ShortcutItemWrapper>
+
+                <ShortcutItemWrapper>
+                  <PlayerProvider.Shortcut
+                    icon={<TagIcon />}
+                    type="observation"
+                  >
+                    {t('UX_SHORTCUT_ADD_OBSERVATION')}
+                  </PlayerProvider.Shortcut>
+                </ShortcutItemWrapper>
               </div>
             </ContentWrapper>
           </Modal.Body>
