7e434948ce5162d42ea0a613b03cfabf09af8e5d
Kariamos <minucci9292@gmail.com>
2024-12-03 16:48:54 +0100

    refactor(bug-shortcut): streamline ShortcutItemWrapper usage and enhance layout in BugShortcutHelper

--- src/pages/Bug/BugShortcutHelper.tsx
+++ src/pages/Bug/BugShortcutHelper.tsx
@@ -1,5 +1,6 @@
 import { Chat, MD, Modal, ModalClose } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
+import { appTheme } from 'src/app/theme';
 import { ReactComponent as BoldIcon } from 'src/assets/icons/bold-stroke.svg';
 import { ReactComponent as DoubleCheck } from 'src/assets/icons/check-double-stroke.svg';
 import { ReactComponent as ItalicIcon } from 'src/assets/icons/italic-stroke.svg';
@@ -14,15 +15,9 @@ const TitleWrapper = styled.div`
   gap: ${({ theme }) => theme.space.xs};
 `;
 
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.space.sm};
-`;
-
 const ShortcutItemWrapper = styled.div`
-  padding: ${({ theme }) => theme.space.sm} 0;
-  margin: ${({ theme }) => theme.space.xxs} 0;
+  padding-bottom: ${({ theme }) => theme.space.sm};
+  margin-top: ${({ theme }) => theme.space.sm};
   border-bottom: 1px solid ${({ theme }) => theme.palette.grey[300]};
 `;
 
@@ -40,36 +35,32 @@ export const BugShortcutHelper = ({ onClose }: { onClose: () => void }) => {
         <ModalClose onClick={onClose} />
       </Modal.Header>
       <Modal.Body>
-        <ContentWrapper>
-          <MD isBold>
-            {t(
-              '__BUGS_PAGE_SHORTCUT_MODAL_CONTENT_TITLE',
-              'Essential Keyboard shortcut for Messages'
-            )}
-          </MD>
-          <div>
-            <ShortcutItemWrapper>
-              <Chat.Shortcut icon={<BoldIcon />} type="bold">
-                {t('__BUGS_PAGE_SHORTCUT_BOLD_TEXT', 'Bold Text')}
-              </Chat.Shortcut>
-            </ShortcutItemWrapper>
-            <ShortcutItemWrapper>
-              <Chat.Shortcut icon={<ItalicIcon />} type="italic">
-                {t('__BUGS_PAGE_SHORTCUT_OBLIQUE_TEXT', 'Oblique Text')}
-              </Chat.Shortcut>
-            </ShortcutItemWrapper>
-            <ShortcutItemWrapper>
-              <Chat.Shortcut icon={<UserGroupIcon />} type="mention">
-                {t('__BUGS_PAGE_SHORTCUT_MENTION', 'Mention')}
-              </Chat.Shortcut>
-            </ShortcutItemWrapper>
-            <ShortcutItemWrapper>
-              <Chat.Shortcut icon={<DoubleCheck />} type="send">
-                {t('__BUGS_PAGE_SHORTCUT_SEND_TEXT', 'Send')}
-              </Chat.Shortcut>
-            </ShortcutItemWrapper>
-          </div>
-        </ContentWrapper>
+        <MD isBold style={{ marginBottom: appTheme.space.sm }}>
+          {t(
+            '__BUGS_PAGE_SHORTCUT_MODAL_CONTENT_TITLE',
+            'Essential Keyboard shortcut for Messages'
+          )}
+        </MD>
+        <ShortcutItemWrapper>
+          <Chat.Shortcut icon={<BoldIcon />} type="bold">
+            {t('__BUGS_PAGE_SHORTCUT_BOLD_TEXT', 'Bold Text')}
+          </Chat.Shortcut>
+        </ShortcutItemWrapper>
+        <ShortcutItemWrapper>
+          <Chat.Shortcut icon={<ItalicIcon />} type="italic">
+            {t('__BUGS_PAGE_SHORTCUT_OBLIQUE_TEXT', 'Oblique Text')}
+          </Chat.Shortcut>
+        </ShortcutItemWrapper>
+        <ShortcutItemWrapper>
+          <Chat.Shortcut icon={<UserGroupIcon />} type="mention">
+            {t('__BUGS_PAGE_SHORTCUT_MENTION', 'Mention')}
+          </Chat.Shortcut>
+        </ShortcutItemWrapper>
+        <ShortcutItemWrapper>
+          <Chat.Shortcut icon={<DoubleCheck />} type="send">
+            {t('__BUGS_PAGE_SHORTCUT_SEND_TEXT', 'Send')}
+          </Chat.Shortcut>
+        </ShortcutItemWrapper>
       </Modal.Body>
     </Modal>
   );
