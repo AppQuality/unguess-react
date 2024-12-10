c88d3356634311cc49cd9a7b2a63c40cc6d731fc
Marco Bonomo <marco.bonomo@unguess.io>
2024-12-10 15:56:08 +0100

    Add move campaign modal basic structure

---
+++ src/pages/Campaign/MoveCampaignModal.tsx
@@ -0,0 +1,116 @@
+import {
+  Button,
+  Col,
+  Grid,
+  Label,
+  MD,
+  Modal,
+  ModalClose,
+  Row,
+  Select,
+  Skeleton,
+} from '@appquality/unguess-design-system';
+import { Trans, useTranslation } from 'react-i18next';
+import { appTheme } from 'src/app/theme';
+import { useGetWorkspacesByWidProjectsQuery } from 'src/features/api';
+import { useActiveWorkspace } from 'src/hooks/useActiveWorkspace';
+import { ReactComponent as RightArrow } from 'src/assets/icons/arrow-right.svg';
+import { useState } from 'react';
+
+const MoveCampaignModal = ({
+  cpTitle,
+  onClose,
+}: {
+  cpTitle: string;
+  onClose: () => void;
+}) => {
+  const { t } = useTranslation();
+  const { activeWorkspace } = useActiveWorkspace();
+  const [selectedProjectId, setSelectedProjectId] = useState<number>();
+
+  const { data, isLoading, isFetching } = useGetWorkspacesByWidProjectsQuery({
+    wid: activeWorkspace?.id.toString() || '',
+  });
+
+  const projects = data?.items;
+
+  return (
+    <Modal onClose={onClose}>
+      <Modal.Header isDanger>
+        {t('__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_TITLE')}
+      </Modal.Header>
+      <Modal.Body>
+        <MD style={{ marginBottom: appTheme.space.sm }}>
+          <Trans i18nKey="__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_DESCRIPTION">
+            The {{ name: cpTitle }} campaign will be removed from the current
+            project.
+          </Trans>
+        </MD>
+        <Grid style={{ margin: `${appTheme.space.md} auto` }}>
+          <Row>
+            <Col size={5} style={{ margin: 0 }}>
+              <Label>
+                {t('__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_LABEL_PROJECT')}
+              </Label>
+              <MD style={{ marginTop: appTheme.space.xs }}>{cpTitle}</MD>
+            </Col>
+            <Col
+              size={2}
+              alignSelf="center"
+              textAlign="center"
+              style={{ margin: 0 }}
+            >
+              <MD>&nbsp;</MD>
+              <RightArrow style={{ marginTop: appTheme.space.xs }} />
+            </Col>
+            <Col size={5} style={{ margin: 0 }}>
+              {!projects || isLoading || isFetching ? (
+                <Skeleton />
+              ) : (
+                <Select
+                  isCompact
+                  label={t('__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_SELECT_LABEL')}
+                  placeholder={t(
+                    '__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_SELECT_PLACEHOLDER'
+                  )}
+                  onSelect={(prjId) => {
+                    setSelectedProjectId(parseInt(prjId, 10));
+                  }}
+                >
+                  {projects.map((item) => (
+                    <Select.Option
+                      key={item.id}
+                      value={item.id.toString()}
+                      label={item.name}
+                      className={`campaign-page-move-campaign-modal-select-option-project-${item.id}`}
+                    >
+                      {item.name}
+                    </Select.Option>
+                  ))}
+                </Select>
+              )}
+            </Col>
+          </Row>
+        </Grid>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button isBasic isDanger onClick={onClose}>
+          {t('__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_BUTTON_CANCEL')}
+        </Button>
+        <Button
+          isPrimary
+          isAccent
+          onClick={() => {
+            // eslint-disable-next-line no-alert
+            alert(`Move campaign action with project id ${selectedProjectId}`);
+          }}
+        >
+          {t('__CAMPAIGN_PAGE_MOVE_CAMPAIGN_MODAL_BUTTON_CONFIRM')}
+        </Button>
+      </Modal.Footer>
+      <ModalClose />
+    </Modal>
+  );
+};
+
+export { MoveCampaignModal };
--- src/pages/Campaign/pageHeader/Meta/index.tsx
+++ src/pages/Campaign/pageHeader/Meta/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import {
   Button,
+  DotsMenu,
   IconButton,
   MD,
   Skeleton,
@@ -25,11 +26,13 @@ import { ReactComponent as InsightsIcon } from '@zendeskgarden/svg-icons/src/16/
 import { CampaignStatus } from 'src/types';
 import { appTheme } from 'src/app/theme';
 import styled from 'styled-components';
+import { ReactComponent as MoveIcon } from 'src/assets/icons/move-icon.svg';
 import { CampaignDurationMeta } from './CampaignDurationMeta';
 import { DesktopMeta } from './DesktopMeta';
 import { SmartphoneMeta } from './SmartphoneMeta';
 import { TabletMeta } from './TabletMeta';
 import { TvMeta } from './TvMeta';
+import { MoveCampaignModal } from '../../MoveCampaignModal';
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -70,15 +73,32 @@ const FooterContainer = styled.div`
 `;
 
 export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
+  const {
+    start_date,
+    end_date,
+    type,
+    status,
+    outputs,
+    family,
+    customer_title,
+  } = campaign;
+  const { t } = useTranslation();
   const [totalVideos, setTotalVideos] = useState<number>(0);
+  const [isMoveModalOpen, setIsMoveModalOpen] = useState<boolean>(false);
+
+  const functionalDashboardRoute = useLocalizeRoute(
+    `campaigns/${campaign.id}/bugs`
+  );
+  const videoDashboardRoute = useLocalizeRoute(
+    `campaigns/${campaign.id}/videos`
+  );
+  const insightsRoute = useLocalizeRoute(`campaigns/${campaign.id}/insights`);
+
   const {
     data: meta,
     isLoading,
     isFetching,
   } = useGetCampaignsByCidMetaQuery({ cid: campaign.id.toString() });
-
-  const { start_date, end_date, type, status, outputs, family } = campaign;
-  const { t } = useTranslation();
   const {
     sorted: videos,
     isLoading: isLoadingVideos,
@@ -86,14 +106,6 @@ export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
   } = useVideos(campaign.id.toString() ?? '');
   const { hasFeatureFlag } = useFeatureFlag();
   const hasTaggingToolFeature = hasFeatureFlag(FEATURE_FLAG_TAGGING_TOOL);
-
-  const functionalDashboardLink = useLocalizeRoute(
-    `campaigns/${campaign.id}/bugs`
-  );
-  const videoDashboardLink = useLocalizeRoute(
-    `campaigns/${campaign.id}/videos`
-  );
-  const insightsRoute = useLocalizeRoute(`campaigns/${campaign.id}/insights`);
 
   useEffect(() => {
     if (videos) {
@@ -109,65 +121,91 @@ export const Metas = ({ campaign }: { campaign: CampaignWithOutput }) => {
     return <Skeleton width="500px" height="20px" />;
 
   return (
-    <FooterContainer>
-      <PageMeta>
-        <StatusMeta status={family.name.toLowerCase() as CampaignStatus}>
-          {type.name}
-        </StatusMeta>
-        <StatusMeta status={status.name as CampaignStatus} />
-        <CampaignDurationMeta start={start_date} end={end_date} />
-        {meta ? (
-          <>
-            <Pipe />
-            {meta.allowed_devices.includes('desktop') && <DesktopMeta />}
-            {meta.allowed_devices.includes('smartphone') && <SmartphoneMeta />}
-            {meta.allowed_devices.includes('tablet') && <TabletMeta />}
-            {meta.allowed_devices.includes('tv') && <TvMeta />}
-          </>
-        ) : null}
-      </PageMeta>
-      <ButtonWrapper>
-        <CampaignSettings />
-        {outputs?.includes('bugs') && (
-          <Link to={functionalDashboardLink}>
-            <Button id="button-bugs-list-header" isPrimary isAccent>
-              {t('__CAMPAIGN_PAGE_BUTTON_DETAIL_BUG')}
-            </Button>
-          </Link>
-        )}
-        {hasTaggingToolFeature && totalVideos > 0 && (
-          <>
-            <MD color={appTheme.palette.blue[600]}>
-              {' '}
-              {t('__INSIGHTS_PAGE_NAVIGATION_LABEL')}
-            </MD>
-            <Link to={videoDashboardLink}>
-              <Tooltip
-                content={t('__UX_CAMPAIGN_PAGE_NAVIGATION_VIDEO_LIST_TOOLTIP')}
-                size="medium"
-                type="light"
-                placement="top"
-              >
-                <IconButton isBasic={false}>
-                  <VideoListIcon />
-                </IconButton>
-              </Tooltip>
+    <>
+      <FooterContainer>
+        <PageMeta>
+          <StatusMeta status={family.name.toLowerCase() as CampaignStatus}>
+            {type.name}
+          </StatusMeta>
+          <StatusMeta status={status.name as CampaignStatus} />
+          <CampaignDurationMeta start={start_date} end={end_date} />
+          {meta ? (
+            <>
+              <Pipe />
+              {meta.allowed_devices.includes('desktop') && <DesktopMeta />}
+              {meta.allowed_devices.includes('smartphone') && (
+                <SmartphoneMeta />
+              )}
+              {meta.allowed_devices.includes('tablet') && <TabletMeta />}
+              {meta.allowed_devices.includes('tv') && <TvMeta />}
+            </>
+          ) : null}
+        </PageMeta>
+        <ButtonWrapper>
+          <CampaignSettings />
+          {outputs?.includes('bugs') && (
+            <Link to={functionalDashboardRoute}>
+              <Button id="button-bugs-list-header" isPrimary isAccent>
+                {t('__CAMPAIGN_PAGE_BUTTON_DETAIL_BUG')}
+              </Button>
             </Link>
-            <Link to={insightsRoute}>
-              <Tooltip
-                content={t('__UX_CAMPAIGN_PAGE_NAVIGATION_INSIGHTS_TOOLTIP')}
-                size="medium"
-                type="light"
-                placement="top"
-              >
-                <IconButton isBasic={false}>
-                  <InsightsIcon />
-                </IconButton>
-              </Tooltip>
-            </Link>
-          </>
-        )}
-      </ButtonWrapper>
-    </FooterContainer>
+          )}
+          {hasTaggingToolFeature && totalVideos > 0 && (
+            <>
+              <MD color={appTheme.palette.blue[600]}>
+                {' '}
+                {t('__INSIGHTS_PAGE_NAVIGATION_LABEL')}
+              </MD>
+              <Link to={videoDashboardRoute}>
+                <Tooltip
+                  content={t(
+                    '__UX_CAMPAIGN_PAGE_NAVIGATION_VIDEO_LIST_TOOLTIP'
+                  )}
+                  size="medium"
+                  type="light"
+                  placement="top"
+                >
+                  <IconButton isBasic={false}>
+                    <VideoListIcon />
+                  </IconButton>
+                </Tooltip>
+              </Link>
+              <Link to={insightsRoute}>
+                <Tooltip
+                  content={t('__UX_CAMPAIGN_PAGE_NAVIGATION_INSIGHTS_TOOLTIP')}
+                  size="medium"
+                  type="light"
+                  placement="top"
+                >
+                  <IconButton isBasic={false}>
+                    <InsightsIcon />
+                  </IconButton>
+                </Tooltip>
+              </Link>
+              <DotsMenu style={{ zIndex: appTheme.levels.front }}>
+                <Button
+                  isBasic
+                  isPill={false}
+                  onClick={() => {
+                    setIsMoveModalOpen(true);
+                  }}
+                >
+                  <Button.StartIcon>
+                    <MoveIcon />
+                  </Button.StartIcon>
+                  {t('__CAMPAIGN_PAGE_DOTS_MENU_MOVE_CAMPAIGN_BUTTON')}
+                </Button>
+              </DotsMenu>
+            </>
+          )}
+        </ButtonWrapper>
+      </FooterContainer>
+      {isMoveModalOpen && (
+        <MoveCampaignModal
+          cpTitle={customer_title}
+          onClose={() => setIsMoveModalOpen(false)}
+        />
+      )}
+    </>
   );
 };
