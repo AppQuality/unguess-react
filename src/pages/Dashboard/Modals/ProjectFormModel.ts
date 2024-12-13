38a3d26293c83172071bbd7d87935b92d222dcb3
iDome89 <dominik.zecoli@gmail.com>
2024-12-13 12:50:07 +0100

    feat: update project form description character limit to 234 characters

--- src/pages/Dashboard/Modals/ProjectFormModel.ts
+++ src/pages/Dashboard/Modals/ProjectFormModel.ts
@@ -11,5 +11,5 @@ export const validationProjectSchema = Yup.object().shape({
   name: Yup.string()
     .required(t('__PROJECT_FORM_NAME_REQUIRED'))
     .max(64, t('__PROJECT_FORM_NAME_MAX')),
-  description: Yup.string().max(299, t('__PROJECT_FORM_DESCRIPTION_MAX')),
+  description: Yup.string().max(234, t('__PROJECT_FORM_DESCRIPTION_MAX')),
 });
