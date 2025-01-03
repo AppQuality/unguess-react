77c2fde8b1ae3e5ea30fe9ab6a822a9e2916c2b3
Iacopo Leardini <iacopo.lea@gmail.com>
2025-01-03 15:30:14 +0100

    feat(BugHeader): integrate GTM event tracking for user actions in header components

--- src/pages/Bug/Header/StatusSelect.tsx
+++ src/pages/Bug/Header/StatusSelect.tsx
@@ -4,11 +4,13 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { SelectedItem } from 'src/common/components/BugDetail/BugStateSelect';
 import { getCustomStatusInfo } from 'src/common/components/utils/getCustomStatusInfo';
+import { useSendGTMevent } from 'src/hooks/useGTMevent';
 import { BugByStateType } from 'src/pages/Bugs/Content/BugsTable/types';
 
 export const StatusSelect = ({ statuses }: { statuses: BugByStateType[] }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const sendGTMEvent = useSendGTMevent();
   const { campaignId } = useParams();
   const [searchParams] = useSearchParams();
   const renderOptions = useMemo(
@@ -56,6 +58,13 @@ export const StatusSelect = ({ statuses }: { statuses: BugByStateType[] }) => {
           const target = statuses.find((u) => u.state.id === Number(stateId))
             ?.bugs[0].id;
           searchParams.set('groupByValue', stateId);
+
+          sendGTMEvent({
+            event: 'bug_header_action',
+            action: 'change_status',
+            target: target?.toString() || 'target_not_found',
+          });
+
           if (target) {
             navigate(
               `/campaigns/${campaignId}/bugs/${target}?${searchParams.toString()}`
