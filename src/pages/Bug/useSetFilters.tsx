4d247338e035fdc071c66df110c71fe0b1c3b6f4
Iacopo Leardini <iacopo.lea@gmail.com>
2025-01-13 12:29:50 +0100

    fix(BugFilters): refactor useSetFilters to utilize getFiltersFromParams and streamline filter mapping

--- src/pages/Bug/useSetFilters.tsx
+++ src/pages/Bug/useSetFilters.tsx
@@ -10,6 +10,7 @@ import {
   updateFilters,
 } from 'src/features/bugsPage/bugsPageSlice';
 import { useCampaign } from '../Bugs/useCampaign';
+import { getFiltersFromParams } from './Header/getFiltersFromParams';
 
 const useSetFilters = ({ campaignId }: { campaignId: string }) => {
   const [searchParams] = useSearchParams();
@@ -35,27 +36,75 @@ const useSetFilters = ({ campaignId }: { campaignId: string }) => {
   }, [campaign]);
 
   useEffect(() => {
-    if (currentCampaign) {
-      const usecaseFiltersIds = searchParams.getAll('useCases');
-      const customStatusFiltersIds = searchParams.getAll('customStatuses');
+    if (currentCampaign && searchParams && campaign) {
+      const filtersFromParams = getFiltersFromParams(searchParams);
+      const availableFilters = campaign.filters; // need to copy the object to extend it
+      availableFilters.tags = [
+        ...(availableFilters.tags || []),
+        { tag_id: 'none', slug: '', display_name: 'No tags' },
+      ];
+      const filters = {
+        useCases: usecases
+          ? filtersFromParams.usecase?.map((u) =>
+              usecases?.find((useCase) => useCase.id.toString() === u)
+            )
+          : [],
+        customStatuses: customStatuses
+          ? filtersFromParams.status?.map((c) =>
+              customStatuses?.find((cs) => cs.id.toString() === c)
+            )
+          : [],
+        devices: filtersFromParams.devices
+          ? filtersFromParams.devices.map((d) =>
+              availableFilters.devices?.find((device) => device.device === d)
+            )
+          : [],
+        os: filtersFromParams.os
+          ? filtersFromParams.os.map((o) =>
+              availableFilters.os?.find((os) => os.os === o)
+            )
+          : [],
+        priorities: filtersFromParams.priorities
+          ? filtersFromParams.priorities.map((p) =>
+              availableFilters.priorities?.find(
+                (priority) => priority.name === p
+              )
+            )
+          : [],
+        replicabilities: filtersFromParams.replicabilities
+          ? filtersFromParams.replicabilities.map((r) =>
+              availableFilters.replicabilities?.find(
+                (replicability) => replicability.name === r
+              )
+            )
+          : [],
+        severities: filtersFromParams.severities
+          ? filtersFromParams.severities.map((s) =>
+              availableFilters.severities?.find(
+                (severity) => severity.name === s
+              )
+            )
+          : [],
+        tags: filtersFromParams.tags
+          ? filtersFromParams.tags.map((t) =>
+              availableFilters.tags?.find((tag) => tag.display_name === t)
+            )
+          : [],
+        types: filtersFromParams.types
+          ? filtersFromParams.types.map((t) =>
+              availableFilters.types?.find((type) => type.name === t)
+            )
+          : [],
+        unique: filtersFromParams.unique ? 'unique' : 'all',
+        read: filtersFromParams.unread ? 'unread' : 'all',
+      };
       dispatch(
         updateFilters({
-          filters: {
-            useCases: usecases
-              ? usecaseFiltersIds.map((u) =>
-                  usecases?.find((useCase) => useCase.id.toString() === u)
-                )
-              : [],
-            customStatuses: customStatuses
-              ? customStatusFiltersIds.map((c) =>
-                  customStatuses?.find((cs) => cs.id.toString() === c)
-                )
-              : [],
-          },
+          filters,
         })
       );
     }
-  }, [currentCampaign, searchParams, usecases, customStatuses]);
+  }, [currentCampaign, searchParams, usecases, customStatuses, campaign]);
 };
 
 export { useSetFilters };
