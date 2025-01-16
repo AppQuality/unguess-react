75c289ce8e93e53ede757b14547a37b5f300a411
Davide Bizzi <davide.bizzi@unguess.io>
2025-01-16 16:13:07 +0100

    feat: update ServiceTiles component to include ExpressDrawer and enhance layout with subtitles

--- src/common/components/ServiceTiles/index.tsx
+++ src/common/components/ServiceTiles/index.tsx
@@ -4,12 +4,19 @@ import {
   Row,
   Separator,
   ServiceTile,
-  SM,
   Tag,
 } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from 'src/app/hooks';
+import {
+  openDrawer,
+  openWizard,
+  setExpressTypeId,
+} from 'src/features/express/expressSlice';
 import { useCampaignTemplates } from 'src/hooks/useCampaignTemplates';
+import { ExpressDrawer } from 'src/pages/ExpressWizard/drawer';
 import styled, { useTheme } from 'styled-components';
+import { SectionTitle } from '../SectionTitle';
 
 const AdditionalInfoTag = styled(Tag)`
   img {
@@ -27,64 +34,75 @@ const CardWrapper = styled.div`
   gap: ${({ theme }) => theme.space.md};
 `;
 
-const StyledSM = styled(SM)`
-  color: ${({ theme }) => theme.palette.grey[700]};
-`;
-
 const ServiceTiles = () => {
   const { data } = useCampaignTemplates();
   const theme = useTheme();
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
   return (
-    <Wrapper>
-      <Row>
-        <Col xs={12} style={{ marginBottom: theme.space.xxs }}>
-          <Paragraph style={{ marginBottom: theme.space.xs }}>
-            <StyledSM>{t('__SERVICE_TILES_HEADER')}</StyledSM>
-          </Paragraph>
-          <Separator />
-        </Col>
-      </Row>
-      <CardWrapper>
-        {data.map((template) => {
-          const icon = <img alt={template.title || ''} src={template.icon} />;
-          const superscript = template?.Price?.previous_price;
-          const outputs = (template.output || []).map((output) => {
-            const { text, iconUrl } = output;
-            return (
-              <AdditionalInfoTag
-                key={text}
-                color="#49545c"
-                hue="#ffff"
-                isPill
-                size="medium"
-              >
-                <img src={iconUrl} alt="icon" />
-                {text}
-              </AdditionalInfoTag>
-            );
-          });
-
-          return (
-            <div style={{ flex: 1 }}>
-              <ServiceTile
-                title={template.title || ''}
-                description={template?.description || ''}
-                background={template?.background || theme.palette.blue[700]}
-                price={template?.Price?.price || '-'}
-                icon={icon}
-                superscript={superscript?.length ? superscript : undefined}
-                isSuperscriptStrikethrough={template?.Price?.is_strikethrough}
-                additionalInfo={
-                  <div style={{ display: 'flex', gap: '4px' }}>{outputs}</div>
-                }
+    <>
+      <ExpressDrawer
+        onCtaClick={() => {
+          dispatch(openWizard());
+        }}
+      />
+      <Wrapper>
+        <Row>
+          <Col xs={12} style={{ marginBottom: 0 }}>
+            <Paragraph>
+              <SectionTitle
+                title={t('__SERVICE_TILES_HEADER')}
+                subtitle={t('__SERVICE_TILES_SUBTITLE')}
               />
-            </div>
-          );
-        })}
-      </CardWrapper>
-    </Wrapper>
+              <Separator style={{ margin: `${theme.space.md} 0` }} />
+            </Paragraph>
+          </Col>
+        </Row>
+        <CardWrapper>
+          {data.map((template) => {
+            const icon = <img alt={template.title || ''} src={template.icon} />;
+            const superscript = template?.Price?.previous_price;
+            const outputs = (template.output || []).map((output) => {
+              const { text, iconUrl } = output;
+              return (
+                <AdditionalInfoTag
+                  key={text}
+                  color={theme.palette.grey[700]}
+                  hue="#ffff"
+                  isPill
+                  size="medium"
+                >
+                  <img src={iconUrl} alt="icon" />
+                  {text}
+                </AdditionalInfoTag>
+              );
+            });
+
+            return (
+              <div style={{ flex: 1 }}>
+                <ServiceTile
+                  title={template.title || ''}
+                  description={template?.description || ''}
+                  background={template?.background || theme.palette.blue[700]}
+                  price={template?.Price?.price || '-'}
+                  icon={icon}
+                  superscript={superscript?.length ? superscript : undefined}
+                  isSuperscriptStrikethrough={template?.Price?.is_strikethrough}
+                  additionalInfo={
+                    <div style={{ display: 'flex', gap: '4px' }}>{outputs}</div>
+                  }
+                  onClick={() => {
+                    dispatch(setExpressTypeId(template.expressId));
+                    dispatch(openDrawer());
+                  }}
+                />
+              </div>
+            );
+          })}
+        </CardWrapper>
+      </Wrapper>
+    </>
   );
 };
 
