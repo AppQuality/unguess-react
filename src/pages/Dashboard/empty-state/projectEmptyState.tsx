5dbfa8a38c874852cce72233f2a358a75006bc01
iDome89 <dominik.zecoli@gmail.com>
2025-01-23 15:27:11 +0100

    feat: add empty state illustration and feature flag handling in ProjectEmptyState component

--- src/pages/Dashboard/empty-state/projectEmptyState.tsx
+++ src/pages/Dashboard/empty-state/projectEmptyState.tsx
@@ -1,13 +1,15 @@
-import { Paragraph, Separator } from '@appquality/unguess-design-system';
+import { Paragraph, Separator, XL } from '@appquality/unguess-design-system';
 import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
 import { SectionTitle } from 'src/common/components/SectionTitle';
 import { ServiceTiles } from 'src/common/components/ServiceTiles';
 import styled from 'styled-components';
+import { useFeatureFlag } from 'src/hooks/useFeatureFlag';
 import { ReactComponent as UGLogoMedium } from './assets/unguess-medium.svg';
 import { ReactComponent as UGLogoSmall } from './assets/unguess-small.svg';
 import { ReactComponent as UGLogoBig } from './assets/unguess-big.svg';
+import { ReactComponent as Illustration } from './assets/illustrazione-new-project.svg';
 
 const EmptyProjectContainer = styled.div`
   display: flex;
@@ -15,14 +17,21 @@ const EmptyProjectContainer = styled.div`
   align-items: center;
   flex-direction: column;
   width: 100%;
-  background-color: #f9feff;
   position: relative;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 
 export const ProjectEmptyState = () => {
   const { t } = useTranslation();
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [distanceFromTop, setDistanceFromTop] = useState(0);
+  const { hasFeatureFlag } = useFeatureFlag();
+  const hasExpress = hasFeatureFlag('express');
 
   useEffect(() => {
     const calculateDistance = () => {
@@ -45,36 +54,58 @@ export const ProjectEmptyState = () => {
   return (
     <EmptyProjectContainer
       ref={containerRef}
-      style={{ height: `calc(100vh - ${distanceFromTop}px)` }}
+      style={{
+        height: `calc(100vh - ${distanceFromTop}px)`,
+        backgroundColor: !hasExpress ? '#f9feff' : 'transparent',
+      }}
     >
-      <Paragraph style={{ textAlign: 'center', width: '100%' }}>
-        <SectionTitle
-          title={t('__SERVICE_TILES_HEADER_EMPTY_STATE')}
-          subtitle={t('__SERVICE_TILES_SUBTITLE_EMPTY_STATE')}
-        />
-      </Paragraph>
-      <Separator
-        style={{
-          margin: `${appTheme.space.md} 0 ${appTheme.space.lg}`,
-          width: '50%',
-        }}
-      />
-      <ServiceTiles />
-      <UGLogoMedium
-        style={{ top: 0, left: 0, position: 'absolute', opacity: 0.5 }}
-      />
-      <UGLogoSmall
-        style={{ bottom: 0, left: '20%', position: 'absolute', opacity: 0.5 }}
-      />
-      <UGLogoBig
-        style={{
-          top: 0,
-          right: 0,
-          position: 'absolute',
-          opacity: 0.5,
-          zIndex: appTheme.levels.base,
-        }}
-      />
+      {' '}
+      {!hasExpress ? (
+        <>
+          <Paragraph style={{ textAlign: 'center', width: '100%' }}>
+            <SectionTitle
+              title={t('__SERVICE_TILES_HEADER_EMPTY_STATE')}
+              subtitle={t('__SERVICE_TILES_SUBTITLE_EMPTY_STATE')}
+            />
+          </Paragraph>
+          <Separator
+            style={{
+              margin: `${appTheme.space.md} 0 ${appTheme.space.lg}`,
+              width: '50%',
+            }}
+          />
+          <ServiceTiles />
+          <UGLogoMedium
+            style={{ top: 0, left: 0, position: 'absolute', opacity: 0.5 }}
+          />
+          <UGLogoSmall
+            style={{
+              bottom: 0,
+              left: '20%',
+              position: 'absolute',
+              opacity: 0.5,
+            }}
+          />
+          <UGLogoBig
+            style={{
+              top: 0,
+              right: 0,
+              position: 'absolute',
+              opacity: 0.5,
+              zIndex: appTheme.levels.base,
+            }}
+          />
+        </>
+      ) : (
+        <ImageWrapper>
+          <Illustration />
+          <Paragraph style={{ textAlign: 'center', width: '70%' }}>
+            <XL color={appTheme.palette.blue[600]}>
+              {t('__EMPTY_SPACE_NO_FEATURE')}
+            </XL>
+          </Paragraph>
+        </ImageWrapper>
+      )}
     </EmptyProjectContainer>
   );
 };
