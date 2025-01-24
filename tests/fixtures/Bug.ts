451dea9b3195439cef8cfee73a71d4ac8984dd00
Iacopo Leardini <iacopo.lea@gmail.com>
2025-01-24 19:04:47 +0100

    test(bugpage): Add tests, example JSON fixtures and update mock data for campaigns and bugs

--- tests/fixtures/Bug.ts
+++ tests/fixtures/Bug.ts
@@ -4,30 +4,84 @@ import { UnguessPage } from './UnguessPage';
 export class BugPage extends UnguessPage {
   readonly page: Page;
 
+  readonly querystrings: { default: string };
+
   constructor(page: Page) {
     super(page);
     this.page = page;
-    this.url = 'campaigns/1/bugs/1';
+    this.url = 'campaigns/4997/bugs/274852';
+    this.querystrings = {
+      default:
+        '?order=DESC&orderBy=severity_id&groupBy=usecase&groupByValue=20883&unread=false&unique=true',
+    };
   }
 
   elements() {
     return {
       ...super.elements(),
-      bugHeader: () => this.page.getByTestId('page-header'),
+      pageHeader: () => this.page.getByTestId('page-header'),
+      breadcrumb: () =>
+        this.elements()
+          .pageHeader()
+          .getByRole('navigation', { name: 'Breadcrumbs' }),
+      linkToBugCollection: () =>
+        this.elements()
+          .breadcrumb()
+          .getByRole('link', {
+            name: this.i18n.t('__PAGE_TITLE_BUGS_COLLECTION'),
+          }),
+      filtersDetailsButton: () =>
+        this.elements()
+          .pageHeader()
+          .getByRole('button', { name: 'Filters details' }),
+      usecaseSelect: () =>
+        this.elements().pageHeader().getByTestId('usecase-select'),
+      pagination: () =>
+        this.elements()
+          .pageHeader()
+          .getByRole('navigation', { name: 'Cursor pagination' }),
+      paginationPrevious: () =>
+        this.elements()
+          .pagination()
+          .getByRole('button', { name: this.i18n.t('__LIST_PAGE_PREVIOUS') }),
+      paginationNext: () =>
+        this.elements()
+          .pagination()
+          .getByRole('button', { name: this.i18n.t('__LIST_PAGE_NEXT') }),
       bugContainer: () => this.page.locator('#container'),
     };
   }
 
   async mockBug() {
-    await this.page.route('*/**/api/campaigns/1/bugs/1', async (route) => {
-      await route.fulfill({
-        path: 'tests/api/campaigns/cid/bugs/bid/_get/200_bug.json',
-      });
-    });
+    await this.page.route(
+      '*/**/api/campaigns/4997/bugs/274852',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/bugs/bid/_get/200_274852.json',
+        });
+      }
+    );
   }
 
   async mockBugs() {
-    await this.page.route('*/**/api/campaigns/1/bugs', async (route) => {
+    // mock initial calls
+    await this.page.route(
+      '*/**/api/campaigns/4997/bugs?order=DESC&orderBy=severity_id&filterBy[usecases]=&filterBy[types]=&filterBy[replicabilities]=&filterBy[os]=&filterBy[devices]=&filterBy[tags]=&filterBy[severities]=&filterBy[priorities]=&filterBy[is_duplicated]=0&filterBy[customStatuses]=',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/bugs/_get/200_orderbySeverity_filterbyDuplicated.json',
+        });
+      }
+    );
+    await this.page.route(
+      '*/**/api/campaigns/4997/bugs?order=DESC&orderBy=severity_id',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/bugs/_get/200_allbugs.json',
+        });
+      }
+    );
+    await this.page.route('*/**/api/campaigns/4997/bugs', async (route) => {
       await route.fulfill({
         path: 'tests/api/campaigns/cid/bugs/_get/200_allbugs.json',
       });
@@ -36,7 +90,7 @@ export class BugPage extends UnguessPage {
 
   async mockCustomStatuses() {
     await this.page.route(
-      '*/**/api/campaigns/1/custom_statuses',
+      '*/**/api/campaigns/4997/custom_statuses',
       async (route) => {
         await route.fulfill({
           path: 'tests/api/campaigns/cid/custom_statuses/_get/200_custom_statuses.json',
@@ -46,16 +100,70 @@ export class BugPage extends UnguessPage {
   }
 
   async mockSeverities() {
-    await this.page.route('*/**/api/campaigns/1/severities', async (route) => {
+    await this.page.route(
+      '*/**/api/campaigns/4997/severities',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/severities/_get/200_severities.json',
+        });
+      }
+    );
+  }
+
+  async mockPriorities() {
+    await this.page.route(
+      '*/**/api/campaigns/4997/priorities',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/priorities/_get/200_Example_2.json',
+        });
+      }
+    );
+  }
+
+  async mockTags() {
+    await this.page.route('*/**/api/campaigns/4997/tags', async (route) => {
       await route.fulfill({
-        path: 'tests/api/campaigns/cid/severities/_get/200_severities.json',
+        path: 'tests/api/campaigns/cid/tags/_get/200_Example_2.json',
       });
     });
   }
 
-  async mockUsecases() {
+  async mockDevices() {
+    await this.page.route('*/**/api/campaigns/4997/devices', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/campaigns/cid/devices/_get/200_Example_2.json',
+      });
+    });
+  }
+
+  async mockOs() {
+    await this.page.route('*/**/api/campaigns/4997/os', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/campaigns/cid/os/_get/200_Example_2.json',
+      });
+    });
+  }
+
+  async mockReplicabilities() {
     await this.page.route(
-      '*/**/api/campaigns/1/usecases?filterBy=bugs',
+      '*/**/api/campaigns/4997/replicabilities',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/replicabilities/_get/200_Example_1.json',
+        });
+      }
+    );
+  }
+
+  async mockUsecases() {
+    await this.page.route('*/**/api/campaigns/4997/usecases', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/campaigns/cid/usecases/_get/200_usecases.json',
+      });
+    });
+    await this.page.route(
+      '*/**/api/campaigns/4997/usecases?filterBy=bugs',
       async (route) => {
         await route.fulfill({
           path: 'tests/api/campaigns/cid/usecases/_get/200_usecases_filterByBugs.json',
@@ -65,7 +173,7 @@ export class BugPage extends UnguessPage {
   }
 
   async mockBugtypes() {
-    await this.page.route('*/**/api/campaigns/1/bugTypes', async (route) => {
+    await this.page.route('*/**/api/campaigns/4997/bugTypes', async (route) => {
       await route.fulfill({
         path: 'tests/api/campaigns/cid/bugTypes/_get/200_bugtypes.json',
       });
