05c4304981b2a8e3588fe450263515a16954485a
Marco Bonomo <marco-bonomo@hotmail.com>
2025-01-27 16:53:58 +0100

    refactor: separate components for modal usecase in express to prevent components re-rendering

---
+++ src/pages/ExpressWizard/steps/express-3/ModalUseCase/useCaseDetailsTitle.tsx
@@ -0,0 +1,65 @@
+import { InputToggle, Span } from '@appquality/unguess-design-system';
+import { FormikProps } from 'formik';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { appTheme } from 'src/app/theme';
+import { WizardModel } from 'src/pages/ExpressWizard/wizardModel';
+import { UseCase } from 'src/pages/ExpressWizard/fields/how';
+import { HelpTextMessage } from 'src/common/components/helpTextMessage';
+
+const UseCaseTitle = ({
+  formikProps,
+  useCase,
+  useCaseIndex,
+}: {
+  formikProps: FormikProps<WizardModel>;
+  useCase: UseCase;
+  useCaseIndex: number;
+}) => {
+  const { t } = useTranslation();
+  const { getFieldProps, validateForm, errors } = formikProps;
+  const [showLabel, setShowLabel] = useState<boolean>(false);
+  const [title, setTitle] = useState(useCase ? useCase.title : '');
+
+  const useCaseErrors =
+    errors && errors.use_cases && Array.isArray(errors.use_cases)
+      ? (errors.use_cases[useCaseIndex as number] as UseCase)
+      : null;
+
+  return (
+    <>
+      <InputToggle.Label style={{ opacity: showLabel ? 1 : 0 }}>
+        {t('__EXPRESS_3_WIZARD_STEP_HOW_USE_CASE_MODAL_TITLE_FIELD_TITLE')}
+        <Span style={{ color: appTheme.components.text.dangerColor }}>*</Span>
+      </InputToggle.Label>
+      <InputToggle isFocused style={{ color: appTheme.palette.grey[800] }}>
+        <InputToggle.Item
+          key={`use_cases[${useCaseIndex}].title`}
+          textSize="xxl"
+          placeholder={t(
+            '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_TITLE_FIELD_PLACEHOLDER'
+          )}
+          {...getFieldProps(`use_cases[${useCaseIndex}].title`)}
+          {...(useCaseErrors &&
+            useCaseErrors?.title && { validation: 'error' })}
+          value={title}
+          onFocus={() => {
+            setShowLabel(true);
+          }}
+          onBlur={() => {
+            validateForm();
+            setShowLabel(false);
+          }}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </InputToggle>
+      {useCaseErrors && useCaseErrors?.title && (
+        <HelpTextMessage validation="error">
+          {t('__EXPRESS_3_WIZARD_STEP_HOW_USE_CASE_MODAL_TITLE_REQUIRED')}
+        </HelpTextMessage>
+      )}
+    </>
+  );
+};
+
+export { UseCaseTitle };
