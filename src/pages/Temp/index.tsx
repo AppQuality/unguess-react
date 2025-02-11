fee3a6ff52ae898e20c3a8eaa534d3a2b512fd1a
sinatragianpaolo <gianpaolosinatra@gmail.com>
2025-02-11 15:45:25 +0100

    feat: Add CustomTitle component to Temp page with editable title functionality

--- src/pages/Temp/index.tsx
+++ src/pages/Temp/index.tsx
@@ -1,5 +1,23 @@
-import { ModuleWrapper } from 'src/features/modules/ModuleWrapper';
 import { Page } from 'src/features/templates/Page';
+import { InputToggle, PageHeader } from '@appquality/unguess-design-system';
+import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
+import { ModuleWrapper } from 'src/features/modules/ModuleWrapper';
+
+const CustomTitle = () => (
+  <LayoutWrapper>
+    <PageHeader.Main mainTitle="temp">
+      <PageHeader.Title>
+        <InputToggle className="editable-title">
+          <InputToggle.Item
+            textSize="xxxl"
+            maxLength={64}
+            style={{ paddingLeft: 0 }}
+          />
+        </InputToggle>
+      </PageHeader.Title>
+    </PageHeader.Main>
+  </LayoutWrapper>
+);
 
 const Temp = () => {
   const a = 1;
@@ -7,6 +25,7 @@ const Temp = () => {
   return (
     <Page title="temp" route="temp">
       <ModuleWrapper>
+        <CustomTitle />
         Temp
         <ModuleWrapper.Debugger />
       </ModuleWrapper>
