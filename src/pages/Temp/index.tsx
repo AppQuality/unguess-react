d44ef4ef7de6cc1b4c0fb067694ccff77d393ab6
Davide Bizzi <davide.bizzi@unguess.io>
2025-02-11 15:30:04 +0100

    feat: Add basic structure for module page

---
+++ src/pages/Temp/index.tsx
@@ -0,0 +1,13 @@
+import { Page } from 'src/features/templates/Page';
+
+const Temp = () => {
+  const a = 1;
+
+  return (
+    <Page title="temp" route="temp">
+      Temp
+    </Page>
+  );
+};
+
+export { Temp };
