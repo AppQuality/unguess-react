7ebedb39e0cfb3622e74d059e495fb7501cebcf1
iacopolea <iacopo.lea@gmail.com>
2025-02-11 16:38:28 +0100

    feat: add Template page with contact and express CTAs

---
+++ src/pages/Template/TemplateContactUsCta.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@appquality/unguess-design-system';
+import { useTranslation } from 'react-i18next';
+
+export const TemplateContactUsCta = ({
+  onCtaClick,
+}: {
+  onCtaClick: () => void;
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Button size="medium" isPrimary isAccent onClick={onCtaClick}>
+      {t('__CATALOG_PAGE_BUTTON_CONTACT_LABEL')}
+    </Button>
+  );
+};
