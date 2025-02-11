765acc3b2cd3496d58eff1b84eb4b822d85b5363
Davide Bizzi <davide.bizzi@unguess.io>
2025-02-11 15:46:35 +0100

    feat: Refactor Temp page to use Title component for editable title functionality

--- src/pages/Temp/index.tsx
+++ src/pages/Temp/index.tsx
@@ -1,23 +1,6 @@
-import { Page } from 'src/features/templates/Page';
-import { InputToggle, PageHeader } from '@appquality/unguess-design-system';
-import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
 import { ModuleWrapper } from 'src/features/modules/ModuleWrapper';
-
-const CustomTitle = () => (
-  <LayoutWrapper>
-    <PageHeader.Main mainTitle="temp">
-      <PageHeader.Title>
-        <InputToggle className="editable-title">
-          <InputToggle.Item
-            textSize="xxxl"
-            maxLength={64}
-            style={{ paddingLeft: 0 }}
-          />
-        </InputToggle>
-      </PageHeader.Title>
-    </PageHeader.Main>
-  </LayoutWrapper>
-);
+import { Page } from 'src/features/templates/Page';
+import { Title } from './Title';
 
 const Temp = () => {
   const a = 1;
@@ -25,7 +8,7 @@ const Temp = () => {
   return (
     <Page title="temp" route="temp">
       <ModuleWrapper>
-        <CustomTitle />
+        <Title />
         Temp
         <ModuleWrapper.Debugger />
       </ModuleWrapper>
