12fe445d19a74f83bfa39ac8a4e06fe710d1a834
Marco Bonomo <marco.bonomo@unguess.io>
2025-02-11 09:50:32 +0100

    Refactor useCaseDetailsDropdown component in ExpressWizard

--- src/pages/ExpressWizard/steps/express-2/ModalUseCase/useCaseDetailsDropdown.tsx
+++ src/pages/ExpressWizard/steps/express-2/ModalUseCase/useCaseDetailsDropdown.tsx
@@ -149,14 +149,14 @@ const UseCaseDropdown = ({
                 marginBottom: appTheme.space.md,
               }}
             />
-            <InlineRow>
+            <div>
+              <InfoIcon className="authentication-info-button" />
               <Paragraph>
                 {t(
                   '__EXPRESS_WIZARD_STEP_HOW_USE_CASE_MODAL_LOGGED_FIELD_DESCRIPTION'
                 )}
               </Paragraph>
-              <InfoIcon className="authentication-info-button" />
-            </InlineRow>
+            </div>
           </>
         ) : null}
       </StyledFormField>
