7a83245b8afa3ca6e2e792b9bb7e64bfe41911a2
iacopolea <iacopo.lea@gmail.com>
2025-02-11 11:45:15 +0100

    feat: enhance ScrollingGrid layout with responsive margins and gradient effects

--- src/common/components/ScrollingGrid.tsx
+++ src/common/components/ScrollingGrid.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const scrollingContainerItemsGap = appTheme.space.md;
 
 const StyledGrid = styled.div`
+  width: 100%;
   container-type: inline-size;
   container-name: scrollingContainer;
   flex-wrap: nowrap;
@@ -12,14 +13,7 @@ const StyledGrid = styled.div`
   overflow-x: auto;
   padding-bottom: ${(p) => p.theme.space.xl};
   scroll-snap-type: x mandatory;
-  margin-right: -${(p) => p.theme.space.md};
-  margin-left: -${(p) => p.theme.space.md};
-
-  @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
-    margin-right: -${(p) => p.theme.space.xxl};
-    margin-left: -${(p) => p.theme.space.xxl};
-  }
-
+  scroll-behavior: smooth;
   // Hide scrollbar
   &::-webkit-scrollbar {
     display: none;
@@ -75,8 +69,59 @@ const StyledItem = styled.div`
   }
 `;
 
-export const ScrollingGrid = StyledGrid as typeof StyledGrid & {
-  Item: typeof StyledItem;
-};
+const GridContainer = styled.div`
+  position: relative;
+  margin-right: -${(p) => p.theme.space.md};
+  margin-left: -${(p) => p.theme.space.md};
+
+  @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
+    margin-right: -${(p) => p.theme.space.xxl};
+    margin-left: -${(p) => p.theme.space.xxl};
+  }
+  @media (min-width: ${(p) => p.theme.breakpoints.xxl}) {
+    &::after,
+    &::before {
+      position: absolute;
+      z-index: 1;
+      display: block;
+      content: '';
+      top: 0;
+      bottom: 0;
+      width: ${(p) => p.theme.space.xxl};
+      min-width: ${(p) => p.theme.space.xxl};
+      pointer-events: none;
+    }
+    &::after {
+      right: 0;
+      background: linear-gradient(
+        to right,
+        rgba(255, 255, 255, 0),
+        ${(p) => p.theme.palette.grey[100]}
+      );
+    }
+    &::before {
+      background: linear-gradient(
+        to left,
+        rgba(255, 255, 255, 0),
+        ${(p) => p.theme.palette.grey[100]}
+      );
+      left: 0;
+    }
+  }
+`;
+
+const ScrollingGridComponent = ({
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <GridContainer {...props}>
+    <StyledGrid>{children}</StyledGrid>
+  </GridContainer>
+);
+
+export const ScrollingGrid =
+  ScrollingGridComponent as typeof ScrollingGridComponent & {
+    Item: typeof StyledItem;
+  };
 
 ScrollingGrid.Item = StyledItem;
