7ca427acb4e696d98864c3aaff2630acb13fa05e
iacopolea <iacopo.lea@gmail.com>
2025-02-13 14:51:37 +0100

    feat(hook): enhance useCampaignTemplateById to include sort order and improve price handling

--- src/hooks/useCampaignTemplateById.ts
+++ src/hooks/useCampaignTemplateById.ts
@@ -23,6 +23,9 @@ const useCampaignTemplateById = (id: string) => {
       express: {
         populate: '*',
       },
+      sort_order: {
+        populate: '*',
+      },
       why: {
         populate: {
           reasons: {
@@ -60,25 +63,40 @@ const useCampaignTemplateById = (id: string) => {
 
   const templateData = {
     price: {
+      price: data?.data?.attributes?.Price?.price || 0, // mandatory field
+      previousPrice: data?.data?.attributes?.Price?.previous_price,
+      isStrikeThrough: data?.data?.attributes?.Price?.is_strikethrough,
       ...data?.data?.attributes?.Price?.tag_price,
       icon: priceIconData?.url ? `${STRAPI_URL}${priceIconData.url}` : '',
     },
-    tags: data?.data?.attributes?.output?.map((o) => {
-      const oUrl = o.Icon?.data?.attributes?.url;
-      return {
-        label: o.Text ?? '',
-        icon: oUrl ? `${STRAPI_URL}${oUrl}` : '',
-        id: o.id ?? '',
-      };
-    }),
+    tags: data?.data?.attributes?.output // output field is mandatory on strapi backoffice
+      ? data?.data?.attributes?.output?.map((o) => {
+          const oUrl = o.Icon?.data?.attributes?.url;
+          return {
+            label: o.Text ?? '',
+            icon: oUrl ? `${STRAPI_URL}${oUrl}` : '',
+            id: o.id ?? '',
+          };
+        })
+      : [],
     requirements: data?.data?.attributes?.requirements,
     description: data?.data?.attributes?.description || '',
     title: data?.data?.attributes?.title || '',
     slug: data?.data?.attributes?.template_slug || '',
+    sortOrder: data?.data?.attributes?.sort_order || 0,
     express: data?.data?.attributes?.express,
     why: data?.data?.attributes?.why,
-    how: data?.data?.attributes?.how,
-    what: data?.data?.attributes?.what,
+    ...(data?.data?.attributes?.how && {
+      how: {
+        timeline: data?.data?.attributes?.how?.timeline || [], // mandatory field
+      },
+    }),
+    ...(data?.data?.attributes?.what && {
+      what: {
+        description: data?.data?.attributes?.what?.description || '', // mandatory field
+        goalText: data?.data?.attributes?.what?.goal_text,
+      },
+    }),
     campaignType: data?.data?.attributes?.campaign_type || '',
     ...(data?.data?.attributes?.output_image?.data && {
       outputImage: `${STRAPI_URL}${
