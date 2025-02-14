69cd722f39773c0221cfb0a0029513be238a5aa8
iacopolea <iacopo.lea@gmail.com>
2025-02-14 18:09:46 +0100

    feat(Plan): add Dates and Title components, update routing and tests for module builder

---
+++ tests/e2e/plan/pending_review.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '../../fixtures/app';
+import { PlanPage } from '../../fixtures/Plan';
+
+test.describe('The module builder', () => {
+  let moduleBuilderPage: PlanPage;
+
+  test.beforeEach(async ({ page }) => {
+    moduleBuilderPage = new PlanPage(page);
+    await moduleBuilderPage.loggedIn();
+    await moduleBuilderPage.mockPreferences();
+    await moduleBuilderPage.mockWorkspace();
+    await moduleBuilderPage.mockWorkspacesList();
+    await moduleBuilderPage.mockGetPendingReviewPlan();
+    await moduleBuilderPage.open();
+  });
+
+  test('has a list of available modules and not others, a save button and a request quote cta', async () => {
+    await expect(moduleBuilderPage.elements().titleModule()).toBeVisible();
+    await expect(moduleBuilderPage.elements().tasksModule()).toBeVisible();
+    // await expect(moduleBuilderPage.elements().datesModule()).toBeVisible();
+    await expect(moduleBuilderPage.elements().submitButton()).toBeVisible();
+    await expect(moduleBuilderPage.elements().submitButton()).toBeDisabled();
+    await expect(moduleBuilderPage.elements().quoteButton()).toBeVisible();
+    await expect(moduleBuilderPage.elements().quoteButton()).toBeDisabled();
+    // to be continued
+  });
+
+  test('if mockGetPlan return a status of pending_review the quotation cta is disabled', async () => {
+    // todo
+  });
+});
---
+++ tests/fixtures/Plan.ts
@@ -0,0 +1,56 @@
+import { type Page } from '@playwright/test';
+import { UnguessPage } from './UnguessPage';
+
+export class PlanPage extends UnguessPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.url = `temp/1`;
+  }
+
+  elements() {
+    return {
+      ...super.elements(),
+      titleModule: () => this.page.getByTestId('title-module'),
+      tasksModule: () => this.page.getByTestId('tasks-module'),
+      datesModule: () => this.page.getByTestId('dates-module'),
+      submitButton: () => this.page.getByRole('button', { name: 'Save' }),
+      quoteButton: () =>
+        this.page.getByRole('button', { name: 'Request Quotation' }),
+    };
+  }
+
+  async mockGetDraftPlan() {
+    await this.page.route('*/**/api/workspaces/1/plans/1', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/workspaces/wid/plans/pid/_get/200_draft.json',
+      });
+    });
+  }
+  async mockGetPendingReviewPlan() {
+    await this.page.route('*/**/api/workspaces/1/plans/1', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/workspaces/wid/plans/pid/_get/200_pending_review.json',
+      });
+    });
+  }
+  async mockPatchPlan() {
+    await this.page.route('*/**/api/workspaces/1/plans/1', async (route) => {
+      await route.fulfill({
+        path: 'tests/api/campaigns/cid/bugs/bid/_get/200_274852.json',
+      });
+    });
+  }
+  async mockPatchStatus() {
+    await this.page.route(
+      '*/**/api/workspaces/1/plans/1/status',
+      async (route) => {
+        await route.fulfill({
+          path: 'tests/api/campaigns/cid/bugs/bid/_get/200_274852.json',
+        });
+      }
+    );
+  }
+}
