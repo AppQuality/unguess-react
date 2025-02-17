2898b65b3bfc55d7571e9b253439cd2bfd64d95d
iacopolea <iacopo.lea@gmail.com>
2025-02-17 15:38:47 +0100

    feat(ModuleBuilder): update ModuleWrapper to use Form component and modify PATCH request structure; enhance Controls to set plan status

--- tests/e2e/plan/draft.spec.ts
+++ tests/e2e/plan/draft.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '../../fixtures/app';
 import { PlanPage } from '../../fixtures/Plan';
+import examplePatch from '../../api/workspaces/wid/plans/pid/_patch/200_Example_1.json';
 
 test.describe('The module builder', () => {
   let moduleBuilderPage: PlanPage;
@@ -28,10 +29,28 @@ test.describe('The module builder', () => {
     await expect(moduleBuilderPage.elements().quoteButton()).toBeVisible();
     await expect(moduleBuilderPage.elements().quoteButton()).not.toBeDisabled();
   });
-  test('Clicking save button calls the PATCH Plan', async () => {
-    // todo
+  test('Clicking save button calls the PATCH Plan', async ({ page }) => {
+    const patchPromise = page.waitForResponse('*/**/api/workspaces/1/plans/1');
+    // todo: come up with some common usecases in qhich the user perform some changes to the form, then click the submit button
+    await moduleBuilderPage.elements().submitButton().click();
+    const response = await patchPromise;
+    const data = response.request().postDataJSON();
+    expect(data).toEqual(examplePatch);
   });
-  test('Clicking request quotation calls the PATCH Plan, then if ok the PATCH Status', async () => {
-    // todo
+  test('Clicking request quotation calls the PATCH Plan, then if ok the PATCH Status', async ({
+    page,
+  }) => {
+    const patchPromise = page.waitForResponse('*/**/api/workspaces/1/plans/1');
+    const patchStatusPromise = page.waitForResponse(
+      '*/**/api/workspaces/1/plans/1/status'
+    );
+    // todo: come up with some common usecases in qhich the user perform some changes to the form, then click the submit button
+    await moduleBuilderPage.elements().quoteButton().click();
+    const response = await patchPromise;
+    const responseStatus = await patchStatusPromise;
+    const data = response.request().postDataJSON();
+    expect(data).toEqual(examplePatch);
+    const dataStatus = responseStatus.request().postDataJSON();
+    expect(dataStatus).toEqual({ status: 'pending_review' });
   });
 });
