d3f54afba2f93a0351443b3e4d1f3da3981292db
Marco Bonomo <marco.bonomo@unguess.io>
2025-02-19 15:35:58 +0100

    fix(ThankYou): setThankYou false from thank you page CTA

--- src/pages/ExpressWizard/index.tsx
+++ src/pages/ExpressWizard/index.tsx
@@ -507,7 +507,7 @@ export const ExpressWizardContainer = () => {
             )}
           </Formik>
         ) : (
-          <ThankYouStep />
+          <ThankYouStep setThankyou={setThankyou} />
         )}
       </StyledModal>
       {showDiscardChangesModal && (
