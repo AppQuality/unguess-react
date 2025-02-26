d88a346efe1ac036ae43995b09a0df24d65f25f8
iacopolea <iacopo.lea@gmail.com>
2025-02-26 17:05:22 +0100

    feat(plan): implement datepicker component and enhance date handling in plan module

--- src/pages/Plan/Dates.tsx
+++ src/pages/Plan/Dates.tsx
@@ -1,13 +1,29 @@
-import { LG } from '@appquality/unguess-design-system';
-import { LayoutWrapper } from 'src/common/components/LayoutWrapper';
+import { Datepicker, Input } from '@appquality/unguess-design-system';
 import { useModuleContext } from 'src/features/modules/ModuleWrapper';
+import { formatModuleDate } from './formatModuleDate';
 
 export const Dates = () => {
   const { value, set, remove } = useModuleContext('dates');
 
+  // get value from module
+  const getValue = () => {
+    if (!value) return undefined;
+    return new Date(value.output.start);
+  };
+
+  const handleChange = (date: Date) => {
+    // todo
+  };
+
   return (
     <div data-qa="dates-module">
-      <LG>Dates</LG>
+      <Datepicker
+        value={getValue()}
+        onChange={handleChange}
+        formatDate={(date) => formatModuleDate(date).input}
+      >
+        <Input />
+      </Datepicker>
     </div>
   );
 };
