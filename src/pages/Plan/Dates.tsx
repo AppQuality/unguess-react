a4caeff31b28cda24269865a31056c338ff15f6a
iacopolea <iacopo.lea@gmail.com>
2025-02-27 17:57:20 +0100

    feat(dates): enhance date module with validation and update UI components

--- src/pages/Plan/Dates.tsx
+++ src/pages/Plan/Dates.tsx
@@ -1,6 +1,13 @@
-import { Datepicker, Input } from '@appquality/unguess-design-system';
+import {
+  Datepicker,
+  Input,
+  Label,
+  Select,
+} from '@appquality/unguess-design-system';
 import { useModuleContext } from 'src/features/modules/ModuleWrapper';
 import { formatModuleDate } from './formatModuleDate';
+import { Field } from 'formik';
+import { addBusinessDays } from 'date-fns';
 
 export const Dates = () => {
   const { value, set, remove } = useModuleContext('dates');
@@ -11,19 +18,34 @@ export const Dates = () => {
     return new Date(value.output.start);
   };
 
+  // validation
+  const validation = (dates: Date) => {
+    let error;
+    if (!value) {
+      error = 'Required';
+    }
+    return error;
+  };
+
   const handleChange = (date: Date) => {
     // todo
   };
 
   return (
-    <div data-qa="dates-module">
+    <Field name="dates" validate={validation} data-qa="dates-module">
+      <Label>Date</Label>
+      <Select data-qa="dates-variant">
+        <Select.Option value="default">Default</Select.Option>
+        <Select.Option value="free">Free</Select.Option>
+      </Select>
       <Datepicker
         value={getValue()}
         onChange={handleChange}
         formatDate={(date) => formatModuleDate(date).input}
+        minValue={addBusinessDays(new Date(), 1)}
       >
         <Input />
       </Datepicker>
-    </div>
+    </Field>
   );
 };
