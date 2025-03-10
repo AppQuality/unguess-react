8051f83cab9c6765996342ee957c29822124911d
iDome89 <dominik.zecoli@gmail.com>
2025-03-10 17:32:33 +0100

    test: Update error message visibility check to use title module error element

--- tests/e2e/plan/modules/title.spec.ts
+++ tests/e2e/plan/modules/title.spec.ts
@@ -32,12 +32,7 @@ test.describe('The title module defines the Plan title.', () => {
       planPage.i18n.t('__PLAN_TITLE_ERROR_EMPTY')
     );
     await planPage.elements().requestQuotationCTA().click();
-    await expect(
-      planPage.elements().requestQuotationErrorMessage()
-    ).toBeVisible();
-    await expect(planPage.elements().requestQuotationErrorMessage()).toHaveText(
-      `${planPage.i18n.t('__PLAN_MISSING_MODULES_ERROR')}: dates`
-    );
+    await expect(planPage.elements().titleModuleError()).toBeVisible();
   });
   test('The title should have a maximum length of 256 characters', async () => {
     await planPage.elements().titleModule().click();
