5a9d9c315a7200cd0a0450d620b7120afd655666
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-11 18:13:52 +0100

    feat: Enhance module validation by refining error handling and integrating task error display in TaskItemNav

--- src/pages/Plan/modules/Tasks/parts/TaskItemNav.tsx
+++ src/pages/Plan/modules/Tasks/parts/TaskItemNav.tsx
@@ -2,6 +2,7 @@ import { Card, MD } from '@appquality/unguess-design-system';
 import { Link } from 'react-scroll';
 import { components } from 'src/common/schema';
 import styled from 'styled-components';
+import { useModuleTasks } from '../hooks';
 import { getIconFromKind } from '../utils';
 
 const StyledCard = styled(Card)`
@@ -20,23 +21,28 @@ const TaskItemNav = ({
   task,
 }: {
   task: components['schemas']['OutputModuleTask'] & { key: number };
-}) => (
-  <Link
-    to={`task-${task.key}`}
-    containerId="main"
-    duration={500}
-    offset={-20}
-    smooth
-    spy
-    style={{ textDecoration: 'none' }}
-  >
-    <StyledCard key={task.key} data-qa="task-item-nav">
-      <StyledContainer>
-        {getIconFromKind(task.kind)}
-        <MD isBold>{task.title}</MD>
-      </StyledContainer>
-    </StyledCard>
-  </Link>
-);
+}) => {
+  const { error } = useModuleTasks();
+
+  return (
+    <Link
+      to={`task-${task.key}`}
+      containerId="main"
+      duration={500}
+      offset={-20}
+      smooth
+      spy
+      style={{ textDecoration: 'none' }}
+    >
+      <StyledCard key={task.key} data-qa="task-item-nav">
+        <StyledContainer>
+          {getIconFromKind(task.kind)}
+          <MD isBold>{task.title}</MD>
+        </StyledContainer>
+        {JSON.stringify(error)}
+      </StyledCard>
+    </Link>
+  );
+};
 
 export { TaskItemNav };
