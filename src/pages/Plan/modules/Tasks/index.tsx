82afe86ba151b3eae1daa584ac41ffe371293bcf
Marco Bonomo <marco.bonomo@unguess.io>
2025-03-11 12:43:03 +0100

    feat: Refactor Tasks component layout to use Grid system for improved responsiveness

--- src/pages/Plan/modules/Tasks/index.tsx
+++ src/pages/Plan/modules/Tasks/index.tsx
@@ -1,3 +1,4 @@
+import { Col, Grid, Row } from '@appquality/unguess-design-system';
 import { ModuleTasksContextProvider } from './context';
 import { AddTaskButton } from './parts/AddTaskButton';
 import { TasksList } from './parts/TasksList';
@@ -5,9 +6,17 @@ import { TasksModal } from './parts/TasksModal';
 
 const Tasks = () => (
   <ModuleTasksContextProvider>
-    <AddTaskButton />
-    <TasksModal />
-    <TasksList />
+    <Grid>
+      <Row>
+        <Col sm={4}>
+          <AddTaskButton />
+          <TasksModal />
+        </Col>
+        <Col sm={8}>
+          <TasksList />
+        </Col>
+      </Row>
+    </Grid>
   </ModuleTasksContextProvider>
 );
 
