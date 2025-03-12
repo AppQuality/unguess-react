61ed20312cb0e93390387f4554149f3083df3fd4
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-03-12 14:40:16 +0100

    feat: Refactor ModulesList to use dynamic module mapping and add new modules

---
+++ src/pages/Plan/modules/Literacy.tsx
@@ -0,0 +1,9 @@
+import { useModule } from 'src/features/modules/useModule';
+
+const Literacy = () => {
+  const { value, setOutput, remove } = useModule('title');
+
+  return <>Module Literacy not implemented yet</>;
+};
+
+export default Literacy;
