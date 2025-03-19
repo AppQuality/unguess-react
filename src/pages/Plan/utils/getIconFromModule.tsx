d0139ce81b989e1116ff2fd86aa758da7ef811fd
Marco Bonomo <marco.bonomo@unguess.io>
2025-03-19 12:12:13 +0100

    feat: Add tasks icon and update getIconFromModule function to include it

--- src/pages/Plan/utils/getIconFromModule.tsx
+++ src/pages/Plan/utils/getIconFromModule.tsx
@@ -1,6 +1,13 @@
 import { getColor } from '@appquality/unguess-design-system';
 import { appTheme } from 'src/app/theme';
-import { ReactComponent as FunctionalTaskIcon } from 'src/assets/icons/functional-task-icon.svg';
+import { ReactComponent as AgeIcon } from 'src/assets/icons/cake-icon-fill.svg';
+import { ReactComponent as GenderIcon } from '@zendeskgarden/svg-icons/src/16/user-group-fill.svg';
+import { ReactComponent as GoalIcon } from 'src/assets/icons/flag-fill.svg';
+import { ReactComponent as LanguageIcon } from 'src/assets/icons/languages.svg';
+import { ReactComponent as LiteracyIcon } from '@zendeskgarden/svg-icons/src/16/book-closed-fill.svg';
+import { ReactComponent as OutOfScopeIcon } from 'src/assets/icons/x-circle.svg';
+import { ReactComponent as TargetIcon } from 'src/assets/icons/user-follow.svg';
+import { ReactComponent as TasksIcon } from 'src/assets/icons/tasks-icon.svg';
 import { components } from 'src/common/schema';
 
 const getIconColor = (module: components['schemas']['Module']) => {
@@ -15,17 +22,24 @@ const getIconFromModuleType = (
   type: components['schemas']['Module']['type']
 ) => {
   switch (type) {
-    case 'dates':
     case 'age':
+      return <AgeIcon />;
     case 'gender':
+      return <GenderIcon />;
     case 'goal':
+      return <GoalIcon />;
     case 'language':
+      return <LanguageIcon />;
     case 'literacy':
+      return <LiteracyIcon />;
     case 'out_of_scope':
+      return <OutOfScopeIcon />;
     case 'target':
+      return <TargetIcon />;
     case 'tasks':
+      return <TasksIcon />;
     case 'title':
-      return <FunctionalTaskIcon />;
+    case 'dates':
     default:
       return null;
   }
