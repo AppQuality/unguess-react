c5f050527dd2505708094a90c1df738e02cc771d
Marco Bonomo <marco.bonomo@unguess.io>
2025-03-19 18:59:04 +0100

    feat: Wrap AddTaskButton in a styled container for improved layout and styling

--- src/pages/Plan/modules/Tasks/parts/AddTaskButton.tsx
+++ src/pages/Plan/modules/Tasks/parts/AddTaskButton.tsx
@@ -2,7 +2,14 @@ import { Button } from '@appquality/unguess-design-system';
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as PlusIcon } from 'src/assets/icons/plus-icon.svg';
+import styled from 'styled-components';
 import { useModuleTasksContext } from '../context';
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: ${({ theme }) => theme.space.md};
+`;
 
 const AddTaskButton = () => {
   const { t } = useTranslation();
@@ -10,18 +17,19 @@ const AddTaskButton = () => {
   const { setModalRef } = useModuleTasksContext();
 
   return (
-    <Button
-      isPrimary
-      isPill={false}
-      ref={triggerRef}
-      onClick={() => setModalRef(triggerRef.current)}
-      isStretched
-    >
-      <Button.StartIcon>
-        <PlusIcon />
-      </Button.StartIcon>
-      {t('__PLAN_PAGE_MODULE_TASKS_ADD_TASK_BUTTON')}
-    </Button>
+    <ButtonContainer>
+      <Button
+        isBasic
+        isPill={false}
+        ref={triggerRef}
+        onClick={() => setModalRef(triggerRef.current)}
+      >
+        <Button.StartIcon>
+          <PlusIcon />
+        </Button.StartIcon>
+        {t('__PLAN_PAGE_MODULE_TASKS_ADD_TASK_BUTTON')}
+      </Button>
+    </ButtonContainer>
   );
 };
 
