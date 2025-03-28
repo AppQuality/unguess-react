8ca64e613a11b7d98024b758f28f44cffd35b721
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-03-28 18:44:19 +0100

    feat(navigation): create reusable ModulesBottomNavigation component

--- src/pages/Plan/ModulesList.tsx
+++ src/pages/Plan/ModulesList.tsx
@@ -1,76 +1,9 @@
 import { appTheme } from 'src/app/theme';
 import { useModuleConfiguration } from 'src/features/modules/useModuleConfiguration';
-import { Button } from '@appquality/unguess-design-system';
-import { ReactComponent as ChevronLeftIcon } from '@zendeskgarden/svg-icons/src/12/chevron-left-stroke.svg';
-import { ReactComponent as ChevronRightIcon } from '@zendeskgarden/svg-icons/src/12/chevron-right-stroke.svg';
-import { MODULE_TABS_ORDER } from 'src/constants';
-import { useTranslation } from 'react-i18next';
-import { PlanTab, usePlanTab } from './context/planContext';
-import { MODULES_BY_TAB, modulesMap } from './modulesMap';
+import { ModulesBottomNavigation } from './common/ModulesBottomNavigation';
 import { TabTitle } from './common/TabTitle';
-
-const ModulesBottomNavigation = ({ tabId }: { tabId: PlanTab }) => {
-  const { setActiveTab } = usePlanTab();
-  const { t } = useTranslation();
-  let leftLabel = '';
-  let rightLabel = '';
-  const previousTab = MODULE_TABS_ORDER[MODULE_TABS_ORDER.indexOf(tabId) - 1];
-  const nextTab = MODULE_TABS_ORDER[MODULE_TABS_ORDER.indexOf(tabId) + 1];
-  const isFirstTab = MODULE_TABS_ORDER.indexOf(tabId) === 0;
-
-  switch (tabId) {
-    case 'setup':
-      rightLabel = t('__MODULES_BOTTOM_NAVIGATION_SETUP_TAB_RIGHT_LABEL');
-      break;
-    case 'target':
-      leftLabel = t('__MODULES_BOTTOM_NAVIGATION_TARGET_TAB_LEFT_LABEL');
-      rightLabel = t('__MODULES_BOTTOM_NAVIGATION_TARGET_TAB_RIGHT_LABEL');
-      break;
-    case 'instructions':
-      leftLabel = t('__MODULES_BOTTOM_NAVIGATION_TASKS_TAB_LEFT_LABEL');
-      rightLabel = t('__MODULES_BOTTOM_NAVIGATION_TASKS_TAB_RIGHT_LABEL');
-      break;
-    default:
-      break;
-  }
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: isFirstTab ? 'flex-end' : 'space-between',
-      }}
-    >
-      {!isFirstTab && (
-        <Button
-          isBasic
-          size="small"
-          onClick={() => {
-            setActiveTab(previousTab);
-          }}
-        >
-          <Button.StartIcon>
-            <ChevronLeftIcon />
-          </Button.StartIcon>
-          {leftLabel}
-        </Button>
-      )}
-
-      <Button
-        isBasic
-        size="small"
-        onClick={() => {
-          setActiveTab(nextTab);
-        }}
-      >
-        {rightLabel}
-        <Button.EndIcon>
-          <ChevronRightIcon />
-        </Button.EndIcon>
-      </Button>
-    </div>
-  );
-};
+import { PlanTab } from './context/planContext';
+import { modulesMap, MODULES_BY_TAB } from './modulesMap';
 
 export const ModulesList = ({ tabId }: { tabId: PlanTab }) => {
   const { getModules } = useModuleConfiguration();
@@ -101,9 +34,7 @@ export const ModulesList = ({ tabId }: { tabId: PlanTab }) => {
           </div>
         );
       })}
-      {tabId === 'target' && <ModulesBottomNavigation tabId={tabId} />}
-      {tabId === 'setup' && <ModulesBottomNavigation tabId={tabId} />}
-      {tabId === 'instructions' && <ModulesBottomNavigation tabId={tabId} />}
+      {tabId !== 'summary' && <ModulesBottomNavigation tabId={tabId} />}
     </>
   );
 };
