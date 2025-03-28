c14d89044788c1595a0f0801223fdd2def1fd881
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-03-28 16:00:49 +0100

    feat(tabs): add dynamic tab titles and update translations for plan module

--- src/pages/Plan/summary/index.tsx
+++ src/pages/Plan/summary/index.tsx
@@ -2,7 +2,7 @@ import { Col, Row } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { SectionTitle } from '../common/SectionTitle';
+import { TabTitle } from '../common/TabTitle';
 import { usePlanTab } from '../context/planContext';
 import { usePlan } from '../hooks/usePlan';
 import { ActivityInfo } from './components/ActivityInfo';
@@ -33,9 +33,7 @@ const SummaryBody = () => {
     <Row>
       <Col sm="6" offsetSm={3}>
         <StyledDiv>
-          <SectionTitle isBold>
-            {t('__PLAN_PAGE_SUMMARY_TAB_TITLE')}
-          </SectionTitle>
+          <TabTitle tabId="summary" />
           <IntroductionCard />
           <ActivityInfo />
           <ConfirmationCard />
