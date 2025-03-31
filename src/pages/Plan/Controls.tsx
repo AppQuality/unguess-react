76fe2019770e5fa741f72a77b39970681d925a52
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-03-31 13:35:34 +0200

    feat(plan): enhance Controls and DetailsCard components with approval functionality

--- src/pages/Plan/Controls.tsx
+++ src/pages/Plan/Controls.tsx
@@ -1,18 +1,39 @@
 import { Button } from '@appquality/unguess-design-system';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
 import { appTheme } from 'src/app/theme';
+import { Pipe } from 'src/common/components/Pipe';
+import { usePatchWorkspacesByWidPlansAndPidStatusMutation } from 'src/features/api';
 import { useValidationContext } from 'src/features/modules/FormProvider';
 import { useModuleConfiguration } from 'src/features/modules/useModuleConfiguration';
 import { useRequestQuotation } from 'src/features/modules/useRequestQuotation';
+import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
+import styled from 'styled-components';
+import { getPlanStatus } from '../Dashboard/hooks/getPlanStatus';
+import { usePlan } from './hooks/usePlan';
 import { SendRequestModal } from './modals/SendRequestModal';
 
+const StyledPipe = styled(Pipe)`
+  display: inline;
+  margin: 0;
+  height: auto;
+`;
+
 export const Controls = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { isSubmitting, getPlanStatus, submitModuleConfiguration } =
-    useModuleConfiguration();
-  const { validateForm } = useValidationContext();
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isSubmitting, submitModuleConfiguration } = useModuleConfiguration();
+  const { planId } = useParams();
+  const { plan, activeWorkspace } = usePlan(planId);
+  const { validateForm } = useValidationContext();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [patchStatus] = usePatchWorkspacesByWidPlansAndPidStatusMutation();
+
+  const campaignRoute = useLocalizeRoute(
+    `campaigns/${plan?.campaign?.id ?? 0}`
+  );
   const { isRequestQuoteCTADisabled } = useRequestQuotation();
   const handleSaveConfiguration = () => {
     validateForm();
@@ -23,16 +44,62 @@ export const Controls = () => {
     setIsModalOpen(true);
   };
 
+  if (!plan) return null;
+
+  const { status } = getPlanStatus(plan, t);
+
+  if (status === 'approved') {
+    return (
+      <div style={{ display: 'flex', gap: appTheme.space.xs }}>
+        <Button
+          type="button"
+          size="small"
+          isAccent
+          isPrimary
+          onClick={() => navigate(campaignRoute)}
+        >
+          {t('__PLAN_PAGE_SUMMARY_TAB_CONFIRMATION_CARD_GO_TO_CAMPAIGN_CTA')}
+        </Button>
+      </div>
+    );
+  }
+
+  if (status !== 'draft') {
+    return (
+      <div style={{ display: 'flex', gap: appTheme.space.xs }}>
+        <Button
+          type="button"
+          size="small"
+          isAccent
+          isPrimary
+          disabled={status === 'submitted' || isSubmitted}
+          onClick={() => {
+            setIsSubmitted(true);
+            patchStatus({
+              wid: activeWorkspace?.id.toString() ?? '',
+              pid: planId?.toString() ?? '',
+              body: { status: 'approved' },
+            }).unwrap();
+            setIsSubmitted(false);
+          }}
+        >
+          {t('__PLAN_PAGE_SUMMARY_TAB_CONFIRMATION_CARD_CONFIRM_CTA')}
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <div style={{ display: 'flex', gap: appTheme.space.xs }}>
       <Button
         type="button"
         size="small"
-        disabled={isSubmitting || getPlanStatus() !== 'draft'}
+        disabled={isSubmitting || status !== 'draft'}
         onClick={handleSaveConfiguration}
       >
         {t('__PLAN_SAVE_CONFIGURATION_CTA')}
       </Button>
+      <StyledPipe />
       <Button
         isAccent
         isPrimary
