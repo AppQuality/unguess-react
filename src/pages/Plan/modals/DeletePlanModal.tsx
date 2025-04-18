3dc0620a24ff5d8caa08e9bfb63ffd4be2c6e138
Kariamos <minucci9292@gmail.com>
2025-04-18 17:52:52 +0200

    feat(controls): add delete plan functionality with confirmation modal

---
+++ src/pages/Plan/modals/DeletePlanModal.tsx
@@ -0,0 +1,122 @@
+import {
+  Button,
+  FooterItem,
+  Label,
+  LG,
+  MD,
+  Message,
+  Modal,
+  ModalClose,
+  Notification,
+  OrderedList,
+  Skeleton,
+  SM,
+  Span,
+  useToast,
+  XL,
+} from '@appquality/unguess-design-system';
+import { Trans, useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+import { appTheme } from 'src/app/theme';
+import { Dots } from '@zendeskgarden/react-loaders';
+import { useDeletePlansByPidMutation } from 'src/features/api';
+import { usePlan } from '../hooks/usePlan';
+
+const DeletePlanModal = ({
+  onQuit,
+  planId,
+  planTitle,
+}: {
+  planId: string;
+  planTitle: string;
+  onQuit: () => void;
+}) => {
+  const { t } = useTranslation();
+  const { addToast } = useToast();
+  const navigate = useNavigate();
+
+  const [deletePlan, { isLoading }] = useDeletePlansByPidMutation();
+
+  const deletePlanId = async (plan: string) => {
+    if (!plan) return;
+    await deletePlan({ pid: plan })
+      .unwrap()
+      .then(() => {
+        navigate(`/`);
+      });
+  };
+
+  const handleConfirm = async () => {
+    try {
+      await deletePlanId(planId);
+    } catch (e) {
+      addToast(
+        ({ close }) => (
+          <Notification
+            onClose={close}
+            type="error"
+            message={
+              e instanceof Error && e.message
+                ? e.message
+                : t('__PLAN_PAGE_DELETE_PLAN_MODAL_ERROR')
+            }
+            closeText={t('__TOAST_CLOSE_TEXT')}
+            isPrimary
+          />
+        ),
+        { placement: 'top' }
+      );
+
+      return;
+    }
+    onQuit();
+  };
+
+  return (
+    <Modal onClose={onQuit} role="dialog" data-qa="delete-plan-modal">
+      <Modal.Header>
+        <Message validation="error" style={{ fontSize: appTheme.fontSizes.md }}>
+          {t('__PLAN_PAGE_DELETE_PLAN_MODAL_TITLE')}
+        </Message>
+      </Modal.Header>
+      <Modal.Body style={{ overflow: 'visible' }}>
+        <Trans
+          i18nKey="__PLAN_PAGE_DELETE_PLAN_MODAL_BODY"
+          components={{
+            md: <MD />,
+            boldSpan: <Span isBold />,
+          }}
+          values={{
+            planTitle,
+          }}
+          defaults=""
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <FooterItem>
+          <Button
+            isBasic
+            isDanger
+            disabled={isLoading}
+            onClick={handleConfirm}
+            data-qa="delete-plan-modal-cta"
+          >
+            {!isLoading ? (
+              t('__PLAN_PAGE_DELETE_PLAN_MODAL_BUTTON_CONFIRM')
+            ) : (
+              <Dots color={appTheme.palette.red[600]} />
+            )}
+          </Button>
+        </FooterItem>
+        <FooterItem>
+          <Button isAccent isPrimary disabled={isLoading} onClick={onQuit}>
+            {t('__PLAN_PAGE_DELETE_PLAN_MODAL_BUTTON_CANCEL')}
+          </Button>
+        </FooterItem>
+      </Modal.Footer>
+      <ModalClose />
+    </Modal>
+  );
+};
+
+export { DeletePlanModal };
