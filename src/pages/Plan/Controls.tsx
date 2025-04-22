773010818aa6b49d822ed22dbdad7eadc7d2189f
Kariamos <minucci9292@gmail.com>
2025-04-22 10:13:28 +0200

    refactor(controls): enhanced delete plan modal title retrieval using the useModule('title') current value instead of the saved value

--- src/pages/Plan/Controls.tsx
+++ src/pages/Plan/Controls.tsx
@@ -3,21 +3,19 @@ import {
   ButtonMenu,
   IconButton,
 } from '@appquality/unguess-design-system';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import { appTheme } from 'src/app/theme';
 import { Pipe } from 'src/common/components/Pipe';
 import { ReactComponent as DotsIcon } from '@zendeskgarden/svg-icons/src/16/overflow-vertical-stroke.svg';
-import {
-  useDeletePlansByPidMutation,
-  usePatchPlansByPidStatusMutation,
-} from 'src/features/api';
+import { usePatchPlansByPidStatusMutation } from 'src/features/api';
 import { useSubmit } from 'src/features/modules/useModuleConfiguration';
 import { useRequestQuotation } from 'src/features/modules/useRequestQuotation';
 import { useValidateForm } from 'src/features/planModules';
 import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
 import styled from 'styled-components';
+import { useModule } from 'src/features/modules/useModule';
 import { getPlanStatus } from '../Dashboard/hooks/getPlanStatus';
 import { usePlan } from './hooks/usePlan';
 import { SendRequestModal } from './modals/SendRequestModal';
@@ -41,6 +39,7 @@ export const Controls = () => {
   const { validateForm } = useValidateForm();
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [patchStatus] = usePatchPlansByPidStatusMutation();
+  const { value: titleValue } = useModule('title'); // to use the current changed title value (also if plan is not saved) in delete modal
 
   const campaignRoute = useLocalizeRoute(
     `campaigns/${plan?.campaign?.id ?? 0}`
@@ -150,11 +149,7 @@ export const Controls = () => {
       {isDeleteModalOpen && planId && (
         <DeletePlanModal
           planId={planId}
-          planTitle={
-            plan?.config.modules
-              .filter((m) => m.type === 'title')[0]
-              ?.output.toString() ?? ''
-          }
+          planTitle={titleValue?.output ?? ''}
           onQuit={() => setIsDeleteModalOpen(false)}
         />
       )}
