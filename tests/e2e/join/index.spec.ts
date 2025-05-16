eb23f2d2812abef743b9e38d790dad83cb0e6174
Gianpaolo <gianpaolosinatra@gmail.com>
2025-05-16 15:45:08 +0200

    test: Add end-to-end tests for Join page steps and logged-in user scenarios

---
+++ tests/e2e/join/index.spec.ts
@@ -0,0 +1,23 @@
+import { test } from '../../fixtures/app';
+
+test.describe('The Join page first step - case new user', () => {
+  test('before rendering there is a loader while evaluating invited or new user parameters from the url', async () => {});
+  test('display a form with user and password input, a CTA to go to the next step', async () => {});
+  test('the password input check if the password is strong enough', async () => {});
+  test('when the user click the next step cta we validate current inputs and if ok goes to the next step', async () => {});
+  test('display two links to go to app.unguess and a link to terms and conditions', async () => {});
+  test('the email input, once blurred check if email is already existing and if is valid', async () => {});
+});
+
+test.describe('The Join page second step', () => {
+  test('display a job role dropdown populated from api options', async () => {});
+  test('display back and next navigation, clicking on next validate this step', async () => {});
+  test('display two require inputs for name and surname', async () => {});
+});
+
+test.describe('The Join page third step', () => {
+  test('display a required text input for the workspace name', async () => {});
+  test('display back and submit-button, clicking on submit-button validate the whole form and calls the api post', async () => {});
+  test('if the POST answer with a projectId then redirect to /projects/{projectId}', async () => {});
+  test('if the POST does NOT answer with a projectId then go to the homepage', async () => {});
+});
---
+++ tests/e2e/join/invited-user.spec.ts
@@ -0,0 +1,13 @@
+import { test } from '../../fixtures/app';
+
+test.describe('The Join page first step - case invited user only', () => {
+  test('the email input, is precompiled (with invited user email from api) and disabled', async () => {});
+});
+
+test.describe('The Join page second step - case invited user only', () => {
+  test('display two inputs for name and surname precopiled if present in api response', async () => {});
+});
+
+test.describe('The Join page third step - case invited user only', () => {
+  test('display the workspace name input precompiled from api and disabled', async () => {});
+});
