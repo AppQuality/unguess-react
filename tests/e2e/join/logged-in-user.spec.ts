c1787e9771d34cae3de9851f4627d55cbd8454a9
Gianpaolo <gianpaolosinatra@gmail.com>
2025-05-16 15:45:08 +0200

    test: Add end-to-end tests for Join page steps and logged-in user scenarios

---
+++ tests/e2e/join/logged-in-user.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '../../fixtures/app';
+
+test.describe('The Join page - already logged in user:', () => {
+  test('If there is not a query parameter redirectTo the user is redirected to home', async () => {});
+  test('If there is a query parameter redirectTo, the user is redirected to a specific page', async () => {});
+});
