49a2efe21f54de521edf99d582751309ae9ba35b
Gianpaolo <gianpaolosinatra@gmail.com>
2025-05-16 17:56:57 +0200

    feat(join): add invite handling and loading state to JoinPage, update tests

--- src/pages/JoinPage/index.tsx
+++ src/pages/JoinPage/index.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
-import { Button, Logo } from '@appquality/unguess-design-system';
+import { Logo } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import {
-  useLocation,
-  useNavigate,
-  useParams,
-  useSearchParams,
-} from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useAppSelector } from 'src/app/hooks';
 import { GoogleTagManager } from 'src/common/GoogleTagManager';
-import { appTheme } from 'src/app/theme';
+import { useGetInvitesByProfileAndTokenQuery } from 'src/features/api';
 
 const StyledLogo = styled(Logo)`
   margin-top: ${({ theme }) => theme.space.xs};
@@ -41,12 +36,29 @@ const JoinPage = () => {
   const [searchParams] = useSearchParams();
   const { status } = useAppSelector((state) => state.user);
   const navigate = useNavigate();
+  const { profile, token } = useParams();
+  const shouldSkipQuery =
+    status === 'logged' || status === 'loading' || !(profile && token);
+
+  const { isLoading, data } = useGetInvitesByProfileAndTokenQuery(
+    {
+      profile: profile || '',
+      token: token || '',
+    },
+    {
+      skip: shouldSkipQuery,
+    }
+  );
 
   useEffect(() => {
     if (status === 'logged') {
       navigate(searchParams.get('redirectTo') || '/');
     }
   }, [navigate, status, searchParams]);
+
+  if (isLoading) {
+    return <div data-qa="join-page-loader">Loading...</div>;
+  }
 
   return (
     <GoogleTagManager title={t('__PAGE_TITLE_JOIN')}>
