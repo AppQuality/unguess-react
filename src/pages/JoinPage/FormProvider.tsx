f10a45b6535a67be709a954fc38e83731c36a611
Gianpaolo <gianpaolosinatra@gmail.com>
2025-05-19 15:17:20 +0200

    WIP

---
+++ src/pages/JoinPage/FormProvider.tsx
@@ -0,0 +1,3 @@
+export const FormProvider = ({ children }: { children: React.ReactNode }) => (
+  <div>{children}</div>
+);
--- tests/e2e/join/index.spec.ts
+++ tests/e2e/join/index.spec.ts
@@ -10,7 +10,7 @@ test.describe('The Join page first step - case new user', () => {
     await join.open();
   });
 
-  test('display a form with user and password input, a CTA to go to the next step', async () => {});
+  test('display a form with user email and password input, a CTA to go to the next step', async () => {});
   test('the password input check if the password is strong enough', async () => {});
   test('when the user click the next step cta we validate current inputs and if ok goes to the next step', async () => {});
   test('display two links to go to app.unguess and a link to terms and conditions', async () => {});
