d86335336951e5125ca441d34722bf42172b24ff
Gianpaolo <gianpaolosinatra@gmail.com>
2025-05-20 15:15:31 +0200

    feat(join): implement dynamic validation for  password fields in Step 1, enhance test coverage for password requirements

--- src/pages/JoinPage/FormProvider.tsx
+++ src/pages/JoinPage/FormProvider.tsx
@@ -40,26 +40,32 @@ export const FormProvider = ({
   const { t } = useTranslation();
 
   const validationSchema = {
-    email: yup
-      .string()
-      .required(t('SIGNUP_FORM_EMAIL_IS_REQUIRED'))
-      .email(t('SIGNUP_FORM_EMAIL_MUST_BE_A_VALID_EMAIL')),
-    password: yup
-      .string()
-      .min(6, t('SIGNUP_FORM_PASSWORD_MUST_BE_AT_LEAST_6_CHARACTER_LONG'))
-      .matches(
-        /[0-9]/,
-        t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_A_NUMBER')
-      )
-      .matches(
-        /[A-Z]/,
-        t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_AN_UPPERCASE_LETTER')
-      )
-      .matches(
-        /[a-z]/,
-        t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_A_LOWERCASE_LETTER')
-      )
-      .required(t('SIGNUP_FORM_PASSWORD_IS_A_REQUIRED_FIELD')),
+    email: yup.string().when('step', {
+      is: 1,
+      then: yup
+        .string()
+        .required(t('SIGNUP_FORM_EMAIL_IS_REQUIRED'))
+        .email(t('SIGNUP_FORM_EMAIL_MUST_BE_A_VALID_EMAIL')),
+    }),
+    password: yup.string().when('step', {
+      is: 1,
+      then: yup
+        .string()
+        .min(6, t('SIGNUP_FORM_PASSWORD_MUST_BE_AT_LEAST_6_CHARACTER_LONG'))
+        .matches(
+          /[0-9]/,
+          t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_A_NUMBER')
+        )
+        .matches(
+          /[A-Z]/,
+          t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_AN_UPPERCASE_LETTER')
+        )
+        .matches(
+          /[a-z]/,
+          t('SIGNUP_FORM_PASSWORD_MUST_CONTAIN_AT_LEAST_A_LOWERCASE_LETTER')
+        )
+        .required(t('SIGNUP_FORM_PASSWORD_IS_A_REQUIRED_FIELD')),
+    }),
     name: yup.string().when('step', {
       is: 2,
       then: yup.string().required(t('SIGNUP_FORM_NAME_IS_REQUIRED')),
@@ -82,17 +88,12 @@ export const FormProvider = ({
 
   // logic to check if the user is invited
   // for the time being we are checking if the mail is not empty
-  const isInvited = useMemo(() => {
-    return !!mail;
-  }, [mail]);
+  const isInvited = useMemo(() => !!mail, [mail]);
 
   return (
     <Formik
       initialValues={initialValues}
-      validateOnChange={false}
-      validateOnBlur={false}
-      validationSchema={validationSchema}
-      enableReinitialize={false}
+      validationSchema={yup.object(validationSchema)}
       initialStatus={{
         isInvited,
       }}
