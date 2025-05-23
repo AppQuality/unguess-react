1e521ce63afd3df61a275d4e2db625fd3ef0835a
Kariamos <minucci9292@gmail.com>
2025-05-23 12:52:30 +0200

    style(ButtonsContainer): adjust padding for consistent layout

--- src/pages/Plan/modules/Tasks/parts/modal/ButtonsContainer.tsx
+++ src/pages/Plan/modules/Tasks/parts/modal/ButtonsContainer.tsx
@@ -6,8 +6,8 @@ export const ButtonsContainer = styled.div`
   gap: ${({ theme }) => theme.space.xs};
   align-items: flex-start;
   padding-top: ${({ theme }) => theme.space.sm};
+  padding-bottom: ${({ theme }) => theme.space.sm};
   padding-right: 0;
-  padding-bottom: ${({ theme }) => 6 * theme.space.base}px;
   padding-left: 0;
 
   > button {
