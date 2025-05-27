1866c719e13d0878376ea5561a9431f077bfd8fa
iacopolea <iacopo.lea@gmail.com>
2025-05-27 11:06:03 +0200

    feat(join): add loading and error states to JoinPage with corresponding components

---
+++ src/pages/JoinPage/JoinPageLoading.tsx
@@ -0,0 +1,9 @@
+export const JoinPageLoading = () => {
+  return (
+    <div id="appq-loading-content">
+      <div className="loadingImg">
+        <img alt="loading" src="/static/unguess_loader_single.gif" />
+      </div>
+    </div>
+  );
+};
