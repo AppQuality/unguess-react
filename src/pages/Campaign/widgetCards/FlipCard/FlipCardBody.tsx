eb5b5a7fdb50e71ae39a28770fca532bc174cf10
iacopolea <iacopo.lea@gmail.com>
2025-06-05 16:36:04 +0200

    Revert "feat(FlipCard): integrate motion for animated transitions and cleanup unused styles"
    This reverts commit 46d122cb905d5cd1a72e8a94cd7588336d2ff56f.

--- src/pages/Campaign/widgetCards/FlipCard/FlipCardBody.tsx
+++ src/pages/Campaign/widgetCards/FlipCard/FlipCardBody.tsx
@@ -1,8 +1,68 @@
 import styled from 'styled-components';
 import { useFlipCardContext } from './context/FlipCardContext';
 import { FlipCardBodyProps } from './types';
-import { AnimatePresence } from 'motion/react';
-import * as motion from 'motion/react-client';
+
+const durationMilliseconds = 500;
+
+const FlipCardFaceContainer = styled.div<{ breakpoint: number }>`
+  container-type: inline-size;
+  .flipcard-face {
+    transform-style: preserve-3d;
+    animation-duration: ${durationMilliseconds}ms;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
+  }
+  .flipcard-face-back {
+    animation-name: show;
+  }
+  .flipcard-face-front {
+    animation-name: hide;
+  }
+  @container (min-width: ${(p) => p.breakpoint}px) {
+    .flipcard-face-front {
+      z-index: 2;
+    }
+    .flipcard-face-back {
+      z-index: 1;
+    }
+    &.flipcard.front {
+      .flipcard-face-front {
+        animation-name: show;
+      }
+      .flipcard-face-back {
+        animation-name: hide;
+      }
+    }
+    &.flipcard.back {
+      .flipcard-face-front {
+        animation-name: hide;
+      }
+      .flipcard-face-back {
+        animation-name: show;
+      }
+    }
+  }
+  @keyframes hide {
+    0% {
+      opacity: 1;
+      height: auto;
+    }
+    100% {
+      opacity: 0;
+      height: 0;
+    }
+  }
+  @keyframes show {
+    0% {
+      opacity: 0;
+      height: 0;
+    }
+    100% {
+      opacity: 1;
+      height: auto;
+    }
+  }
+`;
 
 const FlipCardFaceContent = styled.div`
   display: flex;
@@ -14,23 +74,19 @@ const FlipCardFaceContent = styled.div`
 `;
 
 export const FlipCardBody = ({ front, back }: FlipCardBodyProps) => {
-  const { visibleFace } = useFlipCardContext();
+  const { visibleFace, breakpoint } = useFlipCardContext();
 
   return (
-    <FlipCardFaceContent>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={visibleFace}
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: -10, opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          style={{ width: '100%', height: '100%' }}
-        >
-          {visibleFace === 'front' && front}
-          {visibleFace === 'back' && back}
-        </motion.div>
-      </AnimatePresence>
-    </FlipCardFaceContent>
+    <FlipCardFaceContainer
+      breakpoint={breakpoint}
+      className={`flipcard ${visibleFace}`}
+    >
+      <FlipCardFaceContent className="flipcard-face flipcard-face-back">
+        {back}
+      </FlipCardFaceContent>
+      <FlipCardFaceContent className="flipcard-face flipcard-face-front">
+        {front}
+      </FlipCardFaceContent>
+    </FlipCardFaceContainer>
   );
 };
