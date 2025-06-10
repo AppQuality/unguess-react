352a9b5cf8a5267729df701bfffb573e878f75f9
iacopolea <iacopo.lea@gmail.com>
2025-06-10 10:32:23 +0200

    feat(FlipCardBody): enhance mobile responsiveness and improve animation key handling

--- src/pages/Campaign/widgetCards/FlipCard/FlipCardBody.tsx
+++ src/pages/Campaign/widgetCards/FlipCard/FlipCardBody.tsx
@@ -3,6 +3,7 @@ import * as motion from 'motion/react-client';
 import styled from 'styled-components';
 import { useFlipCardContext } from './context/FlipCardContext';
 import { FlipCardBodyProps } from './types';
+import useWindowSize from 'src/hooks/useWindowSize';
 
 const FlipCardFaceContent = styled.div`
   display: flex;
@@ -14,21 +15,22 @@ const FlipCardFaceContent = styled.div`
 `;
 
 export const FlipCardBody = ({ front, back }: FlipCardBodyProps) => {
-  const { visibleFace } = useFlipCardContext();
+  const { visibleFace, breakpoint } = useFlipCardContext();
+  const { width } = useWindowSize();
+  const isMobile = width < breakpoint;
 
   return (
     <FlipCardFaceContent>
       <AnimatePresence mode="wait">
         <motion.div
-          key={visibleFace}
+          key={isMobile ? 'back' : visibleFace}
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -10, opacity: 0 }}
           transition={{ duration: 0.2 }}
           style={{ width: '100%', height: '100%' }}
         >
-          {visibleFace === 'front' && front}
-          {visibleFace === 'back' && back}
+          {isMobile ? back : visibleFace === 'front' ? front : back}
         </motion.div>
       </AnimatePresence>
     </FlipCardFaceContent>
