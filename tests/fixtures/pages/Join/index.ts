0e7b13f5c9592dbfc7a677a948377cce03a3efa9
Davide Bizzi <davide.bizzi@unguess.io>
2025-06-26 17:31:44 +0200

    test: Update fixtures 11

--- tests/fixtures/pages/Join/index.ts
+++ tests/fixtures/pages/Join/index.ts
@@ -1,6 +1,6 @@
 import { type Page } from '@playwright/test';
-import { UnguessPage } from '../../UnguessPage';
 import validInvitedUser from '../../../api/invites/profile/token/_get/200_Example_1.json';
+import { UnguessPage } from '../../UnguessPage';
 
 export class Join extends UnguessPage {
   readonly page: Page;
@@ -63,7 +63,7 @@ export class Join extends UnguessPage {
     await this.page.route(`*/**/api/users`, async (route) => {
       if (route.request().method() === 'POST') {
         await route.fulfill({
-          path: 'tests/api/users/_post/200_projectId.json',
+          path: 'tests/api/users/_post/201_new_user.json',
         });
       } else {
         await route.fallback();
@@ -76,7 +76,7 @@ export class Join extends UnguessPage {
     await this.page.route(`*/**/api/users`, async (route) => {
       if (route.request().method() === 'POST') {
         await route.fulfill({
-          path: 'tests/api/users/_post/200_Example_1.json',
+          path: 'tests/api/users/_post/201_Invited_user.json',
         });
       } else {
         await route.fallback();
