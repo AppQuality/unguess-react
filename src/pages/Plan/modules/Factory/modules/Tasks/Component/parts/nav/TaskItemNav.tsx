af63f4d98d2fc5462822f976619e6a0a0fa7da0d
iacopolea <iacopo.lea@gmail.com>
2025-07-04 16:19:22 +0200

    wip(tasks): init draggable task list using @atlaskit/pragmatic-drag-and-drop

--- src/pages/Plan/modules/Factory/modules/Tasks/Component/parts/nav/TaskItemNav.tsx
+++ src/pages/Plan/modules/Factory/modules/Tasks/Component/parts/nav/TaskItemNav.tsx
@@ -1,8 +1,9 @@
 import {
-  Card,
   Ellipsis,
   MD,
   Message,
+  Draggable,
+  DraggableList,
   Span,
 } from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
@@ -12,25 +13,36 @@ import { components } from 'src/common/schema';
 import styled from 'styled-components';
 import { useModuleTasks } from '../../hooks';
 import { getIconFromTaskOutput } from '../../utils';
+import { useEffect, useRef, useState } from 'react';
+import { draggable } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 
-const StyledCard = styled(Card)`
-  padding: ${({ theme }) => `${theme.space.sm} ${theme.space.md}`};
-  margin: ${({ theme }) => theme.space.xs} 0;
-  background-color: transparent;
-`;
-
-const StyledContainer = styled.div`
+const StyledDraggableContent = styled(Draggable.Content)`
+  min-width: 0;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   gap: ${({ theme }) => theme.space.sm};
 `;
 
+const StyledDraggableListItem = styled(DraggableList.Item)`
+  display: block;
+`;
+
+const StyledDraggable = styled(Draggable)<{ $dragging: boolean }>`
+  opacity: ${({ $dragging }) => ($dragging ? 0.5 : 1)};
+`;
+
 const TaskItemNavLink = styled(Link)`
+  display: block;
+  text-decoration: none !important;
   &.isCurrent {
-    ${StyledCard} {
-      background-color: white;
-    }
+    background-color: white;
+  }
+  &.hasError {
+    border-color: ${({ theme }) => theme.palette.red[600]};
+  }
+  ${Draggable} {
+    background-color: transparent;
   }
 `;
 
@@ -48,6 +60,20 @@ const TaskItemNav = ({
   const { t } = useTranslation();
   const { error } = useModuleTasks();
   const { key } = task;
+  const ref = useRef(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (!el) {
+      return;
+    }
+    return draggable({
+      element: el,
+      onDragStart: () => setDragging(true),
+      onDrop: () => setDragging(false),
+    });
+  }, []);
 
   const titleError =
     error && typeof error === 'object' && `tasks.${key}.title` in error
@@ -66,48 +92,45 @@ const TaskItemNav = ({
   const hasPlaceholder = !task.title;
 
   return (
-    <TaskItemNavLink
-      to={`task-${key + 1}`}
-      containerId="main"
-      duration={500}
-      offset={-200}
-      smooth
-      spy
-      isDynamic
-      style={{ textDecoration: 'none' }}
-      activeClass="isCurrent"
-    >
-      <StyledCard
-        key={key}
-        data-qa="task-item-nav"
-        {...(hasErrors && {
-          style: {
-            borderColor: appTheme.palette.red[900],
-          },
-        })}
+    <StyledDraggableListItem key={key} data-qa="task-item-nav">
+      <TaskItemNavLink
+        to={`task-${key + 1}`}
+        containerId="main"
+        duration={500}
+        offset={-200}
+        smooth
+        spy
+        isDynamic
+        activeClass="isCurrent"
       >
-        <StyledContainer>
-          <ModuleIconContainer>
-            {getIconFromTaskOutput(task)}
-          </ModuleIconContainer>
-          <MD color={appTheme.palette.blue[600]} style={{ minWidth: '10px' }}>
-            <Ellipsis title={task.title}>
-              {key + 1}.{' '}
-              <Span isBold>
-                {hasPlaceholder
-                  ? t('__PLAN_PAGE_MODULE_TASKS_TASK_TITLE_PLACEHOLDER_EMPTY')
-                  : task.title}
-              </Span>
-            </Ellipsis>
-          </MD>
-        </StyledContainer>
-        {hasErrors && (
-          <Message validation="error" style={{ marginTop: appTheme.space.sm }}>
-            {t('__PLAN_PAGE_MODULE_TASKS_GENERIC_TASK_ERROR')}
-          </Message>
-        )}
-      </StyledCard>
-    </TaskItemNavLink>
+        <StyledDraggable ref={ref} $dragging={dragging}>
+          <Draggable.Grip style={{ cursor: 'grab' }} />
+          <StyledDraggableContent>
+            <ModuleIconContainer>
+              {getIconFromTaskOutput(task)}
+            </ModuleIconContainer>
+            <MD color={appTheme.palette.blue[600]} style={{ minWidth: '10px' }}>
+              <Ellipsis title={task.title}>
+                {key + 1}.{' '}
+                <Span isBold>
+                  {hasPlaceholder
+                    ? t('__PLAN_PAGE_MODULE_TASKS_TASK_TITLE_PLACEHOLDER_EMPTY')
+                    : task.title}
+                </Span>
+              </Ellipsis>
+            </MD>
+            {hasErrors && (
+              <Message
+                validation="error"
+                style={{ marginTop: appTheme.space.sm }}
+              >
+                {t('__PLAN_PAGE_MODULE_TASKS_GENERIC_TASK_ERROR')}
+              </Message>
+            )}
+          </StyledDraggableContent>
+        </StyledDraggable>
+      </TaskItemNavLink>
+    </StyledDraggableListItem>
   );
 };
 
--- src/pages/Plan/modules/Factory/modules/Tasks/Component/parts/nav/TasksListNav.tsx
+++ src/pages/Plan/modules/Factory/modules/Tasks/Component/parts/nav/TasksListNav.tsx
@@ -1,15 +1,69 @@
+import { useState } from 'react';
 import { useModuleTasks } from '../../hooks';
 import { TaskItemNav } from './TaskItemNav';
+import { DraggableList } from '@appquality/unguess-design-system';
 
 const TasksListNav = () => {
   const { value } = useModuleTasks();
+  const [tasks, setTasks] = useState<typeof value>(value);
+
+  // useEffect(() => {
+  //   return monitorForElements({
+  //     canMonitor({ source }) {
+  //       return isTaskData(source.data);
+  //     },
+  //     onDrop({ location, source }) {
+  //       const target = location.current.dropTargets[0];
+  //       if (!target) {
+  //         return;
+  //       }
+
+  //       const sourceData = source.data;
+  //       const targetData = target.data;
+
+  //       if (!isTaskData(sourceData) || !isTaskData(targetData)) {
+  //         return;
+  //       }
+
+  //       const indexOfSource = tasks.findIndex((task) => task.id === sourceData.taskId);
+  //       const indexOfTarget = tasks.findIndex((task) => task.id === targetData.taskId);
+
+  //       if (indexOfTarget < 0 || indexOfSource < 0) {
+  //         return;
+  //       }
+
+  //       const closestEdgeOfTarget = extractClosestEdge(targetData);
+
+  //       // Using `flushSync` so we can query the DOM straight after this line
+  //       flushSync(() => {
+  //         setTasks(
+  //           reorderWithEdge({
+  //             list: tasks,
+  //             startIndex: indexOfSource,
+  //             indexOfTarget,
+  //             closestEdgeOfTarget,
+  //             axis: 'vertical',
+  //           }),
+  //         );
+  //       });
+  //       // Being simple and just querying for the task after the drop.
+  //       // We could use react context to register the element in a lookup,
+  //       // and then we could retrieve that element after the drop and use
+  //       // `triggerPostMoveFlash`. But this gets the job done.
+  //       const element = document.querySelector(`[data-task-id="${sourceData.taskId}"]`);
+  //       if (element instanceof HTMLElement) {
+  //         triggerPostMoveFlash(element);
+  //       }
+  //     },
+  //   });
+  // }, [tasks]);
 
   return (
-    <div data-qa="tasks-module-nav">
-      {value.map((task) => (
+    <DraggableList data-qa="tasks-module-nav">
+      {tasks.map((task) => (
         <TaskItemNav key={task.key} task={task} />
       ))}
-    </div>
+    </DraggableList>
   );
 };
 
