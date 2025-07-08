563b1332afb3fc1d14336e34e0dfa3bcad880092
Gianpaolo <gianpaolosinatra@gmail.com>
2025-07-08 18:19:16 +0200

    feat(profile): enhance user profile form with role selection and email handling

--- src/pages/Profile/FormProvider.tsx
+++ src/pages/Profile/FormProvider.tsx
@@ -1,11 +1,14 @@
 import { Formik } from 'formik';
+import { useGetUsersMeQuery } from 'src/features/api';
 import { ProfileFormValues } from './valuesType';
 
 export const FormProvider = ({ children }: { children: React.ReactNode }) => {
+  const { data } = useGetUsersMeQuery();
   const initialValues: ProfileFormValues = {
     roleId: 0,
-    name: '',
-    surname: '',
+    name: data?.first_name || '',
+    surname: data?.last_name || '',
+    email: data?.email || '',
   };
   const onSubmit = async (values: ProfileFormValues) =>
     console.log(`Submitted values: ${JSON.stringify(values, null, 2)}`);
