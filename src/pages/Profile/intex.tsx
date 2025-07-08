05c301e6210c028f2ee7fc01c061626bf32f7d91
Gianpaolo <gianpaolosinatra@gmail.com>
2025-07-08 16:18:47 +0200

    feat(profile): implement ProfileContext and update Profile component with user input fields

--- src/pages/Profile/intex.tsx
+++ src/pages/Profile/intex.tsx
@@ -7,36 +7,51 @@ import {
   Card,
   Col,
   Grid,
+  Input,
+  Label,
   LG,
+  Message,
   Row,
+  Select,
+  Span,
   theme,
-  Title,
 } from '@appquality/unguess-design-system';
 import { ReactComponent as KeyIcon } from 'src/assets/icons/key.svg';
 import { ReactComponent as UserIcon } from 'src/assets/icons/user.svg';
 import { appTheme } from 'src/app/theme';
-import styled from 'styled-components';
 import {
   AsideNav,
   StickyNavItem,
   StickyNavItemLabel,
   StyledDivider,
 } from 'src/common/components/navigation/asideNav';
-import { useProfileContext } from './ProfileContext';
+import { Field, FieldProps, useFormikContext } from 'formik';
+import { useGetUsersRolesQuery } from 'src/features/api';
+import { useMemo, useRef } from 'react';
+import { ProfileFormValues } from './valuesType';
 
 const Profile = () => {
   const { t } = useTranslation();
-  // const { openAccordions, setOpenAccordions } = useProfileContext();
-  // const isOpen = openAccordions.includes('password');
+
   const isOpen = false; // Temporary, as the context is not fully implemented yet
 
-  // const handleAccordionChange = () => {
-  //   if (isOpen) {
-  //     setOpenAccordions(openAccordions.filter((item) => item !== 'password'));
-  //   } else {
-  //     setOpenAccordions([...openAccordions, 'password']);
-  //   }
-  // };
+  const { setFieldValue, validateForm, setTouched, status } =
+    useFormikContext<ProfileFormValues>();
+  const { data, isLoading } = useGetUsersRolesQuery();
+  const renderOptions = useMemo(
+    () =>
+      isLoading || !data ? (
+        <Select.Option value="loading">loading...</Select.Option>
+      ) : (
+        data?.map((role) => (
+          <Select.Option key={role.id} value={role.id.toString()}>
+            {role.name}
+          </Select.Option>
+        ))
+      ),
+    [data]
+  );
+  const selectRef = useRef<HTMLDivElement>(null);
 
   return (
     <Page
@@ -94,6 +109,87 @@ const Profile = () => {
                     <LG style={{ color: theme.palette.blue[600] }}>
                       <UserIcon /> {t('__PROFILE_PAGE_USER_CARD_LABEL')}
                     </LG>
+                    <div>
+                      <Label>{t('__PROFILE_PAGE_USER_CARD_NAME_LABEL')}</Label>
+                      <Input
+                        type="text"
+                        placeholder={t(
+                          '__PROFILE_PAGE_USER_NAME_INPUT_PLACEHOLDER'
+                        )}
+                        style={{ width: '100%' }}
+                      />
+                    </div>
+                    <div>
+                      <Label>
+                        {t('__PROFILE_PAGE_USER_CARD_SURNAME_LABEL')}
+                      </Label>
+                      <Input
+                        type="text"
+                        placeholder={t(
+                          '__PROFILE_PAGE_USER_SURNAME_INPUT_PLACEHOLDER'
+                        )}
+                        style={{ width: '100%' }}
+                      />
+                    </div>
+                    <div>
+                      <Label>{t('__PROFILE_PAGE_USER_CARD_EMAIL_LABEL')}</Label>
+                      <Input disabled type="text" style={{ width: '100%' }} />
+                    </div>
+                    <div>
+                      <Label>{t('__PROFILE_PAGE_USER_CARD_ROLE_LABEL')}</Label>
+                      {/* <Field name="roleId">
+                        {({ field, meta }: FieldProps) => {
+                          const hasError = meta.touched && Boolean(meta.error);
+                          return (
+                            <div ref={selectRef}>
+                              <Select
+                                data-qa="roleId-select"
+                                {...field}
+                                renderValue={(value) =>
+                                  data?.find(
+                                    (role) =>
+                                      role.id === Number(value.inputValue)
+                                  )?.name
+                                }
+                                isCompact
+                                inputValue={field.value}
+                                selectionValue={field.value}
+                                label={
+                                  <>
+                                    {t('SIGNUP_FORM_ROLE_LABEL')}
+                                    <Span
+                                      style={{
+                                        color: appTheme.palette.red[600],
+                                      }}
+                                    >
+                                      *
+                                    </Span>
+                                  </>
+                                }
+                                onSelect={(roleId) => {
+                                  setFieldValue('roleId', Number(roleId));
+                                  (
+                                    selectRef.current?.querySelector(
+                                      '[role="combobox"]'
+                                    ) as HTMLElement | null
+                                  )?.blur();
+                                }}
+                              >
+                                {renderOptions}
+                              </Select>
+                              {hasError && (
+                                <Message
+                                  data-qa="update-profile-role-error"
+                                  validation="error"
+                                >
+                                  {meta.error}
+                                </Message>
+                              )}
+                            </div>
+                          );
+                        }}
+                      </Field> */}
+                    </div>
                     <div>
                       <Button
                         isAccent
