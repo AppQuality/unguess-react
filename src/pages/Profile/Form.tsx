7c07b0df475c866ddc1eb8bff78b61621dfe3c2e
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-07-09 15:02:34 +0200

    feat(profile): refactor profile form and add user role handling with validation

--- src/pages/Profile/Form.tsx
+++ src/pages/Profile/Form.tsx
@@ -1,6 +1,5 @@
 import {
   AccordionNew,
-  Button,
   Card,
   Col,
   FormField,
@@ -9,54 +8,33 @@ import {
   MediaInput,
   Message,
   Row,
-  Select,
   Span,
-  SpecialCard,
-  Tag,
-  theme,
 } from '@appquality/unguess-design-system';
+import { ReactComponent as Eye } from '@zendeskgarden/svg-icons/src/16/eye-fill.svg';
+import { ReactComponent as EyeHide } from '@zendeskgarden/svg-icons/src/16/eye-hide-fill.svg';
 import { Field, FieldProps, useFormikContext } from 'formik';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
 import { ReactComponent as KeyIcon } from 'src/assets/icons/key.svg';
-import { ReactComponent as UserIcon } from 'src/assets/icons/user.svg';
-import { useGetUsersRolesQuery } from 'src/features/api';
-import { ReactComponent as Eye } from '@zendeskgarden/svg-icons/src/16/eye-fill.svg';
-import { ReactComponent as EyeHide } from '@zendeskgarden/svg-icons/src/16/eye-hide-fill.svg';
-import styled from 'styled-components';
-import { ProfileFormValues } from './valuesType';
+import { ProfileCard } from './parts/ProfileCard';
 import { PasswordRequirements } from './PasswordRequirements';
-
-// styled card header
-const StyledCardHeader = styled.div``;
+import { ProfileFormValues } from './valuesType';
 
 export const Form = () => {
   const { t } = useTranslation();
-  const { data: roleData, isLoading: roleIsLoading } = useGetUsersRolesQuery();
-  const renderOptions = useMemo(
-    () =>
-      roleIsLoading || !roleData ? (
-        <Select.Option value="loading">loading...</Select.Option>
-      ) : (
-        roleData?.map((role) => (
-          <Select.Option key={role.id} value={role.id.toString()}>
-            {role.name}
-          </Select.Option>
-        ))
-      ),
-    [roleData]
-  );
 
   const [inputType, setInputType] = useState('password');
   const handleChangeInputType = () => {
     setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
   };
 
-  const selectRef = useRef<HTMLDivElement>(null);
+  const {
+    setFieldValue,
+    validateForm,
+    values: formValues,
+  } = useFormikContext<ProfileFormValues>();
 
-  const { setFieldValue, validateForm, setTouched, status } =
-    useFormikContext<ProfileFormValues>();
   const isOpen = false; // Temporary, as the context is not fully implemented yet
 
   // On change of the form, console log the form values
@@ -69,169 +47,7 @@ export const Form = () => {
   }, [setFieldValue, validateForm]);
   return (
     <>
-      <SpecialCard
-        id="anchor-profile"
-        style={{ marginBottom: theme.space.xxl }}
-        title={t('__PROFILE_PAGE_USER_CARD_LABEL')}
-      >
-        <SpecialCard.Meta>
-          <Tag size="large" isPill>
-            <Tag.Avatar>
-              <UserIcon
-                style={{
-                  color: theme.palette.blue[600],
-                }}
-              />
-            </Tag.Avatar>
-            {t('__PROFILE_PAGE_USER_CARD_LABEL')}
-          </Tag>
-        </SpecialCard.Meta>
-        <SpecialCard.Body>
-          <Row>
-            <Col>
-              <Field name="name">
-                {({ field, meta }: FieldProps) => {
-                  const hasError = meta.touched && Boolean(meta.error);
-                  return (
-                    <FormField>
-                      <Label>
-                        {t('__PROFILE_PAGE_USER_CARD_NAME_LABEL')}
-                        <Span style={{ color: appTheme.palette.red[600] }}>
-                          *
-                        </Span>
-                      </Label>
-                      <Input
-                        type="text"
-                        {...field}
-                        placeholder={t('SIGNUP_FORM_NAME_PLACEHOLDER')}
-                        {...(hasError && { validation: 'error' })}
-                      />
-                      {hasError && (
-                        <Message data-qa="signup-name-error" validation="error">
-                          {meta.error}
-                        </Message>
-                      )}
-                    </FormField>
-                  );
-                }}
-              </Field>
-            </Col>
-            <Col>
-              <Field name="surname">
-                {({ field, meta }: FieldProps) => {
-                  const hasError = meta.touched && Boolean(meta.error);
-                  return (
-                    <FormField>
-                      <Label>
-                        {t('__PROFILE_PAGE_USER_CARD_SURNAME_LABEL')}
-                        <Span style={{ color: appTheme.palette.red[600] }}>
-                          *
-                        </Span>
-                      </Label>
-                      <Input
-                        type="text"
-                        {...field}
-                        placeholder={t('SIGNUP_FORM_SURNAME_PLACEHOLDER')}
-                        {...(hasError && { validation: 'error' })}
-                      />
-                      {hasError && (
-                        <Message
-                          data-qa="signup-surname-error"
-                          validation="error"
-                        >
-                          {meta.error}
-                        </Message>
-                      )}
-                    </FormField>
-                  );
-                }}
-              </Field>
-            </Col>
-          </Row>
-
-          <Field name="email">
-            {({ field }: FieldProps) => (
-              <FormField>
-                <Label>
-                  {t('__PROFILE_PAGE_USER_CARD_EMAIL_LABEL')}
-                  <Span style={{ color: appTheme.palette.red[600] }}> *</Span>
-                </Label>
-                <Input
-                  disabled
-                  type="email"
-                  role="textbox"
-                  title="Email"
-                  {...field}
-                />
-              </FormField>
-            )}
-          </Field>
-          <Field name="roleId">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && Boolean(meta.error);
-              return (
-                <div ref={selectRef}>
-                  <Select
-                    // TODO: CHECK placeholder not working
-                    placeholder={t('__PROFILE_PAGE_USER_CARD_ROLE_PLACEHOLDER')}
-                    data-qa="roleId-select"
-                    {...field}
-                    renderValue={(value) =>
-                      roleData?.find(
-                        (role) => role.id === Number(value.inputValue)
-                      )?.name
-                    }
-                    isCompact
-                    inputValue={field.value}
-                    selectionValue={field.value}
-                    label={
-                      <>
-                        {t('__PROFILE_PAGE_USER_CARD_ROLE_LABEL')}
-                        <Span
-                          style={{
-                            color: appTheme.palette.red[600],
-                          }}
-                        >
-                          *
-                        </Span>
-                      </>
-                    }
-                    onSelect={(roleId) => {
-                      setFieldValue('roleId', Number(roleId));
-                      (
-                        selectRef.current?.querySelector(
-                          '[role="combobox"]'
-                        ) as HTMLElement | null
-                      )?.blur();
-                    }}
-                  >
-                    {renderOptions}
-                  </Select>
-                  {hasError && (
-                    <Message
-                      data-qa="update-profile-role-error"
-                      validation="error"
-                    >
-                      {meta.error}
-                    </Message>
-                  )}
-                </div>
-              );
-            }}
-          </Field>
-          <div>
-            <Button
-              isAccent
-              isPrimary
-              onClick={() => {
-                alert('User profile update functionality not implemented yet.');
-              }}
-            >
-              Save changes
-            </Button>
-          </div>
-        </SpecialCard.Body>
-      </SpecialCard>
+      <ProfileCard />
       <Card>
         <AccordionNew
           level={3}
--- src/pages/Profile/FormProvider.tsx
+++ src/pages/Profile/FormProvider.tsx
@@ -1,20 +1,69 @@
-import { Formik } from 'formik';
-import { useGetUsersMeQuery } from 'src/features/api';
+import { Formik, FormikHelpers } from 'formik';
+import { useGetUsersMeQuery, usePatchUsersMeMutation } from 'src/features/api';
+import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next';
 import { ProfileFormValues } from './valuesType';
+import { useProfileData } from './useProfileData';
 
 export const FormProvider = ({ children }: { children: React.ReactNode }) => {
-  const { data } = useGetUsersMeQuery();
+  const { t } = useTranslation();
+  const { data, isLoading } = useProfileData();
+  const [updateProfile] = usePatchUsersMeMutation();
+
   const initialValues: ProfileFormValues = {
-    roleId: 0,
-    name: data?.first_name || '',
-    surname: data?.last_name || '',
+    role: data?.role || '',
+    roleId: data?.roleId || 0,
+    name: data?.name || '',
+    surname: data?.surname || '',
     email: data?.email || '',
   };
-  const onSubmit = async (values: ProfileFormValues) =>
-    console.log(`Submitted values: ${JSON.stringify(values, null, 2)}`);
+
+  const onSubmit = async (
+    values: ProfileFormValues,
+    actions: FormikHelpers<ProfileFormValues>
+  ) => {
+    const { setSubmitting } = actions;
+
+    if (values) {
+      setSubmitting(true);
+
+      try {
+        await updateProfile({
+          body: {
+            name: values.name,
+            surname: values.surname,
+            roleId: values.roleId,
+          },
+        }).unwrap();
+        console.log('Profile updated successfully');
+      } catch (error) {
+        console.error('Error updating profile:', error);
+      }
+
+      console.log(`Submitted values: ${JSON.stringify(values, null, 2)}`);
+
+      setSubmitting(false);
+    }
+  };
+
+  if (isLoading) return <>Loading...</>;
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required(t('__PROFILE_PAGE_NAME_REQUIRED_ERROR')),
+    surname: Yup.string().required(t('__PROFILE_PAGE_SURNAME_REQUIRED_ERROR')),
+    roleId: Yup.number()
+      .min(1)
+      .required(t('__PROFILE_PAGE_ROLE_REQUIRED_ERROR')),
+  });
 
   return (
-    <Formik onSubmit={onSubmit} initialValues={initialValues}>
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={schema}
+      enableReinitialize
+      validateOnChange
+    >
       {children}
     </Formik>
   );
