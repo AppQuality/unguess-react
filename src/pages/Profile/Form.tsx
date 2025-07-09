14a755178dc5f536f92b21fc0952a2a71ebd0ccf
Gianpaolo <gianpaolosinatra@gmail.com>
2025-07-09 12:03:13 +0200

    feat(profile): add password confirmation fields and requirements to profile form

--- src/pages/Profile/Form.tsx
+++ src/pages/Profile/Form.tsx
@@ -6,21 +6,30 @@ import {
   FormField,
   Input,
   Label,
-  LG,
+  MediaInput,
   Message,
   Row,
   Select,
   Span,
+  SpecialCard,
+  Tag,
   theme,
 } from '@appquality/unguess-design-system';
 import { Field, FieldProps, useFormikContext } from 'formik';
-import { useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
 import { ReactComponent as KeyIcon } from 'src/assets/icons/key.svg';
 import { ReactComponent as UserIcon } from 'src/assets/icons/user.svg';
 import { useGetUsersRolesQuery } from 'src/features/api';
+import { ReactComponent as Eye } from '@zendeskgarden/svg-icons/src/16/eye-fill.svg';
+import { ReactComponent as EyeHide } from '@zendeskgarden/svg-icons/src/16/eye-hide-fill.svg';
+import styled from 'styled-components';
 import { ProfileFormValues } from './valuesType';
+import { PasswordRequirements } from './PasswordRequirements';
+
+// styled card header
+const StyledCardHeader = styled.div``;
 
 export const Form = () => {
   const { t } = useTranslation();
@@ -38,173 +47,191 @@ export const Form = () => {
       ),
     [roleData]
   );
+
+  const [inputType, setInputType] = useState('password');
+  const handleChangeInputType = () => {
+    setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
+  };
+
   const selectRef = useRef<HTMLDivElement>(null);
 
   const { setFieldValue, validateForm, setTouched, status } =
     useFormikContext<ProfileFormValues>();
   const isOpen = false; // Temporary, as the context is not fully implemented yet
+
+  // On change of the form, console log the form values
+  useEffect(() => {
+    const logFormValues = async () => {
+      const values = await validateForm();
+      console.log(`Form values: ${JSON.stringify(values, null, 2)}`);
+    };
+    logFormValues();
+  }, [setFieldValue, validateForm]);
   return (
     <>
-      <Card
+      <SpecialCard
         id="anchor-profile"
         style={{ marginBottom: theme.space.xxl }}
         title={t('__PROFILE_PAGE_USER_CARD_LABEL')}
       >
-        <Row>
-          <UserIcon
-            style={{
-              color: theme.palette.blue[600],
-              marginRight: theme.space.base,
-            }}
-          />
-          <LG style={{ color: theme.palette.grey[800] }}>
-            {t('__PROFILE_PAGE_USER_CARD_LABEL')}
-          </LG>
-        </Row>
-        <Row>
-          <Col>
-            <Field name="name">
-              {({ field, meta }: FieldProps) => {
-                const hasError = meta.touched && Boolean(meta.error);
-                return (
-                  <FormField>
-                    <Label>
-                      {t('__PROFILE_PAGE_USER_CARD_NAME_LABEL')}
-                      <Span style={{ color: appTheme.palette.red[600] }}>
-                        *
-                      </Span>
-                    </Label>
-                    <Input
-                      type="text"
-                      {...field}
-                      placeholder={t('SIGNUP_FORM_NAME_PLACEHOLDER')}
-                      {...(hasError && { validation: 'error' })}
-                    />
-                    {hasError && (
-                      <Message data-qa="signup-name-error" validation="error">
-                        {meta.error}
-                      </Message>
-                    )}
-                  </FormField>
-                );
-              }}
-            </Field>
-          </Col>
-          <Col>
-            <Field name="surname">
-              {({ field, meta }: FieldProps) => {
-                const hasError = meta.touched && Boolean(meta.error);
-                return (
-                  <FormField>
-                    <Label>
-                      {t('__PROFILE_PAGE_USER_CARD_SURNAME_LABEL')}
-                      <Span style={{ color: appTheme.palette.red[600] }}>
-                        *
-                      </Span>
-                    </Label>
-                    <Input
-                      type="text"
-                      {...field}
-                      placeholder={t('SIGNUP_FORM_SURNAME_PLACEHOLDER')}
-                      {...(hasError && { validation: 'error' })}
-                    />
-                    {hasError && (
-                      <Message
-                        data-qa="signup-surname-error"
-                        validation="error"
-                      >
-                        {meta.error}
-                      </Message>
-                    )}
-                  </FormField>
-                );
-              }}
-            </Field>
-          </Col>
-        </Row>
-
-        <Field name="email">
-          {({ field }: FieldProps) => (
-            <FormField>
-              <Label>
-                {t('__PROFILE_PAGE_USER_CARD_EMAIL_LABEL')}
-                <Span style={{ color: appTheme.palette.red[600] }}> *</Span>
-              </Label>
-              <Input
-                disabled
-                type="email"
-                role="textbox"
-                title="Email"
-                {...field}
+        <SpecialCard.Meta>
+          <Tag size="large" isPill>
+            <Tag.Avatar>
+              <UserIcon
+                style={{
+                  color: theme.palette.blue[600],
+                }}
               />
-            </FormField>
-          )}
-        </Field>
-        <Field name="roleId">
-          {({ field, meta }: FieldProps) => {
-            const hasError = meta.touched && Boolean(meta.error);
-            return (
-              <div ref={selectRef}>
-                <Select
-                  // TODO: CHECK placeholder not working
-                  placeholder={t('__PROFILE_PAGE_USER_CARD_ROLE_PLACEHOLDER')}
-                  data-qa="roleId-select"
+            </Tag.Avatar>
+            {t('__PROFILE_PAGE_USER_CARD_LABEL')}
+          </Tag>
+        </SpecialCard.Meta>
+        <SpecialCard.Body>
+          <Row>
+            <Col>
+              <Field name="name">
+                {({ field, meta }: FieldProps) => {
+                  const hasError = meta.touched && Boolean(meta.error);
+                  return (
+                    <FormField>
+                      <Label>
+                        {t('__PROFILE_PAGE_USER_CARD_NAME_LABEL')}
+                        <Span style={{ color: appTheme.palette.red[600] }}>
+                          *
+                        </Span>
+                      </Label>
+                      <Input
+                        type="text"
+                        {...field}
+                        placeholder={t('SIGNUP_FORM_NAME_PLACEHOLDER')}
+                        {...(hasError && { validation: 'error' })}
+                      />
+                      {hasError && (
+                        <Message data-qa="signup-name-error" validation="error">
+                          {meta.error}
+                        </Message>
+                      )}
+                    </FormField>
+                  );
+                }}
+              </Field>
+            </Col>
+            <Col>
+              <Field name="surname">
+                {({ field, meta }: FieldProps) => {
+                  const hasError = meta.touched && Boolean(meta.error);
+                  return (
+                    <FormField>
+                      <Label>
+                        {t('__PROFILE_PAGE_USER_CARD_SURNAME_LABEL')}
+                        <Span style={{ color: appTheme.palette.red[600] }}>
+                          *
+                        </Span>
+                      </Label>
+                      <Input
+                        type="text"
+                        {...field}
+                        placeholder={t('SIGNUP_FORM_SURNAME_PLACEHOLDER')}
+                        {...(hasError && { validation: 'error' })}
+                      />
+                      {hasError && (
+                        <Message
+                          data-qa="signup-surname-error"
+                          validation="error"
+                        >
+                          {meta.error}
+                        </Message>
+                      )}
+                    </FormField>
+                  );
+                }}
+              </Field>
+            </Col>
+          </Row>
+
+          <Field name="email">
+            {({ field }: FieldProps) => (
+              <FormField>
+                <Label>
+                  {t('__PROFILE_PAGE_USER_CARD_EMAIL_LABEL')}
+                  <Span style={{ color: appTheme.palette.red[600] }}> *</Span>
+                </Label>
+                <Input
+                  disabled
+                  type="email"
+                  role="textbox"
+                  title="Email"
                   {...field}
-                  renderValue={(value) =>
-                    roleData?.find(
-                      (role) => role.id === Number(value.inputValue)
-                    )?.name
-                  }
-                  isCompact
-                  inputValue={field.value}
-                  selectionValue={field.value}
-                  label={
-                    <>
-                      {t('__PROFILE_PAGE_USER_CARD_ROLE_LABEL')}
-                      <Span
-                        style={{
-                          color: appTheme.palette.red[600],
-                        }}
-                      >
-                        *
-                      </Span>
-                    </>
-                  }
-                  onSelect={(roleId) => {
-                    setFieldValue('roleId', Number(roleId));
-                    (
-                      selectRef.current?.querySelector(
-                        '[role="combobox"]'
-                      ) as HTMLElement | null
-                    )?.blur();
-                  }}
-                >
-                  {renderOptions}
-                </Select>
-                {hasError && (
-                  <Message
-                    data-qa="update-profile-role-error"
-                    validation="error"
+                />
+              </FormField>
+            )}
+          </Field>
+          <Field name="roleId">
+            {({ field, meta }: FieldProps) => {
+              const hasError = meta.touched && Boolean(meta.error);
+              return (
+                <div ref={selectRef}>
+                  <Select
+                    // TODO: CHECK placeholder not working
+                    placeholder={t('__PROFILE_PAGE_USER_CARD_ROLE_PLACEHOLDER')}
+                    data-qa="roleId-select"
+                    {...field}
+                    renderValue={(value) =>
+                      roleData?.find(
+                        (role) => role.id === Number(value.inputValue)
+                      )?.name
+                    }
+                    isCompact
+                    inputValue={field.value}
+                    selectionValue={field.value}
+                    label={
+                      <>
+                        {t('__PROFILE_PAGE_USER_CARD_ROLE_LABEL')}
+                        <Span
+                          style={{
+                            color: appTheme.palette.red[600],
+                          }}
+                        >
+                          *
+                        </Span>
+                      </>
+                    }
+                    onSelect={(roleId) => {
+                      setFieldValue('roleId', Number(roleId));
+                      (
+                        selectRef.current?.querySelector(
+                          '[role="combobox"]'
+                        ) as HTMLElement | null
+                      )?.blur();
+                    }}
                   >
-                    {meta.error}
-                  </Message>
-                )}
-              </div>
-            );
-          }}
-        </Field>
-        <div>
-          <Button
-            isAccent
-            isPrimary
-            onClick={() => {
-              alert('User profile update functionality not implemented yet.');
+                    {renderOptions}
+                  </Select>
+                  {hasError && (
+                    <Message
+                      data-qa="update-profile-role-error"
+                      validation="error"
+                    >
+                      {meta.error}
+                    </Message>
+                  )}
+                </div>
+              );
             }}
-          >
-            Save changes
-          </Button>
-        </div>
-      </Card>
+          </Field>
+          <div>
+            <Button
+              isAccent
+              isPrimary
+              onClick={() => {
+                alert('User profile update functionality not implemented yet.');
+              }}
+            >
+              Save changes
+            </Button>
+          </div>
+        </SpecialCard.Body>
+      </SpecialCard>
       <Card>
         <AccordionNew
           level={3}
@@ -220,7 +247,132 @@ export const Form = () => {
                 label={t('__PROFILE_PAGE_PASSWORD_ACCORDION_LABEL')}
               />
             </AccordionNew.Header>
-            <AccordionNew.Panel>bla bla</AccordionNew.Panel>
+            <AccordionNew.Panel>
+              <Field name="current-password">
+                {({ field, meta }: FieldProps) => {
+                  const hasError = meta.touched && Boolean(meta.error);
+                  return (
+                    <FormField>
+                      <Label>
+                        {t('__PAGE_PROFILE_CURRENT_PASSWORD_LABEL')}
+                        <Span style={{ color: appTheme.palette.red[600] }}>
+                          {' '}
+                          *
+                        </Span>
+                      </Label>
+
+                      <MediaInput
+                        type={inputType}
+                        role="textbox"
+                        title="Current Password"
+                        end={
+                          inputType === 'password' ? (
+                            <Eye
+                              style={{ cursor: 'pointer' }}
+                              onClick={handleChangeInputType}
+                              title={t(
+                                '__PAGE_PROFILE_FORM_CURRENT_PASSWORD_SHOW'
+                              )}
+                            />
+                          ) : (
+                            <EyeHide
+                              style={{ cursor: 'pointer' }}
+                              onClick={handleChangeInputType}
+                              title={t(
+                                '__PAGE_PROFILE_FORM_CURRENT_PASSWORD_HIDE'
+                              )}
+                            />
+                          )
+                        }
+                        {...field}
+                        placeholder={t(
+                          '__PAGE_PROFILE_CURRENT_PASSWORD_PLACEHOLDER'
+                        )}
+                        {...(hasError && { validation: 'error' })}
+                      />
+
+                      {hasError && (
+                        <Message
+                          data-qa="message-error-current-password"
+                          validation="error"
+                        >
+                          {meta.error}
+                        </Message>
+                      )}
+                    </FormField>
+                  );
+                }}
+              </Field>
+              <Row>
+                <Col>
+                  <Field name="new-password">
+                    {({ field, meta }: FieldProps) => {
+                      const hasError = meta.touched && Boolean(meta.error);
+                      return (
+                        <FormField>
+                          <Label>
+                            {t('__PAGE_PROFILE_NEW_PASSWORD_LABEL')}
+                            <Span style={{ color: appTheme.palette.red[600] }}>
+                              *
+                            </Span>
+                          </Label>
+                          <Input
+                            type="text"
+                            {...field}
+                            placeholder={t(
+                              '__PAGE_PROFILE_NEW_PASSWORD_PLACEHOLDER'
+                            )}
+                            {...(hasError && { validation: 'error' })}
+                          />
+                          {hasError && (
+                            <Message
+                              data-qa="profile-new-pass-error"
+                              validation="error"
+                            >
+                              {meta.error}
+                            </Message>
+                          )}
+                        </FormField>
+                      );
+                    }}
+                  </Field>
+                </Col>
+                <Col>
+                  <Field name="confirm-password">
+                    {({ field, meta }: FieldProps) => {
+                      const hasError = meta.touched && Boolean(meta.error);
+                      return (
+                        <FormField>
+                          <Label>
+                            {t('__PAGE_PROFILE_CONFIRM_PASSWORD_LABEL')}
+                            <Span style={{ color: appTheme.palette.red[600] }}>
+                              *
+                            </Span>
+                          </Label>
+                          <Input
+                            type="text"
+                            {...field}
+                            placeholder={t(
+                              '__PAGE_PROFILE_CONFIRM_PASSWORD_PLACEHOLDER'
+                            )}
+                            {...(hasError && { validation: 'error' })}
+                          />
+                          {hasError && (
+                            <Message
+                              data-qa="profile-confirm-pass-error"
+                              validation="error"
+                            >
+                              {meta.error}
+                            </Message>
+                          )}
+                        </FormField>
+                      );
+                    }}
+                  </Field>
+                </Col>
+              </Row>
+              <PasswordRequirements />
+            </AccordionNew.Panel>
           </AccordionNew.Section>
         </AccordionNew>
       </Card>
---
+++ src/pages/Profile/PasswordRequirements.tsx
@@ -0,0 +1,70 @@
+import { MD, theme } from '@appquality/unguess-design-system';
+import { ReactComponent as X } from 'src/assets/icons/password-check-x.svg';
+import { ReactComponent as Check } from 'src/assets/icons/password-check-v.svg';
+import { useFormikContext } from 'formik';
+import { useTranslation } from 'react-i18next';
+import { ProfileFormValues } from './valuesType';
+
+const PasswordRequirement = ({
+  check,
+  children,
+}: {
+  check: () => boolean;
+  children: React.ReactNode;
+}) => (
+  <li style={{ display: 'flex', alignItems: 'center' }}>
+    {check() ? (
+      <Check aria-hidden="true" fontSize={16} color={theme.palette.grey[500]} />
+    ) : (
+      <X aria-hidden="true" fontSize={16} />
+    )}
+    <MD
+      isBold={!check()}
+      style={{
+        color: `${check() ? theme.palette.grey[500] : theme.palette.grey[700]}`,
+      }}
+    >
+      {children}
+    </MD>
+  </li>
+);
+
+const PasswordRequirements = () => {
+  const { values } = useFormikContext<ProfileFormValues>();
+  const { t } = useTranslation();
+
+  return (
+    <div data-qa="password-requirements" className="aq-mb-3">
+      <MD>{t('PASSWORD_VALIDATOR_PASSWORD_REQUIREMENTS')}</MD>
+      <ul>
+        <PasswordRequirement
+          check={() => (values.password?.new?.length ?? 0) >= 6}
+        >
+          {t('PASSWORD_VALIDATOR_MINIMUM_OF_6_CHARACTERS')}
+        </PasswordRequirement>
+        <PasswordRequirement
+          check={() => values.password?.new.match(/[A-Z]/) !== null}
+        >
+          {t('PASSWORD_VALIDATOR_CONTAIN_AN_UPPERCASE_LETTER')}
+        </PasswordRequirement>
+        <PasswordRequirement
+          check={() => values.password?.new.match(/[a-z]/) !== null}
+        >
+          {t('PASSWORD_VALIDATOR_CONTAIN_A_LOWERCASE_LETTER')}
+        </PasswordRequirement>
+        <PasswordRequirement
+          check={() => values.password?.new.match(/[0-9]/) !== null}
+        >
+          {t('PASSWORD_VALIDATOR_CONTAIN_A_NUMBER')}
+        </PasswordRequirement>
+        <PasswordRequirement
+          check={() => values.password?.new !== values.password?.confirm}
+        >
+          {t('PASSWORD_VALIDATOR_PASSWORDS_DO_NOT_MATCH')}
+        </PasswordRequirement>
+      </ul>
+    </div>
+  );
+};
+
+export { PasswordRequirements };
