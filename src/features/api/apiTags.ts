cf6e7db9668faa7af8b19b0f7b0c5ec102ab5f48
iacopolea <iacopo.lea@gmail.com>
2025-07-10 22:43:47 +0200

    feat: enhance getPlansByPid endpoint to generate unique IDs for tasks

--- src/features/api/apiTags.ts
+++ src/features/api/apiTags.ts
@@ -1,4 +1,5 @@
-import { unguessApi } from '.';
+import * as uuid from 'uuid';
+import { GetPlansByPidApiResponse, unguessApi } from '.';
 
 unguessApi.enhanceEndpoints({
   endpoints: {
@@ -272,6 +273,33 @@ unguessApi.enhanceEndpoints({
     },
     getPlansByPid: {
       providesTags: ['Plans'],
+      transformResponse: (response: GetPlansByPidApiResponse) => {
+        if (response && response.config) {
+          // find the task module if any
+          const taskModule = response.config.modules.find(
+            (module) => module.type === 'tasks'
+          );
+          if (taskModule && taskModule.output) {
+            // add an id and a symbol to each task for better identification
+            const mappedTasks = taskModule.output.map((task, index) => ({
+              ...task,
+              id: uuid.v4(), // generate a new UUID for each task
+            }));
+            taskModule.output = mappedTasks;
+            // now we can safely return the response
+            response.config.modules = response.config.modules.map((module) => {
+              if (module.type === 'tasks') {
+                return {
+                  ...module,
+                  output: mappedTasks,
+                };
+              }
+              return module;
+            });
+          }
+        }
+        return response;
+      },
     },
     patchPlansByPid: {
       invalidatesTags: ['Plans'],
