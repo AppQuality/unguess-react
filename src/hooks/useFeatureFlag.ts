9448c1a8840c0310d49557bac57552df3fccf8df
Kariamos <minucci9292@gmail.com>
2025-07-15 10:47:45 +0200

    refactor(useFeatureFlag): removed useless useSelector as we are not using the store anymore

--- src/hooks/useFeatureFlag.ts
+++ src/hooks/useFeatureFlag.ts
@@ -1,34 +1,25 @@
-import { shallowEqual } from 'react-redux';
-import { useAppSelector } from 'src/app/hooks';
 import { useGetUsersMeQuery } from 'src/features/api';
 
 export const useFeatureFlag = () => {
   const {
     data: userData,
     isLoading: isUserDataLoading,
+    isFetching: isUserFetching,
     isError,
   } = useGetUsersMeQuery();
 
-  if (isUserDataLoading || isError || !userData) {
+  if (isUserDataLoading || isUserFetching || isError || !userData) {
     return { hasFeatureFlag: () => false };
   }
 
-  const { role, features } = useAppSelector(
-    () => ({
-      role: userData.role,
-      features: userData.features,
-    }),
-    shallowEqual
-  );
+  const { role, features = [] } = userData;
 
   const hasFeatureFlag = (slug?: string) => {
     if (role === 'administrator') {
       return true;
     }
-    if (features) {
-      return features.some((feature) => feature.slug === slug);
-    }
-    return false;
+
+    return features.some((feature) => feature.slug === slug);
   };
 
   return { hasFeatureFlag };
