d3af29464762e8fa997eb9d29c09139daff67196
Gianpaolo <gianpaolosinatra@gmail.com>
2025-07-17 15:48:12 +0200

    fix(playwright): remove default timeout setting from configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  timeout: 300000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
