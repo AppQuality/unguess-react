53e61d83ce9b81419542a346620a6a82c3bd2a7b
Gianpaolo <gianpaolosinatra@gmail.com>
2025-07-25 15:50:27 +0200

    feat: refactor tests for Public Bug page to improve structure and readability

--- tests/e2e/bug_public/index.spec.ts
+++ tests/e2e/bug_public/index.spec.ts
@@ -6,10 +6,8 @@ test.describe('Public Bug page - logged in', () => {
 
   test.beforeEach(async ({ page }) => {
     publicBugPage = new PublicBugPage(page);
-    //
     await publicBugPage.loggedIn();
     await publicBugPage.mockWorkspacesList();
-    //
     await publicBugPage.mockGetPublicBugData();
     await publicBugPage.mockBug();
     await publicBugPage.open();
@@ -19,10 +17,20 @@ test.describe('Public Bug page - logged in', () => {
     await expect(publicBugPage.elements().pageHeader()).toBeVisible();
     await expect(publicBugPage.elements().bugContainer()).toBeVisible();
   });
+});
 
-  // test('does not show the full header with filter recap and pagination', async () => {
-  //   await expect(bugPage.elements().filtersDetailsButton()).not.toBeVisible();
-  //   await expect(bugPage.elements().usecaseSelect()).not.toBeVisible();
-  //   await expect(bugPage.elements().pagination()).not.toBeVisible();
-  // });
+test.describe('Public Bug page - logged out', () => {
+  let publicBugPage: PublicBugPage;
+
+  test.beforeEach(async ({ page }) => {
+    publicBugPage = new PublicBugPage(page);
+    await publicBugPage.mockGetPublicBugData();
+    await publicBugPage.mockBug();
+    await publicBugPage.open();
+  });
+
+  test('Shows the page header and bug container card', async () => {
+    await expect(publicBugPage.elements().pageHeader()).toBeVisible();
+    await expect(publicBugPage.elements().bugContainer()).toBeVisible();
+  });
 });
