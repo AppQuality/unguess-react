f8405974f073f14ce64fad512547072fa88424c4
Gianpaolo <gianpaolosinatra@gmail.com>
2025-08-19 16:05:41 +0200

    Fix company size selection in fillValidFields method

--- tests/fixtures/pages/Join/Step2.ts
+++ tests/fixtures/pages/Join/Step2.ts
@@ -55,7 +55,8 @@ export class Step2 {
   async fillValidFields() {
     await this.elements().roleSelect().click();
     await this.elements().roleSelectOptions().first().click();
-    await this.elements().companySizeSelect().first().click();
+    await this.elements().companySizeSelect().click();
+    await this.elements().companySizeSelectOptions().first().click();
     await this.elements().surnameInput().fill(this.surname);
     await this.elements().nameInput().fill(this.name);
     await this.elements().nameInput().blur();
