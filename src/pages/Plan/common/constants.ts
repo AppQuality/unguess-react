543e7b28d79ea49981d22acad659e75d2ef68a08
iacopolea <iacopo.lea@gmail.com>
2025-08-19 15:49:59 +0200

    Reorder modules in technicalRequirements group for consistency fixes UN-1619

--- src/pages/Plan/common/constants.ts
+++ src/pages/Plan/common/constants.ts
@@ -20,7 +20,7 @@ export const MODULE_GROUPS: Record<PlanTabName, ModuleGroup[]> = {
     {
       id: 'technicalRequirements',
       title: '__PLAN_PAGE_GROUP_TITLE_TECHNICAL_REQUIREMENTS',
-      modules: ['browser', 'touchpoints'],
+      modules: ['touchpoints', 'browser'],
     },
   ],
   target: [
