e37f10b425972a1f99e27df21699aee3b1ed60aa
Gianpaolo <gianpaolosinatra@gmail.com>
2025-08-22 01:35:17 +0200

    feat: integrate company sizes into profile data and update form values

--- src/pages/Profile/useProfileData.ts
+++ src/pages/Profile/useProfileData.ts
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
-import { useGetUsersMeQuery, useGetUsersRolesQuery } from 'src/features/api';
+import {
+  useGetUsersMeQuery,
+  useGetUsersRolesQuery,
+  useGetCompaniesSizesQuery,
+} from 'src/features/api';
 import { ProfileFormValues } from './valuesType';
 
 export const useProfileData = () => {
@@ -8,15 +12,22 @@ export const useProfileData = () => {
   const { data: userData, isLoading: isUserDataLoading } = useGetUsersMeQuery();
   const { data: rolesData, isLoading: isRolesDataLoading } =
     useGetUsersRolesQuery();
+  const { data: companySizesData, isLoading: isCompanySizesLoading } =
+    useGetCompaniesSizesQuery();
 
   useEffect(() => {
-    if (userData && rolesData) {
+    if (userData && rolesData && companySizesData) {
       const roleObj = rolesData.find(
         (role) => role.name === userData.customer_role
+      );
+      const companySizeObj = companySizesData.find(
+        (size) => size.name === userData.company_size
       );
       const initialValues: ProfileFormValues = {
         role: roleObj?.name || undefined,
         roleId: roleObj?.id || 0,
+        companySize: companySizeObj?.name || '',
+        companySizeId: companySizeObj?.id || 0,
         name: userData.first_name || '',
         surname: userData.last_name || '',
         email: userData.email || '',
@@ -27,7 +38,8 @@ export const useProfileData = () => {
 
   return {
     data: profile,
-    isLoading: isUserDataLoading || isRolesDataLoading,
-    isFetching: isUserDataLoading || isRolesDataLoading,
+    isLoading: isUserDataLoading || isRolesDataLoading || isCompanySizesLoading,
+    isFetching:
+      isUserDataLoading || isRolesDataLoading || isCompanySizesLoading,
   };
 };
