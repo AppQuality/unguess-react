9d10d63c67c37f36776a1673fd769a67fc54cd67
iacopolea <iacopo.lea@gmail.com>
2025-08-28 16:37:42 +0200

    feat: implement error handling with toasts for login and nonce retrieval

---
+++ tests/e2e/login.spec.ts
@@ -0,0 +1,34 @@
+import { log } from 'console';
+import { test, expect } from '../fixtures/app';
+import { Login } from '../fixtures/pages/Login';
+
+test.describe('Login page', () => {
+  let login: Login;
+
+  test.beforeEach(async ({ page }) => {
+    login = new Login(page);
+
+    await login.notLoggedIn();
+    await login.open();
+  });
+  test('shows a generic error toast whenever wpapi getnonce responds with a 403', async ({
+    i18n,
+  }) => {
+    await login.mockGetNonce403();
+    await login.fillValidInputs();
+    await login.submit();
+    await expect(login.elements().errorToast()).toContainText(
+      i18n.t('__TOAST_GENERIC_ERROR_MESSAGE')
+    );
+  });
+  test('shows a generic error toast whenever wpapi login responds with a 403', async ({
+    i18n,
+  }) => {
+    await login.mockLogin403();
+    await login.fillValidInputs();
+    await login.submit();
+    await expect(login.elements().errorToast()).toContainText(
+      i18n.t('__TOAST_GENERIC_ERROR_MESSAGE')
+    );
+  });
+});
---
+++ tests/fixtures/pages/Login.ts
@@ -0,0 +1,64 @@
+import { type Page } from '@playwright/test';
+import { UnguessPage } from '../UnguessPage';
+import { error } from 'console';
+
+export class Login extends UnguessPage {
+  readonly page: Page;
+
+  readonly url = '/login';
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+  }
+
+  elements() {
+    return {
+      ...super.elements(),
+      emailInput: () =>
+        this.page.getByPlaceholder(
+          this.i18n.t('__LOGIN_FORM_EMAIL_PLACEHOLDER')
+        ),
+      passwordInput: () =>
+        this.page.getByPlaceholder(
+          this.i18n.t('__LOGIN_FORM_PASSWORD_PLACEHOLDER')
+        ),
+      submitButton: () =>
+        this.page.getByRole('button', {
+          name: this.i18n.t('__LOGIN_FORM_CTA'),
+        }),
+      errorToast: () => this.page.getByRole('alert'),
+    };
+  }
+
+  async fillValidInputs() {
+    await this.elements().emailInput().fill('email@example.com');
+    await this.elements().passwordInput().fill('Password123');
+  }
+
+  async submit() {
+    await this.elements().submitButton().click();
+  }
+
+  async mockGetNonce403() {
+    await this.page.route('**/wp-admin/admin-ajax.php', async (route) => {
+      const postData = route.request().postData();
+      if (postData && postData.includes('action=ug_get_nonce')) {
+        await route.fulfill({ status: 403 });
+      } else {
+        await route.continue();
+      }
+    });
+  }
+
+  async mockLogin403() {
+    await this.page.route('**/wp-admin/admin-ajax.php', async (route) => {
+      const postData = route.request().postData();
+      if (postData && postData.includes('action=ajaxlogin')) {
+        await route.fulfill({ status: 403 });
+      } else {
+        await route.continue();
+      }
+    });
+  }
+}
