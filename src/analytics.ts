f5867fb9df54bee07418671b72d37acc3f01e90f
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2025-09-03 11:57:30 +0200

    fix: ensure analytics plugins are only added in non-test environments

--- src/analytics.ts
+++ src/analytics.ts
@@ -5,18 +5,20 @@ import { isDev } from './common/isDevEnvironment';
 
 const analytics = Analytics({
   app: 'unguess-react',
-  plugins: [
-    googleTagManager({
-      containerId: process.env.REACT_APP_GTM_ID || 'GTM-WVXPS94',
-      ...(isDev() && {
-        auth: 'HjeAxSQB9e685mi-_8YiDw',
-        preview: 'env-4',
+  ...(process.env.NODE_ENV !== 'test' && {
+    plugins: [
+      googleTagManager({
+        containerId: process.env.REACT_APP_GTM_ID || 'GTM-WVXPS94',
+        ...(isDev() && {
+          auth: 'HjeAxSQB9e685mi-_8YiDw',
+          preview: 'env-4',
+        }),
       }),
-    }),
-    userpilot({
-      token: isDev() ? 'STG-NX-54e88e10' : 'NX-54e88e10',
-    }),
-  ],
+      userpilot({
+        token: isDev() ? 'STG-NX-54e88e10' : 'NX-54e88e10',
+      }),
+    ],
+  }),
 });
 
 export default analytics;
