69e507d4f20577bc0ec5bbc2922d07113bdb18ac
iacopolea <iacopo.lea@gmail.com>
2025-09-05 16:08:45 +0200

    feat: add @emotion/is-prop-valid dependency and update App component for styled-components compatibility

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,49 +3,63 @@ import {
   ThemeProvider,
   ToastProvider,
 } from '@appquality/unguess-design-system';
+import isPropValid from '@emotion/is-prop-valid';
 import Helmet from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { Provider } from 'react-redux';
-import { appTheme } from 'src/app/theme';
-import { AnalyticsProvider } from 'use-analytics';
-import Pages from 'src/common/Pages';
 import analytics from 'src/analytics';
+import { appTheme } from 'src/app/theme';
+import Pages from 'src/common/Pages';
+import { StyleSheetManager } from 'styled-components';
+import { AnalyticsProvider } from 'use-analytics';
 import '../i18n';
 import { store } from './store';
+
+// This implements the default behavior from styled-components v5
+function shouldForwardProp(propName: string, target: any) {
+  if (typeof target === 'string') {
+    // For HTML elements, forward the prop if it is a valid HTML attribute
+    return isPropValid(propName);
+  }
+  // For other elements, forward all props
+  return true;
+}
 
 const App = () => {
   const { t } = useTranslation();
   return (
-    <Provider store={store}>
-      <AnalyticsProvider instance={analytics}>
-        <ThemeProvider theme={appTheme}>
-          <GlobalStyle />
-          <Helmet>
-            <meta
-              property="og:title"
-              content={`UNGUESS - ${t('__APP_META_PAYOFF')}`}
-            />
-            <title>UNGUESS - {t('__APP_META_PAYOFF')}</title>
-            <meta name="description" content={t('__APP_META_DESCRIPTION')} />
-          </Helmet>
-          <ToastProvider
-            limit={5}
-            zIndex={500}
-            placementProps={{
-              top: {
-                style: {
-                  top:
-                    parseInt(appTheme.components.chrome.header.height, 10) +
-                    appTheme.space.base * 4,
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <Provider store={store}>
+        <AnalyticsProvider instance={analytics}>
+          <ThemeProvider theme={appTheme}>
+            <GlobalStyle />
+            <Helmet>
+              <meta
+                property="og:title"
+                content={`UNGUESS - ${t('__APP_META_PAYOFF')}`}
+              />
+              <title>UNGUESS - {t('__APP_META_PAYOFF')}</title>
+              <meta name="description" content={t('__APP_META_DESCRIPTION')} />
+            </Helmet>
+            <ToastProvider
+              limit={5}
+              zIndex={500}
+              placementProps={{
+                top: {
+                  style: {
+                    top:
+                      parseInt(appTheme.components.chrome.header.height, 10) +
+                      appTheme.space.base * 4,
+                  },
                 },
-              },
-            }}
-          >
-            <Pages />
-          </ToastProvider>
-        </ThemeProvider>
-      </AnalyticsProvider>
-    </Provider>
+              }}
+            >
+              <Pages />
+            </ToastProvider>
+          </ThemeProvider>
+        </AnalyticsProvider>
+      </Provider>
+    </StyleSheetManager>
   );
 };
 
