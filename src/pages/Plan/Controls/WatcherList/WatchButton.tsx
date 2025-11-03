5c5c8d792d98eea2c8009ec04d61c5cbe3dcb273
Kariamos <minucci9292@gmail.com>
2025-11-03 17:53:39 +0100

    feat: Refactor WatchButton component to improve tooltip handling and button rendering

--- src/pages/Plan/Controls/WatcherList/WatchButton.tsx
+++ src/pages/Plan/Controls/WatcherList/WatchButton.tsx
@@ -20,58 +20,59 @@ const WatchButton = ({ planId }: { planId: string }) => {
   const { t } = useTranslation();
 
   if (!currentUser) return null;
-  return (
-    <>
-      {isLastOne && isWatching && (
-        <Tooltip
-          placement="start"
-          type="light"
-          size="medium"
-          content={t(
-            '__PLAN_PAGE_WATCHER_LIST_MODAL_UNFOLLOW_BUTTON_DISABLED_TOOLTIP',
-            'At least one person must follow this activity '
-          )}
-        >
-          {/* the following div is necessary to make Tooltip work with disabled Button */}
-          <div>
-            <Button
-              isStretched
-              disabled={isLastOne && isWatching}
-              isPrimary={!isWatching}
-              onClick={() => {
-                if (isWatching) {
-                  removeWatcher({
-                    planId,
-                    profileId: currentUser.profile_id.toString(),
-                  });
-                } else {
-                  addUser({
-                    pid: planId,
-                    body: { users: [{ id: currentUser.profile_id }] },
-                  });
-                }
-              }}
-            >
-              <div
-                style={{ display: 'flex', gap: '8px', alignItems: 'center' }}
-              >
-                {isWatching ? <EyeIconSlash /> : <EyeIconFill color="#fff" />}
-                {isWatching
-                  ? t(
-                      '__PLAN_PAGE_WATCHER_LIST_MODAL_UNFOLLOW_BUTTON',
-                      'Unfollow this activity'
-                    )
-                  : t(
-                      '__PLAN_PAGE_WATCHER_LIST_MODAL_FOLLOW_BUTTON',
-                      'Follow this activity'
-                    )}
-              </div>
-            </Button>
-          </div>
-        </Tooltip>
-      )}
-    </>
+
+  const button = (
+    <Button
+      isStretched
+      disabled={isLastOne && isWatching}
+      isPrimary={!isWatching}
+      onClick={() => {
+        if (isWatching) {
+          removeWatcher({
+            planId,
+            profileId: currentUser.profile_id.toString(),
+          });
+        } else {
+          addUser({
+            pid: planId,
+            body: { users: [{ id: currentUser.profile_id }] },
+          });
+        }
+      }}
+    >
+      <div style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+        {isWatching ? <EyeIconSlash /> : <EyeIconFill color="#fff" />}
+        {isWatching
+          ? t(
+              '__PLAN_PAGE_WATCHER_LIST_MODAL_UNFOLLOW_BUTTON',
+              'Unfollow this activity'
+            )
+          : t(
+              '__PLAN_PAGE_WATCHER_LIST_MODAL_FOLLOW_BUTTON',
+              'Follow this activity'
+            )}
+      </div>
+    </Button>
   );
+
+  if (isLastOne && isWatching) {
+    return (
+      <Tooltip
+        placement="start"
+        type="light"
+        size="medium"
+        content={t(
+          '__PLAN_PAGE_WATCHER_LIST_MODAL_UNFOLLOW_BUTTON_DISABLED_TOOLTIP',
+          'At least one person must follow this activity'
+        )}
+      >
+        {/* the following div is necessary to make Tooltip work with disabled Button */}
+        <div>{button}</div>
+      </Tooltip>
+    );
+  }
+
+  return button;
 };
 
 export { WatchButton };
