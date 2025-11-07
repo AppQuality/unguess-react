96456b86a061e222c392c6af46e1f623bcc09e4c
ZecD <dominik.zecoli@gmail.com>
2025-11-07 09:48:51 +0100

    feat: update FollowActivitiesPanel with new descriptions and hint text for improved user guidance

--- src/pages/Profile/parts/FollowActivitiesAccordion.tsx/FollowActivitiesPanel.tsx
+++ src/pages/Profile/parts/FollowActivitiesAccordion.tsx/FollowActivitiesPanel.tsx
@@ -1,13 +1,26 @@
-import { Anchor, Button, Label, SM } from '@appquality/unguess-design-system';
+import {
+  Anchor,
+  Button,
+  Hint,
+  Label,
+  SM,
+} from '@appquality/unguess-design-system';
 import { useTranslation } from 'react-i18next';
+import { ReactComponent as InfoIcon } from 'src/assets/icons/info-icon.svg';
 
 import styled from 'styled-components';
 
-const StyledPanelCardContainer = styled.div`
+const StyledPanelSectionContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.space.xs};
   margin-bottom: ${({ theme }) => theme.space.xxl};
+`;
+
+const StyledHintContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.space.xs};
 `;
 
 const StyledActivityItem = styled.div`
@@ -21,19 +34,51 @@ const StyledActivityItem = styled.div`
 export const FollowActivitiesPanel = () => {
   const { t } = useTranslation();
   return (
-    <StyledPanelCardContainer>
-      <Label>
-        {t('__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_DESCRIPTION')}
-      </Label>
-      <StyledActivityItem>
-        <div>
-          <Anchor>Titolo Attivita</Anchor>
-          <SM>titolo progetto</SM>
-        </div>
-        <Button size="small" isBasic>
-          {t('__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_BUTTON_TEXT')}
-        </Button>
-      </StyledActivityItem>
-    </StyledPanelCardContainer>
+    <div>
+      <StyledPanelSectionContainer>
+        <Label>
+          {t(
+            '__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_SETUP_DESCRIPTION'
+          )}
+          (3)
+        </Label>
+        <StyledActivityItem>
+          <div>
+            <Anchor>Titolo Attivita</Anchor>
+            <SM>titolo progetto</SM>
+          </div>
+          <Button size="small" isBasic>
+            {t(
+              '__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_BUTTON_TEXT'
+            )}
+          </Button>
+        </StyledActivityItem>
+      </StyledPanelSectionContainer>
+      <StyledPanelSectionContainer>
+        <Label>
+          {t(
+            '__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_PROGRESS_DESCRIPTION'
+          )}
+          (3)
+        </Label>
+        <StyledActivityItem>
+          <div>
+            <Anchor>Titolo Attivita</Anchor>
+            <SM>titolo progetto</SM>
+          </div>
+          <Button size="small" isBasic>
+            {t(
+              '__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_BUTTON_TEXT'
+            )}
+          </Button>
+        </StyledActivityItem>
+      </StyledPanelSectionContainer>
+      <StyledHintContainer>
+        <InfoIcon />
+        <Hint>
+          {t('__PROFILE_PAGE_NOTIFICATIONS_CARD_FOLLOW_ACTIVITIES_HINT_TEXT')}
+        </Hint>
+      </StyledHintContainer>
+    </div>
   );
 };
