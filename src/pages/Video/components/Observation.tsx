a9bd10eee5f6e0476a22b5494bdd039cd1411f28
iacopolea <iacopo.lea@gmail.com>
2025-11-13 11:19:39 +0100

    fix: remove focus on input element in handleAccordionChange function

--- src/pages/Video/components/Observation.tsx
+++ src/pages/Video/components/Observation.tsx
@@ -109,7 +109,6 @@ const Observation = ({
               top: activeElement.offsetTop,
               behavior: 'smooth',
             });
-            activeElement.querySelectorAll('input')[0]?.focus();
           }
           setOpenAccordion(undefined);
         }, 100);
