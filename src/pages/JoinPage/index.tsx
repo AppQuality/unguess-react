ced2a99905ce48014bb9ef62a44bbba02e286c67
iacopolea <iacopo.lea@gmail.com>
2025-12-11 09:50:09 +0100

    fix: enable refetching on mount or argument change for user query in JoinPage

--- src/pages/JoinPage/index.tsx
+++ src/pages/JoinPage/index.tsx
@@ -93,7 +93,9 @@ const LogoWrapper = styled.div`
 const JoinPage = () => {
   const { t } = useTranslation();
   const [searchParams] = useSearchParams();
-  const { isSuccess: isLogged } = useGetUsersMeQuery();
+  const { isSuccess: isLogged } = useGetUsersMeQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
   const navigate = useNavigate();
   const { profile, token } = useParams();
   const shouldSkipQuery = isLogged || !(profile && token);
