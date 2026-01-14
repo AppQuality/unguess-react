b849ef501e3778fcddf9b3c91d75233e1823e2db
Davide Bizzi <davide.bizzi@unguess.io>
2026-01-14 17:08:50 +0100

    feat: update Changelog component to initialize Headway widget and improve structure

--- src/common/components/navigation/header/changelog.tsx
+++ src/common/components/navigation/header/changelog.tsx
@@ -1,5 +1,8 @@
 import { HeaderItem } from '@appquality/unguess-design-system';
+import { useEffect } from 'react';
 import { appTheme } from 'src/app/theme';
+import { ReactComponent as ChangelogIcon } from 'src/assets/icons/megaphone-stroke.svg';
+
 import styled from 'styled-components';
 
 const StyledWidget = styled.div`
@@ -7,12 +10,25 @@ const StyledWidget = styled.div`
     margin-top: ${appTheme.space.sm};
   }
   .HW_badge_cont {
+    position: absolute;
     top: -${appTheme.space.xs};
   }
 `;
 
-export const Changelog = () => (
-  <HeaderItem>
-    <StyledWidget>sss </StyledWidget>
-  </HeaderItem>
-);
+export const Changelog = () => {
+  useEffect(() => {
+    const HW_config = {
+      selector: '#headway-widget',
+      account: 'Jn0mVx',
+    };
+    window.Headway.init(HW_config);
+  }, []);
+
+  return (
+    <HeaderItem>
+      <StyledWidget id="headway-widget">
+        <ChangelogIcon />
+      </StyledWidget>
+    </HeaderItem>
+  );
+};
