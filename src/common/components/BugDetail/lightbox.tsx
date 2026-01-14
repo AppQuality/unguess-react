88df0d464bc220db3107e4d24b4d408b81c6614a
Kariamos <minucci9292@gmail.com>
2026-01-14 17:12:35 +0100

    feat: refactor LightboxContainer to show data label on slider fixed on top left corner

--- src/common/components/BugDetail/lightbox.tsx
+++ src/common/components/BugDetail/lightbox.tsx
@@ -7,7 +7,8 @@ import {
   Span,
   Tag,
 } from '@appquality/unguess-design-system';
-import { useCallback, useRef } from 'react';
+import { vi } from 'date-fns/locale';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { appTheme } from 'src/app/theme';
 import { ReactComponent as DownloadIcon } from 'src/assets/icons/download-stroke.svg';
@@ -37,6 +38,12 @@ const SlideContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const SliderArea = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
 `;
 
 const PositionedTag = styled(Tag)`
@@ -73,10 +80,17 @@ export const LightboxContainer = ({
 
   const videoRefs = useRef<Array<HTMLVideoElement | null>>([]);
 
+  const [activeIndex, setActiveIndex] = useState(currentIndex);
+
+  useEffect(() => {
+    setActiveIndex(currentIndex);
+  }, [currentIndex]);
+
   const slideChange = useCallback(
     (index: number) => {
       if (onSlideChange) {
         onSlideChange(index);
+        setActiveIndex(index);
       }
       videoRefs.current.forEach((ref) => {
         if (ref) {
@@ -84,8 +98,10 @@ export const LightboxContainer = ({
         }
       });
     },
-    [videoRefs]
+    [onSlideChange, videoRefs]
   );
+
+  const activeItem = items[activeIndex];
 
   return (
     <Lightbox onClose={onClose}>
@@ -103,42 +119,47 @@ export const LightboxContainer = ({
           </Grey800Span>
         </MD>
       </Lightbox.Header>
+
       <Lightbox.Body>
         <Lightbox.Body.Main style={{ flex: 2 }}>
-          <Slider
-            prevArrow={<Slider.PrevButton isBright />}
-            nextArrow={<Slider.NextButton isBright />}
-            onSlideChange={slideChange}
-            initialSlide={currentIndex}
-          >
-            {items.map((item, index) => (
-              <Slider.Slide>
-                <SlideContent>
-                  {showDateLabels && (
-                    <PositionedTag>
-                      {t(
-                        '__BUGS_PAGE_BUG_DETAIL_ATTACHMENTS_LIGHTBOX_TAG_DATE_LABEL'
-                      )}
-                      <DateValue isBold>
-                        {formatDateDDMMYYYY(new Date(item.creation_date))}
-                      </DateValue>
-                    </PositionedTag>
-                  )}
-                  {item.mime_type.type === 'image' && (
-                    <img src={item.url} alt={`bug ${item.mime_type}`} />
-                  )}
-                  {item.mime_type.type === 'video' && (
-                    <Player
-                      ref={(ref) => {
-                        videoRefs.current[index] = ref;
-                      }}
-                      url={item.url}
-                    />
-                  )}
-                </SlideContent>
-              </Slider.Slide>
-            ))}
-          </Slider>
+          <SliderArea>
+            {showDateLabels && activeItem && (
+              <PositionedTag>
+                {t(
+                  '__BUGS_PAGE_BUG_DETAIL_ATTACHMENTS_LIGHTBOX_TAG_DATE_LABEL'
+                )}
+                <DateValue isBold>
+                  {formatDateDDMMYYYY(new Date(activeItem.creation_date))}
+                </DateValue>
+              </PositionedTag>
+            )}
+
+            <Slider
+              prevArrow={<Slider.PrevButton isBright />}
+              nextArrow={<Slider.NextButton isBright />}
+              onSlideChange={slideChange}
+              initialSlide={currentIndex}
+            >
+              {items.map((item, index) => (
+                <Slider.Slide key={item.url ?? index}>
+                  <SlideContent>
+                    {item.mime_type.type === 'image' && (
+                      <img src={item.url} alt={`bug ${item.mime_type}`} />
+                    )}
+
+                    {item.mime_type.type === 'video' && (
+                      <Player
+                        ref={(ref) => {
+                          videoRefs.current[index] = ref;
+                        }}
+                        url={item.url}
+                      />
+                    )}
+                  </SlideContent>
+                </Slider.Slide>
+              ))}
+            </Slider>
+          </SliderArea>
         </Lightbox.Body.Main>
 
         {hideDetails === false && (
@@ -149,15 +170,13 @@ export const LightboxContainer = ({
           </Lightbox.Body.Details>
         )}
       </Lightbox.Body>
+
       <Lightbox.Footer>
         <Button
           isBasic
           onClick={() => {
-            if (currentIndex in items) {
-              const media = items[currentIndex as number];
-              // eslint-disable-next-line security/detect-non-literal-fs-filename
-              window.open(media.url, '_blank');
-            }
+            const media = items[activeIndex];
+            if (media) window.open(media.url, '_blank');
           }}
         >
           <Button.StartIcon>
@@ -166,6 +185,7 @@ export const LightboxContainer = ({
           {t('__BUGS_PAGE_BUG_DETAIL_ATTACHMENTS_DOWNLOAD_BUTTON')}
         </Button>
       </Lightbox.Footer>
+
       <Lightbox.Close aria-label="Close attachments lightbox" />
     </Lightbox>
   );
