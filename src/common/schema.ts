fb43ab9f26bba0636b493190ebe7b76bda200ad9
iacopolea <iacopo.lea@gmail.com>
2026-03-12 11:57:25 +0100

    chore: update api and schema

--- src/common/schema.ts
+++ src/common/schema.ts
@@ -2119,8 +2119,8 @@ export interface operations {
         "application/json": {
           modules: components["schemas"]["Module"][];
           plan_id?: number;
-          inputPrompt: string;
-          usecaseNumber: number;
+          inputPrompt?: string;
+          usecaseNumber?: number;
         };
       };
     };
