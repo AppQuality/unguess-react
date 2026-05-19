6b28b58772d643db8ab81ba07aaefcc0cafe8b4d
ZecD <dominik.zecoli@gmail.com>
2026-05-19 10:27:12 +0200

    refactor: remove redundant test for generic error toast in legacy WordPress authentication

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -22,18 +22,6 @@ test.describe('Login page - legacy WordPress authentication', () => {
       i18n.t('__LOGIN_FORM_INVALID_CREDENTIALS_TOAST').replace(/<\/?bold>/g, '')
     );
   });
-
-  test('shows a generic error toast whenever wpapi login responds with a 403', async ({
-    i18n,
-  }) => {
-    await login.mockGetNonce200();
-    await login.mockLogin403();
-    await login.fillValidInputs();
-    await login.submit();
-    await expect(login.elements().errorToast()).toContainText(
-      `Login: ${i18n.t('__TOAST_GENERIC_ERROR_MESSAGE')}`
-    );
-  });
 });
 
 test.describe('Login page - Cognito authentication', () => {
