e2cf79c79666bc0a51db9f94122d1479aa52e129
iacopolea <iacopo.lea@gmail.com>
2026-06-11 18:06:48 +0200

    feat: integrate localization for not found route and improve loading state handling

--- src/features/templates/CampaignsHubsMiddleware.tsx
+++ src/features/templates/CampaignsHubsMiddleware.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
+import { useLocalizeRoute } from 'src/hooks/useLocalizedRoute';
 import { useGetCampaignsByCidQuery, useGetHubsByHidQuery } from '../api';
 
 export type CampaignHubContext = {
@@ -9,42 +10,49 @@ export type CampaignHubContext = {
 const CampaignsHubsMiddleware = () => {
   const location = useLocation();
   const { entityId } = useParams<{ entityId: string }>();
-  
+  const notFoundRoute = useLocalizeRoute('oops');
+
   if (!entityId) {
-    return <Navigate to="/404" />;
+    return <Navigate to={notFoundRoute} replace />;
   }
-  
+
   const isHub = location.pathname.includes('/hubs/');
-  
-  const hubQuery = useGetHubsByHidQuery({
-    hid: entityId
-  }, {
-    skip: !isHub,
-  });
-  
-  const campaignQuery = useGetCampaignsByCidQuery({
-    cid: entityId
-  }, {
-    skip: isHub,
-  });
-  
+
+  const hubQuery = useGetHubsByHidQuery(
+    {
+      hid: entityId,
+    },
+    {
+      skip: !isHub,
+    }
+  );
+
+  const campaignQuery = useGetCampaignsByCidQuery(
+    {
+      cid: entityId,
+    },
+    {
+      skip: isHub,
+    }
+  );
+
   const query = isHub ? hubQuery : campaignQuery;
-  
+
   // todo: sostituire con skeleton o loading state
   if (query.isLoading) {
     return <div>Loading...</div>;
   }
-  
+
   if (query.isError || !query.data) {
-    return <Navigate to="/404" />;
+    return <Navigate to={notFoundRoute} replace />;
   }
-  
+
   if (query.isSuccess) {
     const context: CampaignHubContext = { isHub, entityId };
     return <Outlet context={context} />;
   }
-  
-  return <Navigate to="/404" />;
+
+  return <Navigate to={notFoundRoute} replace />;
 };
 
 export default CampaignsHubsMiddleware;
