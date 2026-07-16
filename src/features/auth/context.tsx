c58d925c444677d1b75cbf7318e8ecf84c0e0755
Luca Cannarozzo (@cannarocks) <luca.cannarozzo@unguess.io>
2026-07-16 13:32:26 +0200

    feat: update user attributes to include password change timestamp

--- src/features/auth/context.tsx
+++ src/features/auth/context.tsx
@@ -197,6 +197,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
           userAttributes: {
             email: normalizedEmail,
             name,
+            'custom:psw_changed_at': new Date().toISOString(),
           },
         },
       };
